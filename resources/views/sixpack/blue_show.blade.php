89e2196e529e26a6f7d5dcc73b5db68d3604a233
Diego Lorenzo <diego@diegolorenzo.com>
2017-01-03 17:36:42 -0500

    Trying out Sixpack A/B Testing library.

---
+++ resources/views/sixpack/blue_show.blade.php
@@ -0,0 +1,5 @@
+<h1>Hello!</h1>
+
+<p>This is the blue test!</p>
+
+<a href="/blue">Click here!</a>
--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,34 @@ Route::get('contentful', function () {
 
     return $client->getContentType('campaign');
 });
+
+
+Route::get('sixpack', function() {
+    $sixpack = new SeatGeek\Sixpack\Session\Base;
+
+    $alternate =  $sixpack->participate('test', ['blue', 'red'])->getAlternative();
+
+    if ($alternate === 'blue') {
+        return view('sixpack.blue_show');
+    } else {
+        return view('sixpack.red_show');
+    }
+});
+
+
+Route::get('/blue', function() {
+    $sixpack = new SeatGeek\Sixpack\Session\Base;
+
+    $sixpack->convert('test');
+
+    return 'Blue wins!';
+});
+
+Route::get('/red', function() {
+    $sixpack = new SeatGeek\Sixpack\Session\Base;
+
+    $sixpack->convert('test');
+
+    return 'Red wins!';
+});
+
