80450dc02246c63e600c98a89d48bd71970ad222
Diego Lorenzo <diego@diegolorenzo.com>
2017-01-27 15:52:34 -0500

    Updating to add some styles.

--- resources/views/sixpack/scholarship.blade.php
+++ resources/views/sixpack/scholarship.blade.php
@@ -1,7 +1,30 @@
-<h1>Hey you!</h1>
+@extends('layouts.master')
 
-<a href="/done">Do it!</a>
+@section('content')
+    <header role="banner" class="header -hero">
+        <div class="wrapper">
+            <h1 class="header__title">Join this campaign</h1>
+        </div>
+    </header>
 
-@if ($incentive === 'true')
-    <span><-- $1,000,000</span>
-@endif
+    <div class="container -padded">
+        <div class="wrapper">
+
+            <div class="container__block">
+                <h1>Hey you!</h1>
+
+                @if ($incentive === 'true')
+                    <div class="message-callout -above-horizontal">
+                        <div class="message-callout__copy">
+                          <p>OMG, you could win $1,000,000!</p>
+                        </div>
+                      </div>
+                @endif
+
+                <br />
+                <a class="button" href="/done">Do it!</a>
+            </div>
+
+        </div>
+    </div>
+@endsection
