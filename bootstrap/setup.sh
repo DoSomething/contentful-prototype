100c8a4650630884c4d1c8ed9c78f45648f88cbd
deadlybutter <thedeadlybutter@gmail.com>
2017-02-10 10:41:36 -0500

    Moving build to post install

--- bootstrap/setup.sh
+++ bootstrap/setup.sh
@@ -3,4 +3,4 @@ php artisan migrate --seed --force
 # Normal dependencies are already installed by Heroku, but still need Webpack, Modernizer, etc
 # npm install --only=dev
 
-npm run build
+# npm run build
