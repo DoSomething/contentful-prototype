bebb4979c8ed99311e138fe34f6bd0d6c79dd536
deadlybutter <thedeadlybutter@gmail.com>
2017-02-13 13:33:31 -0500

    Removing asset function

--- resources/views/layouts/takeover.blade.php
+++ resources/views/layouts/takeover.blade.php
@@ -9,14 +9,14 @@
 
     <link rel="icon" type="image/ico" href="/favicon.ico?v1">
     <link rel="stylesheet" href="https://unpkg.com/@dosomething/forge@^6.7.4/dist/forge.css" media="screen, projection" type="text/css">
-    <link rel="stylesheet" href="{{ asset('dist/app.css') }}" media="screen, projection" type="text/css">
+    <link rel="stylesheet" href="/dist/app.css" media="screen, projection" type="text/css">
 </head>
 
 <body class="takeover">
     @yield('content')
     <script type="text/javascript" src="https://unpkg.com/jquery@^3.0.0/dist/jquery.min.js"></script>
     <script type="text/javascript" src="https://unpkg.com/@dosomething/forge@^6.7.4/dist/forge.js"></script>
-    <script type="text/javascript" src="{{ asset('dist/app.js') }}"></script>
+    <script type="text/javascript" src="/dist/app.js"></script>
 </body>
 
 </html>
