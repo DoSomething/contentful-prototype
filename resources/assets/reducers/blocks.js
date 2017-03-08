455ee8a2cbacd75597eebe417cc369d8141a28d9
deadlybutter <thedeadlybutter@gmail.com>
2017-03-08 10:36:07 -0500

    Fills out the blocks reducer

--- resources/assets/reducers/blocks.js
+++ resources/assets/reducers/blocks.js
@@ -1,8 +1,21 @@
+import { REQUESTED_BLOCKS, RECIEVED_BLOCKS } from '../actions';
+
 /**
  * Block reducer:
  */
 const blocks = (state = {}, action) => {
-  return state;
+  switch (action.type) {
+    case REQUESTED_BLOCKS:
+      return state;
+
+    case RECIEVED_BLOCKS:
+      return Object.assign({}, state, {
+        data: state.blocks.concat(action.blocks),
+      });
+
+    default:
+      return state;
+  }
 }
 
 export default blocks;
