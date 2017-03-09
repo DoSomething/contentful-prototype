ae6db1d1b27b77cab2838ba8be35fb2d5231925d
deadlybutter <thedeadlybutter@gmail.com>
2017-03-09 14:57:17 -0500

    Adding user auth state to campaign controller

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -68,7 +68,15 @@ class CampaignController extends Controller
         ]);
         $reportbacks = $response['data'];
 
+        $state = [
+            'campaign' => $campaign,
+            'reportbacks' => [
+                'data' => $reportbacks
+            ],
+            'user' => Auth::check() ? Auth::user()->northstar_id : null,
+        ];
+
         return view('campaigns.show', ['campaign' => $campaign])
-            ->with('state', ['campaign' => $campaign, 'reportbacks' => ['data' => $reportbacks]]);
+            ->with('state', $state);
     }
 }
