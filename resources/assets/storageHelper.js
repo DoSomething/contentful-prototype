edbf41ac0ed5318d924b87de3010d5cc099eddeb
deadlybutter <thedeadlybutter@gmail.com>
2017-03-31 10:45:07 -0400

    Adds storage helper

---
+++ resources/assets/storageHelper.js
@@ -0,0 +1,52 @@
+function key(id, type) {
+  return `${id}-${type}`;
+}
+
+/**
+ * Set data in local storage for the given
+ * unique id and data type.
+ *
+ * @param {string} id   Unique id
+ * @param {string} type Data type
+ * @param {mixed}  data Data to write
+ */
+export function set(id, type, data) {
+  localStorage.setItem(key(id, type), JSON.stringify(data));
+}
+
+/**
+ * Get data in local storage for the given
+ * unique id and data type.
+ *
+ * @param  {string} id   Unique id
+ * @param  {string} type Data type
+ * @return {mixed}       Data saved in local storage
+ */
+export function get(id, type) {
+  return JSON.parse(localStorage.getItem(key(id, type)) || null);
+}
+
+/**
+ * Delete data in local storage for the given
+ * unique id and data type.
+ *
+ * @param {string} id   Unique id
+ * @param {string} type Data type
+ */
+export function delete(id, type) {
+  localStorage.removeItem(key(id, type));
+}
+
+/**
+ * Append data to an array in local storage
+ * for the given unique id and data type.
+ *
+ * @param {string} id   Unique id
+ * @param {string} type Data type
+ * @param {mixed}  data Data to write
+ */
+export function append(id, type, data) {
+  const array = get(id, type) || [];
+  array.push(data);
+  set(id, type, array);
+}
