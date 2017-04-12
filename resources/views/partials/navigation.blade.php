0b027b987294dc37e492041ec90ebed2abe065f3
deadlybutter <thedeadlybutter@gmail.com>
2017-04-12 14:03:19 -0400

    Removing extra slash

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -21,7 +21,7 @@
                 @if (Auth::user())
                     <a id="js-account-toggle" class="navigation__dropdown-toggle">My Profile</a>
                     <ul>
-                        <li><a href="{{ phoenixLink('/northstar/' . Auth::user()->northstar_id) }}">Profile</a></li>
+                        <li><a href="{{ phoenixLink('northstar/' . Auth::user()->northstar_id) }}">Profile</a></li>
                         <li><a href="{{ route('logout') }}" class="secondary-nav-item" id="link--logout">Log Out</a></li>
                     </ul>
                 @else
