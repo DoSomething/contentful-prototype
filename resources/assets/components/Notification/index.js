9aa6216ca2895c7891a54a77b536de6c319424a8
Diego Lorenzo <diego@diegolorenzo.com>
2017-04-20 18:10:01 -0400

    ESLint fixes for Notification and Placeholder components.

--- resources/assets/components/Notification/index.js
+++ resources/assets/components/Notification/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { makeHash } from '../../helpers';
+
 import './notification.scss';
 
 const Notification = ({ message, style, remove }) => (
   <div className={`notification -${style}`}>
-    <div className='notification__content'>
+    <div className="notification__content">
       <p>{ message }</p>
     </div>
-    <span className='notification__close' onClick={ remove }>&times;</span>
+    <button className="notification__close" onClick={remove}>&times;</button>
   </div>
 );
+
+Notification.propTypes = {
+  message: React.PropTypes.string,
+  style: React.PropTypes.string,
+  remove: React.PropTypes.func.isRequired,
+};
 
 Notification.defaultProps = {
   message: 'Agh, looks like we had an error. Try again in a few minutes!',
@@ -16,19 +24,25 @@ Notification.defaultProps = {
 };
 
 const NotificationList = ({ notifications, removeNotification }) => (
-  <div className='notification-list'>
+  <div className="notification-list">
     {notifications.map(({ message, style }, index) => (
       <Notification
-        key={index}
+        key={makeHash(message)}
         message={message}
         style={style}
-        remove={() => removeNotification(index)} />
+        remove={() => removeNotification(index)}
+      />
     ))}
   </div>
 );
 
+NotificationList.propTypes = {
+  notifications: React.PropTypes.arrayOf(React.PropTypes.object),
+  removeNotification: React.PropTypes.func.isRequired,
+};
+
 NotificationList.defaultProps = {
   notifications: [],
-}
+};
 
 export default NotificationList;
--- resources/assets/components/PlaceholderBlock/index.js
+++ resources/assets/components/PlaceholderBlock/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Block from '../Block/index';
 import './placeholder.scss';
 
-const PlaceholderBlock = (props) => {
-  return (
-    <Block className="placeholder">
-      {props.fields.title}
-    </Block>
-  );
+const PlaceholderBlock = props => (
+  <Block className="placeholder">
+    {props.fields.title}
+  </Block>
+);
+
+PlaceholderBlock.propTypes = {
+  fields: React.PropTypes.shape({
+    title: React.PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default PlaceholderBlock;
