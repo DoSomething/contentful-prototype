929fef687f9739fc10eb0ea2f0c715cb6c98bda5
Diego Lorenzo <diego@diegolorenzo.com>
2017-04-20 16:37:39 -0400

    ESLint updates for ReportbackBlock component.

--- resources/assets/components/ReportbackBlock/index.js
+++ resources/assets/components/ReportbackBlock/index.js
@@ -5,10 +5,10 @@ import ReportbackItemContainer from '../../containers/ReportbackItemContainer';
 import { mapDisplayToPoints } from '../../selectors/feed';
 import './reportback-block.scss';
 
-const ReportbackBlock = props => {
-  let items = [];
+const ReportbackBlock = (props) => {
+  const items = [];
 
-  for (let i = 0; i < mapDisplayToPoints(props.fields.displayOptions); i++) {
+  for (let i = 0; i < mapDisplayToPoints(props.fields.displayOptions); i += 1) {
     const id = props.reportbacks[i];
 
     items.push(
@@ -16,11 +16,18 @@ const ReportbackBlock = props => {
         <Block className="reportback-block">
           <ReportbackItemContainer id={id} />
         </Block>
-      </FlexCell>
+      </FlexCell>,
     );
   }
 
   return <FlexCell>{items}</FlexCell>;
+};
+
+ReportbackBlock.propTypes = {
+  fields: React.PropTypes.shape({
+    displayOptions: React.PropTypes.array,
+  }).isRequired,
+  reportbacks: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
 };
 
 export default ReportbackBlock;
