7b94b8166b25363e94c8079ae7c4251ff0304237
Diego Lorenzo <diego@diegolorenzo.com>
2017-04-21 14:48:58 -0400

    ESLint fixes for Flex component.

--- resources/assets/components/Flex/index.js
+++ resources/assets/components/Flex/index.js
@@ -3,20 +3,35 @@ import classnames from 'classnames';
 import { modifiers } from '../../helpers';
 import './flex.scss';
 
-export const Flex = ({className = null, children}) => (
+export const Flex = ({ className = null, children }) => (
   <div className={classnames('flex', className)}>
     {children}
   </div>
 );
 
-export const FlexCell = ({width = [], children}) => {
-  return (
-    <div className={classnames('flex__cell', modifiers(width))}>
-      {children}
-    </div>
-  );
+Flex.propTypes = {
+  className: React.PropTypes.string,
+  children: React.PropTypes.oneOfType([
+    React.PropTypes.object,
+    React.PropTypes.array,
+  ]).isRequired,
 };
+
+Flex.defaultProps = {
+  className: null,
+};
+
+export const FlexCell = ({ width = [], children }) => (
+  <div className={classnames('flex__cell', modifiers(width))}>
+    {children}
+  </div>
+);
 
 FlexCell.propTypes = {
   width: React.PropTypes.oneOf(['full', 'one-third', 'two-thirds']),
+  children: React.PropTypes.node.isRequired,
+};
+
+FlexCell.defaultProps = {
+  width: [],
 };
