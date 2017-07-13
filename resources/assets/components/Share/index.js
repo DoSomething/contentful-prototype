6586f3e2c75fc148a5763d8d7a443d15955be4e1
deadlybutter <thedeadlybutter@gmail.com>
2017-07-13 09:46:49 -0400

    Adding share index comments

--- resources/assets/components/Share/index.js
+++ resources/assets/components/Share/index.js
@@ -1,3 +1,11 @@
+/**
+ * The share component accepts a style variant,
+ * a custom url to share and metadata information
+ * on the share location.
+ */
 export default from './Share';
 
+/**
+ * Full constructed <Share /> component.
+ */
 export ShareContainer from './ShareContainer';
