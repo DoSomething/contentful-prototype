777bfc39b92a09c1dc59805e3c4d105165f5e6d9
deadlybutter <thedeadlybutter@gmail.com>
2017-08-14 16:53:39 -0400

    Fixing test style

--- resources/assets/components/Quiz/Quiz.test.js
+++ resources/assets/components/Quiz/Quiz.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Quiz from './Quiz';
-import Question from './Question';
 
 // Mock Redux containers so we don't need Provider context.
 jest.mock('./QuizContainer', () => 'QuizContainer');
 
 test('it should display a placeholder quiz', () => {
-  const wrapper = shallow(<Quiz quiz={{ title: "test title", json: { questions: [] } }} />);
+  const wrapper = shallow(<Quiz quiz={{ title: 'test title', json: { questions: [] } }} />);
   expect(wrapper.find('div.quiz')).toHaveLength(1);
 });
