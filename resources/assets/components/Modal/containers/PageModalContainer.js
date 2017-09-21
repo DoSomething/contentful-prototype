1433ef3b4089c0162010d3778e4ecde613f5c7c6
deadlybutter <thedeadlybutter@gmail.com>
2017-09-21 15:50:05 -0400

    Renaming blockId to contentfulId

--- resources/assets/components/Modal/containers/PageModalContainer.js
+++ resources/assets/components/Modal/containers/PageModalContainer.js
@@ -3,14 +3,14 @@ import { find } from 'lodash';
 import PageModal from '../configurations/PageModal';
 
 const mapStateToProps = (state) => {
-  const blockId = state.modal.blockId;
-  if (! blockId) {
+  const contentfulId = state.modal.contentfulId;
+  if (! contentfulId) {
     return null;
   }
 
   // TODO: Search other content objects, eg: blocks
 
-  const page = find(state.campaign.pages, { id: blockId });
+  const page = find(state.campaign.pages, { id: contentfulId });
   return page ? { content: page.fields.content } : null;
 };
 
