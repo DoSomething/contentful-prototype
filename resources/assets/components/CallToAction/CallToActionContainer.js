0642c1ec1140b7dc27d0117a5a39cc569a5cd9c1
deadlybutter <thedeadlybutter@gmail.com>
2017-09-22 11:22:38 -0400

    Updating Call To Action component

--- resources/assets/components/CallToAction/CallToActionContainer.js
+++ resources/assets/components/CallToAction/CallToActionContainer.js
@@ -1,6 +1,5 @@
 import { connect } from 'react-redux';
 import CallToAction from './CallToAction';
-import { clickedSignUp } from '../../actions';
 
 /**
  * Provide state from the Redux store as props for this component.
@@ -8,14 +7,6 @@ import { clickedSignUp } from '../../actions';
 const mapStateToProps = state => ({
   campaignId: state.campaign.legacyCampaignId,
 });
-
-/**
- * Provide pre-bound functions that allow the component to dispatch
- * actions to the Redux store as props for this component.
- */
-const mapDispatchToProps = {
-  clickedSignUp,
-};
 
 /**
  * Export the container component.
