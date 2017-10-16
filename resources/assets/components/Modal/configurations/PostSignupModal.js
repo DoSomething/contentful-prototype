74de5a10bcb94771ee216c03338ede1a362e0a7f
deadlybutter <thedeadlybutter@gmail.com>
2017-10-16 14:46:22 -0400

    Removing demo

--- resources/assets/components/Modal/configurations/PostSignupModal.js
+++ resources/assets/components/Modal/configurations/PostSignupModal.js
@@ -4,8 +4,6 @@ import { AffirmationContainer } from '../../Affirmation';
 import { CompetitionBlockContainer } from '../../CompetitionBlock';
 import SlideshowContainer from '../../Slideshow';
 
-import Card from '../../Card';
-
 const PostSignupModal = ({ competitionStep, closeModal }) => (
   <div className="modal__slide">
     <SlideshowContainer slideshowId="post signup modal" onComplete={closeModal}>
@@ -16,10 +14,6 @@ const PostSignupModal = ({ competitionStep, closeModal }) => (
           byline={competitionStep.additionalContent}
         />
       ) : null }
-      <Card className="bordered padded">
-        <h1>Why hello there,</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et libero blandit, fermentum ante vitae, consequat velit. Proin ac sem hendrerit, volutpat elit eget, tincidunt ex. Sed sed lectus tellus. Morbi eleifend mauris nec sem egestas tincidunt. Praesent sollicitudin turpis ac sem dictum tincidunt. Aliquam ut est in enim ullamcorper ullamcorper. In egestas quam quis elit molestie, sed volutpat arcu tincidunt. Quisque eu ultrices magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum sit amet mi dignissim, vulputate augue ut, fermentum metus. Nunc a elit in sapien efficitur imperdiet. Maecenas eget posuere dolor. Nulla hendrerit, mauris eget cursus semper, erat nibh scelerisque lectus, sed scelerisque dui ex et lorem. Maecenas nibh nulla, sodales ut ullamcorper ullamcorper, ullamcorper non nisl.</p>
-      </Card>
       <AffirmationContainer />
     </SlideshowContainer>
   </div>
