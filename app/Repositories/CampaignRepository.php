0a3edde302ebfbf69ecc333387c9e726a0170427
deadlybutter <thedeadlybutter@gmail.com>
2017-10-26 12:03:55 -0400

    updating how campaign model is made

--- app/Repositories/CampaignRepository.php
+++ app/Repositories/CampaignRepository.php
@@ -79,8 +79,10 @@ class CampaignRepository
             Cache::add($slug, json_encode($campaignEntry), $expiresAt);
         }
 
-        $campaignModel = CampaignModel::firstOrCreate(['id' => $campaignEntry->getId()]);
-        $campaignModel->fill(['slug' => $slug])->save();
+        $campaignModel = CampaignModel::updateOrCreate(
+            ['id' => $campaignEntry->getId()],
+            ['slug' => $slug]
+        );
 
         $entity = new CampaignEntity($campaignEntry);
         // TODO: This should be a dispatched event, so it's not blocking the HTTP request.
