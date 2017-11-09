c897296f25ca1af8e1c0a5b6e4cee017ea74fe3a
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-09 11:31:29 -0500

    rendering AdminDashboard is user is admin

--- resources/assets/components/Campaign/Campaign.js
+++ resources/assets/components/Campaign/Campaign.js
@@ -4,12 +4,15 @@ import PropTypes from 'prop-types';
 import ModalSwitch from '../Modal';
 import { CampaignPageContainer, LandingPageContainer } from '../Page';
 import NotificationContainer from '../Notification';
+import AdminDashboard from '../AdminDashboard';
 
 const Campaign = (props) => {
-  const { isAffiliated, useLandingPage } = props;
+  const { isAffiliated, useLandingPage, userRole } = props;
+  const isAdmin = userRole === 'admin';
 
   return (
     <div>
+      { isAdmin ? <AdminDashboard redirectPath={props.location.pathname} /> : null }
       <NotificationContainer />
       <ModalSwitch />
 
@@ -24,11 +27,13 @@ const Campaign = (props) => {
 Campaign.propTypes = {
   isAffiliated: PropTypes.bool,
   useLandingPage: PropTypes.bool,
+  userRole: PropTypes.string,
 };
 
 Campaign.defaultProps = {
   isAffiliated: false,
   useLandingPage: false,
+  userRole: null,
 };
 
 export default Campaign;
