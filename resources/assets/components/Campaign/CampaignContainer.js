9a0ad3616d4761e2fd3ffc46637e49cfb4d78cb4
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-09 11:31:00 -0500

    adding userRole prop from state

--- resources/assets/components/Campaign/CampaignContainer.js
+++ resources/assets/components/Campaign/CampaignContainer.js
@@ -5,6 +5,7 @@ import Campaign from './Campaign';
 const mapStateToProps = state => ({
   isAffiliated: state.signups.thisCampaign,
   useLandingPage: state.campaign.landingPage !== null,
+  userRole: state.user.role,
 });
 
 export default connect(mapStateToProps)(Campaign);
