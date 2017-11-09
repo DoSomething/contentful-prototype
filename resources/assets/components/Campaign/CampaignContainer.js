cbe42424a2d6116f9d2c853ac9e5c594bad186fe
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-09 17:01:37 -0500

    adding slug prop form state

--- resources/assets/components/Campaign/CampaignContainer.js
+++ resources/assets/components/Campaign/CampaignContainer.js
@@ -6,6 +6,7 @@ const mapStateToProps = state => ({
   isAffiliated: state.signups.thisCampaign,
   useLandingPage: state.campaign.landingPage !== null,
   userRole: state.user.role,
+  slug: state.campaign.slug,
 });
 
 export default connect(mapStateToProps)(Campaign);
