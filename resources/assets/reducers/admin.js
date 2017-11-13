2209058897d9702712103bfad09ca56d13b58f5c
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-13 17:04:59 -0500

    adding logic for toggling between action and landing page

--- resources/assets/reducers/admin.js
+++ resources/assets/reducers/admin.js
@@ -1,10 +1,17 @@
-import { SHOW_LANDING_PAGE } from '../actions';
+import { SHOW_LANDING_PAGE, SHOW_ACTION_PAGE } from '../actions';
 
 const admin = (state = {}, action) => {
   switch (action.type) {
     case SHOW_LANDING_PAGE: return {
       ...state,
       shouldShowLandingPage: true,
+      shouldShowActionPage: false,
+    };
+
+    case SHOW_ACTION_PAGE: return {
+      ...state,
+      shouldShowActionPage: true,
+      shouldShowLandingPage: false,
     };
 
     default: return state;
