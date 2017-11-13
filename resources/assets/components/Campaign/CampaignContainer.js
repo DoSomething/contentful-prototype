5f8cfe7ee21abc4a61a58d0fd57dd0200e5e6f3e
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-13 15:29:58 -0500

    adding admin landing page override to props, using value to determine landing page visibility

--- resources/assets/components/Campaign/CampaignContainer.js
+++ resources/assets/components/Campaign/CampaignContainer.js
@@ -2,15 +2,21 @@ import { connect } from 'react-redux';
 
 import Campaign from './Campaign';
 import { clickedShowAffirmation } from '../../actions/signup';
+import clickedShowLandingPage from '../../actions/admin';
 
-const mapStateToProps = state => ({
-  isAffiliated: state.signups.thisCampaign,
-  useLandingPage: state.campaign.landingPage !== null,
-  slug: state.campaign.slug,
-});
+const mapStateToProps = (state) => {
+  const shouldShowLandingPage = ! state.signups.thisCampaign || state.admin.shouldShowLandingPage;
+
+  return {
+    useLandingPage: state.campaign.landingPage !== null,
+    slug: state.campaign.slug,
+    shouldShowLandingPage,
+  };
+};
 
 const actionCreators = {
   clickedShowAffirmation,
+  clickedShowLandingPage,
 };
 
 export default connect(mapStateToProps, actionCreators)(Campaign);
