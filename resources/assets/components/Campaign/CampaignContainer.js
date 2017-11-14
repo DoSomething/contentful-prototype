6901dac9a3b37390414b6f7145fd05359bb8a51d
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-14 13:39:30 -0500

    adding signup actions to state, props, and landing page logic

--- resources/assets/components/Campaign/CampaignContainer.js
+++ resources/assets/components/Campaign/CampaignContainer.js
@@ -1,19 +1,22 @@
 import { connect } from 'react-redux';
 
 import Campaign from './Campaign';
-import { clickedShowAffirmation } from '../../actions/signup';
+import { clickedShowAffirmation, signupCreated, clickedRemoveSignUp } from '../../actions/signup';
 import { clickedShowLandingPage, clickedShowActionPage } from '../../actions/admin';
 
 const mapStateToProps = (state) => {
   const isSignedUp = state.signups.thisCampaign;
+  const shouldShowActionPage = state.admin.shouldShowActionPage;
   const hasLandingPage = state.campaign.landingPage !== null;
 
   const shouldShowLandingPage = hasLandingPage &&
-    (! isSignedUp || state.admin.shouldShowLandingPage);
+    (! isSignedUp || state.admin.shouldShowLandingPage) &&
+    ! shouldShowActionPage;
 
   return {
     slug: state.campaign.slug,
     hasLandingPage,
+    isSignedUp,
     shouldShowLandingPage,
   };
 };
@@ -22,6 +25,8 @@ const actionCreators = {
   clickedShowAffirmation,
   clickedShowLandingPage,
   clickedShowActionPage,
+  signupCreated,
+  clickedRemoveSignUp,
 };
 
 export default connect(mapStateToProps, actionCreators)(Campaign);
