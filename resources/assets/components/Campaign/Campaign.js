36e203d65e1f13003e5386ed878753b007b4b521
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-16 14:48:20 -0500

    removing campaign admin-dashboard specific stuff

--- resources/assets/components/Campaign/Campaign.js
+++ resources/assets/components/Campaign/Campaign.js
@@ -1,68 +1,29 @@
-/* global window */
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 import ModalSwitch from '../Modal';
 import { CampaignPageContainer, LandingPageContainer } from '../Page';
 import NotificationContainer from '../Notification';
-import AdminDashboardContainer from '../AdminDashboard';
+import { AdminDashboardContainer, CampaignDashboardContainer } from '../AdminDashboard';
 
-const Campaign = (props) => {
-  const { hasLandingPage, slug, clickedShowAffirmation, clickedShowLandingPage,
-    clickedShowActionPage, clickedRemoveSignUp, shouldShowLandingPage,
-    signupCreated, isSignedUp } = props;
+const Campaign = props => (
+  <div>
+    <AdminDashboardContainer>
+      <CampaignDashboardContainer />
+    </AdminDashboardContainer>
 
-  const onSignUpClick = () => (! isSignedUp ? signupCreated() : clickedRemoveSignUp());
+    <NotificationContainer />
+    <ModalSwitch />
 
-  return (
-    <div>
-      <AdminDashboardContainer>
-        <a className="button -secondary margin-md" href={`/next/cache/campaign_${slug}?redirect=${window.location.pathname}`}>
-          Clear Cache
-        </a>
-        <button className="button -secondary margin-md" onClick={clickedShowAffirmation}>
-          Show Affirmation
-        </button>
-        { hasLandingPage ?
-          <button className="button -secondary margin-md" onClick={clickedShowLandingPage}>
-            Show Landing Page
-          </button>
-          : null}
-        <Link className="button -secondary margin-horizontal-md" to={`/us/campaigns/${slug}/action`} onClick={clickedShowActionPage}>
-          Show Action Page
-        </Link>
-        <button className="button -secondary margin-horizontal-md" onClick={onSignUpClick}>
-          {`Mock ${isSignedUp ? 'Un-sign Up' : 'Sign Up'}`}
-        </button>
-      </AdminDashboardContainer>
-      <NotificationContainer />
-      <ModalSwitch />
-
-      { shouldShowLandingPage ?
-        <LandingPageContainer {...props} />
-        :
-        <CampaignPageContainer {...props} />}
-    </div>
-  );
-};
+    { props.shouldShowLandingPage ?
+      <LandingPageContainer {...props} />
+      :
+      <CampaignPageContainer {...props} />}
+  </div>
+);
 
 Campaign.propTypes = {
-  hasLandingPage: PropTypes.bool,
-  slug: PropTypes.string.isRequired,
-  isSignedUp: PropTypes.bool.isRequired,
-  clickedShowAffirmation: PropTypes.func.isRequired,
-  clickedShowLandingPage: PropTypes.func.isRequired,
-  clickedShowActionPage: PropTypes.func.isRequired,
-  clickedRemoveSignUp: PropTypes.func.isRequired,
-  signupCreated: PropTypes.func.isRequired,
-  shouldShowLandingPage: PropTypes.bool,
-};
-
-Campaign.defaultProps = {
-  hasLandingPage: false,
-  shouldShowLandingPage: false,
+  shouldShowLandingPage: PropTypes.bool.isRequired,
 };
 
 export default Campaign;
