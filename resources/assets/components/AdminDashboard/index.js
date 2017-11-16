e0681b886f4816f324d503f16c8a595c5686864d
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-16 14:47:53 -0500

    exporting new components

--- resources/assets/components/AdminDashboard/index.js
+++ resources/assets/components/AdminDashboard/index.js
@@ -1,3 +1,6 @@
 export default from './AdminDashboardContainer';
 
+export AdminDashboardContainer from './AdminDashboardContainer';
+export CampaignDashboardContainer from './dashboards/CampaignDashboardContainer';
 export AdminDashboard from './AdminDashboard';
+
