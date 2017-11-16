78fa9c4f813242b624b54ebcc2e80508d492b162
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-16 14:47:31 -0500

    moving campaign admin-dashboard container stuff to its own container

---
+++ resources/assets/components/AdminDashboard/dashboards/CampaignDashboardContainer.js
@@ -0,0 +1,26 @@
+import { connect } from 'react-redux';
+
+import CampaignDashboard from './CampaignDashboard';
+import { clickedShowLandingPage, clickedShowActionPage } from '../../../actions/admin';
+import { clickedShowAffirmation, signupCreated, clickedRemoveSignUp } from '../../../actions/signup';
+
+const mapStateToProps = (state) => {
+  const isSignedUp = state.signups.thisCampaign;
+  const hasLandingPage = state.campaign.landingPage !== null;
+
+  return {
+    slug: state.campaign.slug,
+    hasLandingPage,
+    isSignedUp,
+  };
+};
+
+const actionCreators = {
+  clickedShowAffirmation,
+  clickedShowLandingPage,
+  clickedShowActionPage,
+  signupCreated,
+  clickedRemoveSignUp,
+};
+
+export default connect(mapStateToProps, actionCreators)(CampaignDashboard);
