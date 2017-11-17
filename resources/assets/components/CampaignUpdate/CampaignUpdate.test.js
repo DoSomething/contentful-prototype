9d26c1445a760285985f0db2ca9eb69adbe43d2b
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-17 15:04:58 -0500

    update test with new prop

--- resources/assets/components/CampaignUpdate/CampaignUpdate.test.js
+++ resources/assets/components/CampaignUpdate/CampaignUpdate.test.js
@@ -20,6 +20,7 @@ const component = shallow(
     author={author}
     content="Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Sed posuere consectetur est at lobortis. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum."
     shareLink="http://example.com/link-to-content"
+    titleLink="http://example.com/link-to-content"
   />,
 );
 
