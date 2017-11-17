40dae075002fa032f752f7dd0bd0a9b3170817ac
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-17 14:16:37 -0500

    adding new titleLink prop, changing social link to be modal

--- resources/assets/components/CampaignUpdate/CampaignUpdateContainer.js
+++ resources/assets/components/CampaignUpdate/CampaignUpdateContainer.js
@@ -8,16 +8,18 @@ import { makeShareLink } from '../../helpers';
 const mapStateToProps = (state, props) => {
   const campaignUpdate = find(state.campaign.activityFeed, { id: props.id });
   const { author, content, link } = campaignUpdate.fields;
+  const linkOptions = {
+    domain: window.location.origin,
+    slug: state.campaign.slug,
+    key: props.id,
+  };
 
   return {
     author,
     content,
     link,
-    shareLink: makeShareLink('campaigns', {
-      domain: window.location.origin,
-      slug: state.campaign.slug,
-      key: props.id,
-    }),
+    shareLink: makeShareLink('campaigns', { ...linkOptions, type: 'modal' }),
+    titleLink: makeShareLink('campaigns', { ...linkOptions, type: 'blocks' }),
   };
 };
 
