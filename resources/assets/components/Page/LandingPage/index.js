e214d819fbdf7f754eee8e41cbc357103abdac95
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-30 10:10:27 -0500

    exports for new alternative landing page component

--- resources/assets/components/Page/LandingPage/index.js
+++ resources/assets/components/Page/LandingPage/index.js
@@ -2,4 +2,8 @@ export LandingPage from './LandingPage';
 
 export LandingPageContainer from './LandingPageContainer';
 
+export LandingPageAlt from './LandingPageAlt';
+
+export LandingPageAltContainer from './LandingPageAltContainer';
+
 export default './LandingPage';
--- resources/assets/components/Page/index.js
+++ resources/assets/components/Page/index.js
@@ -4,4 +4,7 @@ export { CampaignPageContainer } from './CampaignPage';
 export { LandingPage } from './LandingPage';
 export { LandingPageContainer } from './LandingPage';
 
+export { LandingPageAlt } from './LandingPage';
+export { LandingPageAltContainer } from './LandingPage';
+
 export default from './Page';
