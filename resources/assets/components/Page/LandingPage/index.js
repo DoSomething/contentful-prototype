695ef2f643b933c3723a2ebf950933cc48481a70
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-12-01 12:19:21 -0500

    removing all traces of alt landing page (LandingPageAlt)

--- resources/assets/components/Page/LandingPage/index.js
+++ resources/assets/components/Page/LandingPage/index.js
@@ -2,8 +2,4 @@ export LandingPage from './LandingPage';
 
 export LandingPageContainer from './LandingPageContainer';
 
-export LandingPageAlt from './LandingPageAlt';
-
-export LandingPageAltContainer from './LandingPageAltContainer';
-
 export default './LandingPage';
--- resources/assets/components/Page/index.js
+++ resources/assets/components/Page/index.js
@@ -4,7 +4,4 @@ export { CampaignPageContainer } from './CampaignPage';
 export { LandingPage } from './LandingPage';
 export { LandingPageContainer } from './LandingPage';
 
-export { LandingPageAlt } from './LandingPage';
-export { LandingPageAltContainer } from './LandingPage';
-
 export default from './Page';
