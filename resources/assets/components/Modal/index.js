be53795401eec5c5a72b66e19536689c490c49f6
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-12-06 11:32:51 -0500

    exporting surveyModal

--- resources/assets/components/Modal/index.js
+++ resources/assets/components/Modal/index.js
@@ -4,7 +4,9 @@ export Modal from './containers/ModalContainer';
 export PostSignupModal from './containers/PostSignupModalContainer';
 export ContentModal from './containers/ContentModalContainer';
 export ReportbackUploaderModal from './configurations/ReportbackUploaderModal';
+export SurveyModal from './configurations/SurveyModal';
 
 export const POST_SIGNUP_MODAL = 'POST_SIGNUP_MODAL';
 export const CONTENT_MODAL = 'CONTENT_MODAL';
 export const REPORTBACK_UPLOADER_MODAL = 'REPORTBACK_UPLOADER_MODAL';
+export const SURVEY_MODAL = 'SURVEY_MODAL';
