1aefb1a6fa05a192d09690204bef76f254bb2302
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-12-07 16:39:06 -0500

    Adding new survey URL

--- resources/assets/constants/survey.js
+++ resources/assets/constants/survey.js
@@ -1,3 +1,3 @@
 // TODO SURVEY_COUNTDOWN should be set to 60. Set to 5 for testing and sanity.
 export const SURVEY_COUNTDOWN = 5;
-export const SURVEY_DATA_URL = 'https://dosomething.typeform.com/to/anehxy';
+export const SURVEY_DATA_URL = 'https://dosomething.typeform.com/to/Bvcwvm';
