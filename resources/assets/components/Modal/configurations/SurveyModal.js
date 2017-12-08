2f164b12d4f9d86c1f51c3c9f453ecf979fd40ba
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-12-08 11:48:13 -0500

    requiring props, removing dataUrl prop and just using Constant

--- resources/assets/components/Modal/configurations/SurveyModal.js
+++ resources/assets/components/Modal/configurations/SurveyModal.js
@@ -11,12 +11,12 @@ class SurveyModal extends React.Component {
   }
 
   render() {
-    const { dataUrl, northstarId, campaignId, legacyCampaignId } = this.props;
+    const { northstarId, campaignId, legacyCampaignId } = this.props;
 
     return (
       <div
         className="modal__slide typeform-widget"
-        data-url={`${dataUrl}?northstar_id=${northstarId}&campaign_id=${campaignId}&legacy_campaign_id=${legacyCampaignId}`}
+        data-url={`${SURVEY_DATA_URL}?northstar_id=${northstarId}&campaign_id=${campaignId}&legacy_campaign_id=${legacyCampaignId}`}
         style={{ width: '100%', height: '500px' }}
       />
     );
@@ -24,17 +24,9 @@ class SurveyModal extends React.Component {
 }
 
 SurveyModal.propTypes = {
-  dataUrl: PropTypes.string,
-  northstarId: PropTypes.string,
-  campaignId: PropTypes.string,
-  legacyCampaignId: PropTypes.string,
-};
-
-SurveyModal.defaultProps = {
-  dataUrl: SURVEY_DATA_URL,
-  northstarId: null,
-  campaignId: null,
-  legacyCampaignId: null,
+  northstarId: PropTypes.string.isRequired,
+  campaignId: PropTypes.string.isRequired,
+  legacyCampaignId: PropTypes.string.isRequired,
 };
 
 export default SurveyModal;
