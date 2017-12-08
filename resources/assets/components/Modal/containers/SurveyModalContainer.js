804559f2bced46498d32a85b97ce7758fe85f7d7
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-12-08 13:31:56 -0500

    passing campaign slug and northstarId props

--- resources/assets/components/Modal/containers/SurveyModalContainer.js
+++ resources/assets/components/Modal/containers/SurveyModalContainer.js
@@ -1,10 +1,12 @@
 import { connect } from 'react-redux';
 import SurveyModal from '../configurations/SurveyModal';
+import { getUserId } from '../../../selectors/user';
 
 const mapStateToProps = state => ({
-  northstarId: state.user.id,
+  northstarId: getUserId(state),
   campaignId: state.campaign.id,
   legacyCampaignId: state.campaign.legacyCampaignId,
+  campaignSlug: state.campaign.slug,
 });
 
 export default connect(mapStateToProps)(SurveyModal);
