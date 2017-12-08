664af004a552572d0224dce3e9df1d79d2effba2
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-12-08 12:03:38 -0500

    removing survey constans file

--- resources/assets/constants/survey.js
+++
@@ -1,3 +0,0 @@
-// TODO SURVEY_COUNTDOWN should be set to 60. Set to 5 for testing and sanity.
-export const SURVEY_COUNTDOWN = 5;
-export const SURVEY_DATA_URL = 'https://dosomething.typeform.com/to/Bvcwvm';
