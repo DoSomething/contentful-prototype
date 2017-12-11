903aa4f1b569ddb9ef7f8f919eb5e07b1cc70e12
Diego Lorenzo <diego@diegolorenzo.com>
2017-12-11 17:03:41 -0500

    Adding post action.

---
+++ resources/assets/actions/post.js
@@ -0,0 +1,9 @@
+/**
+ * Fetch posts for the specified campaign.
+ *
+ * @param  {String} $campaignId
+ * @return {[type]}
+ */
+export function fetchCampaignPosts($campaignId) {
+  console.log($campaignId);
+}
