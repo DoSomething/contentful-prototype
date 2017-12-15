f724c8101f35cebb66b2608ceb1c5c4589fbe0c4
Diego Lorenzo <diego@diegolorenzo.com>
2017-12-15 14:40:48 -0500

    Disabling API call on production.

--- resources/assets/middleware/api.js
+++ resources/assets/middleware/api.js
@@ -17,17 +17,17 @@ const apiMiddleware = () => next => (action) => {
 
   const client = new RestApiClient(PHOENIX_URL);
 
-  client.get(payload.url, payload.query)
-    .then((response) => {
-      // @TODO: more to come with handling the response!
-      if (window.ENV.APP_ENV !== 'production') {
+  if (window.ENV.APP_ENV !== 'production') {
+    client.get(payload.url, payload.query)
+      .then((response) => {
+        // @TODO: more to come with handling the response!
         if (response.data) {
           console.table(response.data);
         } else {
           console.log('Nope, nothing to see here for now...');
         }
-      }
-    });
+      });
+  }
 
   return next(action);
 };
