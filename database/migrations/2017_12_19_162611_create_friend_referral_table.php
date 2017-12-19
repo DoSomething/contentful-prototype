77e02b6659c0359febb5a11ab83ab121d6bc823c
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-12-19 11:31:11 -0500

    adding migration for friend_referral table

---
+++ database/migrations/2017_12_19_162611_create_friend_referral_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendReferralTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friend_referrals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('friend_name');
+            $table->string('friend_email');
+            $table->text('friend_story');
+            $table->string('referrer_name');
+            $table->string('referrer_northstar_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friend_referrals');
+    }
+}
