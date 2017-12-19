332a6c2305e7b520830a1b3a6068880d86d0f461
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-12-19 12:54:09 -0500

    change table name to 'referrals'

--- database/migrations/2017_12_19_162611_create_referrals_table.php
+++ database/migrations/2017_12_19_162611_create_referrals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFriendReferralsTable extends Migration
+class CreateReferralsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateFriendReferralsTable extends Migration
      */
     public function up()
     {
-        Schema::create('friend_referrals', function (Blueprint $table) {
+        Schema::create('referrals', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
             $table->string('friend_name');
