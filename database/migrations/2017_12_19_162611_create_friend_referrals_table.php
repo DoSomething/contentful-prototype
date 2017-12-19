33c892ad73b290b564be37cd9e7b28d5ed00fdd5
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-12-19 11:34:58 -0500

    plural!

--- database/migrations/2017_12_19_162611_create_friend_referrals_table.php
+++ database/migrations/2017_12_19_162611_create_friend_referrals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFriendReferralTable extends Migration
+class CreateFriendReferralsTable extends Migration
 {
     /**
      * Run the migrations.
