2e608d6fe1e2ba262dd0eb26b946e59d14e4d171
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-12-19 14:11:28 -0500

    adding todo

--- resources/assets/components/ReportbackUploader/ReportbackUploader.js
+++ resources/assets/components/ReportbackUploader/ReportbackUploader.js
@@ -93,6 +93,7 @@ class ReportbackUploader extends React.Component {
 
     reportback.media.type = fileType ? fileType.substring(0, fileType.indexOf('/')) : null;
 
+    // TODO Add specific submission method for ReferralRB.
     this.props.submitReportback(
       ReportbackUploader.setFormData(reportback),
     ).then(() => {
