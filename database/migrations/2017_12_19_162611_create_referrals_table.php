342dfabd5a7fa4b906fb25d15571e8227dedfdbe
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-12-20 12:02:37 -0500

    removing referrer name and email columns

--- database/migrations/2017_12_19_162611_create_referrals_table.php
+++ database/migrations/2017_12_19_162611_create_referrals_table.php
@@ -19,8 +19,6 @@ class CreateReferralsTable extends Migration
             $table->string('friend_name');
             $table->string('friend_email');
             $table->text('friend_story');
-            $table->string('referrer_name');
-            $table->string('referrer_email');
             $table->string('referrer_northstar_id');
         });
     }
