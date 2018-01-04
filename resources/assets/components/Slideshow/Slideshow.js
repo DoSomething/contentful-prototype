0b193bea189a84e8433c5b89099c0ec0c7e08860
deadlybutter <thedeadlybutter@gmail.com>
2018-01-04 13:57:52 -0500

    optionally show the close button

--- resources/assets/components/Slideshow/Slideshow.js
+++ resources/assets/components/Slideshow/Slideshow.js
@@ -2,26 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './slideshow.scss';
 
-// TODO: Investigate why the `no-confusing-arrow` rule isn't working in this file.
-/* eslint-disable no-confusing-arrow */
-const Slideshow = ({ isFinalSlide, nextSlide, onComplete, slide, slideshowId }) => (
-  <div className="slideshow">
-    <div className="slideshow__slide">
-      { slide }
-    </div>
+const Slideshow = (props) => {
+  const {
+    hideCloseButton, isFinalSlide, nextSlide,
+    onComplete, slide, slideshowId,
+  } = props;
+
+  const SlideButton = isFinalSlide && hideCloseButton ? null : (
     <button
       className="button slideshow__button margin-top-lg"
       onClick={() => isFinalSlide ? onComplete() : nextSlide(slideshowId)}
     >{ isFinalSlide ? 'Close' : 'Next' }</button>
-  </div>
-);
+  );
+
+  return (
+    <div className="slideshow">
+      <div className="slideshow__slide">
+        { slide }
+      </div>
+      <SlideButton />
+    </div>
+  );
+};
 
 Slideshow.propTypes = {
+  hideCloseButton: PropTypes.bool,
   isFinalSlide: PropTypes.bool.isRequired,
   nextSlide: PropTypes.func.isRequired,
-  onComplete: PropTypes.func.isRequired,
+  onComplete: PropTypes.func,
   slide: PropTypes.node.isRequired,
   slideshowId: PropTypes.string.isRequired,
+};
+
+Slideshow.defaultProps = {
+  hideCloseButton: false,
+  onComplete: null,
 };
 
 export default Slideshow;
