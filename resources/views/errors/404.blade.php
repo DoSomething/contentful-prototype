045e7d3e33e241dfa3592a6eba765c4fadfd8efa
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-01-17 11:21:47 -0500

    updating text and help center link

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -12,7 +12,7 @@
                     </div>
                     <div class="container__block -centered">
                         <p>
-                            We searched our site, but couldn't find what you were looking for. Try <a href="{{ url('/us/campaigns/grab-mic?utm_source=404') }}">Grab the Mic</a> and join our movement to create the most civically active generation ever, <a href="{{ url('/') }}">our homepage</a>, or our <a href="https://help.dosomething.org/hc/en-us">Help Center</a>.
+                            We searched our site, but couldn't find what you were looking for. Try <a href="{{ url('/us/campaigns/grab-mic?utm_source=404') }}">Grab the Mic</a> and join our movement to create the most civically active generation ever. You can also try <a href="{{ url('/') }}">our homepage</a> or <a href="https://help.dosomething.org/hc/en-us/articles/115016093488-Help-The-page-I-m-looking-for-says-Page-Not-Found-">reach out</a> to us.
                         </p>
                     </div>
                 </div>
