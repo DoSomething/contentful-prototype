a2c68437cf17480762d7e4accf9b569ae9f36965
deadlybutter <thedeadlybutter@gmail.com>
2018-01-23 11:27:50 -0500

    Updating campaign routing logic so the quiz is not gated

--- resources/assets/components/Campaign/CampaignContainer.js
+++ resources/assets/components/Campaign/CampaignContainer.js
@@ -2,13 +2,21 @@ import { connect } from 'react-redux';
 
 import Campaign from './Campaign';
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, props) => {
   const hasLandingPage = state.campaign.landingPage !== null;
   const isSignedUp = state.signups.thisCampaign;
 
-  const shouldShowLandingPage = hasLandingPage &&
-    (! isSignedUp || state.admin.shouldShowLandingPage) &&
-    ! state.admin.shouldShowActionPage;
+  const { location, match } = props;
+  const isQuiz = location.pathname.replace(match.url, '').startsWith('/quiz/');
+
+  const blockLandingPage = state.admin.shouldShowActionPage || isQuiz;
+  let shouldShowLandingPage = false;
+
+  if (state.admin.shouldShowLandingPage) {
+    shouldShowLandingPage = true;
+  } else if (hasLandingPage && ! blockLandingPage) {
+    shouldShowLandingPage = ! isSignedUp;
+  }
 
   return {
     shouldShowLandingPage,
