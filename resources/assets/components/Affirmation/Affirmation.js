462d299abe5dd70ffc79f6687e6d1a5e5687cc92
deadlybutter <thedeadlybutter@gmail.com>
2018-01-26 11:30:02 -0500

    Adding Byline to the Affirmation

--- resources/assets/components/Affirmation/Affirmation.js
+++ resources/assets/components/Affirmation/Affirmation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Byline from '../Byline';
 import Card from '../Card';
 import { Flex, FlexCell } from '../Flex';
 import Markdown from '../Markdown';
@@ -8,22 +9,10 @@ import { ShareContainer } from '../Share';
 
 import './affirmation.scss';
 
-/*
-  TODO: waiting on field...
-  import Byline from '../Byline';
-
-  <Byline
-    author={authorFields.name}
-    avatar={authorFields.avatar || undefined}
-    jobTitle={authorFields.jobTitle || undefined}
-    className="float-left"
-  />
- */
-
 const Affirmation = ({ closeModal, content }) => (
   <Card className="affirmation rounded" title="Thanks for joining us!" onClose={closeModal}>
     <Markdown className="padded">{content.quote}</Markdown>
-    <Flex>
+    <Flex className="flex-align-center">
       <FlexCell className="affirmation__cta padded" width="half">
         <h3>{ content.callToActionHeader }</h3>
         <p>{ content.callToActionDescription }</p>
@@ -32,6 +21,7 @@ const Affirmation = ({ closeModal, content }) => (
         <ShareContainer variant="blue" parentSource="affirmation" />
       </FlexCell>
     </Flex>
+    <Byline author={content.author.fields.name} {...content.author.fields} className="padded" />
   </Card>
 );
 
@@ -40,7 +30,7 @@ Affirmation.propTypes = {
   content: PropTypes.shape({
     header: PropTypes.string,
     photo: PropTypes.string,
-    author: PropTypes.string,
+    author: PropTypes.object,
     quote: PropTypes.string,
     callToActionHeader: PropTypes.string,
     callToActionDescription: PropTypes.string,
