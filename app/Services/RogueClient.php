3a558b1f7a57f1c9f2ebef6ec3fedba67d233df3
Diego Lorenzo <diego@diegolorenzo.com>
2018-01-30 12:16:40 -0500

    Override how errors are plucked from validation exception compared to Gateway.

--- app/Services/RogueClient.php
+++ app/Services/RogueClient.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use DoSomething\Gateway\AuthorizesWithOAuth2;
 use DoSomething\Gateway\Common\RestApiClient;
+use DoSomething\Gateway\Exceptions\ValidationException;
 
 class RogueClient extends RestApiClient
 {
@@ -14,10 +15,32 @@ class RogueClient extends RestApiClient
      */
     public function __construct()
     {
-        $this->authorizationServerUri = config('NORTHSTAR_URL');
+        $this->authorizationServerUri = config('services.northstar.url');
+        $this->grant = config('services.northstar.grant');
+        $this->config = config('services.northstar');
+        $this->bridge = \DoSomething\Gateway\Laravel\LaravelOAuthBridge::class;
 
         $base_url = config('services.rogue.url').'/api/';
 
         parent::__construct($base_url);
+    }
+
+    /**
+     * Handle validation exceptions.
+     *
+     * @param string $endpoint - The human-readable route that triggered the error.
+     * @param array $response - The body of the response.
+     * @param string $method - The HTTP method for the request that triggered the error, for optionally resending.
+     * @param string $path - The path for the request that triggered the error, for optionally resending.
+     * @param array $options - The options for the request that triggered the error, for optionally resending.
+     * @return \GuzzleHttp\Psr7\Response|void
+     * @throws UnauthorizedException
+     */
+    public function handleValidationException($endpoint, $response, $method, $path, $options)
+    {
+        // Hackily format the "message" string in key-value format.
+        $errors = $response['errors'];
+
+        throw new ValidationException($errors, $endpoint);
     }
 }
