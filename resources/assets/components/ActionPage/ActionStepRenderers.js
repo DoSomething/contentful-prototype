2e61a606d9e98a6599966bc5e9241c33d9f164f2
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-02-02 14:15:45 -0500

    removing template props from voter reg and third party actions

--- resources/assets/components/ActionPage/ActionStepRenderers.js
+++ resources/assets/components/ActionPage/ActionStepRenderers.js
@@ -85,7 +85,6 @@ export function renderThirdPartyAction(step, stepIndex) {
     <ThirdPartyActionContainer
       key={id}
       title={title}
-      template="legacy"
       content={content}
       stepIndex={stepIndex}
       dynamicLink={additionalContent.dynamicLink || null}
@@ -179,7 +178,7 @@ export function renderLegacyGallery() {
  */
 export function renderVoterRegistration(step, stepIndex) {
   const { title, content, additionalContent } = step.fields;
-  const { template, dynamicLink } = additionalContent;
+  const { dynamicLink } = additionalContent;
 
   return (
     <FlexCell width="full" key={`voter-reg-${stepIndex}`}>
@@ -187,7 +186,6 @@ export function renderVoterRegistration(step, stepIndex) {
         <VoterRegistrationContainer
           content={content}
           title={title}
-          template={template}
           stepIndex={stepIndex}
           dynamicLink={dynamicLink}
         />
