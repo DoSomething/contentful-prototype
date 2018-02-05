950f9844d5dc6bdebc26ea55e6135cb3493812f7
David Furnes <david@dfurnes.com>
2018-02-05 14:06:09 -0500

    Add new share route.
    Rendering this server-side for now to sidestep some more complicated client-side routing rules. :speak_no_evil:

---
+++ resources/views/share/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('content')
+    <div>
+        <div class="feed-enclosure">
+            <div class="flex wrapper">
+                <div class="flex__cell">
+                    <div id="share-page" class="placeholder -full">
+                        <div class="spinner"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
