d63cd73b206283db115b2f5e3a0e8af12295efe5
David Furnes <david@dfurnes.com>
2018-02-06 17:55:15 -0500

    Some refinements to the MVP.

--- resources/assets/share.js
+++ resources/assets/share.js
@@ -4,6 +4,7 @@ import { Engine } from '@dosomething/puck-client';
 import { env, query, showFacebookSharePrompt } from './helpers';
 
 function renderSharePage(element) {
+  const button = element.querySelector('button');
   const shareUrl = query('href');
   const puck = new Engine({
     source: 'phoenix-next',
@@ -15,14 +16,14 @@ function renderSharePage(element) {
   puck.trackEvent('share page loaded', trackingData);
 
   // Wait for 'ready' event so we know FB SDK is loaded.
-  element.addEventListener('click', (event) => {
+  button.addEventListener('click', (event) => {
     event.preventDefault();
 
     console.log('sharing', element, trackingData, window.FB);
     showFacebookSharePrompt({ href: shareUrl }, (response) => {
       console.log('showed prompt?', response);
       if (response) {
-        element.innerHtml = '<h1>Thanks for sharing!</h1>'; // eslint-disable-line
+        element.innerText = 'Thanks for sharing!'; // eslint-disable-line
         puck.trackEvent('share page completed', trackingData);
       } else {
         puck.trackEvent('share page cancelled', trackingData);
--- resources/views/share/show.blade.php
+++ resources/views/share/show.blade.php
@@ -1,12 +1,17 @@
-@extends('layouts.master')
+@extends('layouts.takeover')
 
 @section('content')
-    <div>
-        <div class="feed-enclosure">
-            <div class="flex wrapper">
-                <div class="flex__cell">
-                    <div id="share-page" class="placeholder -full">
-                        <button class="button">Share This</button>
+    <div id="fb-root"></div>
+    <div id="chrome" class="chrome">
+        <div class="wrapper">
+            <div>
+                <div class="feed-enclosure">
+                    <div class="flex wrapper">
+                        <div class="flex__cell">
+                            <div id="share-page" class="placeholder -full">
+                                <button class="button">Share This</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
