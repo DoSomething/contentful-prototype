fa337a8a7c4836d47d56625a64f9406134326bdf
Diego Lorenzo <diego@diegolorenzo.com>
2018-02-06 14:34:47 -0500

    Updating the Voter Reg component with new logic.

--- resources/assets/components/VoterRegistration/VoterRegistration.js
+++ resources/assets/components/VoterRegistration/VoterRegistration.js
@@ -5,14 +5,19 @@ import Card from '../Card';
 import Markdown from '../Markdown';
 import { Flex, FlexCell } from '../Flex';
 import SectionHeader from '../SectionHeader';
-import { dynamicString, makeUrl } from '../../helpers';
+import { dynamicString } from '../../helpers';
 
 const VoterRegistration = (props) => {
-  const { campaignId, campaignRunId, content, hideStepNumber, stepIndex,
-    title, dynamicLink, userId } = props;
-
-  // @TODO: probs do not need these anymore...
-  const { baseUrl, params, type } = dynamicLink;
+  const {
+    campaignId,
+    campaignRunId,
+    content,
+    hideStepNumber,
+    link,
+    stepIndex,
+    title,
+    userId,
+  } = props;
 
   const tokens = {
     northstarId: userId,
@@ -21,43 +26,20 @@ const VoterRegistration = (props) => {
     source: 'web',
   };
 
-  // @TODO: Add "link" field in Voter Registration Content Type and pull URL from there!
-  const link = dynamicString('https://testing-dosomething.turbovote.org/?r=user:{northstarId},campaignID:{campaignId},campaignRunID:{campaignRunId},source:{source}', tokens);
-
-  const query = params.reduce((paramsObject, param) => ({
-    ...paramsObject,
-    [param.property]: param.dynamic ? props[param.value] : param.value,
-  }), {});
-
-
-  // @TODO: no longer need the following...
-  // let oldLink;
-  // if (type === 'turboVote') {
-  //   const queryValue = Object.entries(query)
-  //     .map(([key, value]) => (`${key}:${value}`))
-  //     .join(',');
-
-  //   oldLink = `${baseUrl}?r=${queryValue}`;
-  // } else {
-  //   oldLink = makeUrl(baseUrl, query).href;
-  // }
-
-  // const formattedContent = (
-  //   content || VoterRegistration.defaultProps.content
-  // ).replace(/:::[a-zA-Z]*:::/gi, oldLink);
-
   return (
     <Flex>
       <SectionHeader
         title={title}
         hideStepNumber={hideStepNumber}
-        stepIndex={stepIndex}
+        step={stepIndex}
       />
       <FlexCell width="two-thirds">
         <Card className="rounded bordered" title={title}>
-          <Markdown className="padded">{ content }</Markdown>
+          <div className="padded">
+            <Markdown>{ content }</Markdown>
 
-          <a className="button" href={link} target="blank">Start Registration</a>
+            { link ? <a className="button" href={dynamicString(link, tokens)} target="blank">Start Registration</a> : null }
+          </div>
         </Card>
       </FlexCell>
     </Flex>
@@ -69,19 +51,16 @@ VoterRegistration.propTypes = {
   campaignRunId: PropTypes.string.isRequired,
   content: PropTypes.string,
   hideStepNumber: PropTypes.bool,
+  link: PropTypes.string,
   stepIndex: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
-  dynamicLink: PropTypes.shape({
-    baseUrl: PropTypes.string,
-    params: PropTypes.arrayOf(PropTypes.object),
-    type: PropTypes.string,
-  }).isRequired,
   userId: PropTypes.string.isRequired,
 };
 
 VoterRegistration.defaultProps = {
   content: 'Register to vote!',
   hideStepNumber: true,
+  link: null,
 };
 
 export default VoterRegistration;
