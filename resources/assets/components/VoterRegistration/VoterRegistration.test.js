0a5713caca6af11cd0881faa7fdfb35671f603d4
Diego Lorenzo <diego@diegolorenzo.com>
2018-02-07 14:24:57 -0500

    Updating test slightly.

--- resources/assets/components/VoterRegistration/VoterRegistration.test.js
+++ resources/assets/components/VoterRegistration/VoterRegistration.test.js
@@ -3,15 +3,13 @@ import { render } from 'enzyme';
 
 import VoterRegistration from './VoterRegistration';
 
-// jest.mock('./VoterRegistrationContainer', () => 'VoterRegistrationContainer');
-
 test('VoterRegistration is rendered as a card component with a button', () => {
   const wrapper = render(
     <VoterRegistration
       campaignId="1234"
       campaignRunId="9876"
       content="Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula."
-      link="http://example.com?campaign={}&campaingRun={}&user={}&source={}"
+      link="http://example.com?campaign={campaignId}&campaingRun={campaignRunId}&user={northstarId}&source={source}"
       stepIndex={1}
       title="Register to vote!"
       userId="551234567890abcdefghijkl"
