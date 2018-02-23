a54d01af08da4c8846ddf34101f681b36a9c119d
mendelB <mendelblesofsky@gmail.com>
2018-02-23 11:46:57 -0500

    adding tests for twitter share action

--- resources/assets/components/ShareAction/ShareAction.test.js
+++ resources/assets/components/ShareAction/ShareAction.test.js
@@ -11,34 +11,39 @@ describe('ShareAction component', () => {
   let trackEventMock = jest.fn();
   let openModalMock = jest.fn();
 
-  const getShallow = () => shallow(
+  const getShallow = (socialPlatform) => shallow(
     <ShareAction
       title="Click on this link!"
       content="This is a great link"
       trackEvent={trackEventMock}
       openModal={openModalMock}
       link={url}
+      socialPlatform={socialPlatform}
     />,
   );
 
   // We'll declare this reference to the wrapper object, so we can more elegantly reset the
   // shallow copy of the component and test it as necessary.
-  let wrapper = getShallow();
+  let wrapper = getShallow('facebook');
 
   it('renders a Card component', () => {
     expect(wrapper.find('Card')).toHaveLength(1);
     expect(wrapper.find('Card').find('Embed')).toHaveLength(1);
   });
 
-  it('renders a Facebook Share button', () => {
+  it('renders a proper Share button based on social platform', () => {
     expect(wrapper.find('button').text()).toEqual('Share on Facebook');
+
+    wrapper = getShallow('twitter');
+
+    expect(wrapper.find('button').text()).toEqual('Share on Twitter');
   });
 
-  describe('Clicking the Social Share Button', () => {
+  describe('Clicking the Social Share Button for a Facebook share', () => {
     beforeEach(() => {
       trackEventMock = jest.fn();
       openModalMock = jest.fn();
-      wrapper = getShallow();
+      wrapper = getShallow('facebook');
     });
 
     it('calls the FB ui method to trigger the facebook share', () => {
@@ -56,7 +61,7 @@ describe('ShareAction component', () => {
 
       expect(trackEventMock.mock.calls.length).toBeGreaterThan(0);
 
-      expect(trackEventMock.mock.calls[0]).toEqual(['clicked share action', trackingData]);
+      expect(trackEventMock.mock.calls[0]).toEqual(['clicked facebook share action', trackingData]);
       expect(trackEventMock.mock.calls[1]).toEqual(['share action completed', trackingData]);
     });
 
@@ -66,7 +71,7 @@ describe('ShareAction component', () => {
       wrapper.find('button').simulate('click');
 
       expect(trackEventMock).toHaveBeenCalledTimes(2);
-      expect(trackEventMock.mock.calls[0]).toEqual(['clicked share action', trackingData]);
+      expect(trackEventMock.mock.calls[0]).toEqual(['clicked facebook share action', trackingData]);
     });
 
     it('displays the affirmation modal when social share is successful', () => {
@@ -84,6 +89,40 @@ describe('ShareAction component', () => {
 
       expect(trackEventMock).toHaveBeenCalledTimes(2);
       expect(trackEventMock.mock.calls[1]).toEqual(['share action cancelled', trackingData]);
+    });
+  });
+
+  describe('Clicking the Social Share Button for a Twitter share', () => {
+    beforeEach(() => {
+      trackEventMock = jest.fn();
+      openModalMock = jest.fn();
+      wrapper = getShallow('twitter');
+    });
+
+    it('opens a new window with the proper Twitter intent URL', () => {
+      global.open = jest.fn();
+
+      wrapper.find('button').simulate('click');
+
+      expect(global.open).toHaveBeenCalled();
+      expect(global.open.mock.calls[0][0]).toEqual(`https://twitter.com/intent/tweet?url=${url}&text=${undefined}`);
+    });
+
+    it('tracks clicked share action event', () => {
+      wrapper.find('button').simulate('click');
+
+      expect(trackEventMock.mock.calls.length).toBeGreaterThan(0);
+
+      expect(trackEventMock.mock.calls[0]).toEqual(['clicked twitter share action', trackingData]);
+    });
+
+    // @TODO activate this test when we properly implement the post share affirmaion modal for twitter share.
+    xit('displays the affirmation modal when social share is successful', () => {
+      setFBshare(true);
+
+      wrapper.find('button').simulate('click');
+
+      expect(openModalMock).toHaveBeenCalledTimes(1);
     });
   });
 });
