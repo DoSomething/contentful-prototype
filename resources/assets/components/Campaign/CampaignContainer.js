a618329f43914d838e1b6bd205010fa6ff56e538
David Furnes <david@dfurnes.com>
2018-03-05 09:55:53 -0500

    HACK: Allow viewing block pages without being logged in.
    Authorization rules like this should be extracted into individual pages & blocks.

--- resources/assets/components/Campaign/CampaignContainer.js
+++ resources/assets/components/Campaign/CampaignContainer.js
@@ -6,10 +6,12 @@ const mapStateToProps = (state, props) => {
   const hasLandingPage = state.campaign.landingPage !== null;
   const isSignedUp = state.signups.thisCampaign;
 
+  // @TODO: Move these into the pages themselves.
   const { location, match } = props;
   const isQuiz = location.pathname.replace(match.url, '').startsWith('/quiz/');
+  const isBlock = location.pathname.replace(match.url, '').startsWith('/blocks/');
 
-  const ignoreLandingPage = state.admin.shouldShowActionPage || isQuiz;
+  const ignoreLandingPage = state.admin.shouldShowActionPage || isQuiz || isBlock;
   let shouldShowLandingPage = false;
 
   if (state.admin.shouldShowLandingPage) {
