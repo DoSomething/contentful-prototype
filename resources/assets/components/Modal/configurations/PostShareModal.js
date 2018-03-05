125f461e6fdebf0b88cba6c5f99c8c22ec199273
David Furnes <david@dfurnes.com>
2018-03-05 16:33:13 -0500

    Add optional post-share link to share actions.

--- resources/assets/components/Modal/configurations/PostShareModal.js
+++ resources/assets/components/Modal/configurations/PostShareModal.js
@@ -4,24 +4,33 @@ import Card from '../../Card';
 import Markdown from '../../Markdown';
 
 const PostShareModal = (props) => {
-  const { content, closeModal } = props;
+  const { content, confirmationAction, confirmationActionLink, closeModal } = props;
 
   return (
     <Card title="Thanks for sharing!" className="modal__slide bordered rounded" onClose={closeModal}>
       <Markdown className="padded">
         { content || PostShareModal.defaultProps.content }
       </Markdown>
+      { confirmationAction && confirmationActionLink ? (
+        <ul className="form-actions -padded">
+          <a className="button" href={confirmationActionLink}>{confirmationAction}</a>
+        </ul>
+      ) : null }
     </Card>
   );
 };
 
 PostShareModal.propTypes = {
   content: PropTypes.string,
+  confirmationAction: PropTypes.string,
+  confirmationActionLink: PropTypes.string,
   closeModal: PropTypes.func.isRequired,
 };
 
 PostShareModal.defaultProps = {
   content: 'Thanks for rallying your friends on Facebook!',
+  confirmationAction: null,
+  confirmationActionLink: null,
 };
 
 export default PostShareModal;
