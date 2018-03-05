a6e9fb447de22ec99177ed0752e608c7c2825f58
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-03-05 13:45:10 -0500

    drastically simplify component by using `setTimeout`

--- resources/assets/components/ModalLauncher/ModalLauncher.js
+++ resources/assets/components/ModalLauncher/ModalLauncher.js
@@ -2,31 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class ModalLauncher extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      count: 0,
-    };
-
-    this.incrementOrLaunch = this.incrementOrLaunch.bind(this);
-  }
-
   componentDidMount() {
-    this.timer = setInterval(this.incrementOrLaunch, 1000);
+    const openModal = this.props.openModal.bind(this, this.props.modalType);
+    this.timer = setTimeout(openModal, this.props.countdown * 1000);
   }
 
   componentWillUnmount() {
-    clearInterval(this.timer);
-  }
-
-  incrementOrLaunch() {
-    if (this.state.count < this.props.countdown) {
-      this.setState(prevState => ({ count: prevState.count + 1 }));
-    } else {
-      this.props.openModal(this.props.modalType);
-      clearInterval(this.timer);
-    }
+    clearTimeout(this.timer);
   }
 
   render() {
