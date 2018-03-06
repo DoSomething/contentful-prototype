2597d1eb20f4e584e8c108b83b0deee820ed76bb
David Furnes <david@dfurnes.com>
2018-03-06 13:36:30 -0500

    Allow using a block for custom post-share modals.

--- resources/assets/components/Modal/containers/PostShareModalContainer.js
+++ resources/assets/components/Modal/containers/PostShareModalContainer.js
@@ -9,13 +9,9 @@ const mapStateToProps = (state) => {
     || find(state.campaign.actionSteps, { id })
     || find(state.campaign.activityFeed, { id });
 
-  const confirmationAction = get(json, 'fields.additionalContent.confirmationAction');
-  const confirmationActionLink = get(json, 'fields.additionalContent.confirmationActionLink');
-
   return {
-    content: json ? json.fields.affirmation : {},
-    confirmationAction,
-    confirmationActionLink,
+    affirmationText: get(json, 'fields.affirmation'),
+    affirmationBlock: get(json, 'fields.affirmationBlock'),
   };
 };
 
