0bcd793141de241c082e3f12cc723959424f06e8
mendelB <mendelblesofsky@gmail.com>
2018-03-07 15:13:20 -0500

    adding className prop

--- resources/assets/components/Modal/ModalControls.js
+++ resources/assets/components/Modal/ModalControls.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ModalControls = ({ children, onClose }) => (
-  <div style={{ position: 'relative' }}>
+const ModalControls = ({ children, onClose, className }) => (
+  <div style={{ position: 'relative' }} className={className}>
     <button className="card__exit -floating" onClick={onClose}>&times;</button>
     { children }
   </div>
 );
 
 ModalControls.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.element.isRequired,
   onClose: PropTypes.func.isRequired,
+};
+
+ModalControls.defaultProps = {
+  className: null,
 };
 
 export default ModalControls;
