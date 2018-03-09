dd4a0fcc74cae3bc896e36df48b6ff94da897bb8
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-03-09 14:51:28 -0500

    localstorage set that user dismissed voter reg modal on unmount

--- resources/assets/components/Modal/configurations/VoterRegistrationModal.js
+++ resources/assets/components/Modal/configurations/VoterRegistrationModal.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { set } from '../../../helpers/storage';
 import { ModalControls } from '../../Modal';
 import VoterRegistrationActionContainer from '../../actions/VoterRegistrationAction';
 
-const VoterRegistrationModal = (props) => {
-  const content = 'Register to vote and encourage friends to do the same. YOU have the power to elect the officials who will create the future YOU want to see. Get loud in 2018.';
-  const link = 'https://dosomething.turbovote.org/?r=user:{northstarId},campaignID:{campaignId},campaignRunID:{campaignRunId},source:{source}';
+class VoterRegistrationModal extends React.Component {
+  componentWillUnmount() {
+    // @see: ModalLauncher.js
+    set(`${this.props.northstarId}_dismissed_voter_reg_modal`, 'timestamp', Date.now());
+  }
 
-  return (
-    <ModalControls className="modal__slide" onClose={props.closeModal}>
-      <VoterRegistrationActionContainer content={content} link={link} />
-    </ModalControls>
-  );
-};
+  render() {
+    const content = 'Register to vote and encourage friends to do the same. YOU have the power to elect the officials who will create the future YOU want to see. Get loud in 2018.';
+    const link = 'https://dosomething.turbovote.org/?r=user:{northstarId},campaignID:{campaignId},campaignRunID:{campaignRunId},source:{source}';
+
+    return (
+      <ModalControls className="modal__slide" onClose={this.props.closeModal}>
+        <VoterRegistrationActionContainer content={content} link={link} contentfulId="static-voter-reg-modal" />
+      </ModalControls>
+    );
+  }
+}
 
 VoterRegistrationModal.propTypes = {
   closeModal: PropTypes.func.isRequired,
+  northstarId: PropTypes.string.isRequired,
 };
 
 export default VoterRegistrationModal;
--- resources/assets/components/Modal/containers/VoterRegistrationModalContainer.js
+++ resources/assets/components/Modal/containers/VoterRegistrationModalContainer.js
@@ -1,6 +1,14 @@
 import { connect } from 'react-redux';
 import VoterRegistrationModal from '../configurations/VoterRegistrationModal';
 import { closeModal } from '../../../actions/modal';
+import { getUserId } from '../../../selectors/user';
+
+/**
+ * Provide state from the Redux store as props for this component.
+ */
+const mapStateToProps = state => ({
+  northstarId: getUserId(state),
+});
 
 /**
  * Provide pre-bound functions that allow the component to dispatch
@@ -10,4 +18,4 @@ const actionCreators = {
   closeModal,
 };
 
-export default connect(null, actionCreators)(VoterRegistrationModal);
+export default connect(mapStateToProps, actionCreators)(VoterRegistrationModal);
