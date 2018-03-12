bd5133de2168de76336395b658378b35a8f54ba4
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-03-12 12:58:50 -0400

    Rendering `page`s in a StaticBlock (#749)
    * rendering `page`s in a StaticBlock
    
    * rendering `page` as a StaticBlock from BlockModal
    
    * code styling
    
    * :space_invader:

--- resources/assets/components/Modal/configurations/BlockModal.js
+++ resources/assets/components/Modal/configurations/BlockModal.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import ContentfulEntry from '../../ContentfulEntry';
 import ModalControls from '../ModalControls';
+import StaticBlock from '../../StaticBlock';
+import { parseContentfulType } from '../../../helpers';
 
 const BlockModal = (props) => {
   const { json, closeModal } = props;
 
+  let children = <ContentfulEntry json={json} />;
+
+  if (parseContentfulType(json) === 'page') {
+    children = (
+      <StaticBlock
+        content={json.fields.content}
+        source={json.fields.source}
+        title={json.fields.title}
+      />
+    );
+  }
+
   return (
     <ModalControls className="modal__slide" onClose={closeModal}>
-      <ContentfulEntry json={json} />
+      { children }
     </ModalControls>
   );
 };
