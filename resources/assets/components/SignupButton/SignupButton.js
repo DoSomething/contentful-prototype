9114043c6b4084189f96229bb7f46c4164ff72ed
David Furnes <david@dfurnes.com>
2018-03-12 12:52:45 -0400

    Allow source-specific copy overrides on signup button.

--- resources/assets/components/SignupButton/SignupButton.js
+++ resources/assets/components/SignupButton/SignupButton.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { get } from 'lodash';
+
 import { convertOnSignupIntent } from '../../helpers/sixpack';
 
 const SignupButton = (props) => {
-  const { campaignActionText, className, clickedSignUp, experiments, source,
-    template, text, trackEvent, legacyCampaignId, convertExperiment } = props;
+  const { campaignActionText, sourceActionText, className, clickedSignUp, experiments, source,
+    template, text, trackEvent, trafficSource, legacyCampaignId, convertExperiment } = props;
 
-  const buttonText = text || campaignActionText;
+  // A/B Test: If a user has a traffic source, test different button language!
+  let sourceOverride = null;
+  if (trafficSource) {
+    sourceOverride = get(sourceActionText, trafficSource);
+
+    // @TODO ...
+  }
+
+  // In descending priority: button-specific text prop, source-specific override,
+  // campaign action text override, or standard "Take Action" copy.
+  const buttonText = text || sourceOverride || campaignActionText;
 
   const convertExperiments = () => {
     Object.keys(experiments).forEach((experiment) => {
@@ -37,6 +49,7 @@ const SignupButton = (props) => {
 SignupButton.propTypes = {
   text: PropTypes.string,
   campaignActionText: PropTypes.string,
+  sourceActionText: PropTypes.string,
   className: PropTypes.string,
   clickedSignUp: PropTypes.func.isRequired,
   convertExperiment: PropTypes.func.isRequired,
@@ -44,14 +57,17 @@ SignupButton.propTypes = {
   legacyCampaignId: PropTypes.string.isRequired,
   source: PropTypes.string.isRequired,
   template: PropTypes.string,
+  trafficSource: PropTypes.string,
   trackEvent: PropTypes.func.isRequired,
 };
 
 SignupButton.defaultProps = {
   text: null,
   campaignActionText: 'Take Action',
+  sourceActionText: null,
   className: null,
   template: null,
+  trafficSource: null,
 };
 
 export default SignupButton;
