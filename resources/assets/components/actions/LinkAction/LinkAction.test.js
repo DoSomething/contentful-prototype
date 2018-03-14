57712dc305115afc7fd6005e90c4976aa8333c80
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-03-14 15:15:00 -0400

    updating tests

--- resources/assets/components/actions/LinkAction/LinkAction.test.js
+++ resources/assets/components/actions/LinkAction/LinkAction.test.js
@@ -6,17 +6,27 @@ import LinkAction from './LinkAction';
 describe('LinkAction component', () => {
   const trackEventMock = jest.fn();
 
-  describe('without an affiliateLogo', () => {
-    const wrapper = shallow(
-      <LinkAction
-        title="Click on this link!"
-        content="This is a great link"
-        trackEvent={trackEventMock}
-        link="https://dosomething.org"
-      />,
-    );
+  // mocking the window.location.origin for the isExternal helper method called from LinkAction
+  Object.defineProperty(window.location, 'origin', {
+    writable: true,
+    value: 'https://dosomething.org',
+  });
 
-    it('renders a Card component', () => {
+  const props = {
+    title: 'Click on this link!',
+    content: 'This is a great link',
+    trackEvent: trackEventMock,
+    link: 'https://dosomething.org',
+  };
+
+  const wrapper = shallow(
+    <LinkAction
+      {...props}
+    />,
+  );
+
+  describe('without buttonText', () => {
+    it('renders a Card component with the link embed', () => {
       expect(wrapper.find('Card')).toHaveLength(1);
       expect(wrapper.find('Card').find('Embed')).toHaveLength(1);
     });
@@ -25,12 +35,33 @@ describe('LinkAction component', () => {
       wrapper.find('.link-wrapper').simulate('click');
       expect(trackEventMock).toHaveBeenCalled();
     });
+  });
 
+  describe('with buttonText', () => {
+    const wrapper = shallow(
+      <LinkAction
+        {...props}
+        buttonText="Click this button"
+      />,
+    );
+
+    it('renders a Card component with a button', () => {
+      expect(wrapper.find('Card')).toHaveLength(1);
+      expect(wrapper.find('Card').find('.button')).toHaveLength(1);
+    });
+
+    it('calls the event tracker prop function when the button is clicked', () => {
+      wrapper.find('.button').simulate('click');
+      expect(trackEventMock).toHaveBeenCalled();
+    });
+  });
+
+  describe('without an affiliateLogo', () => {
     it('does not add an "affiliate-content" class to the Card', () => {
       expect(wrapper.find('Card').hasClass('affiliate-content')).toEqual(false);
     });
 
-    it('renders a SponsorPromotion component', () => {
+    it('does not render a SponsorPromotion component', () => {
       expect(wrapper.find('SponsorPromotion')).toHaveLength(0);
     });
   });
@@ -38,10 +69,7 @@ describe('LinkAction component', () => {
   describe('with an affiliateLogo', () => {
     const wrapper = shallow(
       <LinkAction
-        title="Click on this link!"
-        content="This is a great link"
-        trackEvent={trackEventMock}
-        link="https://dosomething.org"
+        {...props}
         affiliateLogo="https://vignette.wikia.nocookie.net/pokemon/images/b/b0/Team_Rocket_trio_OS.png/revision/latest?cb=20150915073657"
       />,
     );
