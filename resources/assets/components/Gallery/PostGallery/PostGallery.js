a7dca7c5d44f5b0e6d970bec7c6ff58890618692
David Furnes <david@dfurnes.com>
2018-03-15 18:12:16 -0400

    Refactor PostGallery to use GraphQL.

--- resources/assets/components/Gallery/PostGallery/PostGallery.js
+++ resources/assets/components/Gallery/PostGallery/PostGallery.js
@@ -3,57 +3,35 @@ import PropTypes from 'prop-types';
 
 import Card from '../../Card';
 import Gallery from '../Gallery';
-import LoadMore from '../../LoadMore';
-import ReportbackItemContainer from '../../ReportbackItem';
+import { ReportbackItem } from '../../ReportbackItem';
 
-class PostGallery extends React.Component {
-  constructor() {
-    super();
+const PostGallery = (props) => {
+  const { loading, postsByCampaignId } = props.data;
 
-    this.renderItem = this.renderItem.bind(this);
-  }
-
-  renderItem(key) {
-    const itemId = this.props.reportbacks.entities[key].reportback_items[0];
-    const post = this.props.reportbacks.itemEntities[itemId];
-
-    return (
-      <Card className="rounded" key={post.id}>
-        <ReportbackItemContainer id={post.reportback.id} />
-      </Card>
-    );
-  }
-
-  render() {
-    const { currentPage, entities, isFetching, total, totalPages } = this.props.reportbacks;
-
-    return ! total ?
-      <div className="spinner -centered" />
-      :
-      <div>
-        <Gallery type="triad" className="expand-horizontal-md">
-          {Object.keys(entities).map(this.renderItem)}
-        </Gallery>
-
-        {
-          currentPage !== totalPages ?
-            <LoadMore className="padding-lg text-centered" text="view more" onClick={this.props.fetchReportbacks} isLoading={isFetching} />
-            :
-            null
-        }
-      </div>;
-  }
-}
+  return loading ? (
+    <div className="spinner -centered" />
+  ) : (
+    <div>
+      <Gallery type="triad" className="expand-horizontal-md">
+        {postsByCampaignId.map(post => (
+          <Card className="rounded" key={post.id}>
+            <ReportbackItem
+              id={String(post.id)}
+              caption={post.media.text}
+              url={post.media.url}
+              firstName={post.user.firstName}
+            />
+          </Card>
+        ))}
+      </Gallery>
+    </div>
+  );
+};
 
 PostGallery.propTypes = {
-  fetchReportbacks: PropTypes.func.isRequired,
-  reportbacks: PropTypes.shape({
-    currentPage: PropTypes.number,
-    entities: PropTypes.objectOf(PropTypes.object),
-    isFetching: PropTypes.bool,
-    itemEntities: PropTypes.objectOf(PropTypes.object),
-    total: PropTypes.number,
-    totalPages: PropTypes.number,
+  data: PropTypes.shape({
+    postsByCampaignId: PropTypes.array, // eslint-disable-line react/forbid-prop-types
+    loading: PropTypes.bool.isRequired,
   }).isRequired,
 };
 
--- resources/assets/components/Gallery/PostGallery/PostGalleryContainer.js
+++ resources/assets/components/Gallery/PostGallery/PostGalleryContainer.js
@@ -1,15 +1,39 @@
 import { connect } from 'react-redux';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import PostGallery from './PostGallery';
-import { fetchReportbacks } from '../../../actions';
 
+/**
+ * Provide state from the Redux store as props for this component. (In
+ * this case, we just need the campaign ID for our GraphQL query!)
+ */
 const mapStateToProps = state => ({
-  legacyCampaignId: state.campaign.legacyCampaignId,
-  reportbacks: state.reportbacks,
+  campaignId: String(state.campaign.legacyCampaignId),
 });
 
-const mapActionsToProps = {
-  fetchReportbacks,
-};
+/**
+ * The GraphQL query to load data for this component.
+ */
+const query = gql`
+  query PostGallery($campaignId: String!, $count: Int) {
+    postsByCampaignId(id: $campaignId, count: $count) {
+      id
+      status
+      media {
+        url
+        text
+      }
+      user {
+        id
+        firstName
+        lastInitial
+      }
+    }
+  }
+`;
 
-export default connect(mapStateToProps, mapActionsToProps)(PostGallery);
+// Export the container component.
+export default connect(mapStateToProps)(
+  graphql(query)(PostGallery),
+);
