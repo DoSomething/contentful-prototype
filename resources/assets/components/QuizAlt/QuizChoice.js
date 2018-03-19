796c0cdd2fa721f550914023e82909298eaf633d
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-03-19 14:48:27 -0400

    adding new alternative quiz component

---
+++ resources/assets/components/QuizAlt/QuizChoice.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import Card from '../Card';
+
+const QuizChoice = (props) => {
+  const { backgroundImage, id, isActive, isFaded, questionId, selectChoice, title } = props;
+
+  const cardClasses = classnames('bordered rounded', {
+    '-active': isActive,
+    faded: isFaded,
+  });
+
+  return (
+    <a
+      className={classnames('choice', { 'background-image': backgroundImage })}
+      onClick={() => selectChoice(questionId, id)}
+      role="button"
+      tabIndex={0}
+    >
+      <Card className={cardClasses}>
+        { backgroundImage ? <img src={backgroundImage} alt="quiz choice background" /> : null }
+        <p className="padding-md">{ title }</p>
+      </Card>
+    </a>
+  );
+};
+
+QuizChoice.propTypes = {
+  backgroundImage: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  isActive: PropTypes.bool.isRequired,
+  isFaded: PropTypes.bool,
+  questionId: PropTypes.string.isRequired,
+  selectChoice: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+QuizChoice.defaultProps = {
+  backgroundImage: null,
+  isFaded: false,
+};
+
+export default QuizChoice;
