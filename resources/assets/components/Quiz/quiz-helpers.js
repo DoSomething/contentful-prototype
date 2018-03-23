3ea1b3b6387e11eb95409d58b69081da52382608
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-03-23 17:15:41 -0400

    adding quiz helper to calculate and tally winning conclusion result and block

---
+++ resources/assets/components/Quiz/quiz-helpers.js
@@ -0,0 +1,46 @@
+/**
+ * Tally the amount of times a quiz result content and result block
+ * have been opted for via a users choice selection for quiz questions, and
+ * return the result opted for the most.
+ *
+ * @param  {Object}   selections
+ * @param  {Array}   questions
+ * @return {Object}
+ */
+const calculateResult = (selections, questions) => {
+  const talliedResults = {};
+  const talliedResultBlocks = {};
+
+  questions.forEach((question) => {
+    // find the choice the user selected for the current question
+    const selectedChoiceId = selections[question.id];
+    const selectedChoice = question.choices[selectedChoiceId];
+
+    // increment the counter for each result of the selected choice
+    const results = selectedChoice.results;
+    results.forEach(resultId => (
+      // ensuring a default value of zero for unset result properties
+      talliedResults[resultId] = (talliedResults[resultId] || 0) + 1
+    ));
+
+    const resultBlockId = selectedChoice.resultBlock;
+    // ensuring a default value of zero for unset result properties
+    if (resultBlockId) {
+      talliedResultBlocks[resultBlockId] = (talliedResultBlocks[resultBlockId] || 0) + 1;
+    }
+  });
+
+  // sorts results by their selection counter in descending order
+  const resultWinner = Object.keys(talliedResults).sort((resultA, resultB) => (
+    talliedResults[resultB] - talliedResults[resultA]
+  ))[0];
+
+  // sorts results by their selection counter in descending order
+  const resultBlockWinner = Object.keys(talliedResultBlocks).sort((blockA, blockB) => (
+    talliedResultBlocks[blockB] - talliedResultBlocks[blockA]
+  ))[0];
+
+  return { resultWinner, resultBlockWinner };
+};
+
+export default calculateResult;
---
+++ resources/assets/components/Quiz/quiz-helpers.test.js
@@ -0,0 +1,27 @@
+import calculateResult from './quiz-helpers';
+
+const questions = [
+  {
+    id: '0',
+    choices: [
+      { id: '0', results: ['0', '1'], resultBlock: '1234' },
+      { id: '1', results: ['1', '2'], resultBlock: '2345' },
+      { id: '2', results: ['0'], resultBlock: '2345' },
+    ],
+  },
+  {
+    id: '1',
+    choices: [
+      { id: '0', results: ['0', '1'] },
+      { id: '1', results: ['1', '2'] },
+      { id: '2', results: ['0'] },
+    ],
+  },
+];
+
+const selections = { '0': '0', '1': '1' };
+
+test('it tallies the results and returns the one with the most selections', () => {
+  const expectedResult = { resultWinner: '1', resultBlockWinner: '1234'};
+  expect(calculateResult(selections, questions)).toEqual(expectedResult);
+});
