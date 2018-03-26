00503abab025aa83c329d82d63acd9e5abf25b9a
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-03-26 11:07:23 -0400

    updating migration to work on an array type field

--- contentful/migrations/2018_03_23_001_change_campaign_quizzes_field_to_allow_quiz.js
+++ contentful/migrations/2018_03_23_001_change_campaign_quizzes_field_to_allow_quiz.js
@@ -1,10 +1,15 @@
 module.exports = function (migration) {
   const campaign = migration.editContentType('campaign');
 
+  // https://www.contentfulcommunity.com/t/confusing-validations-error-from-the-migrations-cli/776/4
   campaign.editField('quizzes')
-    .validations([{
-      linkContentType: [
-        'quizBeta', 'quiz'
-      ],
-    }]);
+    .items({
+      type: 'Link',
+      linkType: 'Entry',
+      validations: [{
+        linkContentType: [
+          'quizBeta', 'quiz'
+        ],
+      }]
+    });
 }
