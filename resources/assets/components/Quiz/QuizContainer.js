4a793e3999a22ad7c581bc6cbd64db20c93ef680
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-03-28 11:23:32 -0400

    Not passing any data fields to quiz

--- resources/assets/components/Quiz/QuizContainer.js
+++ resources/assets/components/Quiz/QuizContainer.js
@@ -1,21 +1,6 @@
-import { find } from 'lodash';
 import { connect } from 'react-redux';
 import { PuckConnector } from '@dosomething/puck-client';
 
 import Quiz from './Quiz';
 
-/**
- * Provide state from the Redux store as props for this component.
- */
-const mapStateToProps = (state, ownProps) => {
-  const { slug } = ownProps.match.params;
-
-  const quiz = find(state.campaign.quizzes, { fields: { slug } });
-
-  const fields = quiz ? quiz.fields : null;
-  const id = quiz ? quiz.id : null;
-
-  return { ...fields, id };
-};
-
-export default connect(mapStateToProps)(PuckConnector(Quiz));
+export default connect()(PuckConnector(Quiz));
