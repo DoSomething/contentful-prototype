c5202375b4661106796a364b225261082777fb07
Diego Lorenzo <diego@diegolorenzo.com>
2018-03-29 17:37:17 -0400

    Fixing tests.

--- resources/assets/components/actions/TextSubmissionAction/TextSubmissionAction.test.js
+++ resources/assets/components/actions/TextSubmissionAction/TextSubmissionAction.test.js
@@ -6,9 +6,14 @@ import TextSubmissionAction from './TextSubmissionAction';
 describe('TextSubmissionAction component', () => {
   const wrapper = shallow(
     <TextSubmissionAction
+      action="textSubmissionAction"
       campaignId="1234"
       campaignRunId="6789"
       contentfulId="1122334455667788990011"
+      id="abcdefghi123456789"
+      storeCampaignPost={jest.fn()}
+      submissions={{ isPending: false }}
+      type="text"
       userId="666655554444333322221111"
     />,
   );
