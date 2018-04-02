1c033e78dd4bb140aeef29016f72b583d5010abe
Diego Lorenzo <diego@diegolorenzo.com>
2018-04-02 18:31:24 -0400

    Adding content block content type migration file.

--- contentful/migrations/2018_04_02_001_add_content_block_content_type.js
+++ contentful/migrations/2018_04_02_001_add_content_block_content_type.js
@@ -1,7 +1,7 @@
 module.exports = function (migration) {
   const contentBlock = migration.createContentType('contentBlock')
     .name('Content Block')
-    .description('')
+    .description('Content block for adding text content with optional titles and single image.')
     .displayField('internalTitle');
 
   contentBlock.createField('internalTitle')
@@ -10,9 +10,40 @@ module.exports = function (migration) {
     .required(true)
     .localized(false);
 
-  contentBlock.createField('internalTitle')
-    .name('Internal Title')
+  contentBlock.createField('superTitle')
+    .name('Supertitle')
+    .type('Symbol')
+    .required(false)
+    .localized(true);
+
+  contentBlock.createField('title')
+    .name('Title')
+    .type('Symbol')
+    .required(false)
+    .localized(true);
+
+  contentBlock.createField('subTitle')
+    .name('Subtitle')
+    .type('Symbol')
+    .required(false)
+    .localized(true);
+
+  contentBlock.createField('content')
+    .name('Content')
     .type('Symbol')
     .required(true)
+    .localized(true);
+
+  contentBlock.createField('image')
+    .name('Image')
+    .type('Link')
+    .linkType('Asset')
+    .required(false)
+    .localized(false);
+
+  contentBlock.createField('additionalContent')
+    .name('Additional Content')
+    .type('Object')
+    .required(false)
     .localized(false);
 }
