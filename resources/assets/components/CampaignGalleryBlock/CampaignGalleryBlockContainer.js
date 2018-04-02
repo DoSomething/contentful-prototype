3e7fe6ea5b190a07f637f98a45a6bdda93931f0b
David Furnes <david@dfurnes.com>
2018-04-02 15:30:48 -0400

    Separate Redux/GraphQL & extract generalized PaginatedQuery.

--- resources/assets/components/CampaignGalleryBlock/CampaignGalleryBlockContainer.js
+++ resources/assets/components/CampaignGalleryBlock/CampaignGalleryBlockContainer.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
 
-import CampaignGalleryBlock from './CampaignGalleryBlock';
-import ErrorBlock from '../ErrorBlock/ErrorBlock';
-import { NetworkStatus } from '../../constants';
+import CampaignGalleryBlockQuery from './CampaignGalleryBlockQuery';
 
 /**
  * Provide state from the Redux store as props for this component. (In
@@ -16,80 +10,5 @@ const mapStateToProps = state => ({
   campaignId: String(state.campaign.legacyCampaignId),
 });
 
-/**
- * The GraphQL query to load data for this component.
- */
-const POST_GALLERY_QUERY = gql`
-  query PostGallery($campaignId: String!, $count: Int, $page: Int) {
-    postsByCampaignId(id: $campaignId, count: $count, page: $page) {
-      id
-      status
-      url
-      text
-      reactions
-      reacted
-      user {
-        id
-        firstName
-        lastInitial
-      }
-    }
-  }
-`;
-
-/**
- * Fetch results via GraphQL using a query component.
- */
-const CampaignGalleryQuery = ({ campaignId }) => (
-  <Query
-    query={POST_GALLERY_QUERY}
-    variables={{ campaignId, count: 9, page: 1 }}
-    notifyOnNetworkStatusChange
-  >
-    {({ data, error, networkStatus, variables, fetchMore }) => {
-      // On initial load, just display a loading spinner.
-      if (networkStatus === NetworkStatus.LOADING) {
-        return <div className="spinner -centered" />;
-      }
-
-      if (error) {
-        console.error(`CampaignGalleryQuery ERROR: ${error}`);
-        return <ErrorBlock />;
-      }
-
-      return (
-        <CampaignGalleryBlock
-          postsByCampaignId={data.postsByCampaignId}
-          loading={networkStatus === NetworkStatus.FETCH_MORE}
-          loadMorePosts={() => fetchMore({
-            variables: {
-              // The value in `variables.page` doesn't get updated here on
-              // subsequent clicks, so we have to recalculate each time...
-              page: (data.postsByCampaignId.length / variables.count) + 1,
-            },
-            updateQuery: (previousResult, { fetchMoreResult }) => {
-              if (! fetchMoreResult.postsByCampaignId) {
-                return previousResult;
-              }
-
-              return {
-                ...previousResult,
-                postsByCampaignId: [
-                  ...previousResult.postsByCampaignId,
-                  ...fetchMoreResult.postsByCampaignId,
-                ],
-              };
-            },
-          })}
-        />
-      );
-    }}
-  </Query>
-);
-
-CampaignGalleryQuery.propTypes = {
-  campaignId: PropTypes.string.isRequired,
-};
-
-// Export the Redux/GraphQL container component.
-export default connect(mapStateToProps)(CampaignGalleryQuery);
+// Export the Redux container component.
+export default connect(mapStateToProps)(CampaignGalleryBlockQuery);
---
+++ resources/assets/components/PaginatedQuery.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Query } from 'react-apollo';
+
+import ErrorBlock from './ErrorBlock/ErrorBlock';
+import { NetworkStatus } from '../constants';
+
+/**
+ * Fetch results via GraphQL using a query component.
+ */
+const PaginatedQuery = ({ query, queryName, variables, count, children }) => (
+  <Query
+    query={query}
+    variables={{ ...variables, count, page: 1 }}
+    notifyOnNetworkStatusChange
+  >
+    {(result) => {
+      // On initial load, just display a loading spinner.
+      if (result.networkStatus === NetworkStatus.LOADING) {
+        return <div className="spinner -centered" />;
+      }
+
+      if (result.error) {
+        console.error(`${queryName} ERROR: ${result.error}`);
+        return <ErrorBlock />;
+      }
+
+      return children({
+        result: result.data[queryName],
+        fetching: result.networkStatus === NetworkStatus.FETCH_MORE,
+        fetchMore: () => result.fetchMore({
+          variables: {
+            // The value in `variables.page` doesn't get updated here on
+            // subsequent clicks, so we have to recalculate each time...
+            page: (result.data[queryName].length / result.variables.count) + 1,
+          },
+          updateQuery: (previousResult, { fetchMoreResult }) => {
+            if (! fetchMoreResult[queryName]) {
+              return previousResult;
+            }
+
+            return {
+              ...previousResult,
+              [queryName]: [
+                ...previousResult[queryName],
+                ...fetchMoreResult[queryName],
+              ],
+            };
+          },
+        }),
+      });
+    }}
+  </Query>
+);
+
+PaginatedQuery.propTypes = {
+  query: PropTypes.any.isRequired, // eslint-disable-line react/forbid-prop-types
+  queryName: PropTypes.string.isRequired,
+  variables: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+  count: PropTypes.number.isRequired,
+  children: PropTypes.func.isRequired,
+};
+
+PaginatedQuery.defaultProps = {
+  variables: {},
+};
+
+export default PaginatedQuery;
