c9261364614c89622f9d76ee3ed94de53a841b86
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-04-09 15:06:40 -0400

    Adding Quiz Fields Contentful migration (#817)
    * adding quiz fields contentful migration
    
    * leaving questions field after resultBlocks field

---
+++ contentful/migrations/2018_04_09_001_add_fields_to_quiz.js
@@ -0,0 +1,16 @@
+module.exports = function (migration) {
+  const quiz = migration.editContentType('quiz');
+
+  quiz.createField('results')
+    .name('Results')
+    .type('Object')
+    .required(true);
+
+  quiz.createField('questions')
+    .name('Questions')
+    .type('Object')
+    .required(true);
+
+  quiz.moveField('results').beforeField('resultBlocks');
+  quiz.moveField('questions').beforeField('additionalContent');
+}
