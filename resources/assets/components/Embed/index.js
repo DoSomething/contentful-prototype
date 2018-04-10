66c14b1fff6eac0bc6a78e217c691e0df042150f
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-04-10 13:55:40 -0400

    Lil update for the timed out embeds (#820)
    * adding link icon svg
    
    * changing the output for timed out embed
    
    * truncating timed out url in title

--- resources/assets/components/Embed/index.js
+++ resources/assets/components/Embed/index.js
@@ -2,10 +2,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import classnames from 'classnames';
 import { Phoenix } from '@dosomething/gateway';
+import { truncate } from 'lodash';
 
 import { Figure } from '../Figure';
 import { isExternal } from '../../helpers';
+
 import './embed.scss';
+import linkIcon from './linkIcon.svg';
 
 class Embed extends React.Component {
   constructor(props) {
@@ -39,7 +42,14 @@ class Embed extends React.Component {
         </a>
       );
     } else if (this.state.requestFailed) {
-      embed = <a href={this.props.url}>{this.props.url}</a>;
+      embed = (
+        <a href={this.props.url}>
+          <Figure className="padded margin-bottom-none bordered rounded" image={linkIcon} alt="link icon" alignment="left-collapse" size="small">
+            <h3>{ truncate(this.props.url, { length: 50 }) }</h3>
+            <p className="footnote">(Click or share this link)</p>
+          </Figure>
+        </a>
+      );
     }
 
     return (
