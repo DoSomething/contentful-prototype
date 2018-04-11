55adb6f6829187b4231294eae3e8e9b15052627f
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-04-11 14:50:03 -0400

    Add ContentBlock Contentful Entity

---
+++ app/Entities/ContentBlock.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entities;
+
+use JsonSerializable;
+
+class ContentBlock extends Entity implements JsonSerializable
+{
+    /**
+     * Convert the object into something JSON serializable.
+     *
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'id' => $this->entry->getId(),
+            'type' => $this->getContentType(),
+            'fields' => [
+                'superTitle' => $this->superTitle,
+                'title' => $this->title,
+                'subTitle' => $this->subTitle,
+                'content' => $this->content,
+                'image' => get_image_url($this->image, 'square'),
+                'imageAlignment' => $this->imageAlignment,
+                'additionalContent' => $this->additionalContent,
+            ],
+        ];
+    }
+}
