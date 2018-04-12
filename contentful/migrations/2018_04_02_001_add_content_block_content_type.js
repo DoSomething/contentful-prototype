3659713ef3fe85f031e5c73cd13865dfdb34d180
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-04-12 11:20:36 -0400

    adjusting Type to be Text

--- contentful/migrations/2018_04_02_001_add_content_block_content_type.js
+++ contentful/migrations/2018_04_02_001_add_content_block_content_type.js
@@ -1,49 +1,59 @@
-module.exports = function (migration) {
-  const contentBlock = migration.createContentType('contentBlock')
+module.exports = function(migration) {
+  const contentBlock = migration
+    .createContentType('contentBlock')
     .name('Content Block')
-    .description('Content block for adding text content with optional titles and single image.')
+    .description(
+      'Content block for adding text content with optional titles and single image.',
+    )
     .displayField('internalTitle');
 
-  contentBlock.createField('internalTitle')
+  contentBlock
+    .createField('internalTitle')
     .name('Internal Title')
     .type('Symbol')
     .required(true)
     .localized(false);
 
-  contentBlock.createField('superTitle')
+  contentBlock
+    .createField('superTitle')
     .name('Supertitle')
     .type('Symbol')
     .required(false)
     .localized(true);
 
-  contentBlock.createField('title')
+  contentBlock
+    .createField('title')
     .name('Title')
     .type('Symbol')
     .required(false)
     .localized(true);
 
-  contentBlock.createField('subTitle')
+  contentBlock
+    .createField('subTitle')
     .name('Subtitle')
     .type('Symbol')
     .required(false)
     .localized(true);
 
-  contentBlock.createField('content')
+  contentBlock
+    .createField('content')
     .name('Content')
-    .type('Symbol')
+    .type('Text')
     .required(true)
     .localized(true);
 
-  contentBlock.createField('image')
+  contentBlock
+    .createField('image')
     .name('Image')
     .type('Link')
     .linkType('Asset')
     .required(false)
     .localized(false);
 
-  contentBlock.createField('additionalContent')
+  contentBlock
+    .createField('additionalContent')
     .name('Additional Content')
     .type('Object')
     .required(false)
     .localized(false);
-}
+};
