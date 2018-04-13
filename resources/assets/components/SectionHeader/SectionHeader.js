23bc3402c1094871de181309b11506bb8a5f79e0
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-04-13 11:37:27 -0400

    changing title to optional, adding todo comment

--- resources/assets/components/SectionHeader/SectionHeader.js
+++ resources/assets/components/SectionHeader/SectionHeader.js
@@ -4,24 +4,29 @@ import PropTypes from 'prop-types';
 import { convertNumberToWord } from '../../helpers';
 import './section-header.scss';
 
+// @TODO:
+// - change preTitle prop to superTitle,
+// - remove hideStepNumber prop and associated logic
+
 const SectionHeader = ({ preTitle, title, hideStepNumber, step }) => (
   <div className="flex__cell -two-thirds section-header">
     <span className="heading -emphasized section-header__pre-title">
       {hideStepNumber ? preTitle : `Step ${convertNumberToWord(step)}`}
     </span>
-    <h1 className="section-header__title">{title}</h1>
+    {title ? <h1 className="section-header__title">{title}</h1> : null}
   </div>
 );
 
 SectionHeader.propTypes = {
   preTitle: PropTypes.string,
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   hideStepNumber: PropTypes.bool,
   step: PropTypes.number,
 };
 
 SectionHeader.defaultProps = {
   step: null,
+  title: null,
   preTitle: null,
   hideStepNumber: false,
 };
