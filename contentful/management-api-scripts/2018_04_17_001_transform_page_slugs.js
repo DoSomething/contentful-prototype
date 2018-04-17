e7dccaa1ecdff1f0c5d403ba2903152df673b558
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-04-17 14:26:57 -0400

    Add page slug transform script

---
+++ contentful/management-api-scripts/2018_04_17_001_transform_page_slugs.js
@@ -0,0 +1,54 @@
+const contentful = require('contentful-management');
+
+const spaceId = process.argv[3];
+const accessToken = process.argv[5];
+
+if (!spaceId || !accessToken) {
+  console.log(
+    'Please provide the space-id and access token arguments in the following format:',
+  );
+  console.log('--space-id [space-id] --access-token [access-token]');
+  return;
+}
+
+async function transformPageSlugs() {
+  const client = contentful.createClient({
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: accessToken,
+  });
+
+  // This API call will request a space with the specified ID
+  const space = await client.getSpace(spaceId);
+
+  // This API call will request the environment with the specified - as of now hardcoded - id
+  // (Contentful requires this scoping, as the `space.getEntries` is being deprecated)
+  const environment = await space.getEnvironment('master');
+
+  // Now that we have an environment, we can get entries from that environment
+  const campaigns = await environment.getEntries({
+    content_type: 'campaign',
+  });
+
+  campaigns.items.forEach(campaign => {
+    const pages = campaign.fields.pages['en-US'];
+
+    pages.forEach(page => {
+      transformPageSlug(campaign, page, environment);
+    });
+
+    // give the API a breather
+    sleep(1);
+  });
+}
+
+async function transformPageSlug(campaign, page, environment) {
+  const pageEntry = await environment.getEntry(page.sys.id);
+
+  pageEntry.fields.slug['en-US'] = `${campaign.fields.slug['en-US']}/${
+    pageEntry.fields.slug['en-US']
+  }`;
+
+  pageEntry.update().then(page => page.publish());
+}
+
+transformPageSlugs();
