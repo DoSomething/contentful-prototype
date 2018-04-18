276276b32113c7bb8ecb0af23edb85fff8e4e3aa
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-04-18 15:58:52 -0400

    Contentful slug transform script (#844)
    * Add contentful-management package
    
    * Add page slug transform script
    
    * Remove this foolishness
    
    * Add a safeguard and some console logs
    
    * Break out the environment setup into seperate function
    
    * Some improvements
    
    * Use join to create new page slugs
    
    * Check that page slug is not already the correct format before transforming
    
    * Remove rogue console log
    
    * Import ordering :angel:
    
    * Use lodash get to simplify field checking
    
    * Add another API rest sleep
    
    * Add condition for campaign slug in index 1
    
    * Add minimist package to dev
    
    * Use minimist to parse arguments
    
    * Remove extra sleep. Use await
    
    * Just one condition for campaign slug in page slug

---
+++ contentful/management-api-scripts/2018_04_17_001_transform_page_slugs.js
@@ -0,0 +1,81 @@
+const { join } = require('path');
+const { get } = require('lodash');
+const parseArgs = require('minimist');
+const contentful = require('contentful-management');
+
+const locale = 'en-US';
+
+const args = parseArgs(process.argv, {
+  alias: { 'access-token': 'accessToken', spaceId: 'space-id' },
+});
+
+const { spaceId, accessToken } = args;
+
+if (!spaceId || !accessToken) {
+  console.log(
+    'Please provide the space-id and access token arguments in the following format:',
+  );
+  console.log('--space-id [space-id] --access-token [access-token]');
+  return;
+}
+
+// Helper Function
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getEnvironment() {
+  const client = contentful.createClient({
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: accessToken,
+  });
+
+  // This API call will request a space with the specified ID
+  const space = await client.getSpace(spaceId);
+
+  // This API call will request the environment with the specified - as of now hardcoded - id
+  // (Contentful requires this scoping, as the `space.getEntries` is being deprecated)
+  return space.getEnvironment('master');
+}
+
+async function transformPageSlugs() {
+  const environment = await getEnvironment();
+
+  // Now that we have an environment, we can get entries from that environment
+  const campaigns = await environment.getEntries({
+    content_type: 'campaign',
+  });
+
+  campaigns.items.forEach(campaign => {
+    const pages = get(campaign.fields.pages, locale, []);
+
+    pages.forEach(page => {
+      transformPageSlug(campaign, page, environment);
+    });
+
+    console.log(`Processed Campaign! [ID: ${campaign.sys.id}]\n`);
+  });
+}
+
+async function transformPageSlug(campaign, page, environment) {
+  const pageEntry = await environment.getEntry(page.sys.id);
+
+  const campaignSlug = get(campaign.fields.slug, locale);
+  const pageSlug = get(pageEntry.fields.slug, locale);
+
+  if (!campaignSlug || !pageSlug || pageSlug.indexOf(campaignSlug) >= 0) {
+    return;
+  }
+
+  pageEntry.fields.slug[locale] = join(campaignSlug, pageSlug);
+
+  pageEntry.update().then(page => page.publish());
+
+  console.log(`Updated Page! [ID: ${pageEntry.sys.id}]`);
+  console.log(`--> ${pageEntry.fields.slug[locale]}\n`);
+
+  // API breather room
+  await sleep(1000);
+}
+
+transformPageSlugs();
