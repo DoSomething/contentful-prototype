46e395d411c7384297465daf0947e72b71995282
mendelB <mendelblesofsky@gmail.com>
2018-04-20 12:02:38 -0400

    Add Content Management API client

---
+++ contentful/management-api-scripts/contentManagementClient.js
@@ -0,0 +1,59 @@
+const { get } = require('lodash');
+const parseArgs = require('minimist');
+const contentful = require('contentful-management');
+
+const LOCALE = 'en-US';
+
+async function initContentManagementClient(callback) {
+  const args = parseArgs(process.argv, {
+    alias: { 'access-token': 'accessToken', spaceId: 'space-id' },
+  });
+
+  const { spaceId, accessToken } = args;
+
+  if (!spaceId || !accessToken) {
+    console.log(
+      'Please provide the space-id and access token arguments in the following format:',
+    );
+    console.log('--space-id [space-id] --access-token [access-token]');
+    return;
+  }
+  const environment = await getEnvironment(spaceId, accessToken);
+  callback(environment);
+}
+
+async function getEnvironment(spaceId, accessToken) {
+  const client = contentful.createClient({
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: accessToken,
+  });
+
+  // This API call will request a space with the specified ID
+  const space = await client.getSpace(spaceId);
+
+  // This API call will request the environment with the specified - as of now hardcoded - id
+  // (Contentful requires this scoping, as the `space.getEntries` is being deprecated)
+  return space.getEnvironment('master');
+}
+
+// Helper Functions
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getField(entry, field, defaultVal = null) {
+  return get(entry.fields[field], LOCALE, defaultVal);
+}
+
+module.exports = {
+  contentManagementClient: {
+    init: initContentManagementClient,
+    constants: {
+      LOCALE: LOCALE,
+    },
+    helpers: {
+      sleep: sleep,
+      getField: getField,
+    },
+  },
+};
