4c108e71c5e80ab714f4078ae16e06fc059ece01
David Furnes <david@dfurnes.com>
2018-04-25 14:33:06 -0400

    Remove unused container prop.

--- resources/assets/components/Share/ShareContainer.js
+++ resources/assets/components/Share/ShareContainer.js
@@ -1,10 +1,5 @@
-import { connect } from 'react-redux';
 import { PuckConnector } from '@dosomething/puck-client';
 import Share from './Share';
 
-const mapStateToProps = state => ({
-  user: state.user,
-});
-
-// Export the container component.
-export default connect(mapStateToProps)(PuckConnector(Share));
+// @TODO: Remove PuckConnector.
+export default PuckConnector(Share);
