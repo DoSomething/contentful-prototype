c34e9fdcfb6c6e86b24df59f3f6c475ab10123f4
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-04-25 19:02:50 -0400

    Update reference to helpers

--- contentful/management-api-scripts/2018_04_17_001_transform_page_slugs.js
+++ contentful/management-api-scripts/2018_04_17_001_transform_page_slugs.js
@@ -1,8 +1,8 @@
 const { join } = require('path');
+const { constants, getField, sleep } = './helpers';
 const { contentManagementClient } = require('./contentManagementClient');
 
 const { LOCALE } = contentManagementClient.constants;
-const { sleep, getField } = contentManagementClient.helpers;
 
 async function transformPageSlugs(environment) {
   const campaigns = await environment.getEntries({
