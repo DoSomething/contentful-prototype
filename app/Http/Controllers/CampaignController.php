947cd0d84a404f8fee88af4e84cf4a7af7c2e145
David Furnes <david@dfurnes.com>
2018-04-26 15:27:08 -0400

    Set up general server template for all routes.

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -47,7 +47,7 @@ class CampaignController extends Controller
     {
         $campaign = $this->campaignRepository->findBySlug($slug);
 
-        return view('campaigns.show', [
+        return view('app', [
             // This is used to build campaign-specific login links in the
             // server-rendered top navigation bar.
             'campaign' => $campaign,
