9618f1f7231838532cf5cacf88357523def2471d
Diego Lorenzo <diego@diegolorenzo.com>
2018-04-27 17:34:34 -0400

    Photo submission action migration (#866)
    * Adding new migration for Photo Submission Action.
    
    * Wrapping up updates to migration script.

---
+++ contentful/migrations/2018_04_27_001_add_photo_submission_action_content_type.js
@@ -0,0 +1,91 @@
+module.exports = function(migration) {
+  const photoSubmissionAction = migration
+    .createContentType('photoSubmissionAction')
+    .name('Photo Submission Action')
+    .description('Action block for submitting photo reportback posts.')
+    .displayField('internalTitle');
+
+  photoSubmissionAction
+    .createField('internalTitle')
+    .name('Internal Title')
+    .type('Symbol')
+    .required(true)
+    .localized(false);
+
+  photoSubmissionAction
+    .createField('title')
+    .name('Title')
+    .type('Symbol')
+    .required(false)
+    .localized(true);
+
+  photoSubmissionAction
+    .createField('captionFieldLabel')
+    .name('Caption Field Label')
+    .type('Symbol')
+    .required(false)
+    .localized(true);
+
+  photoSubmissionAction
+    .createField('captionFieldPlaceholder')
+    .name('Caption Field Placeholder')
+    .type('Symbol')
+    .required(false)
+    .localized(true);
+
+  photoSubmissionAction
+    .createField('showQuantityField')
+    .name('Show Quantity Field')
+    .type('Boolean')
+    .required(false)
+    .localized(false);
+
+  photoSubmissionAction
+    .createField('quantityFieldLabel')
+    .name('Quantity Field Label')
+    .type('Symbol')
+    .required(false)
+    .localized(true);
+
+  photoSubmissionAction
+    .createField('quantityFieldPlaceholder')
+    .name('Quantity Field Placeholder')
+    .type('Symbol')
+    .required(false)
+    .localized(true);
+
+  photoSubmissionAction
+    .createField('whyParticipatedFieldLabel')
+    .name('Why Participated Field Label')
+    .type('Symbol')
+    .required(false)
+    .localized(true);
+
+  photoSubmissionAction
+    .createField('whyParticipatedFieldPlaceholder')
+    .name('Why Participated Field Placeholder')
+    .type('Symbol')
+    .required(false)
+    .localized(true);
+
+  photoSubmissionAction
+    .createField('buttonText')
+    .name('Button Text')
+    .type('Symbol')
+    .required(false)
+    .localized(true);
+
+  photoSubmissionAction
+    .createField('affirmationContent')
+    .name('Affirmation Content')
+    .type('Text')
+    .required(false)
+    .localized(true);
+
+  photoSubmissionAction
+    .createField('additionalContent')
+    .name('Additional Content')
+    .type('Object')
+    .required(false)
+    .localized(false);
+};
