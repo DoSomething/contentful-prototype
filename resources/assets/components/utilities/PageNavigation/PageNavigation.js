a56d4f0f76e74cc28de6679a644fd739a27ecf80
Diego Lorenzo <diego@diegolorenzo.com>
2018-04-30 09:55:18 -0400

    Remove the no longer used TabbedNavigation component. (#868)

--- resources/assets/components/utilities/PageNavigation/PageNavigation.js
+++ resources/assets/components/utilities/PageNavigation/PageNavigation.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 import { MEDIA_MEDIUM_SIZE_MIN } from '../../../constants';
-import NavigationLink from '../../Navigation/NavigationLink';
+import NavigationLink from '../../utilities/NavigationLink/NavigationLink';
 
 import './page-navigation.scss';
 
