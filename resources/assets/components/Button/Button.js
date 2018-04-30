9cba595660949372d99f754bf4303622a353d148
David Furnes <david@dfurnes.com>
2018-04-30 14:31:15 -0400

    Update Button "attached" styling & text prop.

--- resources/assets/components/Button/Button.js
+++ resources/assets/components/Button/Button.js
@@ -1,33 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+
 import './button.scss';
 
-const Button = ({ onClick, className, isLoading, text }) => {
+const Button = ({ onClick, className, attached, isLoading, children }) => {
   const classNames = classnames(
     'button',
-    { 'is-loading': isLoading },
+    {
+      'is-loading': isLoading,
+      '-attached': attached,
+    },
     className,
   );
 
-  return (
+  const buttonText = Array.isArray(children) ? children.join('') : children;
+  const button = (
     <button className={classNames} disabled={isLoading} onClick={onClick}>
-      {text}
+      {buttonText}
     </button>
   );
+
+  // If we're rendering an "attached" button, wrap it in a padded container:
+  if (attached) {
+    return <div className="button__attached-container">{button}</div>;
+  }
+
+  return button;
 };
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
   className: PropTypes.string,
   isLoading: PropTypes.bool,
-  text: PropTypes.string,
+  attached: PropTypes.bool,
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+  ]).isRequired,
 };
 
 Button.defaultProps = {
   className: null,
   isLoading: false,
-  text: 'Join Us',
+  attached: false,
 };
 
 export default Button;
