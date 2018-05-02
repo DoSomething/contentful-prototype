66fdbfdae9cb15bf01a37182fd3097b389864b38
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-05-02 18:10:14 -0400

    Fix naming for field id

--- contentful/migrations/2018_05_02_001_add_subtitle_to_page_content_type.js
+++ contentful/migrations/2018_05_02_001_add_subtitle_to_page_content_type.js
@@ -2,11 +2,11 @@ module.exports = function(migration) {
   const page = migration.editContentType('page');
 
   page
-    .createField('subtitle')
+    .createField('subTitle')
     .name('Subtitle')
     .type('Symbol')
     .required(false)
     .localized(true);
 
-  page.moveField('subtitle').beforeField('slug');
+  page.moveField('subTitle').beforeField('slug');
 };
