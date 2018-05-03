59bb5ac18a923f5e6d0c073cdccc84c9c4f7a7cc
Diego Lorenzo <diego@diegolorenzo.com>
2018-05-03 13:47:19 -0400

    Remove debugging code.

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -47,8 +47,6 @@ class CampaignController extends Controller
     {
         $campaign = $this->campaignRepository->findBySlug($slug);
 
-        // dd($campaign);
-
         return view('app', [
             // This is used to build campaign-specific login links in the
             // server-rendered top navigation bar.
