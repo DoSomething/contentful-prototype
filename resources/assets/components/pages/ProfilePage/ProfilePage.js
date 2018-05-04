957591dea67a75777881ce056eb3d10754c668ac
katiecrane <kcrane@dosomething.org>
2018-05-04 10:34:47 -0400

    More clean up

--- resources/assets/components/pages/ProfilePage/ProfilePage.js
+++ resources/assets/components/pages/ProfilePage/ProfilePage.js
@@ -3,17 +3,15 @@ import React from 'react';
 import Enclosure from '../../Enclosure';
 import Card from '../../utilities/Card/Card';
 
-const ProfilePage = () => {
-  return (
-    <div className="main clearfix">
-      <Enclosure className="default-container margin-top-lg margin-bottom-lg">
-        <h1>Hey, it's your profile!</h1>
-        <Card className="bordered rounded" title="My Campaigns">
-          <div className="padded">My campaigns will be here!</div>
-        </Card>
-      </Enclosure>
-    </div>
-  );
-};
+const ProfilePage = () => (
+  <div className="main clearfix">
+    <Enclosure className="default-container margin-top-lg margin-bottom-lg">
+      <h1>Hey, it&apos;s your profile!</h1>
+      <Card className="bordered rounded" title="My Campaigns">
+        <div className="padded">My campaigns will be here</div>
+      </Card>
+    </Enclosure>
+  </div>
+);
 
 export default ProfilePage;
