aad1ba893246cf3de8c7d7f35b80af873f957611
David Furnes <david@dfurnes.com>
2018-05-09 13:12:39 -0400

    Add a badge to rejected posts.

--- resources/assets/components/utilities/PostCard/PostBadge.js
+++ resources/assets/components/utilities/PostCard/PostBadge.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PostBadge = ({ status, tags }) =>
-  status !== 'ACCEPTED' || tags.includes('hide-in-gallery') ? (
-    <div
-      className="post-badge"
-      title="This post is awaiting review or hidden in the gallery."
-    />
-  ) : null;
+const PostBadge = ({ status, tags }) => {
+  if (status === 'REJECTED') {
+    return (
+      <div
+        className="post-badge -rejected"
+        title="This post has been marked as rejected."
+      />
+    );
+  }
+
+  if (status !== 'ACCEPTED' || tags.includes('hide-in-gallery')) {
+    return (
+      <div
+        className="post-badge"
+        title="This post is awaiting review or hidden in the gallery."
+      />
+    );
+  }
+
+  return null;
+};
 
 PostBadge.propTypes = {
   status: PropTypes.bool.isRequired,
