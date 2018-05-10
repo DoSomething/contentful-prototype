29b4aadf19029d6ee6932f90cb655d885bab2c9c
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-05-10 15:16:02 -0400

    Add PageController with show action

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\PageRepository;
+
+class PageController extends Controller
+{
+    /**
+     * The page repository.
+     *
+     * @var PageRepository
+     */
+    protected $pageRepository;
+
+    /**
+     * Make a new PageController, inject dependencies,
+     * and set middleware for this controller's methods.
+     *
+     * @param PageRepository $pageRepository
+     */
+    public function __construct(PageRepository $pageRepository)
+    {
+        $this->pageRepository = $pageRepository;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  string  $slug
+     * @return \Illuminate\View\View
+     */
+    public function show($slug)
+    {
+        $page = $this->pageRepository->findBySlug($slug);
+
+        return view('app')->with('state', [
+            'page' => $page,
+        ]);
+    }
+}
