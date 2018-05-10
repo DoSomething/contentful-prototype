17d30ea68f7bf77425bd22bbe56978abdab2d8f3
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-05-10 17:57:20 -0400

    Update container to pass along page fields from state

--- resources/assets/components/pages/GeneralPage/GeneralPageContainer.js
+++ resources/assets/components/pages/GeneralPage/GeneralPageContainer.js
@@ -1,34 +1,15 @@
 import { connect } from 'react-redux';
-import { find } from 'lodash';
 
 import GeneralPage from './GeneralPage';
 
 /**
  * Provide state from the Redux store as props for this component.
  */
-const mapStateToProps = (state, ownProps) => {
-  // @todo This setup is temporary to test general pages
-  // this container should just be fetching the props from state
-  const subPage = find(
-    state.campaign.pages,
-    page =>
-      page.type === 'page'
-        ? page.fields.slug.endsWith(ownProps.match.params.slug)
-        : false,
-  );
-
-  if (!subPage) {
-    return null;
-  }
-
-  const { title, subTitle, blocks } = subPage.fields;
-
-  return {
-    title,
-    subTitle,
-    blocks,
-  };
-};
+const mapStateToProps = state => ({
+  title: state.page.title,
+  subTitle: state.page.subTitle,
+  blocks: state.page.blocks,
+});
 
 // Export the container component.
 export default connect(mapStateToProps)(GeneralPage);
