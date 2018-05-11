0a4295668bf43b756cca1545148fa9e04f1bf473
mendelB <mendelblesofsky@gmail.com>
2018-05-11 11:02:47 -0400

    Fix container

--- resources/assets/components/pages/GeneralPage/GeneralPageContainer.js
+++ resources/assets/components/pages/GeneralPage/GeneralPageContainer.js
@@ -5,11 +5,15 @@ import GeneralPage from './GeneralPage';
 /**
  * Provide state from the Redux store as props for this component.
  */
-const mapStateToProps = state => ({
-  title: state.page.title,
-  subTitle: state.page.subTitle,
-  blocks: state.page.blocks,
-});
+const mapStateToProps = state => {
+  const { title, subTitle, blocks } = state.page.fields;
+
+  return {
+    title,
+    subTitle,
+    blocks,
+  };
+};
 
 // Export the container component.
 export default connect(mapStateToProps)(GeneralPage);
