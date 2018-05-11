c167e447a4791899dacafa63f9d89b5c639c3ecf
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-05-11 10:38:21 -0400

    Standalone 'Page' backend setup (#902)
    * Add PageRepository with findBySlug method
    
    * Add PageController with show action
    
    * Revert to stubbed 501 until we implement front end
    
    * Route /{slug} requests to PageController@show
    
    * Cleaner return statement

---
+++ app/Repositories/PageRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Entities\Page;
+use Contentful\Delivery\Query;
+use Contentful\Delivery\Client as Contentful;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class PageRepository
+{
+    /**
+     * Contentful client instance.
+     */
+    private $contentful;
+
+    /**
+     * PageRepository constructor.
+     *
+     * @param Contentful $contentful
+     */
+    public function __construct(Contentful $contentful)
+    {
+        $this->contentful = $contentful;
+    }
+
+    /**
+     * Find a page by its slug.
+     *
+     * @param  string $slug
+     * @return stdClass
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    public function findBySlug($slug)
+    {
+        $query = (new Query)
+            ->setContentType('page')
+            ->where('fields.slug', $slug)
+            ->setInclude(1)
+            ->setLimit(1);
+
+        $pages = $this->contentful->getEntries($query);
+
+        if (! $pages->count()) {
+            throw new ModelNotFoundException;
+        }
+
+        return new Page($pages[0]);
+    }
+}
