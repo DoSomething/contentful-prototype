4800d21bee8d376b386d4af3a193abe663458ab8
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-05-11 13:52:52 -0400

    Standalone Page - frontend (#906)
    * Add reducer and initial state for standalone pages
    
    * Update container to pass along page fields from state
    
    * Add standalone /:slug (page) route
    
    * Adjust position to ensure nav profile dropdown isn't hidden
    
    * Fix container
    
    * Activate page view

--- resources/assets/components/pages/GeneralPage/GeneralPageContainer.js
+++ resources/assets/components/pages/GeneralPage/GeneralPageContainer.js
@@ -1,27 +1,12 @@
 import { connect } from 'react-redux';
-import { find } from 'lodash';
 
 import GeneralPage from './GeneralPage';
 
 /**
  * Provide state from the Redux store as props for this component.
  */
-const mapStateToProps = (state, ownProps) => {
-  // @todo This setup is temporary to test general pages
-  // this container should just be fetching the props from state
-  const subPage = find(
-    state.campaign.pages,
-    page =>
-      page.type === 'page'
-        ? page.fields.slug.endsWith(ownProps.match.params.slug)
-        : false,
-  );
-
-  if (!subPage) {
-    return null;
-  }
-
-  const { title, subTitle, blocks } = subPage.fields;
+const mapStateToProps = state => {
+  const { title, subTitle, blocks } = state.page.fields;
 
   return {
     title,
