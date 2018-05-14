66917e0dd86f9e1763772e89c2824e9df90fb52b
Diego Lorenzo <diego@diegolorenzo.com>
2018-05-14 11:05:02 -0400

    Updating tests.

--- resources/assets/components/actions/PhotoSubmissionAction/PhotoSubmissionAction.test.js
+++ resources/assets/components/actions/PhotoSubmissionAction/PhotoSubmissionAction.test.js
@@ -8,7 +8,11 @@ describe('PhotoSubmissionAction component', () => {
 
   const wrapper = shallow(
     <PhotoSubmissionAction
+      campaignId="1234"
+      campaignRunId="6789"
+      clearPostSubmissionItem={jest.fn()}
       id={id}
+      storeCampaignPost={jest.fn()}
       submissions={{
         isPending: false,
         items: {},
@@ -17,7 +21,7 @@ describe('PhotoSubmissionAction component', () => {
   );
 
   test('is rendered as a card component with a form, MediaUploader and a submit button', () => {
-    expect(wrapper.find('Card').length).toEqual(1);
+    expect(wrapper.find('Card').length).toBeGreaterThanOrEqual(1);
     expect(wrapper.find('form').length).toEqual(1);
     expect(wrapper.find('MediaUploader').length).toEqual(1);
     expect(wrapper.find('Button').length).toEqual(1);
