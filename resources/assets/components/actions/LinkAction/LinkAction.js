eb21b08b6d47865c7d7734fdc06ba302d64db818
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-05-15 17:54:56 -0400

    Link Action - CTA template (#916)
    * Add template field to LinkAction entity
    
    * Add CTA link action template
    
    * Add puck tracking and some cleanup
    
    * Rename the component
    
    * Rename to CtaTemplate
    
    * update styling and tags
    
    * Commit filename change
    
    * temp rename to be rid of upcase file
    
    * Rename to proper naming
    
    * Use Markdown for content
    
    * Remove now unneeded util class

--- resources/assets/components/actions/LinkAction/LinkAction.js
+++ resources/assets/components/actions/LinkAction/LinkAction.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import CtaTemplate from './templates/CtaTemplate';
 import DefaultTemplate from './templates/DefaultTemplate';
 
 const LinkAction = props => {
   const { template } = props;
 
   switch (template) {
+    case 'cta':
+      return <CtaTemplate {...props} />;
     default:
       return <DefaultTemplate {...props} />;
   }
