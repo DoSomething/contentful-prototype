0144368ae83bcc69e5d037a5114ff6fd7ea6fd2a
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-05-18 12:21:08 -0400

    Add basic test

---
+++ resources/assets/components/actions/ReferralSubmissionAction/ReferralSubmissionAction.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ReferralSubmissionAction from './ReferralSubmissionAction';
+
+describe('PhotoSubmissionAction component', () => {
+  const id = 'abcdefghi123456789';
+
+  const wrapper = shallow(
+    <ReferralSubmissionAction
+      campaignId="1234"
+      campaignRunId="6789"
+      clearPostSubmissionItem={jest.fn()}
+      id={id}
+      storeCampaignPost={jest.fn()}
+      submissions={{
+        isPending: false,
+        items: {},
+      }}
+      type="referral"
+    />,
+  );
+
+  test('is rendered as a card component with a form and a submit button', () => {
+    expect(wrapper.find('Card').length).toBeGreaterThanOrEqual(1);
+    expect(wrapper.find('form').length).toEqual(1);
+    expect(wrapper.find('Button').length).toEqual(1);
+  });
+});
