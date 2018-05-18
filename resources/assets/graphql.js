0daa0e98d2ae1ea5ad6623e818d8c21e3b4ba589
David Furnes <david@dfurnes.com>
2018-05-18 14:14:53 -0400

    Use persisted queries for smaller network payload.

--- resources/assets/graphql.js
+++ resources/assets/graphql.js
@@ -6,9 +6,10 @@ import { ApolloClient } from 'apollo-client';
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { onError } from 'apollo-link-error';
+import { createPersistedQueryLink } from 'apollo-link-persisted-queries';
 
 // Create an authentication link with the user's access token.
-const authenticationLink = setContext((request, context) => {
+const authLink = setContext((request, context) => {
   const accessToken = window.AUTH.token;
 
   if (accessToken) {
@@ -38,12 +39,15 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
 
 // Configure Apollo Client.
 export default uri => {
+  // Handle making persisted queries so we send fewer bytes per request.
+  const persistedLink = createPersistedQueryLink();
+
   // Create the HTTP link! This is our terminating link that batches up
   // GraphQL queries and makes the actual HTTP request to our server.
   const httpLink = new BatchHttpLink({ uri });
 
   return new ApolloClient({
-    link: ApolloLink.from([errorLink, authenticationLink, httpLink]),
+    link: ApolloLink.from([errorLink, authLink, persistedLink, httpLink]),
     cache: new InMemoryCache(),
   });
 };
