614f2bab42601dfc26772d102c8f49c8f3878287
Diego Lorenzo <diego@diegolorenzo.com>
2018-05-21 13:38:17 -0400

    Updating test.

--- resources/assets/__mocks__/api.js
+++ resources/assets/__mocks__/api.js
@@ -4,16 +4,18 @@ export function getUserCampaignSignups(
   campaignRunId = null,
 ) {
   const signup = {
-    data: {
-      id: 123456,
-      northstar_id: userId,
-      campaign_id: campaignId,
-      campaign_run_id: campaignRunId,
-      quantity: 30,
-      why_participated: 'Because I love to test things.',
-      source: 'phoenix',
-      details: null,
-    },
+    data: [
+      {
+        id: 123456,
+        northstar_id: userId,
+        campaign_id: campaignId,
+        campaign_run_id: campaignRunId,
+        quantity: 30,
+        why_participated: 'Because I love to test things.',
+        source: 'phoenix',
+        details: null,
+      },
+    ],
   };
 
   return new Promise((resolve, reject) => {
