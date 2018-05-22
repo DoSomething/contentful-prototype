0ee174282d8f6fe72153f2821e4ba4f3812246fd
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-05-22 15:03:23 -0400

    Pass through action page blocks as action steps

--- resources/assets/components/pages/ActionPage/ActionPageContainer.js
+++ resources/assets/components/pages/ActionPage/ActionPageContainer.js
@@ -7,12 +7,22 @@ import { isSignedUp } from '../../../selectors/signup';
 /**
  * Provide state from the Redux store as props for this component.
  */
-const mapStateToProps = state => ({
-  steps: state.campaign.actionSteps,
-  dashboard: state.campaign.dashboard,
-  signedUp: isSignedUp(state),
-  featureFlags: get(state.campaign.additionalContent, 'featureFlags'),
-});
+const mapStateToProps = state => {
+  const actionPage = state.campaign.pages.find(page =>
+    page.fields.slug.endsWith('action'),
+  );
+
+  const steps = state.campaign.actionSteps.length
+    ? state.campaign.actionSteps
+    : actionPage.fields.blocks;
+
+  return {
+    steps,
+    dashboard: state.campaign.dashboard,
+    signedUp: isSignedUp(state),
+    featureFlags: get(state.campaign.additionalContent, 'featureFlags'),
+  };
+};
 
 // Export the container component.
 export default connect(mapStateToProps)(ActionPage);
