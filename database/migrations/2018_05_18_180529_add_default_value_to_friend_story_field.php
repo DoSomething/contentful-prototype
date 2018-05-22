e13d2444391b946c7f50e25d9e2e227fe772c68e
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-05-22 12:17:40 -0400

    Referrals backend (#935)
    * Add migration to change referrals friend_story field to nullable
    
    * Update referral controller for ReferralSubmissionAction format
    
    * Clean up the referrals controller
    
    * Style Ci :imp:
    
    * fix down function for referrals migration

---
+++ database/migrations/2018_05_18_180529_add_default_value_to_friend_story_field.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDefaultValueToFriendStoryField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('referrals', function (Blueprint $table) {
+            $table->text('friend_story')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('referrals', function (Blueprint $table) {
+            $table->text('friend_story')->nullable(false)->change();
+        });
+    }
+}
