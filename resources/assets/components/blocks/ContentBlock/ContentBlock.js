ca97039acda6adbf1db12308b32d10f142a6e0ff
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-05-24 12:22:33 -0400

    Remove the default image alignment again

--- resources/assets/components/blocks/ContentBlock/ContentBlock.js
+++ resources/assets/components/blocks/ContentBlock/ContentBlock.js
@@ -11,8 +11,6 @@ import './content-block.scss';
 const ContentBlock = props => {
   const { content, image, imageAlignment, superTitle, title } = props;
 
-  const defaultImageAlignment = ContentBlock.defaultProps.imageAlignment;
-
   const contentNode = content ? <Markdown>{content}</Markdown> : null;
 
   return (
@@ -28,7 +26,7 @@ const ContentBlock = props => {
           <Figure
             image={image}
             alt="content-block"
-            alignment={`${imageAlignment || defaultImageAlignment}-collapse`}
+            alignment={`${imageAlignment}-collapse`}
             size="one-third"
           >
             {contentNode}
