0ac46e3b1f8bfc62de72cc3451238be9b0d3e92e
David Furnes <david@dfurnes.com>
2018-06-05 12:06:40 -0400

    Display count inside Social Drive component.

--- resources/assets/components/actions/SocialDriveAction/SocialDriveAction.js
+++ resources/assets/components/actions/SocialDriveAction/SocialDriveAction.js
@@ -37,8 +37,8 @@ class SocialDriveAction extends React.Component {
 
     const href = dynamicString(this.props.link, { userId });
 
-    postRequest('api/v2/links', { url: withoutTokens(href) }, token).then(
-      response => this.setState({ shortenedLink: response.url }),
+    postRequest('/api/v2/links', { url: withoutTokens(href) }, token).then(
+      ({ url, count }) => this.setState({ shortenedLink: url, count }),
     );
   }
 
@@ -143,7 +143,7 @@ class SocialDriveAction extends React.Component {
               <span className="page-views__text caps-lock">
                 total page views
               </span>
-              <h1 className="page-views__amount">5</h1>
+              <h1 className="page-views__amount">{this.state.count || 0}</h1>
             </div>
           </div>
         ) : null}
@@ -160,7 +160,7 @@ SocialDriveAction.propTypes = {
 };
 
 SocialDriveAction.defaultProps = {
-  showPageViews: false,
+  showPageViews: true,
 };
 
 export default SocialDriveAction;
