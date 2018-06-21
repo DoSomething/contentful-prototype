572dcb4f63d7e4a441d55814a896363f2f955b14
David Furnes <david@dfurnes.com>
2018-06-21 11:45:57 -0400

    Fix fatal error on campaigns index, introduced in #989.

--- resources/views/campaigns/index.blade.php
+++ resources/views/campaigns/index.blade.php
@@ -18,7 +18,7 @@
                                         <h1 class="tile__title">{{ $item->title }}</h1>
                                     </div>
                                     @if ($item->coverImage)
-                                        <img alt="{{ $item->title }}" src="{{ $item->coverImage }}" />
+                                        <img alt="{{ $item->title }}" src="{{ $item->coverImage->url }}?w=400&h=400&fit=fill" />
                                     @else
                                         <img alt="No cover image!" src="data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=" />
                                     @endif
