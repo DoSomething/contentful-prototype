bb1959e2b40c5cd872324d543ee554b7d4c71099
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-06-21 10:55:08 -0400

    Remove manual return for empty IDs

--- app/Http/Controllers/Api/CampaignsController.php
+++ app/Http/Controllers/Api/CampaignsController.php
@@ -34,11 +34,7 @@ class CampaignsController extends Controller
     {
         $ids = array_get($request->query('filter'), 'id');
 
-        if (! $ids) {
-            return response()->json(['data' => []]);
-        }
-
-        $idsArray = explode(',', $ids);
+        $idsArray = count($ids) ? explode(',', $ids) : [];
 
         // Resetting the filter[id] param value to an array so that we can properly validate.
         $request->query('filter')['id'] = $idsArray;
