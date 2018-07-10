3368139ed9ee74216cc1f677b47cedb401e24895
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-07-10 15:42:41 -0400

    Remove `hideStepNumber` and `step` props

--- resources/assets/components/SectionHeader/SectionHeader.js
+++ resources/assets/components/SectionHeader/SectionHeader.js
@@ -4,14 +4,12 @@ import PropTypes from 'prop-types';
 import { convertNumberToWord } from '../../helpers';
 import './section-header.scss';
 
-// @TODO:
-// - change preTitle prop to superTitle,
-// - remove hideStepNumber prop and associated logic
+// @TODO: change preTitle prop to superTitle.
 
-const SectionHeader = ({ preTitle, title, hideStepNumber, step }) => (
+const SectionHeader = ({ preTitle, title }) => (
   <div className="flex__cell -two-thirds section-header">
     <span className="heading -emphasized section-header__pre-title">
-      {hideStepNumber ? preTitle : `Step ${convertNumberToWord(step)}`}
+      {preTitle}
     </span>
     {title ? <h1 className="section-header__title">{title}</h1> : null}
   </div>
@@ -20,15 +18,11 @@ const SectionHeader = ({ preTitle, title, hideStepNumber, step }) => (
 SectionHeader.propTypes = {
   preTitle: PropTypes.string,
   title: PropTypes.string,
-  hideStepNumber: PropTypes.bool,
-  step: PropTypes.number,
 };
 
 SectionHeader.defaultProps = {
-  step: null,
   title: null,
   preTitle: null,
-  hideStepNumber: false,
 };
 
 export default SectionHeader;
