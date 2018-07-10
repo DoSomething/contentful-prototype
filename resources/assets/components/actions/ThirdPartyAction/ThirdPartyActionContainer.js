e5b15d1c4c81981d1c91c9520a635864ea52f478
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-07-10 14:57:41 -0400

    Remove the Third Party Action component (#1020)
    * Remove all references to `third-party-action`
    
    * Remove ThirdPartyAction component
    
    * Use const for the non re-assigned stepIndex

--- resources/assets/components/actions/ThirdPartyAction/ThirdPartyActionContainer.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-
-import ThirdPartyAction from './ThirdPartyAction';
-
-const mapStateToProps = state => ({
-  userId: state.user.id,
-});
-
-export default connect(mapStateToProps)(ThirdPartyAction);
--- resources/assets/components/pages/ActionPage/ActionSteps.js
+++ resources/assets/components/pages/ActionPage/ActionSteps.js
@@ -57,15 +57,10 @@ const ActionSteps = props => {
     template,
   } = props;
 
-  let stepIndex = 0;
+  const stepIndex = 0;
 
   const stepComponents = actionSteps.map(json => {
     const type = parseContentfulType(json);
-
-    // Is this a "numbered" step? If so, increment our step index.
-    if (['third-party-action'].includes(type)) {
-      stepIndex += 1;
-    }
 
     // Some components have built-in section headers. For those, append it.
     // @TODO: These should be split out into separate "content" blocks.
