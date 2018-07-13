bc4cf7e9e9bff6a0d0040b918acd52060fcf153f
deguzmankevin <deguzmankevin@hotmail.com>
2018-07-13 12:11:08 -0400

    proptypes changes and revert accountquery change

--- resources/assets/components/pages/ProfilePage/AccountQuery.js
+++ resources/assets/components/pages/ProfilePage/AccountQuery.js
@@ -19,7 +19,15 @@ const ACCOUNT_QUERY = gql`
 
 const AccountQuery = ({ userId }) => (
   <Query query={ACCOUNT_QUERY} queryName="user" variables={{ userId }}>
-    {({ data }) => <AccountNavigation {...data} />};
+    {({ loading, error, data }) => {
+      if (loading) {
+        return 'Loading...';
+      }
+      if (error) {
+        return `Error! ${error.message}`;
+      }
+      return <AccountNavigation {...data} />;
+    }}
   </Query>
 );
 
