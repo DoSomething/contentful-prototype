017cb9b345fd6b4a7be1da16e6d695cba2fb7e42
deguzmankevin <deguzmankevin@hotmail.com>
2018-07-20 12:23:26 -0400

    delete AccountContainer

--- resources/assets/components/pages/ProfilePage/AccountContainer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import Account from './Account';
-
-const AccountContainer = props => <Account {...props} />;
-
-export default AccountContainer;
--- resources/assets/components/pages/ProfilePage/ProfileRoute.js
+++ resources/assets/components/pages/ProfilePage/ProfileRoute.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import AccountContainer from './AccountContainer';
+import Account from './Account';
 import TestCampaign from './TestCampaign';
 
 const ProfileRoute = props => (
   <div>
     <Switch>
-      <Route
-        path="/us/profile/info"
-        render={() => <AccountContainer {...props} />}
-      />
+      <Route path="/us/profile/info" render={() => <Account {...props} />} />
       <Route path="/us/profile/campaigns" component={TestCampaign} />
     </Switch>
   </div>
