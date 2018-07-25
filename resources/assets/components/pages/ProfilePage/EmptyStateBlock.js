99f2735f36aedb26ccf59a1f79eed5abd673faae
deguzmankevin <deguzmankevin@hotmail.com>
2018-07-25 16:50:19 -0400

    remove find campaign button

--- resources/assets/components/pages/ProfilePage/EmptyStateBlock.js
+++ resources/assets/components/pages/ProfilePage/EmptyStateBlock.js
@@ -6,16 +6,6 @@ const EmptyStateBlock = () => (
       You {"don't"} have any uploads...yet! Find a cause you care about <br />and
       get started today!
     </p>
-    <div className="margin-top-lg">
-      <a
-        href="/us/campaigns"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="button"
-      >
-        Find a Campaign!
-      </a>
-    </div>
   </div>
 );
 export default EmptyStateBlock;
