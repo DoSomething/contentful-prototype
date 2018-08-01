7c153a3c80dfc4d8a9e64bfd85f1bc04bc214c0c
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-08-01 16:19:45 -0400

    Render content and sidebar fields

--- resources/assets/components/pages/GeneralPage/GeneralPage.js
+++ resources/assets/components/pages/GeneralPage/GeneralPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import Enclosure from '../../Enclosure';
 import ContentfulEntry from '../../ContentfulEntry';
+import Markdown from '../../utilities/Markdown/Markdown';
 
 import './general-page.scss';
 
@@ -12,7 +14,7 @@ import './general-page.scss';
  * @returns {XML}
  */
 const GeneralPage = props => {
-  const { title, subTitle, blocks } = props;
+  const { title, subTitle, content, sidebar, blocks } = props;
 
   return (
     <div>
@@ -24,6 +26,22 @@ const GeneralPage = props => {
               <p className="general-page__subtitle">{subTitle}</p>
             ) : null}
           </div>
+
+          {content ? (
+            <div className={classnames({ row: sidebar.length })}>
+              <div className="primary">
+                <Markdown className="margin-horizontal-md">{content}</Markdown>
+              </div>
+
+              <div className="secondary">
+                {sidebar.map(block => (
+                  <div className="margin-bottom-lg" key={block.id}>
+                    <ContentfulEntry json={block} />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : null}
 
           {blocks.map(block => (
             <div className="general-page__block margin-vertical" key={block.id}>
@@ -39,11 +57,15 @@ const GeneralPage = props => {
 GeneralPage.propTypes = {
   title: PropTypes.string.isRequired,
   subTitle: PropTypes.string,
+  content: PropTypes.string,
+  sidebar: PropTypes.arrayOf(PropTypes.object),
   blocks: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 GeneralPage.defaultProps = {
   subTitle: null,
+  content: null,
+  sidebar: [],
 };
 
 export default GeneralPage;
