a649bcc77f46fd2a520705d76f07775cd2b8a788
deguzmankevin <deguzmankevin@hotmail.com>
2018-08-01 10:48:20 -0400

    use html entity

--- resources/assets/components/pages/ProfilePage/EmptyStateBlock.js
+++ resources/assets/components/pages/ProfilePage/EmptyStateBlock.js
@@ -3,7 +3,7 @@ import React from 'react';
 const EmptyStateBlock = () => (
   <div className="margin-top-md margin-bottom-lg">
     <p>
-      You don\'t have any uploads...yet! Find a cause you care about <br />and
+      You don&#39;t have any uploads...yet! Find a cause you care about <br />and
       get started today!
     </p>
   </div>
