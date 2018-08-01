3e6895be7479aba75e42ece5c92ecc492c9f62b0
deguzmankevin <deguzmankevin@hotmail.com>
2018-08-01 10:58:22 -0400

    use p element instead of br

--- resources/assets/components/pages/ProfilePage/EmptyStateBlock.js
+++ resources/assets/components/pages/ProfilePage/EmptyStateBlock.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const EmptyStateBlock = () => (
   <div className="margin-top-md margin-bottom-lg">
-    <p>
-      You don&#39;t have any uploads...yet! Find a cause you care about <br />and
-      get started today!
+    <p>You don&#39;t have any uploads...yet!</p>
+    <p className="margin-top-none">
+      Find a cause you care about and get started today!
     </p>
   </div>
 );
