f6f743cd19e740cb2b1551d98cc126b312742536
deguzmankevin <deguzmankevin@hotmail.com>
2018-08-01 14:27:34 -0400

    rename Profile to Account

--- resources/assets/components/pages/ProfilePage/ProfileRoute.js
+++ resources/assets/components/pages/ProfilePage/ProfileRoute.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import Profile from './Profile';
+import Account from './Account';
 import UserPostsQuery from './UserPostsQuery';
 
 const ProfileRoute = props => (
   <Switch>
-    <Route path="/us/profile/info" render={() => <Profile {...props} />} />
+    <Route path="/us/profile/info" render={() => <Account {...props} />} />
     <Route
       path="/us/profile/posts"
       render={() => <UserPostsQuery userId={props.userId} />}
