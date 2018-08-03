79aa53e92c2b628fa3fd45203b8104c39a2ca03d
deguzmankevin <deguzmankevin@hotmail.com>
2018-08-03 12:54:46 -0400

    change profile text

--- resources/assets/components/pages/AccountPage/Profile.js
+++ resources/assets/components/pages/AccountPage/Profile.js
@@ -7,9 +7,9 @@ import { env } from '../../../helpers/index';
 const login = '/login';
 const Profile = props => (
   <div className="bg-gray padding-bottom-lg wrapper">
-    <h2 className="caps-lock league-gothic-sm">Your Account</h2>
+    <h2 className="caps-lock league-gothic-sm">Your Profile</h2>
     <div className="margin-top-lg">
-      <h3>Account Info</h3>
+      <h3>Profile Info</h3>
     </div>
     <FormItem title="Name" value={props.user.firstName} />
     <FormItem title="Birthday" value={props.user.birthdate} />
