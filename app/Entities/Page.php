e9e9991b34f86d69f24616b9d7352a90a7f9348d
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-08-08 18:12:55 -0400

    Remove sidebar parsing method in favor of general parseBlocks

--- app/Entities/Page.php
+++ app/Entities/Page.php
@@ -7,27 +7,6 @@ use JsonSerializable;
 class Page extends Entity implements JsonSerializable
 {
     /**
-     * Parse and extract data for the sidebar.
-     *
-     * @param  array $sidebarItems
-     * @return array
-     */
-    public function parseSidebar($sidebarItems)
-    {
-        return collect($sidebarItems)->map(function ($sidebarItem) {
-            switch ($sidebarItem->getContentType()) {
-                case 'callToAction':
-                    return new CallToAction($sidebarItem->entry);
-
-                case 'customBlock':
-                    return new CustomBlock($sidebarItem->entry);
-                default:
-                    return $sidebarItem->entry;
-            }
-        });
-    }
-
-    /**
      * Parse blocks, and reverse parsed blocks for community pages.
      *
      * @param  array $blocks
@@ -59,7 +38,7 @@ class Page extends Entity implements JsonSerializable
                 'title' => $this->title,
                 'subTitle' => $this->subTitle,
                 'content' => $this->content,
-                'sidebar' => $this->parseSidebar($this->sidebar),
+                'sidebar' => $this->parseBlocks($this->sidebar),
                 'blocks' => $this->parseBlocks($this->blocks),
                 // @TODO: we want to eventually remove the need for hideFromNavigation field
                 // in favor of always linking to pages referenced in the `pages` field.
