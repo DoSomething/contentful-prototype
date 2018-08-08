c5a9c490f61769bcf24d04c01bee577a292873a3
Diego Lorenzo <diego@diegolorenzo.com>
2018-08-08 13:51:52 -0400

    Migration to add the sixpack experiment content type.

---
+++ contentful/migrations/2018_08_08_001_add_sixpack_experiment_content_type.js
@@ -0,0 +1,47 @@
+module.exports = function(migration) {
+  const sixpackExperiment = migration
+    .createContentType('sixpackExperiment')
+    .name('Sixpack Experiment')
+    .description('Used to help set up A/B testing experiments.')
+    .displayField('internalTitle');
+
+  sixpackExperiment
+    .createField('internalTitle')
+    .name('Internal Title')
+    .type('Symbol')
+    .required(true)
+    .localized(false);
+
+  sixpackExperiment
+    .createField('title')
+    .name('Title')
+    .type('Symbol')
+    .required(true)
+    .localized(true);
+
+  sixpackExperiment
+    .createField('alternatives')
+    .name('Test Alternatives')
+    .type('Array')
+    .items({
+      type: 'Link',
+      linkType: 'Entry',
+    })
+    .validations([{ size: { min: 2 } }])
+    .required(true)
+    .localized(false);
+
+  sixpackExperiment
+    .createField('trafficFraction')
+    .name('Traffic Fraction')
+    .type('Integer')
+    .required(false)
+    .localized(false);
+
+  sixpackExperiment
+    .createField('kpi')
+    .name('Key Performance Indicator (KPI)')
+    .type('Symbol')
+    .required(false)
+    .localized(true);
+};
