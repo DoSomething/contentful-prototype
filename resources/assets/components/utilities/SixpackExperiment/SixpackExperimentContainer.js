b8bce69d5e7f527b1441ab48c0729aae4437b9db
Diego Lorenzo <diego@diegolorenzo.com>
2018-08-09 13:35:03 -0400

    Updating the SixpackExperiment component.

--- resources/assets/components/utilities/SixpackExperiment/SixpackExperimentContainer.js
+++ resources/assets/components/utilities/SixpackExperiment/SixpackExperimentContainer.js
@@ -6,8 +6,7 @@ import SixpackExperiment from './SixpackExperiment';
  * Provide state from the Redux store as props for this component.
  */
 const mapStateToProps = state => ({
-  campaignId: state.campaign.id,
-  slug: state.campaign.slug,
+  campaignSlug: state.campaign.slug,
 });
 
 /**
