86f34716ed3faf3325967b30f70c10ae6f84399e
deguzmankevin <deguzmankevin@hotmail.com>
2018-08-10 12:23:24 -0400

    adds VoterRegStatusBlock component

---
+++ resources/assets/components/pages/AccountPage/VoterRegStatusBlock.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import checkmark from './checkmark.svg';
+import rejectedIcon from './rejectedIcon.svg';
+import pendingIcon from './pendingIcon.svg';
+
+const VoterRegStatusBlock = ({ status }) => {
+  if (status === 'PENDING') {
+    return (
+      <div className="voter-reg-yellow display-flex flex-align-center">
+        <img className="post-badge icon-clock" src={pendingIcon} alt="hello" />
+        <div className="margin-left-md">Your voter reg status is pending!</div>
+      </div>
+    );
+  }
+
+  if (
+    status === 'CONFIRMED' ||
+    status === 'REGISTER_FORM' ||
+    status === 'REGISTER_OVR'
+  ) {
+    return (
+      <div className="voter-reg-green display-flex flex-align-center">
+        <img className="post-badge icon-check" src={checkmark} alt="hello" />
+        <div className="margin-left-md">
+          Your voter reg status is confirmed! Woo!
+        </div>
+      </div>
+    );
+  }
+
+  // if (status === null || status === 'INELIGIBLE' || status === 'UNCERTAIN') {
+
+  return (
+    <div className="voter-reg-red display-flex flex-align-center">
+      <img className="post-badge icon-x" src={rejectedIcon} alt="hello" />
+      <div className="margin-left-md">
+        We {"don't"} have your voter registration. Register here!
+      </div>
+    </div>
+  );
+};
+
+VoterRegStatusBlock.propTypes = {
+  status: PropTypes.string.isRequired,
+};
+
+export default VoterRegStatusBlock;
