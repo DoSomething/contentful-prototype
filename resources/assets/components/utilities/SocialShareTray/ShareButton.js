dde6f7e19a14be2eec8f69a6c115bef47e89caa1
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-08-20 15:57:08 -0400

    Add SocialShareTray utility component

---
+++ resources/assets/components/utilities/SocialShareTray/ShareButton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+const ShareButton = ({ className, onClick, disabled, icon, text }) => (
+  <button
+    className={classnames('button share-button margin-sm', className)}
+    onClick={onClick}
+    disabled={disabled}
+  >
+    <div className="icon-bounding-box">
+      <img src={icon} alt="share icon" />
+    </div>
+    <span className="button-text">{text}</span>
+  </button>
+);
+
+ShareButton.propTypes = {
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  icon: PropTypes.string.isRequired,
+  text: PropTypes.string,
+};
+
+ShareButton.defaultProps = {
+  className: null,
+  onClick: () => {},
+  disabled: false,
+  text: 'Share',
+};
+
+export default ShareButton;
