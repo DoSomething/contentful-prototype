b94a706443222b6851919e000fed4fb95448754d
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-08-31 12:30:50 -0400

    Remove outdated property docblock

--- app/Entities/Campaign.php
+++ app/Entities/Campaign.php
@@ -7,15 +7,6 @@ use Contentful\Delivery\Asset;
 
 /**
  * The Campaign entity.
- *
- * @property string $title
- * @property string $callToAction
- * @property Asset $coverImage
- * @property array $problemFact
- * @property array $solutionFact
- * @property array $solutionStatement
- * @property array $facts
- * @property string $faqs
  */
 class Campaign extends Entity implements JsonSerializable
 {
