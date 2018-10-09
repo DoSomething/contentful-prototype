03589456e6b1f1c0e37920228f3ec39ac1b514b8
Diego Lorenzo <diego@diegolorenzo.com>
2018-10-09 14:41:05 -0400

    Getting rid of old PostGallery no longer in use.

--- resources/assets/components/Gallery/PostGallery/index.js
+++
@@ -1,8 +0,0 @@
-export PostGallery from './PostGallery';
-
-export PostGalleryContainer from './PostGalleryContainer';
-
-/**
- * By default we prefer to export the container.
- */
-export default from './PostGalleryContainer';
