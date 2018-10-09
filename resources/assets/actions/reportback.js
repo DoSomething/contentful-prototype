78442186ce7bb0d79bc5ee2e7ca89f11c9749a91
Diego Lorenzo <diego@diegolorenzo.com>
2018-10-09 10:58:54 -0400

    Removing old reportbacks code and route (#1122)
    * Removing unused code.
    
    * Removing old /next/reportbacks resource route and controller
    
    * Removing more unused methods from reportback actions.
    
    * ESLint fixes.

--- resources/assets/actions/reportback.js
+++ resources/assets/actions/reportback.js
@@ -1,5 +1,3 @@
-/* global window, document */
-
 import { Phoenix } from '@dosomething/gateway';
 import { has, get } from 'lodash';
 
@@ -10,14 +8,6 @@ import {
   RECEIVED_REPORTBACKS,
   REACTION_CHANGED,
   REACTION_COMPLETE,
-  STORE_REPORTBACK_PENDING,
-  STORE_REPORTBACK_FAILED,
-  STORE_REPORTBACK_SUCCESSFUL,
-  ADD_SUBMISSION_METADATA,
-  ADD_SUBMISSION_ITEM_TO_LIST,
-  REQUESTED_USER_SUBMISSIONS,
-  REQUESTED_USER_SUBMISSIONS_FAILED,
-  RECEIVED_USER_SUBMISSIONS,
   queueEvent,
 } from '../actions';
 
@@ -57,59 +47,6 @@ export function reactionChanged(reportbackItemId, value) {
 // Action: reaction successfully created or updated.
 export function reactionComplete(reportbackItemId, reactionId) {
   return { type: REACTION_COMPLETE, reportbackItemId, reactionId };
-}
-
-// Action: store new user submitted reportback.
-export function storeReportback(reportback) {
-  return {
-    type: STORE_REPORTBACK_PENDING,
-    reportback,
-  };
-}
-
-// Action: storing new user submitted reportback failed.
-export function storeReportbackFailed(error) {
-  return {
-    type: STORE_REPORTBACK_FAILED,
-    error,
-  };
-}
-
-// Action: storing new user submitted reportback was successful.
-export function storeReportbackSuccessful(reportbackAffirmation) {
-  return {
-    type: STORE_REPORTBACK_SUCCESSFUL,
-    reportbackAffirmation,
-  };
-}
-
-export function requestingUserReportbacks() {
-  return { type: REQUESTED_USER_SUBMISSIONS };
-}
-
-export function requestingUserReportbacksFailed() {
-  return { type: REQUESTED_USER_SUBMISSIONS_FAILED };
-}
-
-export function receivedUserReportbacks() {
-  return { type: RECEIVED_USER_SUBMISSIONS };
-}
-
-// Action: add user reportback submission metadata to submissions store.
-export function addSubmissionMetadata(reportback, id) {
-  return {
-    type: ADD_SUBMISSION_METADATA,
-    reportback,
-    id,
-  };
-}
-
-// Action: add user reportback item submission to submissions store list.
-export function addSubmissionItemToList(reportbackItem) {
-  return {
-    type: ADD_SUBMISSION_ITEM_TO_LIST,
-    reportbackItem,
-  };
 }
 
 // Async Action: user reacted to a photo.
@@ -153,88 +90,6 @@ export function toggleReactionOff(reportbackItemId, reactionId) {
         dispatch(reactionComplete(reportbackItemId, null));
       })
       .catch(() => dispatch(reactionChanged(reportbackItemId, true)));
-  };
-}
-
-// Async Action: submit a new reportback and place in submissions gallery.
-export function submitReportback(url, reportback) {
-  return dispatch => {
-    dispatch(storeReportback(reportback));
-
-    const token = document.querySelector('meta[name="csrf-token"]');
-
-    // @TODO: Refactor once update to Gateway JS is made
-    // to allow overriding header configs properly.
-    return window
-      .fetch(url, {
-        method: 'POST',
-        headers: {
-          'X-CSRF-Token': token ? token.getAttribute('content') : null,
-          Accept: 'application/json',
-        },
-        credentials: 'same-origin',
-        body: reportback.formData,
-      })
-      .then(response => {
-        if (response.status >= 300) {
-          response.json().then(json => {
-            dispatch(storeReportbackFailed(json));
-          });
-        } else {
-          dispatch(storeReportbackSuccessful());
-
-          response.json().then(json => {
-            dispatch(addSubmissionMetadata(reportback, json.shift()));
-            dispatch(addSubmissionItemToList(reportback));
-          });
-        }
-      })
-      .catch(error => console.log(error));
-  };
-}
-
-export function submitReferralPost(post) {
-  const url = `${window.location.origin}/next/referrals`;
-  return submitReportback(url, post);
-}
-
-export function submitPhotoPost(post) {
-  const url = `${window.location.origin}/next/reportbacks`;
-  return submitReportback(url, post);
-}
-
-export function fetchUserReportbacks(userId, campaignId, campaignRunId) {
-  if (!userId) {
-    return dispatch => {
-      dispatch(requestingUserReportbacksFailed());
-    };
-  }
-
-  return dispatch => {
-    dispatch(requestingUserReportbacks());
-
-    return new Phoenix()
-      .get('next/signups', {
-        campaigns: campaignId,
-        users: userId,
-        runs: campaignRunId,
-      })
-      .then(json => {
-        dispatch(receivedUserReportbacks());
-
-        if (json.data.length) {
-          const reportback = json.data.shift().reportback;
-
-          if (!reportback) {
-            return;
-          }
-
-          dispatch(addSubmissionMetadata(reportback));
-          reportback.reportback_items.data.forEach(reportbackItem => {
-            dispatch(addSubmissionItemToList(reportbackItem));
-          });
-        }
-      });
   };
 }
 
