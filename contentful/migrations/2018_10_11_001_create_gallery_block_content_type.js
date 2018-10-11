442179393b7d0d244d11ca4f4febb81b8a7d5a3f
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-10-11 12:15:56 -0400

    Add GalleryBlock Contentful migration script

---
+++ contentful/migrations/2018_10_11_001_create_gallery_block_content_type.js
@@ -0,0 +1,37 @@
+module.exports = function(migration) {
+  const galleryBlock = migration
+    .createContentType('galleryBlock')
+    .name('Gallery Block')
+    .description('Displays a gallery of referenced entries')
+    .displayField('internalTitle');
+
+  galleryBlock
+    .createField('internalTitle')
+    .name('Internal Title')
+    .type('Symbol')
+    .required(true)
+    .localized(false);
+
+  galleryBlock
+    .createField('title')
+    .name('Title')
+    .type('Symbol')
+    .required(false)
+    .localized(true);
+
+  galleryBlock
+    .createField('blocks')
+    .name('Blocks')
+    .type('Array')
+    .items({
+      type: 'Link',
+      validations: [
+        {
+          linkContentType: ['person', 'campaign'],
+        },
+      ],
+      linkType: 'Entry',
+    })
+    .required(true)
+    .localized(false);
+};
