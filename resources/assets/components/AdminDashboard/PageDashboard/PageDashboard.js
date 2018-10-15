2a6507ac6cfafca90520e39434ae050731309d4b
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-10-15 10:22:56 -0400

    Page cache clearing fix (#1133)
    * Replace forward slashes with underscores for the slug cache key for pages
    
    * Format slug by replacing forward slashes with underscores for correct cache key

--- resources/assets/components/AdminDashboard/PageDashboard/PageDashboard.js
+++ resources/assets/components/AdminDashboard/PageDashboard/PageDashboard.js
@@ -7,7 +7,9 @@ const PageDashboard = ({ slug }) => (
   <div>
     <a
       className="button -secondary margin-md"
-      href={`/next/cache/page_${slug}?redirect=${window.location.pathname}`}
+      href={`/next/cache/page_${slug.replace(/\//g, '_')}?redirect=${
+        window.location.pathname
+      }`}
     >
       Clear Cache
     </a>
