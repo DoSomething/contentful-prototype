c7ac7fcc2a7f2d0bc9d8f55681c8f4659fc5f141
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-10-16 12:42:08 -0400

    Update GalleryBlock

--- resources/assets/components/blocks/GalleryBlock/GalleryBlock.js
+++ resources/assets/components/blocks/GalleryBlock/GalleryBlock.js
@@ -1,43 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Figure } from '../../Figure';
+import Person from '../../Person/Person';
 import Gallery from '../../utilities/Gallery/Gallery';
-import { contentfulImageUrl } from '../../../helpers';
 
 const renderBlock = block => {
-  let imageUrl;
-  let text;
-
   switch (block.type) {
     case 'person':
-      imageUrl = block.fields.photo;
-      text = block.fields.name;
-      break;
+      return <Person key={block.id} json={block} />;
 
     case 'campaign':
-      imageUrl = block.coverImage.url;
-      text = block.title;
-      break;
+      return null;
 
     default:
       return null;
   }
-
-  return (
-    <Figure
-      key={block.id}
-      alt={`${block.type}-${text}`}
-      image={contentfulImageUrl(imageUrl, '400', '400', 'fill')}
-    >
-      <p className="league-gothic">{text}</p>
-    </Figure>
-  );
 };
 
 const GalleryBlock = ({ title, blocks }) => (
   <div className="gallery-block">
-    {title ? <h1 className="text-centered">{title}</h1> : null}
+    {title ? <h1>{title}</h1> : null}
 
     <Gallery type="triad">{blocks.map(renderBlock)}</Gallery>
   </div>
