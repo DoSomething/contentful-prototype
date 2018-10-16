5aebc29922fd6181aa0a3efc01512eafd45855d1
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-10-16 17:07:53 -0400

    Add case for 'board member'

--- resources/assets/components/Person/Person.js
+++ resources/assets/components/Person/Person.js
@@ -2,11 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import StaffTemplate from './templates/StaffTemplate/StaffTemplate';
+import BoardTemplate from './templates/BoardTemplate/BoardTemplate';
 
 const Person = ({ json }) => {
   switch (json.fields.type) {
     case 'staff':
       return <StaffTemplate {...json.fields} />;
+
+    case 'board member':
+      return <BoardTemplate {...json.fields} />;
+
     default:
       return null;
   }
