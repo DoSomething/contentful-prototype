5f00570161aedfec652b4676b398bfe182156cad
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-10-19 15:37:55 -0400

    Gallery Block - Advisory board template (#1147)
    * Add advisory board template
    
    * Add case for rendering People of type `advisory board member`

--- resources/assets/components/Person/Person.js
+++ resources/assets/components/Person/Person.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import StaffTemplate from './templates/StaffTemplate/StaffTemplate';
 import BoardMemberTemplate from './templates/BoardMemberTemplate/BoardMemberTemplate';
+import AdvisoryBoardMemberTemplate from './templates/AdvisoryBoardMemberTemplate/AdvisoryBoardMemberTemplate';
 
 const Person = props => {
   switch (props.type) {
@@ -11,6 +12,9 @@ const Person = props => {
 
     case 'board member':
       return <BoardMemberTemplate {...props} />;
+
+    case 'advisory board member':
+      return <AdvisoryBoardMemberTemplate {...props} />;
 
     default:
       return null;
