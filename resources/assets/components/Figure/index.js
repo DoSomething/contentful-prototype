337094d6571d99b6a7355ebd3f7d54150ebeae13
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-10-23 15:42:01 -0400

    Remove unused helper import

--- resources/assets/components/Figure/index.js
+++ resources/assets/components/Figure/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-import { modifiers, isExternal } from '../../helpers';
+import { modifiers } from '../../helpers';
 import LazyImage from '../utilities/LazyImage';
 
 import './figure.scss';
