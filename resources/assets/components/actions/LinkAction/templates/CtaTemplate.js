77d8f1613c035efd742fb9b3ab6e191d0aafc19f
mendelB <mendelblesofsky@gmail.com>
2018-10-26 14:51:15 -0400

    Add dynamicString parsing for links, with the source prop

--- resources/assets/components/actions/LinkAction/templates/CtaTemplate.js
+++ resources/assets/components/actions/LinkAction/templates/CtaTemplate.js
@@ -4,10 +4,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Card from '../../../utilities/Card/Card';
-import { isExternal } from '../../../../helpers';
 import Button from '../../../utilities/Button/Button';
 import Markdown from '../../../utilities/Markdown/Markdown';
 import { trackPuckEvent } from '../../../../helpers/analytics';
+import { isExternal, dynamicString } from '../../../../helpers';
 
 import './cta-template.scss';
 
@@ -16,30 +16,35 @@ const onLinkClick = link => {
   trackPuckEvent('clicked link action', { link });
 };
 
-const CtaTemplate = props => (
-  <Card className="cta-template rounded padded text-centered bg-black dark caps-lock">
-    <h3 className="cta-template__title margin-top-lg">{props.title}</h3>
+const CtaTemplate = ({ title, content, link, buttonText, source }) => {
+  const href = dynamicString(link, {
+    source,
+  });
 
-    <Markdown className="cta-template__content">{props.content}</Markdown>
+  return (
+    <Card className="cta-template rounded padded text-centered bg-black dark caps-lock">
+      <h3 className="cta-template__title margin-top-lg">{title}</h3>
 
-    <Button
-      className="margin-vertical-md"
-      onClick={() => onLinkClick(props.link)}
-    >
-      {props.buttonText}
-    </Button>
-  </Card>
-);
+      <Markdown className="cta-template__content">{content}</Markdown>
+
+      <Button className="margin-vertical-md" onClick={() => onLinkClick(href)}>
+        {buttonText}
+      </Button>
+    </Card>
+  );
+};
 
 CtaTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
   buttonText: PropTypes.string,
+  source: PropTypes.string,
 };
 
 CtaTemplate.defaultProps = {
   buttonText: "Let's do this!",
+  source: 'web',
 };
 
 export default CtaTemplate;
