78d64f234e6497873a82bf284b72f3b7013d1ebb
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-10-29 17:10:50 -0400

    Gallery Block Image Alignment field (#1163)
    * Add image alignment field
    
    * Add image alignment field
    
    * fix the content type id

--- contentful/content-types/galleryBlock.js
+++ contentful/content-types/galleryBlock.js
@@ -43,6 +43,14 @@ module.exports = function(migration) {
     .localized(false);
 
   galleryBlock
+    .createField('imageAlignment')
+    .name('Image Alignment')
+    .type('Symbol')
+    .validations([{ in: ['top', 'left'] }])
+    .required(true)
+    .localized(false);
+
+  galleryBlock
     .createField('itemsPerRow')
     .name('Items Per Row')
     .type('Integer')
@@ -53,6 +61,11 @@ module.exports = function(migration) {
     ])
     .required(true)
     .localized(false);
+
+  galleryBlock.changeEditorInterface('imageAlignment', 'radio', {
+    helpText:
+      "Determines where the gallery item's images are aligned relative to their text.",
+  });
 
   galleryBlock.changeEditorInterface('itemsPerRow', 'radio', {
     helpText:
