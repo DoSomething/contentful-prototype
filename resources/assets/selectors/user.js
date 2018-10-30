9c0f84ee49283cb22b03b4c555d32dced1c1fed3
Diego Lorenzo <diego@diegolorenzo.com>
2018-10-30 14:04:59 -0400

    Adding new getUserToken selector function.

--- resources/assets/selectors/user.js
+++ resources/assets/selectors/user.js
@@ -1,8 +1,8 @@
 /**
  * Get the user id from the state.
  *
- * @param state
- * @returns {String}
+ * @param  {Object} state
+ * @return {String}
  */
 export function isAuthenticated(state) {
   return state.user.isAuthenticated;
@@ -11,19 +11,29 @@ export function isAuthenticated(state) {
 /**
  * Get the user id from the state.
  *
- * @param state
- * @returns {String}
+ * @param  {Object} state
+ * @return {String}
  */
 export function getUserId(state) {
   return state.user.id;
 }
 
 /**
+ * Get the user token from the state.
+ *
+ * @param  {Object} state
+ * @return {string}
+ */
+export function getUserToken(state) {
+  return state.user.token;
+}
+
+/**
  * Find out if the user had one of provided roles.
  *
- * @param {object} state
- * @param {array}  roles
- * @returns {boolean}
+ * @param  {Object} state
+ * @param  {Array}  roles
+ * @return {Boolean}
  */
 export function userHasRole(state, roles) {
   return roles.includes(state.user.role);
