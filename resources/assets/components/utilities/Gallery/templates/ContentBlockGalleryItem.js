600a5714c5fc7b21fa9ee592647b972d0956a45c
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-10-31 11:48:10 -0400

    Add imageAlignment logic

--- resources/assets/components/utilities/Gallery/templates/ContentBlockGalleryItem.js
+++ resources/assets/components/utilities/Gallery/templates/ContentBlockGalleryItem.js
@@ -2,20 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Figure } from '../../../Figure';
+import { contentfulImageUrl } from '../../../../helpers';
 import Markdown from '../../../utilities/Markdown/Markdown';
 
-const ContentBlockGalleryItem = ({ title, image, content }) => (
-  <Figure alt={`${title}-photo`} image={image}>
-    <h3>{title}</h3>
+const ContentBlockGalleryItem = ({ title, image, content, imageAlignment }) => {
+  // Image formatting needs to be smaller if they are left-aligned.
+  const imageFormatting = imageAlignment === 'left' ? '100' : '400';
+  // Ensure we don't pass the unsupported 'top' as the alignment prop to Figure.
+  const alignment = imageAlignment === 'top' ? null : imageAlignment;
 
-    {content ? <Markdown>{content}</Markdown> : null}
-  </Figure>
-);
+  return (
+    <Figure
+      alt={`${title}-photo`}
+      image={contentfulImageUrl(
+        image,
+        imageFormatting,
+        imageFormatting,
+        'fill',
+      )}
+      alignment={alignment}
+    >
+      <h3>{title}</h3>
+
+      {content ? <Markdown>{content}</Markdown> : null}
+    </Figure>
+  );
+};
 
 ContentBlockGalleryItem.propTypes = {
   title: PropTypes.string.isRequired,
   image: PropTypes.string,
   content: PropTypes.string.isRequired,
+  imageAlignment: PropTypes.oneOf(['top', 'left']).isRequired,
 };
 
 ContentBlockGalleryItem.defaultProps = {
