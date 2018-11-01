f0db3014cd4da301b5299e628f70996c6faff2dd
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-11-01 12:13:21 -0400

    Campaign Gallery Item Template (#1160)
    * Add Campaign Gallery Item component
    
    * Update case for gallery item of type campaign
    
    * Fix class name

---
+++ resources/assets/components/utilities/Gallery/templates/CampaignGalleryItem/CampaignGalleryItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Figure } from '../../../../Figure';
+import { contentfulImageUrl } from '../../../../../helpers';
+
+import './campaign-gallery-item.scss';
+
+const CampaignGalleryItem = ({ title, tagline, coverImage, slug }) => (
+  <a
+    className="campaign-gallery-item display-block"
+    href={`/us/campaigns/${slug}`}
+  >
+    <Figure
+      alt={`${coverImage.description || title}-photo`}
+      image={contentfulImageUrl(coverImage.url, '400', '400', 'fill')}
+    >
+      <h3>{title}</h3>
+
+      {tagline ? <p className="description">{tagline}</p> : null}
+    </Figure>
+  </a>
+);
+
+CampaignGalleryItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  tagline: PropTypes.string.isRequired,
+  coverImage: PropTypes.shape({
+    url: PropTypes.string,
+    description: PropTypes.string,
+  }).isRequired,
+  slug: PropTypes.string.isRequired,
+};
+
+export default CampaignGalleryItem;
