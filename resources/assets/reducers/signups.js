be745c7b48217cabeabfb2cc9593a44d48f2ec7e
Diego Lorenzo <diego@diegolorenzo.com>
2018-11-01 16:38:41 -0400

    Switching to use Boolean() to simplify logic.

--- resources/assets/reducers/signups.js
+++ resources/assets/reducers/signups.js
@@ -45,7 +45,7 @@ const signupReducer = (state = {}, action) => {
         ...state,
         data: signups,
         isPending: false,
-        thisCampaign: data.length ? true : false, // eslint-disable-line no-unneeded-ternary
+        thisCampaign: Boolean(data.length),
       };
 
     case SIGNUP_CREATED:
