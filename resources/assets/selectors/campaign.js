9a3491d55aeaa54edcdac8904e1ea96703e249a1
Diego Lorenzo <diego@diegolorenzo.com>
2018-11-06 17:10:15 -0500

    Adding new campaign selector

---
+++ resources/assets/selectors/campaign.js
@@ -0,0 +1,11 @@
+/**
+ * Get the Campaign additional content from the state.
+ *
+ * @param  {Object} state
+ * @return {Object}
+ */
+export function getCampaignAdditionalContent(state) {
+  return state.campaign.additionalContent;
+}
+
+export default null;
