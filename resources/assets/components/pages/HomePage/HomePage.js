c6a256eebbc054975276f4507b02015b1a7b793d
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-11-15 11:40:27 -0500

    Use window.HOMEPAGE as local component state

--- resources/assets/components/pages/HomePage/HomePage.js
+++ resources/assets/components/pages/HomePage/HomePage.js
@@ -1,5 +1,18 @@
+/* global window */
+
 import React from 'react';
+import { get } from 'lodash';
 
 const HomePage = () => <h1>Home Page</h1>;
+class HomePage extends React.Component {
+  constructor() {
+    super();
+    this.state = get(window.HOMEPAGE, 'fields', {});
+  }
+
+  render() {
+    return this.state.title ? <h1>{this.state.title}</h1> : null;
+  }
+}
 
 export default HomePage;
