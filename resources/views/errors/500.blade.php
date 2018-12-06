019f932fd3cc1f5ddfdc9169b4a96eb9b6346a95
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-12-06 15:32:02 -0500

    Update the copy

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -12,9 +12,11 @@
                     </div>
                     <div class="container__block -centered">
                         <p>
-                            Try returning to <a href="{{ url('/us/campaigns/sincerely-us') }}">Sincerely Us</a>, and
-                            doing the same thing again - it may work the second time! If not, we've already noted
+                            Try doing the same thing again - it may work the second time! If not, we've already noted
                             the problem and our tech team will get it fixed as soon as possible!
+
+                            You can also try <a href="{{ url('/us/campaigns/grab-mic?utm_source=500') }}">Grab the Mic</a>
+                            and join our movement to create the most civically active generation ever.
                         </p>
                         <p class="footnote">
                             If you continue to run into problems, contact our <a href="https://help.dosomething.org">support squad</a>!
