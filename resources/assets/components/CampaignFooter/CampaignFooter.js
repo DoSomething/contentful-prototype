ca51a73b15be012bc837ebbbbc4fd84f5c351327
Diego Lorenzo <diego@diegolorenzo.com>
2018-12-07 12:20:04 -0500

    Moving AffiliateCredits to utilities and removing index file.

--- resources/assets/components/CampaignFooter/CampaignFooter.js
+++ resources/assets/components/CampaignFooter/CampaignFooter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import AffiliateCredits from '../AffiliateCredits';
+import AffiliateCredits from '../utilities/AffiliateCredits/AffiliateCredits';
 
 const CampaignFooterContact = ({ email }) => {
   const contactEmail = email || CampaignFooterContact.defaultProps.email;
