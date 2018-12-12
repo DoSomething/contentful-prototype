f570fb75d09c6d723644e5d83cf5c398f2316663
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-12-12 15:10:11 -0500

    Updated Analytics Tracking Method (#1205)
    * Add app name local constant
    
    * Add event name parser helper
    
    * Format docblock
    
    * Add beta general analytics event tracker
    
    * Add service event dispatcher helper method
    
    * Use helper method to dispatch event to service/s
    
    * Update to use param name
    
    * Move dispatch helper beneath its invoked methods
    
    * Rename app name constant
    
    * Add default null value for optional adjective param
    
    * Update usages of changes app name constant
    
    * Update erroneous doc Block
    
    * Update docblock for data param
    
    * Add safeguard object in case of falsey nameParams arg
    
    * Switch to options object parameter
    
    * Rename event name formatter helper
    
    * Update service dispatching method name so it doesnt clash w redux
    
    * Use destructured object for options argument
    
    * SnakeCase all the event name params
    
    * Update dockblock
    
    * Update docblocks

--- resources/assets/helpers/analytics.js
+++ resources/assets/helpers/analytics.js
@@ -1,5 +1,6 @@
 /* global window */
 
+import { snakeCase } from 'lodash';
 import { Engine as PuckClient } from '@dosomething/puck-client';
 import {
   dimensionByCookie,
@@ -10,6 +11,25 @@ import {
 
 import { PUCK_URL } from '../constants';
 import { get as getHistory } from '../history';
+
+// App name prefix used for event naming.
+const APP_PREFIX = 'phoenix';
+
+/**
+ * Parse analytics event name parameters into a snake cased string.
+ *
+ * @param  {String}      verb
+ * @param  {String}      noun
+ * @param  {String|Null} [adjective=null]
+ * @return {void}
+ */
+const formatEventName = (verb, noun, adjective = null) => {
+  let eventName = `${APP_PREFIX}_${snakeCase(verb)}_${snakeCase(noun)}`;
+  // Append adjective if defined.
+  eventName += adjective ? `_${snakeCase(adjective)}` : '';
+
+  return eventName;
+};
 
 /**
  * Send event to analyze with Google Analytics.
@@ -52,6 +72,31 @@ export function analyzeWithPuck(name, data) {
 }
 
 /**
+ * Dispatch analytics event to specified service, or all services by default.
+ *
+ * @param  {String}      category
+ * @param  {String}      name
+ * @param  {Object|Null} [data]
+ * @param  {String|Null} [service]
+ * @return {void}
+ */
+const sendToServices = (category, name, data, service) => {
+  switch (service) {
+    case 'ga':
+      analyzeWithGoogleAnalytics(category, name);
+      break;
+
+    case 'puck':
+      analyzeWithPuck(name, data);
+      break;
+
+    default:
+      analyzeWithGoogleAnalytics(category, name);
+      analyzeWithPuck(name, data);
+  }
+};
+
+/**
  * Watch the given parameters for changes in their state
  * and record it to Google Analytics.
  */
@@ -91,6 +136,37 @@ export function trackAnalyticsEvent(name, data, service) {
         `The "${service}" service is missing, not supported or incorrect!`,
       );
   }
+}
+
+/**
+ * Track an analytics event with a specified service. (Defaults to tracking with all services.)
+ *
+ * @param  {Object}      options
+ * @param  {String}      options.verb
+ * @param  {String}      options.noun
+ * @param  {String}      [options.adjective]
+ * @param  {Object|Null} [options.data]
+ * @param  {String|Null} [options.service]
+ * @return {void}
+ */
+export function trackAnalyticsEventBeta({
+  verb,
+  noun,
+  adjective,
+  data,
+  service,
+}) {
+  if (!verb || !noun) {
+    console.error('The Verb or Noun is missing!');
+    return;
+  }
+
+  const eventName = formatEventName(verb, noun, adjective);
+
+  // Define category parameter for Google Analytics.
+  const category = `${APP_PREFIX}_${snakeCase(noun)}`;
+
+  sendToServices(category, eventName, data, service);
 }
 
 /**
