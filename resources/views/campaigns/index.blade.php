c0bc11dd7cb4e2ee119a217e9988615036171e21
Diego Lorenzo <diego@diegolorenzo.com>
2018-12-17 16:51:19 -0500

    Applying CSS grid to the explore campaigns page.

--- resources/views/campaigns/index.blade.php
+++ resources/views/campaigns/index.blade.php
@@ -6,27 +6,28 @@
             <div class="container__block">
                 <h1>Explore Campaigns</h1>
             </div>
-            <ul class="gallery-grid">
+            <ul class="gallery-grid-quartet">
                 @foreach($campaigns as $item)
                     <li>
-                        <article class="tile">
+                        <article class="gallery-item">
                             <a class="wrapper" href="{{ url('us/campaigns/'.$item->slug) }}">
-                                <div class="tile__meta">
-                                    <h1 class="tile__title">{{ $item->title }}</h1>
-                                </div>
                                 @if ($item->coverImage)
                                     <img alt="{{ $item->title }}" src="{{ $item->coverImage->url }}?w=400&h=400&fit=fill" />
                                 @else
                                     <img alt="No cover image!" src="data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=" />
                                 @endif
+
+                                <div class="gallery-item__meta">
+                                    <h1 class="gallery-item__title">{{ $item->title }}</h1>
+                                    <p class="gallery-item__tagline">{{ $item->tagline }}</p>
+                                </div>
                             </a>
                         </article>
-                        <p>{{ $item->tagline }}</p>
                     </li>
                 @endforeach
             </ul>
 
-            <div class="pagination clearfix">
+            <div class="pagination clearfix margin-top-md">
                 @if($previousPage > 0)
                     <a class="button -tertiary float-left clear-none" href="?page={{$previousPage}}">Previous</a>
                 @endif
