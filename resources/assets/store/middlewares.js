63aa30fceabcc4c8a7384b2119c43ad85b8082f8
Diego Lorenzo <diego@diegolorenzo.com>
2019-01-11 10:49:11 -0500

    Adding useful comments.

--- resources/assets/store/middlewares.js
+++ resources/assets/store/middlewares.js
@@ -4,9 +4,14 @@ import sixpackExperimentsMiddleware from '../middleware/sixpackExperiments';
 import requiresAuthenticationMiddleware from '../middleware/requiresAuthentication';
 
 const middlewares = [
+  // Event data collection for analysis needs to execute first.
   analyticsMiddleware,
   sixpackExperimentsMiddleware,
+
+  // Authentication executes after event data collection, since it could redirect user out of app.
   requiresAuthenticationMiddleware,
+
+  // User requests and activity execute last.
   apiMiddleware,
 ];
 
