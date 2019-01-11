da16c81601dc9738c9fcdc82743a0a6b041f1326
Diego Lorenzo <diego@diegolorenzo.com>
2019-01-11 11:00:59 -0500

    Updating middleware order of operations. (#1223)
    * Updating middleware order of operations.
    
    * Adding useful comments.

--- resources/assets/store/middlewares.js
+++ resources/assets/store/middlewares.js
@@ -4,10 +4,15 @@ import sixpackExperimentsMiddleware from '../middleware/sixpackExperiments';
 import requiresAuthenticationMiddleware from '../middleware/requiresAuthentication';
 
 const middlewares = [
-  requiresAuthenticationMiddleware,
+  // Event data collection for analysis needs to execute first.
   analyticsMiddleware,
-  apiMiddleware,
   sixpackExperimentsMiddleware,
+
+  // Authentication executes after event data collection, since it could redirect user out of app.
+  requiresAuthenticationMiddleware,
+
+  // User requests and activity execute last.
+  apiMiddleware,
 ];
 
 export default middlewares;
