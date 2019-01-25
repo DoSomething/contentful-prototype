e9a34c19fa1214327f6f1c78149996918e595fd0
mendelB <mendelblesofsky@gmail.com>
2019-01-25 13:28:13 -0500

    Use error report method for puck errors

--- resources/assets/components/App.js
+++ resources/assets/components/App.js
@@ -10,6 +10,7 @@ import { env } from '../helpers';
 import graphqlClient from '../graphql';
 import { initializeStore } from '../store/store';
 import HomePage from './pages/HomePage/HomePage';
+import { puckErrorReport } from '../helpers/analytics';
 import CampaignContainer from './Campaign/CampaignContainer';
 import { getUserId, isAuthenticated } from '../selectors/user';
 import AccountContainer from './pages/AccountPage/AccountContainer';
@@ -28,6 +29,7 @@ const App = ({ store, history }) => {
         isAuthenticated={() => isAuthenticated(state)}
         history={history}
         puckUrl={env('PUCK_URL')}
+        onError={puckErrorReport}
       >
         <ApolloProvider client={graphqlClient(env('GRAPHQL_URL'))}>
           <ConnectedRouter history={history}>
