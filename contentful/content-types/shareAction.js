8f87e83be8f0cf3b8821ab5765d55a90a6d7ebb7
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-02-11 11:59:09 -0500

    Add Action ID field to RB Action Content Types (#1250)
    * Replace photoSubmissionAction /migrations file with /content-type migration script
    
    * Add shareAction contentful /content-type and remove related /migration scripts
    
    * Replace textSubmissionAction contentful /migration scripts with /content-type script
    
    * Add actionId field
    
    * Add punctuation to help text
    
    * Add actionId field

---
+++ contentful/content-types/shareAction.js
@@ -0,0 +1,186 @@
+module.exports = function(migration) {
+  const shareAction = migration
+    .createContentType('shareAction')
+    .name('Share Action')
+    .description(
+      'Used for adding a dedicated share action to the campaign. For info on how to use this component go to http://bit.ly/share-action',
+    )
+    .displayField('internalTitle');
+
+  shareAction
+    .createField('internalTitle')
+    .name('Internal Title')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  shareAction
+    .createField('actionId')
+    .name('Action ID')
+    .type('Integer')
+    .required(false)
+    .localized(false)
+    .validations([
+      {
+        unique: true,
+      },
+    ]);
+
+  shareAction
+    .createField('title')
+    .name('Title')
+    .type('Symbol')
+    .localized(true)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  shareAction
+    .createField('socialPlatform')
+    .name('Social Platform')
+    .type('Array')
+    .localized(false)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Symbol',
+
+      validations: [
+        {
+          in: ['facebook', 'twitter'],
+          message:
+            'Please select either Facebook or Twitter as the social platform for this Share Action',
+        },
+      ],
+    });
+
+  shareAction
+    .createField('content')
+    .name('Content')
+    .type('Text')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  shareAction
+    .createField('link')
+    .name('Link')
+    .type('Symbol')
+    .localized(false)
+    .required(true)
+    .validations([
+      {
+        regexp: {
+          pattern:
+            '^(ftp|http|https):\\/\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?(\\/|\\/([\\w#!:.?+=&%@!\\-\\/]))?$',
+        },
+      },
+    ])
+    .disabled(false)
+    .omitted(false);
+
+  shareAction
+    .createField('hideEmbed')
+    .name('Hide Embed')
+    .type('Boolean')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  shareAction
+    .createField('affirmationBlock')
+    .name('Affirmation Block')
+    .type('Link')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        linkContentType: [
+          'campaignUpdate',
+          'customBlock',
+          'linkAction',
+          'page',
+          'affirmation',
+          'photoUploaderAction',
+          'shareAction',
+          'voterRegistrationAction',
+        ],
+      },
+    ])
+    .disabled(false)
+    .omitted(false)
+    .linkType('Entry');
+
+  shareAction
+    .createField('affirmation')
+    .name('Affirmation Text')
+    .type('Text')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  shareAction
+    .createField('additionalContent')
+    .name('Additional Content')
+    .type('Object')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  shareAction.changeEditorInterface('internalTitle', 'singleLine', {});
+
+  shareAction.changeEditorInterface('actionId', 'numberEditor', {
+    helpText: 'The Action ID associated with this action in Rogue.',
+  });
+
+  shareAction.changeEditorInterface('title', 'singleLine', {
+    helpText: 'Eg: "Share this link"',
+  });
+
+  shareAction.changeEditorInterface('socialPlatform', 'checkbox', {
+    helpText:
+      "Select the social platform to be used by the Share Action's share button. (Limited to Facebook or Twitter).",
+  });
+
+  shareAction.changeEditorInterface('content', 'markdown', {
+    helpText: 'Descriptive content for the share action',
+  });
+
+  shareAction.changeEditorInterface('link', 'singleLine', {
+    helpText: 'Link to be shared',
+  });
+
+  shareAction.changeEditorInterface('hideEmbed', 'boolean', {
+    helpText: 'This will hide the link preview "embed" on the share action.',
+    trueLabel: 'Yes',
+    falseLabel: 'No',
+  });
+
+  shareAction.changeEditorInterface('affirmationBlock', 'entryLinkEditor', {
+    helpText:
+      'This block will be displayed in a modal after a successful share. It may be used to thank the user or prompt them to complete another action.',
+  });
+
+  shareAction.changeEditorInterface('affirmation', 'markdown', {
+    helpText:
+      'A quick text-only affirmation message. If an affirmation block is set (above), this will be ignored!',
+  });
+
+  shareAction.changeEditorInterface('additionalContent', 'objectEditor', {
+    helpText: '',
+  });
+};
---
+++ contentful/content-types/textSubmissionAction.js
@@ -0,0 +1,137 @@
+module.exports = function(migration) {
+  const textSubmissionAction = migration
+    .createContentType('textSubmissionAction')
+    .name('Text Submission Action')
+    .description('Action block for submitting text reportback posts.')
+    .displayField('internalTitle');
+
+  textSubmissionAction
+    .createField('internalTitle')
+    .name('Internal Title')
+    .type('Symbol')
+    .localized(false)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  textSubmissionAction
+    .createField('actionId')
+    .name('Action ID')
+    .type('Integer')
+    .required(false)
+    .localized(false)
+    .validations([
+      {
+        unique: true,
+      },
+    ]);
+
+  textSubmissionAction
+    .createField('title')
+    .name('Title')
+    .type('Symbol')
+    .localized(true)
+    .required(false)
+    .validations([
+      {
+        size: {
+          max: 65,
+        },
+      },
+    ])
+    .disabled(false)
+    .omitted(false);
+
+  textSubmissionAction
+    .createField('textFieldLabel')
+    .name('Text Field Label')
+    .type('Symbol')
+    .localized(true)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  textSubmissionAction
+    .createField('textFieldPlaceholder')
+    .name('Text Field Placeholder Message')
+    .type('Symbol')
+    .localized(true)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  textSubmissionAction
+    .createField('buttonText')
+    .name('Button Text')
+    .type('Symbol')
+    .localized(true)
+    .required(false)
+    .validations([
+      {
+        size: {
+          max: 25,
+        },
+      },
+    ])
+    .disabled(false)
+    .omitted(false);
+
+  textSubmissionAction
+    .createField('affirmationContent')
+    .name('Affirmation Content')
+    .type('Text')
+    .localized(true)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  textSubmissionAction
+    .createField('additionalContent')
+    .name('Additional Content')
+    .type('Object')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  textSubmissionAction.changeEditorInterface('internalTitle', 'singleLine', {});
+
+  textSubmissionAction.changeEditorInterface('actionId', 'numberEditor', {
+    helpText: 'The Action ID associated with this action in Rogue.',
+  });
+
+  textSubmissionAction.changeEditorInterface('title', 'singleLine', {});
+
+  textSubmissionAction.changeEditorInterface('textFieldLabel', 'singleLine', {
+    helpText:
+      'The label for the "text" field, helping describe or prompt the user regarding what to submit.',
+  });
+
+  textSubmissionAction.changeEditorInterface(
+    'textFieldPlaceholder',
+    'singleLine',
+    {
+      helpText:
+        'The placeholder for the "text" field, providing an example of what a text submission should look like.',
+    },
+  );
+
+  textSubmissionAction.changeEditorInterface('buttonText', 'singleLine', {});
+
+  textSubmissionAction.changeEditorInterface(
+    'affirmationContent',
+    'markdown',
+    {},
+  );
+
+  textSubmissionAction.changeEditorInterface(
+    'additionalContent',
+    'objectEditor',
+    {},
+  );
+};
