6ceb7422226db12988811dbfc0e7a5f73e65d988
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-02-20 13:02:40 -0500

    Set requiresAuthentication to true for text RBs

--- resources/assets/actions/post.js
+++ resources/assets/actions/post.js
@@ -105,6 +105,7 @@ export function storeCampaignPost(campaignId, data) {
           sixpackExperiments,
           type,
         },
+        requiresAuthentication: type === 'text',
         pending: POST_SUBMISSION_PENDING,
         success: POST_SUBMISSION_SUCCESSFUL,
         url,
