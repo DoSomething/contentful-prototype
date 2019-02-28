180bd4f95ce4c0bb114e8910975d1d10b1a26130
Diego Lorenzo <diego@diegolorenzo.com>
2019-02-28 10:51:04 -0500

    Adding coverImage support and styling.

--- resources/assets/components/pages/StoryPage/StoryPage.js
+++ resources/assets/components/pages/StoryPage/StoryPage.js
@@ -2,30 +2,58 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ContentfulEntry from '../../ContentfulEntry';
+import { contentfulImageUrl, withoutNulls } from '../../../helpers';
+
+import './story-page.scss';
 
 const StoryPage = props => {
-  const { blocks, subTitle, title } = props;
+  const { blocks, coverImage, subTitle, title } = props;
+
+  const backgroundImage = coverImage
+    ? `url(${contentfulImageUrl(coverImage.url, '1440', '610', 'fill')})`
+    : null;
+
+  const styles = {
+    backgroundImage,
+  };
+
+  console.log(styles);
 
   return (
-    <div>
-      <h1>{title}</h1>
-      {subTitle ? <h2>{subTitle}</h2> : null}
+    <article className="story-page">
+      <header
+        role="banner"
+        className="lede-banner"
+        style={withoutNulls(styles)}
+      >
+        <wrapper className="text-center">
+          <h1 className="lede-banner__headline-title color-white caps-lock">
+            {title}
+          </h1>
+          {subTitle ? <h2 className="color-yellow">{subTitle}</h2> : null}
+        </wrapper>
+      </header>
 
       {blocks.map(block => (
         <ContentfulEntry key={block.id} json={block} />
       ))}
-    </div>
+    </article>
   );
 };
 
 StoryPage.propTypes = {
   blocks: PropTypes.arrayOf(PropTypes.object),
+  coverImage: PropTypes.shape({
+    url: PropTypes.string,
+    descsription: PropTypes.string,
+  }),
   subTitle: PropTypes.string,
   title: PropTypes.string.isRequired,
 };
 
 StoryPage.defaultProps = {
   blocks: [],
+  coverImage: null,
   subTitle: null,
 };
 
