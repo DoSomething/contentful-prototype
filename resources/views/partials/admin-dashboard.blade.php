d669b50fef660aeb1ef586e463bbc83d7c0d4ccc
Diego Lorenzo <diego@diegolorenzo.com>
2019-03-06 18:46:59 -0500

    Adding Admin Dashboard output.

---
+++ resources/views/partials/admin-dashboard.blade.php
@@ -0,0 +1,6 @@
+<div id="admin-dashboard" class="admin-dashboard">
+    <div class="wrapper">
+        <a class="button -secondary" href="{{ $cacheUrl }}">Clear Cache</a>
+        <a id="js-admin-dashboard-toggle" class="toggle">@include('svg.bug-icon', ['class' => 'icon icon-bug'])</a>
+    </div>
+</div>
