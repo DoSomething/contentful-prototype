8949d09f92be6ed5e2c7d2b64b9bb70819e6de22
katiecrane <kcrane@dosomething.org>
2019-03-12 09:29:45 -0700

    Get mutation working and add all four topics

--- resources/assets/components/pages/AccountPage/EmailSubscriptions.js
+++ resources/assets/components/pages/AccountPage/EmailSubscriptions.js
@@ -10,7 +10,32 @@ import Button from '../../utilities/Button/Button';
 class EmailSubscriptions extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      emailSubscriptionTopics: this.props.user.emailSubscriptionTopics
+        ? this.props.user.emailSubscriptionTopics
+        : [],
+    };
+
+    this.handleTopicChange = this.handleTopicChange.bind(this);
+  }
+
+  handleTopicChange(event) {
+    const target = event.target;
+    const value = target.checked;
+
+    const name = target.name;
+
+    const newTopics = this.state.emailSubscriptionTopics;
+
+    if (value) {
+      newTopics.push(name);
+    } else {
+      newTopics.pop(name);
+    }
+
+    this.setState({
+      emailSubscriptionTopics: newTopics,
+    });
   }
 
   render() {
@@ -28,65 +53,110 @@ class EmailSubscriptions extends React.Component {
       }
     `;
 
-    <Mutation mutation={EMAIL_SUBSCRIPTIONS_MUTATION}>
-      {emailSubscriptionsMutation => {
-        const shutUp = 'face';
+    return (
+      <Mutation mutation={EMAIL_SUBSCRIPTIONS_MUTATION}>
+        {emailSubscriptionsMutation => {
+          return (
+            <form
+              onSubmit={event => {
+                event.preventDefault();
+                // determine if there should be topics here
+                emailSubscriptionsMutation({
+                  variables: {
+                    userId: this.props.user.id,
+                    emailSubscriptionTopics: this.state.emailSubscriptionTopics,
+                  },
+                });
+              }}
+            >
+              <div className="padded">
+                <div className="form-wrapper clear-both">
+                  <label className="option -checkbox" htmlFor="email_topics">
+                    <input
+                      type="checkbox"
+                      id="opt_in"
+                      name="NEWS"
+                      defaultChecked={
+                        this.props.user.emailSubscriptionTopics
+                          ? this.props.user.emailSubscriptionTopics.includes(
+                              'NEWS',
+                            )
+                          : false
+                      }
+                      className="form-checkbox"
+                      onChange={this.handleTopicChange}
+                    />
+                    <span className="option__indicator" />
+                    NEWS
+                  </label>
 
-        function handleTopicChange(event) {
-          const target = event.target;
-          const value = target.checked;
+                  <label className="option -checkbox" htmlFor="email_topics">
+                    <input
+                      type="checkbox"
+                      id="opt_in"
+                      name="SCHOLARSHIPS"
+                      defaultChecked={
+                        this.props.user.emailSubscriptionTopics
+                          ? this.props.user.emailSubscriptionTopics.includes(
+                              'SCHOLARSHIPS',
+                            )
+                          : false
+                      }
+                      className="form-checkbox"
+                      onChange={this.handleTopicChange}
+                    />
+                    <span className="option__indicator" />
+                    SCHOLARSHIPS
+                  </label>
 
-          const name = target.name;
+                  <label className="option -checkbox" htmlFor="email_topics">
+                    <input
+                      type="checkbox"
+                      id="opt_in"
+                      name="ACTIONS"
+                      defaultChecked={
+                        this.props.user.emailSubscriptionTopics
+                          ? this.props.user.emailSubscriptionTopics.includes(
+                              'ACTIONS',
+                            )
+                          : false
+                      }
+                      className="form-checkbox"
+                      onChange={this.handleTopicChange}
+                    />
+                    <span className="option__indicator" />
+                    ACTIONS
+                  </label>
 
-          this.setState({
-            [name]: value,
-          });
-        }
-
-        return (
-          <form
-            onSubmit={event => {
-              event.preventDefault();
-              const topics = [];
-              // determine if there should be topics here
-              emailSubscriptionsMutation({
-                variables: {
-                  userId: this.props.user.id,
-                  emailSubscriptionTopics: topics,
-                },
-              });
-            }}
-          >
-            <div className="padded">
-              <div className="form-wrapper affiliate-option clear-both">
-                <label className="option -checkbox" htmlFor="email_topics">
-                  <input
-                    type="checkbox"
-                    id="opt_in"
-                    name="news"
-                    defaultChecked={
-                      this.props.user.emailSubscriptionTopics
-                        ? this.props.user.emailSubscriptionTopics.includes(
-                            'NEWS',
-                          )
-                        : false
-                    }
-                    className="form-checkbox"
-                    onChange={handleTopicChange}
-                  />
-                  <span className="option__indicator" />
-                  NEWS
-                </label>
+                  <label className="option -checkbox" htmlFor="email_topics">
+                    <input
+                      type="checkbox"
+                      id="opt_in"
+                      name="LIFESTYLE"
+                      defaultChecked={
+                        this.props.user.emailSubscriptionTopics
+                          ? this.props.user.emailSubscriptionTopics.includes(
+                              'LIFESTYLE',
+                            )
+                          : false
+                      }
+                      className="form-checkbox"
+                      onChange={this.handleTopicChange}
+                    />
+                    <span className="option__indicator" />
+                    LIFESTYLE
+                  </label>
+                </div>
               </div>
-            </div>
 
-            <Button type="submit" attached>
-              Save subscriptions {shutUp}
-            </Button>
-          </form>
-        );
-      }}
-    </Mutation>;
+              <Button type="submit" attached>
+                Save subscriptions
+              </Button>
+            </form>
+          );
+        }}
+      </Mutation>
+    );
   }
 }
 
