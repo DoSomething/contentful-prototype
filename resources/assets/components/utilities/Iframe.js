c69b6b2ae1e56d73c4a90053ad2d5c7007043563
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-03-14 10:25:16 -0400

    Iframe State for Mobile View (#1308)
    * Add state for mobile (fetch and display preview image)
    
    * Add alt prop to LazyImage
    
    * Add some margin to the mobile image
    
    * Npm install react-media
    
    * Refactor Iframe with responsive toggling for small/big screen rendering

--- resources/assets/components/utilities/Iframe.js
+++ resources/assets/components/utilities/Iframe.js
@@ -1,19 +1,70 @@
 import React from 'react';
+import Media from 'react-media';
 import PropTypes from 'prop-types';
 
+import LazyImage from './LazyImage';
+import { getRequest } from '../../helpers/api';
 import ErrorBlock from '../ErrorBlock/ErrorBlock';
 
 const permittedHostnames = ['dosomething.carto.com'];
 
+class IframePreview extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      image: null,
+      hasError: false,
+    };
+  }
+
+  componentDidMount() {
+    getRequest('next/embed', { url: this.props.url })
+      .then(({ image }) =>
+        image ? this.setState({ image }) : this.setState({ hasError: true }),
+      )
+      .catch(() => this.setState({ hasError: true }));
+  }
+
+  render() {
+    return this.state.hasError ? (
+      <ErrorBlock />
+    ) : (
+      <div className="margin-horizontal-md">
+        <LazyImage
+          src={this.state.image}
+          alt="Preview image for embedded content"
+        />
+      </div>
+    );
+  }
+}
+
+IframePreview.propTypes = {
+  url: PropTypes.string.isRequired,
+};
+
 const Iframe = ({ id, url }) => {
-  const hostname = url && new URL(url).hostname;
+  const hostname = new URL(url).hostname;
 
   if (!permittedHostnames.includes(hostname)) {
     console.warn(`Invalid URL ${url} supplied to Iframe component`);
     return <ErrorBlock />;
   }
 
-  return <iframe title={`embed ${id}`} src={url} width="100%" height="520" />;
+  return (
+    <div id={id}>
+      <Media query="(max-width: 759px)">
+        {matches =>
+          matches ? (
+            <IframePreview url={url} />
+          ) : (
+            <iframe title={`embed ${id}`} src={url} width="100%" height="520" />
+          )
+        }
+      </Media>
+    </div>
+  );
 };
 
 Iframe.propTypes = {
