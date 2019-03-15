bdfa4fe2fccd3c8b638a2ef18017755f978bcaa8
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-03-15 15:15:53 -0400

    Ugh workaround for that pesky jsx-a11y bug

--- resources/assets/components/actions/TextSubmissionAction/TextSubmissionAction.js
+++ resources/assets/components/actions/TextSubmissionAction/TextSubmissionAction.js
@@ -150,6 +150,8 @@ class TextSubmissionAction extends React.Component {
               <p className="footnote padding-horizontal-md padding-bottom-md">
                 The data you submit in this form will be handled in accordance
                 with the DoSomething website{' '}
+                {/* Workaround for this jsx-a11y bug https://git.io/fN814 */}
+                {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
                 <a
                   href="https://www.dosomething.org/us/about/privacy-policy"
                   target="_blank"
