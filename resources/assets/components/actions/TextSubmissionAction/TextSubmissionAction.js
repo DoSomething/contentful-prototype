70b498fd9f430a3c3b3987bd28aa1b62a7e68d36
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-03-18 09:54:57 -0400

    Update Text Submission Action Disclaimer Content (#1318)
    * Update disclaimer content
    
    * Ugh workaround for that pesky jsx-a11y bug
    
    * Add todo to update jsx-a11y bug workaround

--- resources/assets/components/actions/TextSubmissionAction/TextSubmissionAction.js
+++ resources/assets/components/actions/TextSubmissionAction/TextSubmissionAction.js
@@ -148,8 +148,21 @@ class TextSubmissionAction extends React.Component {
                 {this.props.buttonText}
               </Button>
               <p className="footnote padding-horizontal-md padding-bottom-md">
-                Your submission will be reviewed by a DoSomething.org staffer
-                and added to our public gallery.
+                The data you submit in this form will be handled in accordance
+                with the DoSomething website{' '}
+                {/*
+                  Workaround for this jsx-a11y bug https://git.io/fN814.
+                  @TODO: Update once the eslint-config package is updated (https://git.io/fjejY).
+                */}
+                {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
+                <a
+                  href="https://www.dosomething.org/us/about/privacy-policy"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  privacy policy
+                </a>
+                . It will be reviewed by a DoSomething staff member.
               </p>
             </form>
           </Card>
