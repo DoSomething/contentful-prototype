1692e88b7c313be82fdaa6cd9e82b699eb40a780
Diego Lorenzo <diego@diegolorenzo.com>
2019-03-29 14:24:21 -0400

    Updating the PostGallery migration script. (#1348)

--- contentful/content-types/postGallery.js
+++ contentful/content-types/postGallery.js
@@ -10,38 +10,62 @@ module.exports = function(migration) {
     .name('Internal Title')
     .type('Symbol')
     .localized(false)
-    .required(true);
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
 
   postGallery
     .createField('actionIds')
     .name('Action IDs')
     .type('Array')
+    .localized(false)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
     .items({
       type: 'Symbol',
+
       validations: [
         {
           regexp: {
             pattern: '[0-9]+',
             flags: null,
           },
+
           message: 'This must be a numeric Action ID.',
         },
       ],
-    })
-    .localized(false)
-    .required(true);
+    });
 
   postGallery
     .createField('itemsPerRow')
     .name('Items Per Row')
     .type('Integer')
+    .localized(false)
+    .required(true)
     .validations([
       {
         in: [2, 3],
       },
     ])
-    .required(true)
-    .localized(false);
+    .disabled(false)
+    .omitted(false);
+
+  postGallery
+    .createField('filterType')
+    .name('Filter Type')
+    .type('Symbol')
+    .localized(true)
+    .required(false)
+    .validations([
+      {
+        in: ['location', 'none'],
+      },
+    ])
+    .disabled(false)
+    .omitted(false);
 
   postGallery
     .createField('hideReactions')
@@ -52,12 +76,11 @@ module.exports = function(migration) {
     .validations([])
     .disabled(false)
     .omitted(false);
-
   postGallery.changeEditorInterface('internalTitle', 'singleLine', {});
 
   postGallery.changeEditorInterface('actionIds', 'listInput', {
-    helpText: `A comma-separated list of one or more Action IDs to display in this gallery. Action IDs
-    can be found in Rogue: https://dosome.click/nyshrf`,
+    helpText:
+      'A comma-separated list of one or more Action IDs to display in this gallery. Action IDs\n    can be found in Rogue: https://dosome.click/nyshrf',
   });
 
   postGallery.changeEditorInterface('itemsPerRow', 'radio', {
@@ -65,8 +88,12 @@ module.exports = function(migration) {
       'The maximum number of items in a single row when viewing the gallery in a large display.',
   });
 
+  postGallery.changeEditorInterface('filterType', 'radio', {
+    helpText: 'Allow filtering user-submitted posts by a specified type.',
+  });
+
   postGallery.changeEditorInterface('hideReactions', 'boolean', {
-    helpText: 'Hide post reactions in this gallery.',
+    helpText: 'Hide the post reactions for this gallery.',
     trueLabel: 'Yes',
     falseLabel: 'No',
   });
