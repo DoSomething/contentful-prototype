06db59489acd1c2dc5a89ae37e5b45ac22452cb4
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-04-03 15:49:24 -0400

    Use previewImage prop as image for small screens if set. Default to always render iframe

--- resources/assets/components/utilities/Iframe.js
+++ resources/assets/components/utilities/Iframe.js
@@ -3,7 +3,6 @@ import gql from 'graphql-tag';
 import Media from 'react-media';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import { Query } from 'react-apollo';
 import { PuckWaypoint } from '@dosomething/puck-client';
 
 import LazyImage from './LazyImage';
@@ -14,18 +13,14 @@ const PERMITTED_HOSTNAMES = ['dosomething.carto.com'];
 export const EmbedBlockFragment = gql`
   fragment EmbedBlockFragment on EmbedBlock {
     url
-  }
-`;
-
-const EMBED_QUERY = gql`
-  query EmbedQuery($url: AbsoluteUrl!) {
-    embed(url: $url) {
-      thumbnailUrl
+    previewImage {
+      url(w: 700, h: 700)
+      description
     }
   }
 `;
 
-const Iframe = ({ className, id, url }) => {
+const Iframe = ({ className, id, url, previewImage }) => {
   const hostname = new URL(url).hostname;
 
   if (!PERMITTED_HOSTNAMES.includes(hostname)) {
@@ -33,35 +28,35 @@ const Iframe = ({ className, id, url }) => {
     return <ErrorBlock />;
   }
 
+  const iframeElement = () => (
+    <iframe title={`embed ${id}`} src={url} width="100%" height="520" />
+  );
+
   return (
     <div id={id} className={classnames('embed', className)}>
       <PuckWaypoint name="embed-top" waypointData={{ contentfulId: id }} />
-      <Media query="(max-width: 759px)">
-        {matches =>
-          matches ? (
-            <Query query={EMBED_QUERY} variables={{ url }}>
-              {({ loading, error, data }) => {
-                const isLoaded = !loading;
-                const { embed } = data;
 
-                if (error) {
-                  return <ErrorBlock />;
+      {previewImage ? (
+        <Media query="(max-width: 759px)">
+          {matches =>
+            matches ? (
+              <LazyImage
+                className="rounded"
+                src={previewImage.url}
+                alt={
+                  previewImage.description ||
+                  'Preview image for embedded content'
                 }
+              />
+            ) : (
+              iframeElement()
+            )
+          }
+        </Media>
+      ) : (
+        iframeElement()
+      )}
 
-                return (
-                  <LazyImage
-                    className="rounded"
-                    src={isLoaded ? embed.thumbnailUrl : null}
-                    alt="Preview image for embedded content"
-                  />
-                );
-              }}
-            </Query>
-          ) : (
-            <iframe title={`embed ${id}`} src={url} width="100%" height="520" />
-          )
-        }
-      </Media>
       <PuckWaypoint name="embed-bottom" waypointData={{ contentfulId: id }} />
     </div>
   );
@@ -71,10 +66,15 @@ Iframe.propTypes = {
   className: PropTypes.string,
   id: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
+  previewImage: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    description: PropTypes.string,
+  }),
 };
 
 Iframe.defaultProps = {
   className: null,
+  previewImage: null,
 };
 
 export default Iframe;
