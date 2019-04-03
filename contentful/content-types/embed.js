0655e073b8b0b0ae4ddfb5b7a2d424fc4c964cbe
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-04-03 13:14:55 -0400

    Preview Image Field for Embed Content Type (#1359)
    * Add previewImage field to embed migration script
    
    * Make previewImage field optional

--- contentful/content-types/embed.js
+++ contentful/content-types/embed.js
@@ -36,6 +36,26 @@ module.exports = function(migration) {
     .disabled(false)
     .omitted(false);
 
+  embed
+    .createField('previewImage')
+    .name('Preview Image')
+    .type('Link')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        linkMimetypeGroup: ['image'],
+      },
+      {
+        assetFileSize: {
+          max: 20971520,
+        },
+      },
+    ])
+    .disabled(false)
+    .omitted(false)
+    .linkType('Asset');
+
   embed.changeEditorInterface('internalTitle', 'singleLine', {
     helpText:
       'This title is used internally to help find this content. It will not be displayed anywhere on the rendered web page.',
@@ -44,5 +64,10 @@ module.exports = function(migration) {
   embed.changeEditorInterface('url', 'singleLine', {
     helpText:
       'The URL for the embed. (Currently only supporting Carto map embed URLs. (https://dosomething.carto.com/dashboard)).',
+  });
+
+  embed.changeEditorInterface('previewImage', 'assetLinkEditor', {
+    helpText:
+      'If set, replaces the embed on smaller screens as a preview of the embed content.',
   });
 };
