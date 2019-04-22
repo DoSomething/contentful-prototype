ecbcf7d448c05322d708bb10f17bb9a5a3c5251d
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-04-22 17:21:10 -0400

    Add basic scholarship card skeleton

--- resources/assets/components/pages/LandingPage/templates/PitchTemplate.js
+++ resources/assets/components/pages/LandingPage/templates/PitchTemplate.js
@@ -5,8 +5,23 @@ import Card from '../../../utilities/Card/Card';
 import TextContent from '../../../utilities/TextContent/TextContent';
 
 const PitchTemplate = ({ content, sidebarCTA }) => (
-  <div className="campaign-page">
+  <div className="campaign-page pitch-page">
     <div className="primary">
+      <Card className="rounded bordered padded margin-bottom-lg">
+        <p className="padding-bottom-md">
+          <b>Welcome from FASTWEB!</b> This text will be how a user can earn the
+          scholarship. Join the bone marrow registry.
+        </p>
+
+        <div className="grid">
+          <div className="section-1">Amount</div>
+          <div className="section-2">
+            <div className="section-2a">Deadline</div>
+            <div className="section-2b">Qualifications</div>
+          </div>
+        </div>
+      </Card>
+
       <TextContent>{content}</TextContent>
     </div>
     <div className="secondary">
