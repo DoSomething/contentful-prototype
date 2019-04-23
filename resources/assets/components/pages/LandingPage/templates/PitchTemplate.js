533a059db771c9f9d007f7dc85dcd4d74a66c8b6
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-04-23 15:58:38 -0400

    Remove scholarship card wip

--- resources/assets/components/pages/LandingPage/templates/PitchTemplate.js
+++ resources/assets/components/pages/LandingPage/templates/PitchTemplate.js
@@ -7,20 +7,6 @@ import TextContent from '../../../utilities/TextContent/TextContent';
 const PitchTemplate = ({ content, sidebarCTA }) => (
   <div className="campaign-page pitch-page">
     <div className="primary">
-      <Card className="rounded bordered padded margin-bottom-lg">
-        <p className="padding-bottom-md">
-          <b>Welcome from FASTWEB!</b> This text will be how a user can earn the
-          scholarship. Join the bone marrow registry.
-        </p>
-
-        <div className="grid">
-          <div className="section-1">Amount</div>
-          <div className="section-2">
-            <div className="section-2a">Deadline</div>
-            <div className="section-2b">Qualifications</div>
-          </div>
-        </div>
-      </Card>
 
       <TextContent>{content}</TextContent>
     </div>
