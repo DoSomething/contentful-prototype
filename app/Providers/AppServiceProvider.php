3d181febf9ac759113b358119395d3f81286fbcd
David Furnes <david@dfurnes.com>
2019-04-25 15:52:02 -0400

    Add "cacheable" macro to Response.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Response;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -21,6 +23,17 @@ class AppServiceProvider extends ServiceProvider
     {
         // @see: https://laravel-news.com/laravel-5-4-key-too-long-error
         Schema::defaultStringLength(191);
+
+        // A simple $response->cacheable() macro:
+        Response::macro('cacheable', function ($seconds = 5 * 60) {
+            // If this is an anonymous request, this macro tells Fastly to cache for
+            // N seconds so we can handle sudden traffic spikes, e.g. https://git.io/fjs2W
+            if (Auth::guest()) {
+                $this->setPublic()->setMaxAge($seconds);
+            }
+
+            return $this;
+        });
 
         // Attach "client-safe" environment variables to views.
         View::composer('*', function ($view) {
