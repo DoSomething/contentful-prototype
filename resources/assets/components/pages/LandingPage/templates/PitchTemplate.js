ada72d5ecd447ba1d03aab796e58785ccc73979a
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-04-29 10:06:50 -0400

    Remove temp hardcoded rendering of affiliateScholarshipBlock

--- resources/assets/components/pages/LandingPage/templates/PitchTemplate.js
+++ resources/assets/components/pages/LandingPage/templates/PitchTemplate.js
@@ -3,16 +3,11 @@ import PropTypes from 'prop-types';
 
 import Card from '../../../utilities/Card/Card';
 import TextContent from '../../../utilities/TextContent/TextContent';
-import AffiliateScholarshipBlockContainer from '../../../blocks/AffiliateScholarshipBlock/AffiliateScholarshipBlockContainer';
 
 const PitchTemplate = ({ content, sidebarCTA }) => (
   <div className="campaign-page">
     <div className="primary">
       <TextContent>{content}</TextContent>
-        <AffiliateScholarshipBlockContainer
-          affiliateLogo="https://images.ctfassets.net/81iqaqpfd8fy/5vKnhszTzOs8Y6EkoY02Ey/f4397f4c03171efcc3f67261f2aeb279/PIFstacked.png"
-          affiliateTitle="Fastweb"
-        />
     </div>
     <div className="secondary">
       <Card title={sidebarCTA.title} className="rounded bordered">
