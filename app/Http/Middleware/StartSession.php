a927020569b65e8b26554cefc69c4f4d69d5282a
David Furnes <david@dfurnes.com>
2019-04-29 13:31:07 -0400

    Override built-in StartSession middleware.

---
+++ app/Http/Middleware/StartSession.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Session\Middleware\StartSession as Middleware;
+
+class StartSession extends Middleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, \Closure $next)
+    {
+        $isAnonymousSession = ! $request->cookies->has(config('session.cookie'));
+        $isAuthenticating = $request->path() === 'next/login';
+
+        // If we don't have a session cookie, use the 'array' driver.
+        if ($isAnonymousSession && ! $isAuthenticating) {
+            config(['session.driver' => 'array']);
+        }
+
+        return parent::handle($request, $next);
+    }
+}
