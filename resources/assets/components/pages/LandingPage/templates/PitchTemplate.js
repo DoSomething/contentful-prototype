8283acdb6b35e67522c6a0a06f69c4811f770dc8
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-04-30 14:12:34 -0400

    Revert "Undo inadvertent pitch page changes"
    This reverts commit e1ed805636ba906cbcba215be0b88e4124d28db2.
    
    Redo pitch page bg-color changes which were actually not inadvertent

--- resources/assets/components/pages/LandingPage/templates/PitchTemplate.js
+++ resources/assets/components/pages/LandingPage/templates/PitchTemplate.js
@@ -5,8 +5,9 @@ import Card from '../../../utilities/Card/Card';
 import TextContent from '../../../utilities/TextContent/TextContent';
 
 const PitchTemplate = ({ content, sidebarCTA }) => (
-  <div className="campaign-page">
+  <div className="campaign-page pitch-page">
     <div className="primary">
+
       <TextContent>{content}</TextContent>
     </div>
     <div className="secondary">
