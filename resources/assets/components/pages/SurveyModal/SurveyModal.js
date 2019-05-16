083433eac72de523dc6997476357e96e77f00d79
Diego Lorenzo <diego@diegolorenzo.com>
2019-05-16 11:42:23 -0400

    Incremental updates to the SurveyModal

--- resources/assets/components/pages/SurveyModal/SurveyModal.js
+++ resources/assets/components/pages/SurveyModal/SurveyModal.js
@@ -5,15 +5,13 @@ import PropTypes from 'prop-types';
 
 import { makeUrl } from '../../../helpers';
 
-const SURVEY_DATA_URL = 'https://dosomething.typeform.com/to/Bvcwvm';
-
 class SurveyModal extends React.Component {
   componentDidMount() {
     window.typeformInit();
   }
 
   render() {
-    const { northstarId, campaignId } = this.props;
+    const { campaignId, northstarId, typeformUrl } = this.props;
 
     const typeformQuery = {
       northstar_id: northstarId,
@@ -21,12 +19,12 @@ class SurveyModal extends React.Component {
       origin: window.location.pathname,
     };
 
-    const typeformUrl = makeUrl(SURVEY_DATA_URL, typeformQuery);
+    const url = makeUrl(typeformUrl, typeformQuery);
 
     return (
       <div
         className="modal__slide typeform-widget"
-        data-url={typeformUrl.href}
+        data-url={url.href}
         style={{ width: '100%', height: '500px' }}
       />
     );
@@ -34,8 +32,14 @@ class SurveyModal extends React.Component {
 }
 
 SurveyModal.propTypes = {
-  northstarId: PropTypes.string.isRequired,
-  campaignId: PropTypes.string.isRequired,
+  typeformUrl: PropTypes.string.isRequired,
+  northstarId: PropTypes.string,
+  campaignId: PropTypes.string,
+};
+
+SurveyModal.defaultProps = {
+  northstarId: null,
+  campaignId: null,
 };
 
 export default SurveyModal;
