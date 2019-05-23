21bc8d621c063ca228070b80c8977912d2807fd1
David Furnes <david@dfurnes.com>
2019-05-23 13:40:30 -0400

    Add support for mocking 'fetch' calls.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -7,3 +7,18 @@
 // https://on.cypress.io/configuration
 
 import './commands';
+
+Cypress.on('window:before:load', window => {
+  const document = window.document;
+
+  // Remove built-in 'fetch' support since it cannot yet be mocked by Cypress.
+  // eslint-disable-next-line no-param-reassign
+  delete window.fetch;
+
+  // Force the application to load a 'fetch' polyfill (bypassing polyfill.io's
+  // user-agent detection, which would assume Chrome has support built in).
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://unpkg.com/unfetch/polyfill';
+  document.head.appendChild(script);
+});
