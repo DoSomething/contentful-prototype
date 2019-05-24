5781c67c72cce3608883bb4a7e31c395bbcb2471
Sena Heydari <sheydari@dosomething.org>
2019-05-24 10:20:10 -0700

    Add snowplow tracker code when Snowplow URI is present.

--- resources/views/partials/analytics.blade.php
+++ resources/views/partials/analytics.blade.php
@@ -5,15 +5,6 @@
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
       ga('create', '{{ config('services.analytics.google_id') }}', 'auto');
-
-      @if (config('services.analytics.snowplow_url'))
-        ga('require', 'spGaPlugin', { endpoint: '{{config('services.analytics.snowplow_url')}}' });
-      @endif
-    </script>
-
-    @if (config('services.analytics.snowplow_url'))
-      <script async src="https://d1fc8wv8zag5ca.cloudfront.net/sp-ga-plugin/0.1.0/sp-ga-plugin.js"></script>
-    @endif
 @endif
 
 @if(config('services.analytics.customer_io_id') && auth()->check())
@@ -34,5 +25,19 @@
         })();
 
         _cio.identify({id: '{{ auth()->id() }}'});
+    </script>
+@endif
+
+@if (config('services.analytics.snowplow_url'))
+    <script type='text/javascript'>
+      ;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
+      p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)
+      };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;
+      n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,'script','//d1fc8wv8zag5ca.cloudfront.net/2.5.3/sp.js','snowplow'));
+      window.snowplow('newTracker', 'cf', '{{config('services.analytics.snowplow_url')}}', { // Initialize a tracker
+        appId: 'dosomething-web' ,
+        cookieDomain: null
+      });
+    window.snowplow('trackPageView');
     </script>
 @endif
