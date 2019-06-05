f40525b3202fcbf21f67d9a8d4e234c67e64731c
Chloe Lee <chloealessandra@gmail.com>
2019-06-05 14:39:52 -0400

    uses container__block -half instead of gallery

--- resources/assets/components/pages/AccountPage/Profile.js
+++ resources/assets/components/pages/AccountPage/Profile.js
@@ -10,44 +10,37 @@ const login = '/login';
 const Profile = props => (
   <div className="bg-gray padding-bottom-lg wrapper">
     <h2 className="caps-lock league-gothic -sm">Your Profile</h2>
-    <ul className="gallery -duo">
-      <li>
-        <div className="margin-top-lg float-left">
-          <h3>Profile Info</h3>
+    <div className="container__block -half">
+      <h3>Profile Info</h3>
 
-          <FormItem title="Name" value={props.user.firstName} />
-          <FormItem title="Birthday" value={props.user.birthdate} />
-          <FormItem
-            title="Password"
-            value="&#9679; &#9679; &#9679; &#9679; &#9679; &#9679;"
-          />
-          <FormItem title="Email" value={props.user.email} />
-          <FormItem title="Phone Number" value={props.user.mobile} />
-          <div className="margin-top-lg">
-            <a
-              href={env('NORTHSTAR_URL') + login}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="button"
-            >
-              Edit Profile
-            </a>
-          </div>
-        </div>
-      </li>
-      <li>
-        <div className="margin-top-lg float-left">
-          <h3>Data and Privacy</h3>
-          <a
-            href="mailto:trust@dosomething.org?subject=Delete my account"
-            className="secondary"
-          >
-            Delete my account
-          </a>
-        </div>
-      </li>
-    </ul>
-
+      <FormItem title="Name" value={props.user.firstName} />
+      <FormItem title="Birthday" value={props.user.birthdate} />
+      <FormItem
+        title="Password"
+        value="&#9679; &#9679; &#9679; &#9679; &#9679; &#9679;"
+      />
+      <FormItem title="Email" value={props.user.email} />
+      <FormItem title="Phone Number" value={props.user.mobile} />
+      <div className="margin-top-lg">
+        <a
+          href={env('NORTHSTAR_URL') + login}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="button"
+        >
+          Edit Profile
+        </a>
+      </div>
+    </div>
+    <div className="container__block -half">
+      <h3>Data and Privacy</h3>
+      <a
+        href="mailto:trust@dosomething.org?subject=Delete my account"
+        className="secondary"
+      >
+        Delete my account
+      </a>
+    </div>
     {/* Commenting this out until we have proper Voter registration info being pulled from Rogue.
       <div className="float-right clear-right padding-top-md">
         <h2>Registration Status</h2>
