807c2552c452b3672014bc57295a07b257a0cf09
Diego Lorenzo <diego@diegolorenzo.com>
2019-06-06 16:12:41 -0400

    Removing 👾

--- resources/views/partials/snowplow_script.blade.php
+++ resources/views/partials/snowplow_script.blade.php
@@ -5,7 +5,7 @@
           };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;
           n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,'script','//d1fc8wv8zag5ca.cloudfront.net/2.5.3/sp.js','snowplow'));
           window.snowplow('newTracker', 'cf', '{{config('services.analytics.snowplow_url')}}', {
-            appId: 'dosomething-web' ,
+            appId: 'dosomething-web',
             cookieDomain: null
         });
 
