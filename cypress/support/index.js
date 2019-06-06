21923a5549c29b1b16ff6da0188194d7e28437c5
David Furnes <david@dfurnes.com>
2019-06-06 11:05:34 -0400

    Inline fetch polyfill so network flakiness can't fail our tests.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -12,13 +12,12 @@ Cypress.on('window:before:load', window => {
   const document = window.document;
 
   // Remove built-in 'fetch' support since it cannot yet be mocked by Cypress.
-  // eslint-disable-next-line no-param-reassign
-  delete window.fetch;
+  delete window.fetch; // eslint-disable-line no-param-reassign
 
-  // Force the application to load a 'fetch' polyfill (bypassing polyfill.io's
+  // And force the application to load an inlined 'fetch' polyfill (bypassing polyfill.io's
   // user-agent detection, which would assume Chrome has support built in).
   const script = document.createElement('script');
   script.type = 'text/javascript';
-  script.src = 'https://unpkg.com/unfetch/polyfill';
+  script.textContent = `window.fetch=function(e,n){return n=n||{},new Promise(function(t,r){var s=new XMLHttpRequest,o=[],u=[],i={},a=function(){return{ok:2==(s.status/100|0),statusText:s.statusText,status:s.status,url:s.responseURL,text:function(){return Promise.resolve(s.responseText)},json:function(){return Promise.resolve(JSON.parse(s.responseText))},blob:function(){return Promise.resolve(new Blob([s.response]))},clone:a,headers:{keys:function(){return o},entries:function(){return u},get:function(e){return i[e.toLowerCase()]},has:function(e){return e.toLowerCase()in i}}}};for(var l in s.open(n.method||"get",e,!0),s.onload=function(){s.getAllResponseHeaders().replace(/^(.*?):[^\\S\\n]*([\\s\\S]*?)$/gm,function(e,n,t){o.push(n=n.toLowerCase()),u.push([n,t]),i[n]=i[n]?i[n]+","+t:t}),t(a())},s.onerror=r,s.withCredentials="include"==n.credentials,n.headers)s.setRequestHeader(l,n.headers[l]);s.send(n.body||null)})};`;
   document.head.appendChild(script);
 });
