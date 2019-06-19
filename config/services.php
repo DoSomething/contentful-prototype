d2e6f4b5e75e27d5bb82563cae8f86e558c1b633
Diego Lorenzo <diego@diegolorenzo.com>
2019-06-19 12:53:57 -0400

    Snowplow page view tracking updates. (#1468)
    * Adding some console logging for Snowplow events.
    
    * Updates to more accurately track page views and pass context payload.
    
    * Removing vestiges of GA.
    
    * Removing @dosomething/analytics from package dependencies for now.

--- config/services.php
+++ config/services.php
@@ -17,7 +17,6 @@ return [
     'analytics' => [
         'customer_io_id' => env('CUSTOMER_IO_ID'),
         'facebook_id' => env('FACEBOOK_APP_ID'),
-        'google_id' => env('GOOGLE_ANALYTICS_ID'),
         'google_tag_manager_id' => env('GOOGLE_TAG_MANAGER_ID'),
         'puck_url' => env('PUCK_URL'),
         'snowplow_url' => env('SNOWPLOW_URL'),
