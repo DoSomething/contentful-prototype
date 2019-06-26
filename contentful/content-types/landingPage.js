7c0269405f12be4bc76806b4c4597f28d1d47231
Diego Lorenzo <diego@diegolorenzo.com>
2019-06-26 17:29:22 -0400

    Updating/adding content type migration for landingPage.

--- contentful/content-types/landingPage.js
+++ contentful/content-types/landingPage.js
@@ -4,71 +4,111 @@ module.exports = function(migration) {
     .name('Landing Page')
     .description('A custom landing page for a DoSomething.org campaign.')
     .displayField('internalTitle');
-
   landingPage
     .createField('internalTitle')
     .name('Internal Title')
     .type('Symbol')
+    .localized(false)
     .required(true)
-    .localized(false);
-
+    .validations([])
+    .disabled(false)
+    .omitted(false);
   landingPage
     .createField('title')
     .name('Title')
     .type('Symbol')
+    .localized(true)
     .required(true)
-    .localized(true);
-
+    .validations([])
+    .disabled(false)
+    .omitted(false);
   landingPage
     .createField('subTitle')
     .name('Subtitle')
     .type('Symbol')
+    .localized(true)
     .required(false)
-    .localized(true);
-
+    .validations([])
+    .disabled(false)
+    .omitted(false);
   landingPage
     .createField('content')
     .name('Content')
     .type('Text')
+    .localized(true)
     .required(false)
-    .localized(true);
+    .validations([])
+    .disabled(false)
+    .omitted(false);
 
   landingPage
     .createField('sidebar')
     .name('Sidebar')
     .type('Array')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
     .items({
       type: 'Link',
-      linkType: 'Entry',
+
       validations: [
         {
-          linkContentType: ['callToAction', 'customBlock'],
+          linkContentType: ['campaignUpdate', 'customBlock', 'cardBlock'],
         },
       ],
-    })
-    .required(false)
-    .localized(false);
+
+      linkType: 'Entry',
+    });
 
   landingPage
     .createField('blocks')
     .name('Blocks')
     .type('Array')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
     .items({
       type: 'Link',
-      linkType: 'Entry',
+
       validations: [
         {
-          linkContentType: ['callToAction', 'contentBlock', 'customBlock'],
+          linkContentType: [
+            'callToAction',
+            'contentBlock',
+            'customBlock',
+            'galleryBlock',
+          ],
         },
       ],
-    })
-    .required(false)
-    .localized(false);
+
+      linkType: 'Entry',
+    });
 
   landingPage
     .createField('additionalContent')
     .name('Additional Content')
     .type('Object')
+    .localized(false)
     .required(false)
-    .localized(false);
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+  landingPage.changeEditorInterface('internalTitle', 'singleLine', {});
+  landingPage.changeEditorInterface('title', 'singleLine', {});
+  landingPage.changeEditorInterface('subTitle', 'singleLine', {});
+  landingPage.changeEditorInterface('content', 'markdown', {});
+
+  landingPage.changeEditorInterface('sidebar', 'entryLinksEditor', {
+    bulkEditing: false,
+  });
+
+  landingPage.changeEditorInterface('blocks', 'entryLinksEditor', {
+    bulkEditing: false,
+  });
+
+  landingPage.changeEditorInterface('additionalContent', 'objectEditor', {});
 };
