564091ba8e78712e0de6adac82f18660bc14cb43
DoSomething <dosomething@DoSomethings-MacBook-Pro.local>
2019-07-02 11:35:20 -0400

    Adjusted position of the titles of cards with images and added dates to all of the cards

--- resources/assets/components/utilities/PostCard/PostCard.js
+++ resources/assets/components/utilities/PostCard/PostCard.js
@@ -3,6 +3,7 @@ import { get } from 'lodash';
 import gql from 'graphql-tag';
 import { format } from 'date-fns';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import { propType } from 'graphql-anywhere';
 
 import Card from '../Card/Card';
@@ -79,7 +80,9 @@ const PostCard = ({ post, hideReactions }) => {
   return (
     <Card className={`rounded h-full post-ornament-${post.type}`} key={post.id}>
       <div className="post">
-        <div className="flex-grow">{media}</div>
+        <div className={classnames({ 'flex-grow': post.type === 'text' })}>
+          {media}
+        </div>
         <BaseFigure
           media={reactionElement}
           alignment="right"
@@ -95,10 +98,8 @@ const PostCard = ({ post, hideReactions }) => {
               {post.quantity} {post.actionDetails.noun}
             </p>
           ) : null}
-          {isAnonymous ? (
-            <p className="footnote">{format(post.createdAt, 'PPP')}</p>
-          ) : null}
           {post.type !== 'text' && post.text ? <p>{post.text}</p> : null}
+          <p className="footnote">{format(post.createdAt, 'PPP')}</p>
         </BaseFigure>
       </div>
     </Card>
