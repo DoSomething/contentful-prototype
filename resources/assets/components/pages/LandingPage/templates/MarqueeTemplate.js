801a0988e3d44db5367a7aa004b43a0828cc3248
Diego Lorenzo <diego@diegolorenzo.com>
2019-07-08 16:28:34 -0400

    Using srcset and messing with the grid.

--- resources/assets/components/pages/LandingPage/templates/MarqueeTemplate.js
+++ resources/assets/components/pages/LandingPage/templates/MarqueeTemplate.js
@@ -14,29 +14,37 @@ const MarqueeTemplate = ({
   tagline,
   title,
 }) => {
-  console.log('🖼', { campaignId, title, coverImage, tagline });
+  const coverImageUrls = {
+    extraLarge: contentfulImageUrl(coverImage.url, '2880', '1620', 'fill'),
+    large: contentfulImageUrl(coverImage.url, '1440', '810', 'fill'),
+    medium: contentfulImageUrl(coverImage.url, '720', '405', 'fill'),
+    small: contentfulImageUrl(coverImage.url, '360', '202', 'fill'),
+  };
 
-  // @TODO: try using srcset
-  const coverImageSrc = contentfulImageUrl(
-    coverImage.url,
-    '1440',
-    '810',
-    'fill',
-  );
+  console.log('🖼', { campaignId, title, coverImage, coverImageUrls, tagline });
 
   return (
     <React.Fragment>
-      <img className="cover-image" src={coverImageSrc} />
-      <div className="clearfix bg-white">
-        <Enclosure className="default-container margin-lg marquee-landing-page">
-          <header role="banner">
-            <h1>{title}</h1>
-            <h2>{subtitle}</h2>
-          </header>
+      <article className="marquee-landing-page">
+        <div className="base-16-grid bg-white">
+          <img
+            className="cover-image grid-wide"
+            srcSet={`${coverImageUrls.small} 360w, ${coverImageUrls.medium} 720w, ${coverImageUrls.large} 1440w, ${coverImageUrls.extraLarge} 2880w`}
+            src={coverImageUrls.small}
+          />
+        </div>
 
-          <TextContent>{content}</TextContent>
-        </Enclosure>
-      </div>
+        <div className="base-16-grid clearfix bg-white">
+          <Enclosure className="grid-wide">
+            <header role="banner">
+              <h1>{title}</h1>
+              <h2>{subtitle}</h2>
+            </header>
+
+            <TextContent>{content}</TextContent>
+          </Enclosure>
+        </div>
+      </article>
     </React.Fragment>
   );
 };
