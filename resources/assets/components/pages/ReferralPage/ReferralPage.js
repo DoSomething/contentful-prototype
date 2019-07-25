49f44d56c05fbbe5e82020bd5b860c335c6050a5
Aaron Schachter <aaronschachter@gmail.com>
2019-07-25 13:01:24 -0700

    Adds About Us copy, secondary Campaign ID

--- resources/assets/components/pages/ReferralPage/ReferralPage.js
+++ resources/assets/components/pages/ReferralPage/ReferralPage.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 import CampaignLink from './ReferralPageCampaignLink';
 
+// TODO: Allow override via config variable.
+const SECONDARY_CAMPAIGN_ID = 7951;
+
 const ReferralPage = props => {
   return (
     <div>
@@ -18,10 +21,36 @@ const ReferralPage = props => {
             DoSomething.org. Once you and {props.firstName} complete the
             campaign, you’ll both earn a $5 gift card!
           </p>
-          <CampaignLink
-            campaignId={props.primaryCampaignId}
-            userId={props.userId}
-          />
+          <ul>
+            <li>
+              <CampaignLink
+                campaignId={props.primaryCampaignId}
+                userId={props.userId}
+              />
+            </li>
+          </ul>
+          <p>
+            <strong>
+              Interested in doing a different campaign to get your gift card?
+            </strong>{' '}
+            In less than 5 minutes, you can join 193,242 young people putting an
+            end to gun violence.
+          </p>
+          <ul>
+            <li>
+              <CampaignLink
+                campaignId={`${SECONDARY_CAMPAIGN_ID}`}
+                userId={props.userId}
+              />
+            </li>
+          </ul>
+          <h3>About Us</h3>
+          DoSomething is the largest not-for-profit for young people and social
+          change. Using our digital platform, millions of young people make
+          real-world impact through our volunteer, social change, and civic
+          action campaigns. We’ve got hundreds of campaigns to choose from (but
+          only the two above are offering the gift card reward right now). Let’s
+          do this!
         </div>
       </div>
     </div>
