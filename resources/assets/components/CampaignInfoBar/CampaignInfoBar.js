62b3fffb88119947e3401cb1d18eebdab1c9cf6f
sabina <aktersabina122@gmail.com>
2019-08-06 12:32:53 -0400

    Modified some parts on CampaignInfoBar

--- resources/assets/components/CampaignInfoBar/CampaignInfoBar.js
+++ resources/assets/components/CampaignInfoBar/CampaignInfoBar.js
@@ -1,28 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Card from '../utilities/Card/Card';
+import Modal from '../utilities/Modal/Modal';
 import AffiliateCredits from '../utilities/AffiliateCredits/AffiliateCredits';
 
-const CampaignInfoBar = ({
-  affiliateCreditText,
-  affiliatePartners,
-  affiliateSponsors,
-  contactEmail,
-}) => (
-  <div className="info-bar bg-gray-700">
-    <div className="clearfix md:w-3/4 mx-auto px-3 py-6">
-      <AffiliateCredits
-        affiliateCreditText={affiliateCreditText}
-        affiliateSponsors={affiliateSponsors}
-        affiliatePartners={affiliatePartners}
-      />
+class CampaignInfoBar extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <div className="info-bar__secondary">
-        Questions? <a href={`mailto:${contactEmail}`}>Contact {contactEmail}</a>
+    this.state = {
+      showModal: false,
+    };
+  }
+
+  render() {
+    const {
+      affiliateCreditText,
+      affiliateSponsors,
+      affiliatePartners,
+      contactEmail,
+    } = this.props;
+
+    return (
+      <div className="info-bar">
+        <div className="default-container padding-vertical-lg padding-horizontal-md">
+          <AffiliateCredits
+            affiliateCreditText={affiliateCreditText}
+            affiliateSponsors={affiliateSponsors}
+            affiliatePartners={affiliatePartners}
+          />
+
+          <div className="info-bar__secondary">
+            Questions?{' '}
+            <button
+              type="button"
+              className="button -tertiary"
+              style={{ color: 'white' }}
+              onClick={() => this.setState({ showModal: true })}
+            >
+              Contact {contactEmail}
+            </button>
+          </div>
+
+          {this.state.showModal ? (
+            <Modal onClose={() => this.setState({ showModal: false })}>
+              <Card title="Test" className="rounded bordered">
+                Hi!
+              </Card>
+            </Modal>
+          ) : null}
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 CampaignInfoBar.propTypes = {
   affiliateCreditText: PropTypes.string,
