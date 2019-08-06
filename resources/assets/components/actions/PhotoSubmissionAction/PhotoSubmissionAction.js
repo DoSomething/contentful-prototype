def57bc57a9ce02f8d5454e1387905b1747f6ae4
katiecrane <kcrane@dosomething.org>
2019-08-06 16:07:08 -0700

    Photo submission badges

--- resources/assets/components/actions/PhotoSubmissionAction/PhotoSubmissionAction.js
+++ resources/assets/components/actions/PhotoSubmissionAction/PhotoSubmissionAction.js
@@ -21,6 +21,8 @@ import {
   getFieldErrors,
   formatPostPayload,
 } from '../../../helpers/forms';
+import Badge from '../../pages/AccountPage/Badge';
+import Query from '../../Query';
 
 import './photo-submission-action.scss';
 
@@ -40,6 +42,20 @@ export const PhotoSubmissionBlockFragment = gql`
     informationContent
     affirmationContent
     additionalContent
+  }
+`;
+
+const BADGE_QUERY = gql`
+  query AccountQuery($userId: String!) {
+    user(id: $userId) {
+      hasBadgesFlag: hasFeatureFlag(feature: "badges")
+    }
+  }
+`;
+
+const POST_COUNT_BADGE = gql`
+  query PostsCountQuery($userId: String!) {
+    postsCount(userId: $userId, limit: 3)
   }
 `;
 
@@ -418,6 +434,85 @@ class PhotoSubmissionAction extends React.Component {
         {this.state.showModal ? (
           <Modal onClose={() => this.setState({ showModal: false })}>
             <Card className="bordered rounded" title="We got your photo!">
+              <Query
+                query={BADGE_QUERY}
+                variables={{ userId: this.props.userId }}
+                hideSpinner
+              >
+                {badgeData =>
+                  badgeData.user.hasBadgesFlag ? (
+                    <Query
+                      query={POST_COUNT_BADGE}
+                      variables={{ userId: this.props.userId }}
+                      hideSpinner
+                    >
+                      {postData => {
+                        if (postData.postsCount === 1) {
+                          return (
+                            <Badge
+                              earned
+                              className="badge padded"
+                              size="medium"
+                              name="onePostBadge"
+                            >
+                              <h4>1 Action</h4>
+                              <p>
+                                Ohhh HECK yes! You just earned a new badge for
+                                completing your first campaign. Congratulations!
+                              </p>
+                              <a href="/us/account/profile/badges">
+                                View all my badges
+                              </a>
+                            </Badge>
+                          );
+                        }
+                        if (postData.postsCount === 2) {
+                          return (
+                            <Badge
+                              earned
+                              className="badge padded"
+                              size="medium"
+                              name="twoPostsBadge"
+                            >
+                              <h4>2 Actions</h4>
+                              <p>
+                                Ohhh HECK yes! You just earned a new badge for
+                                completing your second campaign.
+                                Congratulations!
+                              </p>
+                              <a href="/us/account/profile/badges">
+                                View all my badges
+                              </a>
+                            </Badge>
+                          );
+                        }
+                        if (postData.postsCount === 3) {
+                          return (
+                            <Badge
+                              earned
+                              className="badge padded"
+                              size="medium"
+                              name="threePostsBadge"
+                            >
+                              <h4>3 Actions</h4>
+                              <p>
+                                Ohhh HECK yes! You just earned a new badge for
+                                completing your third campaign. Congratulations!
+                              </p>
+                              <a href="/us/account/profile/badges">
+                                View all my badges
+                              </a>
+                            </Badge>
+                          );
+                        }
+
+                        return null;
+                      }}
+                    </Query>
+                  ) : null
+                }
+              </Query>
+
               <TextContent className="padded">
                 {this.props.affirmationContent ||
                   PhotoSubmissionAction.defaultProps.affirmationContent}
