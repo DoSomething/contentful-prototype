e8f160afdedbc1e26a792e6157befa900861e1a1
mendelB <mendelblesofsky@gmail.com>
2019-08-12 18:46:36 -0400

    Add container with northstar redirect method for signup button click

--- resources/assets/components/utilities/RegisterCTABanner/RegisterCTABanner.js
+++ resources/assets/components/utilities/RegisterCTABanner/RegisterCTABanner.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './register-cta-banner.scss';
 
-const RegisterCTABanner = () => (
+const RegisterCTABanner = ({ redirectToNorthstar }) => (
   <div className="register-cta-banner base-12-grid">
     <div className="grid-narrow margin-md">
       <h3 className="text-lg color-yellow font-bold">JOIN DOSOMETHING TODAY</h3>
@@ -13,11 +14,16 @@ const RegisterCTABanner = () => (
       <button
         className="register-cta-banner__button button padded mt-4"
         type="button"
+        onClick={redirectToNorthstar}
       >
         Sign Up
       </button>
     </div>
   </div>
 );
+
+RegisterCTABanner.propTypes = {
+  redirectToNorthstar: PropTypes.func.isRequired,
+};
 
 export default RegisterCTABanner;
---
+++ resources/assets/components/utilities/RegisterCTABanner/RegisterCTABannerContainer.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+
+import RegisterCTABanner from './RegisterCTABanner';
+import { redirectToNorthstar } from '../../../helpers';
+
+/**
+ * Provide state from the Redux store as props for this component.
+ */
+const mapStateToProps = state => ({
+  redirectToNorthstar: () => redirectToNorthstar(state),
+});
+
+/**
+ * Export the container component.
+ */
+export default connect(mapStateToProps)(RegisterCTABanner);
