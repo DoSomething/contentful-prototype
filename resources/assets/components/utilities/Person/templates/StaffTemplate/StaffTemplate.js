39b58f85ea99c1db779ff9c8027346bb2ba450f2
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2019-08-23 11:37:26 -0400

    moves Person to utilities and updates references

--- resources/assets/components/utilities/Person/templates/StaffTemplate/StaffTemplate.js
+++ resources/assets/components/utilities/Person/templates/StaffTemplate/StaffTemplate.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Figure } from '../../../Figure';
-import { contentfulImageUrl } from '../../../../helpers';
+import { Figure } from '../../../../Figure';
+import { contentfulImageUrl } from '../../../../../helpers';
 
 const StaffTemplate = props => {
   const { name, jobTitle, alternatePhoto, twitterId } = props;
