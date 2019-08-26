40fb2a4319baac5506c653ac55226935eab43b04
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2019-08-26 11:43:40 -0400

    moves Figure to utilities component

--- resources/assets/components/utilities/Figure/index.js
+++ resources/assets/components/utilities/Figure/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-import { modifiers } from '../../helpers';
-import LazyImage from '../utilities/LazyImage';
+import { modifiers } from '../../../helpers';
+import LazyImage from '../LazyImage';
 
 import './figure.scss';
 
