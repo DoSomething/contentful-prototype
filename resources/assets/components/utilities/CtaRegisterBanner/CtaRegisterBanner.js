cf483137eabf9816c25d184523721aafdbab4f60
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-08-28 12:02:02 -0400

    Use uppercase utility class to upcase the title

--- resources/assets/components/utilities/CtaRegisterBanner/CtaRegisterBanner.js
+++ resources/assets/components/utilities/CtaRegisterBanner/CtaRegisterBanner.js
@@ -6,7 +6,9 @@ import './cta-register-banner.scss';
 const CtaRegisterBanner = ({ redirectToNorthstar }) => (
   <div className="cta-register-banner base-12-grid">
     <div className="grid-narrow margin-md">
-      <h3 className="text-m color-yellow font-bold">JOIN DOSOMETHING TODAY</h3>
+      <h3 className="text-m color-yellow font-bold uppercase">
+        Join DoSomething Today
+      </h3>
       <p className="text-white mt-4">
         After you learn something, do something! Help change the world one
         Fantini at a time. Join the global movement because apathy sucks.
