5e2a59f3737511de0b78f57ea1be17f1026fcc19
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-08-29 19:11:57 -0400

    Remove Register from CtaBanner component naming

--- resources/assets/components/utilities/CtaBanner/CtaBanner.js
+++ resources/assets/components/utilities/CtaBanner/CtaBanner.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './cta-register-banner.scss';
+import './cta-banner.scss';
 
-const CtaRegisterBanner = ({ buttonText, content, link, title }) => (
-  <div className="cta-register-banner base-12-grid">
+const CtaBanner = ({ buttonText, content, link, title }) => (
+  <div className="cta-banner base-12-grid">
     <div className="grid-narrow margin-md">
       <h3 className="text-m color-yellow font-bold uppercase">{title}</h3>
       <p className="text-white mt-4">{content}</p>
       <a
-        className="cta-register-banner__button button padded mt-4"
+        className="cta-banner__button button padded mt-4"
         type="button"
         href={link}
       >
@@ -19,11 +19,11 @@ const CtaRegisterBanner = ({ buttonText, content, link, title }) => (
   </div>
 );
 
-CtaRegisterBanner.propTypes = {
+CtaBanner.propTypes = {
   buttonText: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
 };
 
-export default CtaRegisterBanner;
+export default CtaBanner;
