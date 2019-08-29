7224ee811e2323c0bc2ebf7bbdc8a3921054cc2a
Aaron Schachter <aaronschachter@gmail.com>
2019-08-29 16:11:26 -0700

    Adds redirectToReferralPage

--- resources/assets/components/utilities/CtaReferralPageBanner/CtaReferralPageBanner.js
+++ resources/assets/components/utilities/CtaReferralPageBanner/CtaReferralPageBanner.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CtaReferralPageBanner = ({ campaignId }) => (
-  <div>
-    <h3>Benefits With Friends</h3>
-    <p>
-      Refer a friend to this campaign, and you’ll *both* earn a $5 gift card!
-    </p>
-    <a href={`/us/refer-friends?campaign_id=${campaignId}`}>Refer A Friend</a>
-  </div>
-);
+class CtaReferralPageBanner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.redirectToReferralPage = this.redirectToReferralPage.bind(this);
+  }
+
+  redirectToReferralPage() {
+    window.location.href = `/us/refer-friends?campaign_id=${this.props.campaignId}`;
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Benefits With Friends</h3>
+        <p>
+          Refer a friend to this campaign, and you’ll *both* earn a $5 gift
+          card!
+        </p>
+        <button
+          className="button padded"
+          type="button"
+          onClick={this.redirectToReferralPage}
+        >
+          Refer A Friend
+        </button>
+      </div>
+    );
+  }
+}
 
 CtaReferralPageBanner.propTypes = {
   campaignId: PropTypes.string.isRequired,
