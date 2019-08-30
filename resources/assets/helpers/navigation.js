3dde59c0f89b37df0388c2fe542fa814fb2f866b
Diego Lorenzo <diego@diegolorenzo.com>
2019-08-30 15:08:02 -0400

    Finalizing adding analytics to navigation links and search form.

--- resources/assets/helpers/navigation.js
+++ resources/assets/helpers/navigation.js
@@ -1,48 +1,84 @@
 /* global document */
 
-import { findKey } from 'lodash';
+import { snakeCase } from 'lodash';
 
+import {
+  getPageContext,
+  getUtmContext,
+  trackAnalyticsEvent,
+} from './analytics';
 import { toggleClassHandler } from '.';
-import { trackAnalyticsEvent } from './analytics';
-
-export const campaignPaths = {
-  community: '/community',
-  action: '/action',
-  blocks: '/blocks/',
-  quiz: '/quiz/',
-};
 
 /**
- * Get the display name of the given route,
+ * Handle collecting Navigation link data and triggering analytics event.
  *
- * @param  {String} route
- * @return {String}
+ * @param {Object} link
  */
-export function getRouteName(route) {
-  // When doing path comparisons, we want the least specific
-  // (eg: '/') paths at the end of the array.
-  const pathValues = Object.values(campaignPaths).sort(
-    (pathA, pathB) => pathB.length - pathA.length,
+export function linkAnalyticsHandler(link) {
+  link.addEventListener(
+    'mousedown',
+    event => {
+      trackAnalyticsEvent({
+        context: {
+          ...getUtmContext(),
+          ...getPageContext(),
+          referrer: document.referrer,
+          pathname: document.pathname,
+        },
+        metadata: {
+          category: 'navigation',
+          noun: snakeCase(event.target.textContent),
+          target: 'link',
+          verb: 'clicked',
+        },
+      });
+    },
+    false,
+  );
+}
+
+/**
+ * Handle collecting Navigation search form data and triggering analytics event.
+ *
+ * @param {Object} form
+ */
+export function searchAnalyticsHandler(form) {
+  const context = {
+    ...getUtmContext(),
+    ...getPageContext(),
+  };
+
+  form.addEventListener(
+    'mousedown',
+    () => {
+      trackAnalyticsEvent({
+        context,
+        metadata: {
+          category: 'search',
+          noun: 'search form',
+          target: 'form',
+          verb: 'clicked',
+        },
+      });
+    },
+    false,
   );
 
-  // Check if /pages/faq starts with /pages/.
-  const match = pathValues.find(path => route.startsWith(path));
-  if (!match) {
-    return 'undefined route';
-  }
-
-  // Find the display name for the matched path value.
-  // This is a bit crazy because we need to find the index
-  // in the `campaignPaths` object and the `pathValues` array is in
-  // a different order.
-  let name = findKey(campaignPaths, path => path === match);
-  if (name === 'pages') {
-    // Remove /pages/ from /pages/faq
-    // Not the most fullproof solution in the world but should suffice.
-    name = route.replace(match, '');
-  }
-
-  return name;
+  form.addEventListener(
+    'submit',
+    () => {
+      trackAnalyticsEvent({
+        context,
+        metadata: {
+          category: 'search',
+          noun: 'search form',
+          target: 'form',
+          verb: 'submitted',
+        },
+      });
+    },
+    false,
+  );
 }
 
 /**
@@ -54,38 +90,25 @@ export function bindNavigationEvents() {
   const navToggle = document.getElementById('js-navigation-toggle');
   const nav = document.getElementsByClassName('navigation')[0];
   const chrome = document.getElementsByClassName('chrome')[0];
+
   toggleClassHandler(navToggle, nav, 'is-visible');
   toggleClassHandler(navToggle, chrome, 'has-mobile-menu');
 
   // Account profile dropdown toggle.
   const accountToggle = document.getElementById('js-account-toggle');
   const dropdown = document.getElementsByClassName('navigation__dropdown')[0];
+
   toggleClassHandler(accountToggle, dropdown, 'is-visible');
 
   // Navigation link analytics.
   const navLinks = document.querySelectorAll('.navigation__menu a');
-  navLinks.forEach(link => {
-    link.addEventListener(
-      'click',
-      function(event) {
-        event.preventDefault();
-        console.log('### Event:', event);
 
-        trackAnalyticsEvent({
-          context: {},
-          metadata: {
-            adjective: '',
-            category: '',
-            label: '',
-            noun: '',
-            target: '',
-            verb: 'clicked',
-          },
-        });
-      },
-      false,
-    );
+  navLinks.forEach(link => {
+    linkAnalyticsHandler(link);
   });
 
-  console.log('@@', navLinks);
+  // Search form analytics.
+  const searchForm = document.querySelector('.navigation__menu .form-search');
+
+  searchAnalyticsHandler(searchForm);
 }
