30b652a47538d82ea984b5e8ed765487f9aae1f6
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-08-30 11:31:48 -0400

    Remove Register from CTA Register Banner  (#1579)
    * Refactor to use props for copy and link
    
    * Remove now-uneeded container
    
    * Remove Register from CtaBanner component naming

---
+++ resources/assets/components/utilities/CtaBanner/CtaBanner.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './cta-banner.scss';
+
+const CtaBanner = ({ buttonText, content, link, title }) => (
+  <div className="cta-banner base-12-grid">
+    <div className="grid-narrow margin-md">
+      <h3 className="text-m color-yellow font-bold uppercase">{title}</h3>
+      <p className="text-white mt-4">{content}</p>
+      <a
+        className="cta-banner__button button padded mt-4"
+        type="button"
+        href={link}
+      >
+        {buttonText}
+      </a>
+    </div>
+  </div>
+);
+
+CtaBanner.propTypes = {
+  buttonText: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default CtaBanner;
