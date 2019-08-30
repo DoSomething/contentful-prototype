30d2d54c28153cb3086118d74cc9a5b9ed888804
mendelB <mendelblesofsky@gmail.com>
2019-08-30 12:03:53 -0400

    Remove 'register' from CtaPopover component naming

--- resources/assets/components/utilities/CtaPopover/CtaPopover.js
+++ resources/assets/components/utilities/CtaPopover/CtaPopover.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './cta-register-popover.scss';
+import './cta-popover.scss';
 
-const CtaRegisterPopover = ({ buttonText, content, link, title }) => (
-  <div className="cta-register-popover padded bordered rounded">
+const CtaPopover = ({ buttonText, content, link, title }) => (
+  <div className="cta-popover padded bordered rounded">
     <h3 className="text-m color-yellow font-bold uppercase">{title}</h3>
     <p className="text-white mt-4">{content}</p>
-    <a className="cta-register-popover__button button padded mt-4" href={link}>
+    <a className="cta-popover__button button padded mt-4" href={link}>
       {buttonText}
     </a>
   </div>
 );
 
-CtaRegisterPopover.propTypes = {
+CtaPopover.propTypes = {
   buttonText: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
 };
 
-export default CtaRegisterPopover;
+export default CtaPopover;
