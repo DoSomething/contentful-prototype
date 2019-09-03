3182fa82833c5fe350defae235b37394bf821244
Aaron Schachter <aaronschachter@gmail.com>
2019-09-03 11:46:22 -0700

    Changes per https://github.com/DoSomething/phoenix-next/pull/1583#pullrequestreview-283164724

--- resources/assets/components/utilities/CtaReferralPageBanner/CtaReferralPageBanner.js
+++ resources/assets/components/utilities/CtaReferralPageBanner/CtaReferralPageBanner.js
@@ -7,14 +7,14 @@ const CtaReferralPageBanner = props => (
   <div className="p-4">
     <div className="cta-register-banner p-4">
       <div>
-        <h3 className="color-white">Benefits With Friends</h3>
-        <p className="color-white padding-bottom-md">
+        <h3 className="text-white">Benefits With Friends</h3>
+        <p className="text-white padding-bottom-md">
           Refer a friend to this campaign, and you’ll *both* earn a $5 gift
           card!
         </p>
         <a
           href={`/us/refer-friends?campaign_id=${props.campaignId}`}
-          className="button padded share -blue"
+          className="button padded -attached"
         >
           Refer A Friend
         </a>
