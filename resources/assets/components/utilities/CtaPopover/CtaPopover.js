03959687dbee2862d5e18ab4d22e370751a21064
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-09-03 14:14:06 -0400

    Add ex button and handleClose prop

--- resources/assets/components/utilities/CtaPopover/CtaPopover.js
+++ resources/assets/components/utilities/CtaPopover/CtaPopover.js
@@ -3,8 +3,15 @@ import PropTypes from 'prop-types';
 
 import './cta-popover.scss';
 
-const CtaPopover = ({ buttonText, content, link, title }) => (
+const CtaPopover = ({ buttonText, content, handleClose, link, title }) => (
   <div className="cta-popover p-4 bordered rounded">
+    <button
+      type="button"
+      className="modal__close -white -small"
+      onClick={handleClose}
+    >
+      &times;
+    </button>
     <h3 className="text-m text-yellow font-bold uppercase">{title}</h3>
     <p className="text-white mt-4">{content}</p>
     <a className="cta-popover__button button p-4 mt-4" href={link}>
@@ -16,6 +23,7 @@ const CtaPopover = ({ buttonText, content, link, title }) => (
 CtaPopover.propTypes = {
   buttonText: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
+  handleClose: PropTypes.func.isRequired,
   link: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
 };
