ee05b2b37acff5a397627a55d999ae2e21c68784
Aaron Schachter <aaronschachter@gmail.com>
2019-09-03 10:15:25 -0700

    Cleanup per https://github.com/DoSomething/phoenix-next/pull/1583#discussion_r320354193

--- resources/assets/components/utilities/CtaReferralPageBanner/CtaReferralPageBanner.js
+++ resources/assets/components/utilities/CtaReferralPageBanner/CtaReferralPageBanner.js
@@ -3,39 +3,25 @@ import PropTypes from 'prop-types';
 
 import './cta-referral-page-banner.scss';
 
-class CtaReferralPageBanner extends React.Component {
-  constructor(props) {
-    super(props);
-    this.redirectToReferralPage = this.redirectToReferralPage.bind(this);
-  }
-
-  redirectToReferralPage() {
-    window.location.href = `/us/refer-friends?campaign_id=${this.props.campaignId}`;
-  }
-
-  render() {
-    return (
-      <div className="p-4">
-        <div className="cta-register-banner p-4">
-          <div>
-            <h3 className="color-white">Benefits With Friends</h3>
-            <p className="color-white padding-bottom-md">
-              Refer a friend to this campaign, and you’ll *both* earn a $5 gift
-              card!
-            </p>
-            <button
-              className="button padded share -blue"
-              type="button"
-              onClick={this.redirectToReferralPage}
-            >
-              Refer A Friend
-            </button>
-          </div>
-        </div>
+const CtaReferralPageBanner = props => (
+  <div className="p-4">
+    <div className="cta-register-banner p-4">
+      <div>
+        <h3 className="color-white">Benefits With Friends</h3>
+        <p className="color-white padding-bottom-md">
+          Refer a friend to this campaign, and you’ll *both* earn a $5 gift
+          card!
+        </p>
+        <a
+          href={`/us/refer-friends?campaign_id=${props.campaignId}`}
+          className="button padded share -blue"
+        >
+          Refer A Friend
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 CtaReferralPageBanner.propTypes = {
   campaignId: PropTypes.string.isRequired,
