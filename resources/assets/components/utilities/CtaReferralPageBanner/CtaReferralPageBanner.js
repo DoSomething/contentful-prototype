e57730c538a680f852367182024c000a8b853d76
Aaron Schachter <aaronschachter@gmail.com>
2019-09-03 11:51:00 -0700

    Starts on CtaReferralPageBanner (#1583)
    * Starts on CtaReferralPageBanner
    
    * Adds redirectToReferralPage
    
    * Adds CSS
    
    * Fixes bug per https://github.com/DoSomething/phoenix-next/pull/1583#discussion_r320332927
    
    * Removes unnecessary CSS per https://github.com/DoSomething/phoenix-next/pull/1583#discussion_r320378763
    
    * Cleanup per https://github.com/DoSomething/phoenix-next/pull/1583#discussion_r320355437
    
    * Cleanup per https://github.com/DoSomething/phoenix-next/pull/1583#discussion_r320354193
    
    * Changes per https://github.com/DoSomething/phoenix-next/pull/1583#pullrequestreview-283164724

---
+++ resources/assets/components/utilities/CtaReferralPageBanner/CtaReferralPageBanner.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './cta-referral-page-banner.scss';
+
+const CtaReferralPageBanner = props => (
+  <div className="p-4">
+    <div className="cta-register-banner p-4">
+      <div>
+        <h3 className="text-white">Benefits With Friends</h3>
+        <p className="text-white padding-bottom-md">
+          Refer a friend to this campaign, and you’ll *both* earn a $5 gift
+          card!
+        </p>
+        <a
+          href={`/us/refer-friends?campaign_id=${props.campaignId}`}
+          className="button padded -attached"
+        >
+          Refer A Friend
+        </a>
+      </div>
+    </div>
+  </div>
+);
+
+CtaReferralPageBanner.propTypes = {
+  campaignId: PropTypes.string.isRequired,
+};
+
+export default CtaReferralPageBanner;
