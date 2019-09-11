8d136181f667c2386280679d046487f8fb61996e
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-09-11 18:24:37 -0400

    Add analytics event tracking

--- resources/assets/components/utilities/AnalyticsWaypoint/AnalyticsWaypoint.js
+++ resources/assets/components/utilities/AnalyticsWaypoint/AnalyticsWaypoint.js
@@ -1,19 +1,42 @@
+import PropTypes from 'prop-types';
 import React, { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
-const AnalyticsWaypoint = () => {
+import { trackAnalyticsEvent } from '../../../helpers/analytics';
+
+const AnalyticsWaypoint = ({ name, context }) => {
   const [ref, inView] = useInView({
     triggerOnce: true,
   });
 
   useEffect(() => {
     if (inView) {
-      // @TODO: Track analytics event here!
-      console.log('ANALYTICS WAYPOINT!');
+      trackAnalyticsEvent({
+        metadata: {
+          verb: 'reached',
+          noun: 'waypoint',
+          target: 'waypoint',
+          category: 'waypoint',
+          label: name,
+        },
+        context: {
+          ...context,
+          name,
+        },
+      });
     }
   }, [inView]);
 
   return <div ref={ref} />;
+};
+
+AnalyticsWaypoint.propTypes = {
+  name: PropTypes.string.isRequired,
+  context: PropTypes.object,
+};
+
+AnalyticsWaypoint.defaultProps = {
+  context: {},
 };
 
 export default AnalyticsWaypoint;
