a8e737095269a7cdaeacfaff2569cadfe5e2d5b7
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-09-23 10:44:32 -0400

    Add CtaBanner analytics event for button click

--- resources/assets/components/utilities/CtaBanner/CtaBanner.js
+++ resources/assets/components/utilities/CtaBanner/CtaBanner.js
@@ -2,24 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { sixpack } from '../../../helpers';
+import { trackAnalyticsEvent } from '../../../helpers/analytics';
 
 import './cta-banner.scss';
 
-const CtaBanner = ({ buttonText, content, link, title }) => (
-  <div className="cta-banner base-12-grid">
-    <div className="grid-narrow m-4">
-      <h3 className="text-m text-yellow font-bold uppercase">{title}</h3>
-      <p className="text-white mt-4">{content}</p>
-      <a
-        className="cta-banner__button button p-4 mt-4"
-        href={link}
-        onClick={() => sixpack().convertOnAction('ctaButtonClick')}
-      >
-        {buttonText}
-      </a>
+const CtaBanner = ({ buttonText, content, link, title }) => {
+  const handleClick = () => {
+    sixpack().convertOnAction('ctaButtonClick');
+
+    trackAnalyticsEvent({
+      metadata: {
+        category: 'site_action',
+        target: 'button',
+        verb: 'clicked',
+        noun: 'call_to_action',
+        adjective: 'banner',
+        label: 'call_to_action_banner',
+      },
+      context: {
+        url: link,
+      },
+    });
+  };
+
+  return (
+    <div className="cta-banner base-12-grid">
+      <div className="grid-narrow m-4">
+        <h3 className="text-m text-yellow font-bold uppercase">{title}</h3>
+        <p className="text-white mt-4">{content}</p>
+        <a
+          className="cta-banner__button button p-4 mt-4"
+          href={link}
+          onClick={handleClick}
+        >
+          {buttonText}
+        </a>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 CtaBanner.propTypes = {
   buttonText: PropTypes.string.isRequired,
