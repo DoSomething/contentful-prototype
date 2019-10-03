7e35f21ef598cf1a6735b19a4416086000d124e5
mendelB <mendelblesofsky@gmail.com>
2019-10-03 11:54:56 -0400

    Use prop defined 'Display Type' specific styling

--- resources/assets/components/utilities/TypeFormEmbed/TypeFormEmbed.js
+++ resources/assets/components/utilities/TypeFormEmbed/TypeFormEmbed.js
@@ -5,13 +5,30 @@ import PropTypes from 'prop-types';
 
 import { appendToQuery, makeUrl, withoutNulls } from '../../../helpers';
 
+const DISPLAY_STYLES = {
+  block: {
+    height: '815px',
+    border: '1px #dcdcdc solid',
+    borderRadius: '4px',
+    overflow: 'hidden',
+  },
+  modal: {
+    height: '500px',
+  },
+};
+
 class TypeFormEmbed extends React.Component {
   componentDidMount() {
     window.typeformInit();
   }
 
   render() {
-    const { queryParameters, redirectParameters, typeformUrl } = this.props;
+    const {
+      queryParameters,
+      redirectParameters,
+      typeformUrl,
+      displayType,
+    } = this.props;
 
     const redirectUrl = appendToQuery(redirectParameters, window.location.href);
 
@@ -28,10 +45,7 @@ class TypeFormEmbed extends React.Component {
         data-url={url.href}
         style={{
           width: '100%',
-          height: '815px',
-          border: '1px #dcdcdc solid',
-          borderRadius: '4px',
-          overflow: 'hidden',
+          ...DISPLAY_STYLES[displayType],
         }}
       />
     );
@@ -42,11 +56,13 @@ TypeFormEmbed.propTypes = {
   queryParameters: PropTypes.object,
   redirectParameters: PropTypes.object,
   typeformUrl: PropTypes.string.isRequired,
+  displayType: PropTypes.oneOf(['block', 'modal']),
 };
 
 TypeFormEmbed.defaultProps = {
   queryParameters: {},
   redirectParameters: {},
+  displayType: 'block',
 };
 
 export default TypeFormEmbed;
