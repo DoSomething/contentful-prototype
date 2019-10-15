d143525a42021b55dc98cab6b87bbf45b5805644
Diego Lorenzo <diego@diegolorenzo.com>
2019-10-15 11:12:59 -0400

    Meganav js behavior (#1638)
    * Experimenting with different approaches.
    
    * Functionality, cleanup and removing idea of templating system.
    
    * Finalizing new CloseButton component.
    
    * CloseButton component cleanup.
    
    * Updating to finalize JS behavior for Meganav.
    
    * Updating onClick for other main nav items.
    
    * Restoring legacy nav for now.

---
+++ resources/assets/components/utilities/CloseButton/CloseButton.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+const CloseButton = ({
+  callback,
+  className,
+  color,
+  dataLabel,
+  dataNoun,
+  size,
+}) => (
+  <button
+    type="button"
+    className={classnames('cursor-pointer', className)}
+    data-label={dataLabel}
+    data-noun={dataNoun}
+    onClick={callback}
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      style={{
+        display: 'block',
+        width: `${size}`,
+        height: `${size}`,
+        pointerEvents: 'none',
+      }}
+    >
+      <path
+        fill={color}
+        fillRule="evenodd"
+        d="M17.24 0L20 2.76 12.76 10 20 17.24 17.24 20 10 12.76 2.76 20 0 17.24 7.24 10 0 2.76 2.76 0 10 7.24 17.24 0z"
+      />
+    </svg>
+  </button>
+);
+
+export default CloseButton;
+
+CloseButton.propTypes = {
+  callback: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  color: PropTypes.string,
+  dataLabel: PropTypes.string,
+  dataNoun: PropTypes.string,
+  size: PropTypes.string,
+};
+
+CloseButton.defaultProps = {
+  className: null,
+  color: '#222',
+  dataLabel: null,
+  dataNoun: null,
+  size: '18px',
+};
--- resources/assets/components/utilities/SiteNavigation/SiteNavigation.js
+++ resources/assets/components/utilities/SiteNavigation/SiteNavigation.js
@@ -1,167 +1,362 @@
+/* global document */
+/* eslint-disable id-length, jsx-a11y/interactive-supports-focus */
+
 import React from 'react';
+import { get } from 'lodash';
+import classnames from 'classnames';
+
+import CloseButton from '../CloseButton/CloseButton';
+import searchIcon from '../../../images/search-icon.svg';
+import DoSomethingLogo from '../DoSomethingLogo/DoSomethingLogo';
+import {
+  trackAnalyticsEvent,
+  getUtmContext,
+  getPageContext,
+} from '../../../helpers/analytics';
 
 import './site-navigation.scss';
-import dsLogo from '../../../images/ds_logo.svg';
-import searchIcon from '../../../images/search_icon.svg';
 
-const SiteNavigation = () => (
-  <nav role="navigation" className="nav base-12-grid">
-    <div className="logo-nav">
-      <a href="/">
-        <img src={dsLogo} alt="DoSomething" />
-      </a>
-    </div>
+class SiteNavigation extends React.Component {
+  constructor(props) {
+    super(props);
 
-    <ul className="main-nav menu-nav">
-      <li className="menu-nav__item">
-        <a href="/">Causes</a>
+    this.state = {
+      activeSubNav: null,
+      isSubNavFixed: false,
+    };
+  }
 
-        <div className="main-subnav menu-subnav">
-          <div className="wrapper base-12-grid">
-            <section className="main-subnav__links-causes menu-subnav__links">
-              <h1>
-                <a href="/">All Causes</a>
-              </h1>
-              <ul>
-                <li>
-                  <a href="/">Education</a>
-                </li>
-                <li>
-                  <a href="/">Mental Health</a>
-                </li>
-                <li>
-                  <a href="/">Homelessness & Poverty</a>
-                </li>
-                <li>
-                  <a href="/">Environment</a>
-                </li>
-                <li>
-                  <a href="/">Animal Welfare</a>
-                </li>
-                <li>
-                  <a href="/">Sexual Harassment</a>
-                </li>
-                <li>
-                  <a href="/">Bullying</a>
-                </li>
-                <li>
-                  <a href="/">Gender Rights</a>
-                </li>
-                <li>
-                  <a href="/">Racial Justice</a>
-                </li>
-                <li>
-                  <a href="/">Immigration & Refugees</a>
-                </li>
-                <li>
-                  <a href="/">LGBT+ Rights</a>
-                </li>
-                <li>
-                  <a href="/">Get Out the Vote!</a>
-                </li>
-              </ul>
-            </section>
+  analyzeEvent = event => {
+    const targets = {
+      A: 'link',
+      BUTTON: 'button',
+    };
 
-            <section className="main-subnav__links-campaigns menu-subnav__links">
-              <h1>
-                <a href="/">All Campaigns</a>
-              </h1>
-              <ul>
-                <li>
-                  <a href="/">Online</a>
-                </li>
-                <li>
-                  <a href="/">In-person</a>
-                </li>
-                <li>
-                  <a href="/">Petitions</a>
-                </li>
-                <li>
-                  <a href="/">Collections</a>
-                </li>
-              </ul>
-            </section>
+    const target = get(targets, event.target.tagName, 'element');
 
-            <section className="main-subnav__featured menu-subnav__content">
-              <img
-                className="mb-4"
-                src="http://placekitten.com/g/550/250"
-                alt="temporary place kitten"
-              />
-              <h1>Take Back the Kittens</h1>
-              <p>Donec ullamcorper nulla non metus auctor fringilla.</p>
-              <a href="/">Learn More</a>
-            </section>
+    trackAnalyticsEvent({
+      context: {
+        ...getUtmContext(),
+        ...getPageContext(),
+        referrer: document.referrer,
+      },
+      metadata: {
+        adjective: event.target.dataset.label,
+        category: 'navigation',
+        label: event.target.dataset.label,
+        noun: event.target.dataset.noun || 'nav_link',
+        target,
+        verb: 'clicked',
+      },
+    });
+  };
+
+  handleMouseEnter = subNavName => {
+    const { isSubNavFixed } = this.state;
+
+    if (isSubNavFixed) {
+      return;
+    }
+
+    this.setState({
+      activeSubNav: subNavName,
+    });
+  };
+
+  handleMouseLeave = () => {
+    const { isSubNavFixed } = this.state;
+
+    if (isSubNavFixed) {
+      return;
+    }
+
+    this.setState({
+      activeSubNav: null,
+    });
+  };
+
+  handleOnClickLink = event => {
+    this.analyzeEvent(event);
+
+    this.setState({
+      activeSubNav: null,
+      isSubNavFixed: false,
+    });
+  };
+
+  handleOnClickToggle = (event, subNavName) => {
+    event.preventDefault();
+
+    this.analyzeEvent(event);
+
+    const { activeSubNav, isSubNavFixed } = this.state;
+
+    if (activeSubNav === subNavName && isSubNavFixed) {
+      this.setState({
+        activeSubNav: null,
+        isSubNavFixed: false,
+      });
+
+      return;
+    }
+
+    if (activeSubNav === subNavName && !isSubNavFixed) {
+      this.setState({
+        isSubNavFixed: true,
+      });
+
+      return;
+    }
+
+    this.setState({
+      activeSubNav: subNavName,
+      isSubNavFixed: true,
+    });
+  };
+
+  handleOnClickClose = event => {
+    this.analyzeEvent(event);
+
+    this.setState({
+      activeSubNav: null,
+      isSubNavFixed: false,
+    });
+  };
+
+  render() {
+    return (
+      <nav role="navigation" id="nav" className="nav">
+        <div className="wrapper base-12-grid">
+          <div className="logo-nav">
+            <a href="/">
+              <DoSomethingLogo />
+            </a>
           </div>
+
+          <ul className="main-nav menu-nav">
+            <li
+              className={classnames('menu-nav__item', {
+                'is-active': this.state.activeSubNav === 'CausesSubNav',
+              })}
+              onMouseEnter={() => this.handleMouseEnter('CausesSubNav')}
+              onMouseLeave={() => this.handleMouseLeave('CausesSubNav')}
+            >
+              <a
+                href="/"
+                data-label="causes"
+                onClick={event =>
+                  this.handleOnClickToggle(event, 'CausesSubNav')
+                }
+              >
+                Causes
+              </a>
+
+              {this.state.activeSubNav === 'CausesSubNav' ? (
+                <div className="main-subnav menu-subnav">
+                  <div className="wrapper base-12-grid">
+                    <section className="main-subnav__links-causes menu-subnav__links">
+                      <h1>
+                        <a href="/">All Causes</a>
+                      </h1>
+                      <ul>
+                        <li>
+                          <a href="/">Education</a>
+                        </li>
+                        <li>
+                          <a href="/">Mental Health</a>
+                        </li>
+                        <li>
+                          <a href="/">Homelessness & Poverty</a>
+                        </li>
+                        <li>
+                          <a href="/">Environment</a>
+                        </li>
+                        <li>
+                          <a href="/">Animal Welfare</a>
+                        </li>
+                        <li>
+                          <a href="/">Sexual Harassment</a>
+                        </li>
+                        <li>
+                          <a href="/">Bullying</a>
+                        </li>
+                        <li>
+                          <a href="/">Gender Rights</a>
+                        </li>
+                        <li>
+                          <a href="/">Racial Justice</a>
+                        </li>
+                        <li>
+                          <a href="/">Immigration & Refugees</a>
+                        </li>
+                        <li>
+                          <a href="/">LGBT+ Rights</a>
+                        </li>
+                        <li>
+                          <a href="/">Get Out the Vote!</a>
+                        </li>
+                      </ul>
+                    </section>
+
+                    <section className="main-subnav__links-campaigns menu-subnav__links">
+                      <h1>
+                        <a href="/">All Campaigns</a>
+                      </h1>
+                      <ul>
+                        <li>
+                          <a href="/">Online</a>
+                        </li>
+                        <li>
+                          <a href="/">In-person</a>
+                        </li>
+                        <li>
+                          <a href="/">Petitions</a>
+                        </li>
+                        <li>
+                          <a href="/">Collections</a>
+                        </li>
+                      </ul>
+                    </section>
+
+                    <section className="main-subnav__featured menu-subnav__content">
+                      <img
+                        className="mb-4"
+                        src="http://placekitten.com/g/550/250"
+                        alt="temporary place kitten"
+                      />
+                      <h1>Take Back the Kittens</h1>
+                      <p>Donec ullamcorper nulla non metus auctor fringilla.</p>
+                      <a href="/">Learn More</a>
+                    </section>
+
+                    <CloseButton
+                      callback={this.handleOnClickClose}
+                      className="btn__close--subnav btn__close--main-subnav block p-1"
+                      dataLabel="close_subnav"
+                      dataNoun="nav_button"
+                      size="22px"
+                    />
+                  </div>
+                </div>
+              ) : null}
+            </li>
+
+            <li className="menu-nav__item">
+              <a
+                href="/"
+                data-label="scholarships"
+                onClick={this.handleOnClickLink}
+              >
+                Scholarships
+              </a>
+            </li>
+
+            <li className="menu-nav__item">
+              <a
+                href="/"
+                data-label="articles"
+                onClick={this.handleOnClickLink}
+              >
+                Articles
+              </a>
+            </li>
+
+            <li className="menu-nav__item">
+              <a href="/" data-label="about" onClick={this.handleOnClickLink}>
+                About
+              </a>
+            </li>
+          </ul>
+
+          <ul className="utility-nav menu-nav">
+            <li className="utility-nav__search menu-nav__item">
+              <a
+                href="/"
+                className="utility-nav__search-icon"
+                onClick={event =>
+                  this.handleOnClickToggle(event, 'SearchSubNav')
+                }
+              >
+                <img src={searchIcon} alt="search icon" />
+              </a>
+
+              {this.state.activeSubNav === 'SearchSubNav' ? (
+                <form
+                  className="utility-subnav menu-subnav"
+                  id="utility-subnav__search"
+                >
+                  <div className="wrapper base-12-grid">
+                    <div className="search">
+                      <input type="text" />
+                    </div>
+
+                    <div className="top-searches">
+                      <h1>Top Searches</h1>
+                      <ul>
+                        <li>
+                          <a href="https://www.dosomething.org/us/search?query=scholarships">
+                            scholarships
+                          </a>
+                        </li>
+
+                        <li>
+                          <a href="https://www.dosomething.org/us/search?query=cyberbullying">
+                            cyberbullying
+                          </a>
+                        </li>
+
+                        <li>
+                          <a href="https://www.dosomething.org/us/search?query=gun+violence">
+                            gun violence
+                          </a>
+                        </li>
+
+                        <li>
+                          <a href="https://www.dosomething.org/us/search?query=climate+change">
+                            climate change
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+
+                    <CloseButton
+                      callback={this.handleOnClickClose}
+                      className="btn__close--subnav btn__close--search-subnav block p-1"
+                      dataLabel="close_subnav"
+                      dataNoun="nav_button"
+                      size="22px"
+                    />
+                  </div>
+                </form>
+              ) : null}
+            </li>
+
+            <li className="utility-nav__auth menu-nav__item">
+              <a href="/" data-label="log-in" onClick={this.analyzeEvent}>
+                Log In
+              </a>
+            </li>
+
+            <li className="utility-nav__join menu-nav__item">
+              <a href="/" data-label="join_now" onClick={this.analyzeEvent}>
+                Join Now
+              </a>
+            </li>
+          </ul>
         </div>
-      </li>
 
-      <li className="menu-nav__item">
-        <a href="/">Scholarships</a>
-      </li>
-
-      <li className="menu-nav__item">
-        <a href="/">Articles</a>
-      </li>
-
-      <li className="menu-nav__item">
-        <a href="/">About</a>
-      </li>
-    </ul>
-
-    <ul className="utility-nav menu-nav">
-      <li className="utility-nav__search menu-nav__item">
-        <a href="/" className="utility-nav__search-icon">
-          <img src={searchIcon} alt="search icon" />
-        </a>
-
-        <form className="utility-subnav menu-subnav">
-          <div className="wrapper base-12-grid">
-            <div className="search">
-              <input type="text" />
-            </div>
-
-            <div className="top-searches">
-              <h1>Top Searches</h1>
-              <ul>
-                <li>
-                  <a href="https://www.dosomething.org/us/search?query=scholarships">
-                    scholarships
-                  </a>
-                </li>
-
-                <li>
-                  <a href="https://www.dosomething.org/us/search?query=cyberbullying">
-                    cyberbullying
-                  </a>
-                </li>
-
-                <li>
-                  <a href="https://www.dosomething.org/us/search?query=gun+violence">
-                    gun violence
-                  </a>
-                </li>
-
-                <li>
-                  <a href="https://www.dosomething.org/us/search?query=climate+change">
-                    climate change
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </form>
-      </li>
-
-      <li className="utility-nav__auth menu-nav__item">
-        <a href="/">Log In</a>
-      </li>
-
-      <li className="utility-nav__join menu-nav__item">
-        <a href="/">Join Now</a>
-      </li>
-    </ul>
-  </nav>
-);
+        {this.state.activeSubNav ? (
+          <div
+            className="underlay"
+            data-label="underlay_close_subnav"
+            data-noun="nav_element"
+            onClick={this.handleOnClickClose}
+            role="button"
+          />
+        ) : null}
+      </nav>
+    );
+  }
+}
 
 export default SiteNavigation;
