631236f440f50e4cdb8194d7ded8236d7bf9ad5b
Diego Lorenzo <diego@diegolorenzo.com>
2019-10-24 10:51:47 -0400

    Meganav cleanup and analytics (#1654)
    * Removing old, no longer used code.
    
    * Cleaning up components and adding more analytics.
    
    * Finalizing analytics for search in Meganav.
    
    * Adding analytics event for log out link.
    
    * Updating analytics event for Log Out link.
    
    * Fixes based on feedback.

--- resources/assets/components/utilities/DoSomethingLogo/DoSomethingLogo.js
+++ resources/assets/components/utilities/DoSomethingLogo/DoSomethingLogo.js
@@ -3,7 +3,11 @@ import React from 'react';
 import dosomethingLogo from './dosomething_logo.svg';
 
 const DoSomethingLogo = () => (
-  <img src={dosomethingLogo} alt="DoSomething logo" />
+  <img
+    src={dosomethingLogo}
+    alt="DoSomething logo"
+    style={{ pointerEvents: 'none' }}
+  />
 );
 
 export default DoSomethingLogo;
