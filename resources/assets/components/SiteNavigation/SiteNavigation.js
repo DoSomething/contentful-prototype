aea5f27e8c9fe44b1563c9a838c1224d307e1479
katiecrane <kcrane@dosomething.org>
2019-10-30 13:12:43 -0700

    Boo Something logo component

--- resources/assets/components/SiteNavigation/SiteNavigation.js
+++ resources/assets/components/SiteNavigation/SiteNavigation.js
@@ -11,6 +11,7 @@ import CloseButton from '../artifacts/CloseButton/CloseButton';
 import ProfileIcon from '../artifacts/ProfileIcon/ProfileIcon';
 // @TODO: After Halloween 2019, uncomment the next line and delete this line
 // import DoSomethingLogo from '../utilities/DoSomethingLogo/DoSomethingLogo';
+import BooSomethingLogo from '../utilities/DoSomethingLogo/BooSomethingLogo';
 import {
   trackAnalyticsEvent,
   getUtmContext,
@@ -156,8 +157,8 @@ class SiteNavigation extends React.Component {
               href="/"
               onClick={e => this.handleOnClickLink(e, { label: 'homepage' })}
             >
-              {/* @TODO: After Halloween 2019, uncomment the next line and delete this line */}
-              {/* <DoSomethingLogo /> */}
+              {/* @TODO: After Halloween 2019, change this back to DoSomethingLogo */}
+              <BooSomethingLogo />
             </a>
           </div>
 
