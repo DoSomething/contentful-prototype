4294b2852167ddf084a664f6e4f542f57c3e1798
Aaron Schachter <aaronschachter@gmail.com>
2019-10-29 18:56:42 -0700

    Adds react-select

--- resources/assets/components/utilities/SchoolFinder/SchoolFinder.js
+++ resources/assets/components/utilities/SchoolFinder/SchoolFinder.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Select from 'react-select';
 
 import Card from '../Card/Card';
 import TextContent from '../TextContent/TextContent';
 
+const colourOptions = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
+
 const SchoolFinder = ({ campaignId, userId }) => {
-  console.log(campaignId, userId);
+  if (campaignId !== '9001') {
+    return null;
+  }
+
+  console.log(userId);
 
   return (
     <div className="margin-bottom-lg margin-horizontal-md clear-both primary">
       <Card title="Find Your School" className="rounded bordered">
         <TextContent className="padded">Testing</TextContent>
+        <Select options={colourOptions} />
       </Card>
     </div>
   );
