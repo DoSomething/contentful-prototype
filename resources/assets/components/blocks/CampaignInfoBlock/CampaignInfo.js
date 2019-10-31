c593093e4d3885aaaaeeb81760f4d91a5d2fabe2
Lindsay Maher <lmaher@Lindsays-MBP.home>
2019-10-31 12:29:44 -0400

    moves campaign info card to marquee template

--- resources/assets/components/blocks/CampaignInfoBlock/CampaignInfo.js
+++ resources/assets/components/blocks/CampaignInfoBlock/CampaignInfo.js
@@ -11,7 +11,7 @@ import defineCampaignActionInfo from '../../../helpers/campaign-actions';
  * The GraphQL query to load data for this component.
  */
 const CAMPAIGN_INFO_QUERY = gql`
-  query CampaignInfoQuery($campaignId: String!) {
+  query CampaignInfoQuery($campaignId: Int!) {
     campaign(id: $campaignId) {
       endDate
       actions {
@@ -24,66 +24,69 @@ const CAMPAIGN_INFO_QUERY = gql`
   }
 `;
 
-const CampaignInfo = ({ campaignId, scholarshipAmount }) => (
-  <Card className="bordered padded rounded campaign-info">
-    <h1 className="mb-4 text-m uppercase">Campaign Info</h1>
-    <dl className="clearfix">
-      <Query query={CAMPAIGN_INFO_QUERY} variables={campaignId}>
-        {/* is scholarship end date always the same as campaign end date???? */}
-        {res => {
-          const endDate = res.campaign.endDate;
-          const actions = res.campaign.actions
-            ? res.campaign.actions.filter(
-                action => action.scholarshipEntry && action.reportback,
-              )
-            : [];
-          const timeCommitment = defineCampaignActionInfo(
-            actions,
-            'timeCommitmentLabel',
-          );
-          const actionType = defineCampaignActionInfo(actions, 'actionLabel');
-          return (
-            <>
-              <dt>Deadline</dt>
-              <dd>
-                {endDate
-                  ? format(endDate, 'MMMM do, yyyy', {
-                      awareOfUnicodeTokens: true,
-                    })
-                  : 'Evergreen'}
-              </dd>
-              {timeCommitment ? (
-                <React.Fragment>
-                  <dt>Time</dt>
-                  <dd>{timeCommitment}</dd>
-                </React.Fragment>
-              ) : null}
-              {actionType ? (
-                <React.Fragment>
-                  <dt>Action Type</dt>
-                  <dd>{actionType}</dd>
-                </React.Fragment>
-              ) : null}
-              {scholarshipAmount ? (
-                <React.Fragment>
-                  <dt className="campaign-info__scholarship">
-                    Win A Scholarship
-                  </dt>
-                  <dd className="campaign-info__scholarship">
-                    {`$${scholarshipAmount}`}
-                  </dd>
-                </React.Fragment>
-              ) : null}
-            </>
-          );
-        }}
-      </Query>
-    </dl>
-  </Card>
-);
+const CampaignInfo = ({ campaignId, scholarshipAmount }) => {
+  console.log('this is the campaignId', campaignId);
+  return (
+    <Card className="bordered padded rounded campaign-info">
+      <h1 className="mb-4 text-m uppercase">Campaign Info</h1>
+      <dl className="clearfix">
+        <Query query={CAMPAIGN_INFO_QUERY} variables={{ campaignId }}>
+          {/* is scholarship end date always the same as campaign end date???? */}
+          {res => {
+            const endDate = res.campaign.endDate;
+            const actions = res.campaign.actions
+              ? res.campaign.actions.filter(
+                  action => action.scholarshipEntry && action.reportback,
+                )
+              : [];
+            const timeCommitment = defineCampaignActionInfo(
+              actions,
+              'timeCommitmentLabel',
+            );
+            const actionType = defineCampaignActionInfo(actions, 'actionLabel');
+            return (
+              <>
+                <dt>Deadline</dt>
+                <dd>
+                  {endDate
+                    ? format(String(endDate), 'MMMM do, yyyy', {
+                        awareOfUnicodeTokens: true,
+                      })
+                    : 'Evergreen'}
+                </dd>
+                {timeCommitment ? (
+                  <React.Fragment>
+                    <dt>Time</dt>
+                    <dd>{timeCommitment}</dd>
+                  </React.Fragment>
+                ) : null}
+                {actionType ? (
+                  <React.Fragment>
+                    <dt>Action Type</dt>
+                    <dd>{actionType}</dd>
+                  </React.Fragment>
+                ) : null}
+                {scholarshipAmount ? (
+                  <React.Fragment>
+                    <dt className="campaign-info__scholarship">
+                      Win A Scholarship
+                    </dt>
+                    <dd className="campaign-info__scholarship">
+                      {`$${scholarshipAmount}`}
+                    </dd>
+                  </React.Fragment>
+                ) : null}
+              </>
+            );
+          }}
+        </Query>
+      </dl>
+    </Card>
+  );
+};
 
 CampaignInfo.propTypes = {
-  campaignId: PropTypes.string.isRequired,
+  campaignId: PropTypes.number.isRequired,
   scholarshipAmount: PropTypes.number,
 };
 
--- resources/assets/components/blocks/CampaignInfoBlock/CampaignInfoContainer.js
+++ resources/assets/components/blocks/CampaignInfoBlock/CampaignInfoContainer.js
@@ -5,9 +5,12 @@ import CampaignInfo from './CampaignInfo';
  * Provide state from the Redux store as props for this component. (In
  * this case, we just need the campaign ID for our GraphQL query!)
  */
-const mapStateToProps = state => ({
-  campaignId: String(state.campaign.campaignId),
-});
+const mapStateToProps = state => {
+  console.log('campaignId', state.campaign.campaignId);
+  return {
+    campaignId: parseInt(state.campaign.campaignId, 10),
+  };
+};
 
 // Export the Redux container component.
 export default connect(mapStateToProps)(CampaignInfo);
