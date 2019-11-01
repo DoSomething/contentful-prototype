2c75e3f63777fc7fa524b692b04d3e6992f150d3
Lindsay Maher <lindsaymah3r@gmail.com>
2019-11-01 10:44:37 -0400

    Campaign info card pt 1 (#1671)
    * creates info component, writes query string for component
    
    * adds campaign info container and renames some files
    
    * adds minimal component structuring with Query
    
    * creates filters for specific action types, adjusts placement of react fragments and adds a helper func
    
    * moves campaign info card to marquee template
    
    * fixes graphql loader bug
    
    * simplifies the action assignment
    
    * removes helper
    
    * renames CampaignInfo file and components
    
    * cha cha cha changes

---
+++ resources/assets/components/blocks/CampaignInfoBlock/CampaignInfoBlock.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { format } from 'date-fns';
+import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
+
+import Query from '../../Query';
+import Card from '../../utilities/Card/Card';
+
+/**
+ * The GraphQL query to load data for this component.
+ */
+const CAMPAIGN_INFO_QUERY = gql`
+  query CampaignInfoQuery($campaignId: Int!) {
+    campaign(id: $campaignId) {
+      id
+      endDate
+      actions {
+        actionLabel
+        timeCommitmentLabel
+        scholarshipEntry
+        reportback
+      }
+    }
+  }
+`;
+
+const CampaignInfoBlock = ({ campaignId, scholarshipAmount }) => (
+  <Card className="bordered padded rounded campaign-info">
+    <h1 className="mb-4 text-m uppercase">Campaign Info</h1>
+    <dl className="clearfix">
+      <Query query={CAMPAIGN_INFO_QUERY} variables={{ campaignId }}>
+        {res => {
+          const endDate = res.campaign.endDate;
+          const actions = res.campaign.actions
+            ? res.campaign.actions.filter(
+                action => action.scholarshipEntry && action.reportback,
+              )
+            : [];
+          const action = actions.length ? actions[0] : null;
+
+          return (
+            <>
+              <dt>Deadline</dt>
+              <dd>
+                {endDate
+                  ? format(String(endDate), 'MMMM do, yyyy', {
+                      awareOfUnicodeTokens: true,
+                    })
+                  : 'Evergreen'}
+              </dd>
+              {action && action.timeCommitmentLabel ? (
+                <React.Fragment>
+                  <dt>Time</dt>
+                  <dd>{action.timeCommitmentLabel}</dd>
+                </React.Fragment>
+              ) : null}
+              {action && action.actionLabel ? (
+                <React.Fragment>
+                  <dt>Action Type</dt>
+                  <dd>{action.actionLabel}</dd>
+                </React.Fragment>
+              ) : null}
+              {scholarshipAmount ? (
+                <React.Fragment>
+                  <dt className="campaign-info__scholarship">
+                    Win A Scholarship
+                  </dt>
+                  <dd className="campaign-info__scholarship">
+                    {`$${scholarshipAmount}`}
+                  </dd>
+                </React.Fragment>
+              ) : null}
+            </>
+          );
+        }}
+      </Query>
+    </dl>
+  </Card>
+);
+
+CampaignInfoBlock.propTypes = {
+  campaignId: PropTypes.number.isRequired,
+  scholarshipAmount: PropTypes.number,
+};
+
+CampaignInfoBlock.defaultProps = {
+  scholarshipAmount: null,
+};
+
+export default CampaignInfoBlock;
---
+++ resources/assets/components/blocks/CampaignInfoBlock/CampaignInfoBlockContainer.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+
+import CampaignInfoBlock from './CampaignInfoBlock';
+
+/**
+ * Provide state from the Redux store as props for this component. (In
+ * this case, we just need the campaign ID for our GraphQL query!)
+ */
+const mapStateToProps = state => ({
+  campaignId: parseInt(state.campaign.campaignId, 10),
+});
+
+// Export the Redux container component.
+export default connect(mapStateToProps)(CampaignInfoBlock);
