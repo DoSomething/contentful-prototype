273e17d2c8699b208e9e341eb905497c792bde77
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2019-11-01 10:35:35 -0400

    cha cha cha changes

--- resources/assets/components/blocks/CampaignInfoBlock/CampaignInfoBlockContainer.js
+++ resources/assets/components/blocks/CampaignInfoBlock/CampaignInfoBlockContainer.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 
 import CampaignInfoBlock from './CampaignInfoBlock';
+
 /**
  * Provide state from the Redux store as props for this component. (In
  * this case, we just need the campaign ID for our GraphQL query!)
