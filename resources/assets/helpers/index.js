b52ba512b9f012313705ea71ef0d4d47ebf9a4df
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-11-04 16:47:01 -0500

    contentfulImageUrl helper update (#1676)
    * Move data formatting helpers higher on the page
    
    * Refactor contentfulImageUrl to support pre-existing query params
    
    * Update tests to use valid image url

--- resources/assets/helpers/index.js
+++ resources/assets/helpers/index.js
@@ -86,6 +86,37 @@ export function isEmptyArray(data) {
 }
 
 /**
+ * Remove items with null properties.
+ * Helps with React defaultProps.
+ *
+ * @param  {Object} data
+ * @return {Object}
+ */
+export function withoutNulls(data) {
+  return omitBy(data, isNull);
+}
+
+/**
+ * Remove items with undefined properties.
+ *
+ * @param  {Object} data
+ * @return {Object}
+ */
+export function withoutUndefined(data) {
+  return omitBy(data, isUndefined);
+}
+
+/**
+ * Remove items from object with null, undefined, or empty string values.
+ *
+ * @param  {Object} data
+ * @return {Object}
+ */
+export function withoutValueless(data) {
+  return omitBy(omitBy(omitBy(data, isNil), isEmptyArray), isEmptyString);
+}
+
+/**
  * Generate a Contentful Image URL with added url parameters.
  *
  * @param  {String} url
@@ -104,21 +135,13 @@ export function contentfulImageUrl(
     return undefined;
   }
 
-  const params = [];
+  const params = withoutNulls({
+    w: width, // eslint-disable-line id-length
+    h: height, // eslint-disable-line id-length
+    fit,
+  });
 
-  if (width) {
-    params.push(`w=${width}`);
-  }
-
-  if (height) {
-    params.push(`h=${height}`);
-  }
-
-  if (fit) {
-    params.push(`fit=${fit}`);
-  }
-
-  return params.length ? `${url}?${params.join('&')}` : url;
+  return Object.keys(params).length ? appendToQuery(params, url).href : url;
 }
 
 /**
@@ -834,37 +857,6 @@ export function stringifyNestedObjects(data) {
 
     return value;
   });
-}
-
-/**
- * Remove items with null properties.
- * Helps with React defaultProps.
- *
- * @param  {Object} data
- * @return {Object}
- */
-export function withoutNulls(data) {
-  return omitBy(data, isNull);
-}
-
-/**
- * Remove items with undefined properties.
- *
- * @param  {Object} data
- * @return {Object}
- */
-export function withoutUndefined(data) {
-  return omitBy(data, isUndefined);
-}
-
-/**
- * Remove items from object with null, undefined, or empty string values.
- *
- * @param  {Object} data
- * @return {Object}
- */
-export function withoutValueless(data) {
-  return omitBy(omitBy(omitBy(data, isNil), isEmptyArray), isEmptyString);
 }
 
 /**
--- resources/assets/helpers/test.js
+++ resources/assets/helpers/test.js
@@ -12,35 +12,37 @@ import {
  */
 test('generate contentful image url with defaults', () => {
   const contentfulImage = contentfulImageUrl(
-    '//images.contentful.com/somecrazystring.jpg',
+    'http://images.contentful.com/somecrazystring.jpg',
   );
 
-  expect(contentfulImage).toBe('//images.contentful.com/somecrazystring.jpg');
+  expect(contentfulImage).toBe(
+    'http://images.contentful.com/somecrazystring.jpg',
+  );
 });
 
 test('generate contentful image url with specified fit', () => {
   const contentfulImage = contentfulImageUrl(
-    '//images.contentful.com/somecrazystring.jpg',
+    'http://images.contentful.com/somecrazystring.jpg',
     undefined,
     undefined,
     'fill',
   );
 
   expect(contentfulImage).toBe(
-    '//images.contentful.com/somecrazystring.jpg?fit=fill',
+    'http://images.contentful.com/somecrazystring.jpg?fit=fill',
   );
 });
 
 test('generate contentful image url with all specified parameters', () => {
   const contentfulImage = contentfulImageUrl(
-    '//images.contentful.com/somecrazystring.jpg',
+    'http://images.contentful.com/somecrazystring.jpg',
     800,
     600,
     'fill',
   );
 
   expect(contentfulImage).toBe(
-    '//images.contentful.com/somecrazystring.jpg?w=800&h=600&fit=fill',
+    'http://images.contentful.com/somecrazystring.jpg?fit=fill&h=600&w=800',
   );
 });
 
