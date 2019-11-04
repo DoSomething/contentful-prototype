b42cc3bd951dd5cac7e35452a8db2af1c20dcc7c
Aaron Schachter <aaronschachter@gmail.com>
2019-11-04 10:10:27 -0800

    Check for empty input before querying

--- resources/assets/components/utilities/SchoolFinder/SchoolSelect.js
+++ resources/assets/components/utilities/SchoolFinder/SchoolSelect.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 import AsyncSelect from 'react-select/async';
 
 const SchoolSelect = ({ filterByState, onChange }) => {
-  // Debounce school search to query after 250 ms typing pause.
+  // Debounce school search to query for schools after 250 ms typing pause.
   // @see https://github.com/JedWatson/react-select/issues/614#issuecomment-244006496
-  const fetchSchools = debounce((searchString, callback) => {
+  const debouncedFetch = debounce((searchString, callback) => {
     fetch(
       `https://lofischools.herokuapp.com/search?state=${filterByState}&query=${searchString}`,
     )
@@ -22,7 +22,14 @@ const SchoolSelect = ({ filterByState, onChange }) => {
         `${school.name} - ${school.city}, ${school.state}`
       }
       getOptionValue={school => school.id}
-      loadOptions={fetchSchools}
+      loadOptions={(input, callback) => {
+        // Avoid querying by empty school name on page load.
+        // @see https://github.com/JedWatson/react-select/issues/614#issuecomment-380763225
+        if (!input) {
+          return Promise.resolve([]);
+        }
+        return debouncedFetch(input, callback);
+      }}
       placeholder="Enter your school name"
       noOptionsMessage={select =>
         select.inputValue.length > 1
