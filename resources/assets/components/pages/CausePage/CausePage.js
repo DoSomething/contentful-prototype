db902775abe13d3635d84a477c8c4db347d37edc
Luke Patton <lpatton@dosomething.org>
2019-11-06 14:42:50 -0500

    Fleshes out Cause page with grid and ulitiy classes

--- resources/assets/components/pages/CausePage/CausePage.js
+++ resources/assets/components/pages/CausePage/CausePage.js
@@ -2,22 +2,56 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import TextContent from '../../utilities/TextContent/TextContent';
+import { contentfulImageUrl, withoutNulls } from '../../../helpers';
+import SiteNavigationContainer from '../../SiteNavigation/SiteNavigationContainer';
 
-const CausePage = ({ coverImage, superTitle, title, description, content }) => (
-  <div>
-    <img src={coverImage.url} alt={coverImage.description} />
-    <p>{superTitle}</p>
-    <h1>{title}</h1>
-    <TextContent>{description}</TextContent>
+import './cause-page.scss';
 
-    <TextContent>{content}</TextContent>
-  </div>
-);
+const CausePage = ({ coverImage, superTitle, title, description, content }) => {
+  const backgroundImage = coverImage
+    ? `url(${contentfulImageUrl(coverImage.url, '1440', '610', 'fill')})`
+    : null;
+
+  const styles = {
+    backgroundImage,
+  };
+
+  return (
+    <>
+      <SiteNavigationContainer />
+
+      <article className="cause-page">
+        <header
+          role="banner"
+          className="lede-banner base-12-grid"
+          style={withoutNulls(styles)}
+        >
+          <div className="title-lockup pt-8">
+            <h2 className="my-4 uppercase color-white text-lg">{superTitle}</h2>
+            <h1 className="lede-banner__headline-title my-4 font-normal font-secondary color-white caps-lock">
+              {title}
+            </h1>
+            <TextContent styles={{ textColor: '#FFF' }}>
+              {description}
+            </TextContent>
+          </div>
+        </header>
+        <TextContent
+          className="base-12-grid"
+          classNameByEntry={{
+            GalleryBlock: 'grid-full',
+          }}
+        >
+          {content}
+        </TextContent>
+      </article>
+    </>
+  );
+};
 
 CausePage.propTypes = {
   coverImage: PropTypes.shape({
     url: PropTypes.string.isRequired,
-    description: PropTypes.string,
   }).isRequired,
   superTitle: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
