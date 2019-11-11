a24afa6b89306bb856ab34890654dfa61850cabe
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-11-11 15:43:22 -0500

    Set the grid columns for Content blocks (#1699)

--- resources/assets/components/pages/CausePage/CausePage.js
+++ resources/assets/components/pages/CausePage/CausePage.js
@@ -42,6 +42,7 @@ const CausePage = ({ coverImage, superTitle, title, description, content }) => {
           className="base-12-grid"
           classNameByEntry={{
             GalleryBlock: 'grid-full',
+            ContentBlock: 'content-block-grid',
           }}
         >
           {content}
