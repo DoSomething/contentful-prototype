727857bbd7460d7aef3316e85ab6ba25c75e90cd
Aaron Schachter <aaronschachter@gmail.com>
2019-11-12 09:42:44 -0800

    How is this possible

--- cypress/fixtures/graphql.js
+++ cypress/fixtures/graphql.js
@@ -28,8 +28,8 @@ export const operations = {
   },
   EmbedQuery: {
     embed: {
-      title: faker.lorem.words(),
-      description: faker.lorem.sentence(),
+      title: 'Example Campaign',
+      description: 'This is an example campaign for automated testing.',
     },
   },
   ReferralPageCampaignQuery: {
