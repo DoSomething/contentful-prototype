dc63acfb1d3455a99ec96415dbbd8bda6ba89d55
Aaron Schachter <aaronschachter@gmail.com>
2019-11-21 13:33:24 -0800

    DRY TextSubmissionAction with PostCreatedModal, ActionInformation

--- resources/assets/components/actions/TextSubmissionAction/TextSubmissionAction.js
+++ resources/assets/components/actions/TextSubmissionAction/TextSubmissionAction.js
@@ -6,35 +6,19 @@ import { get, has, invert, mapValues } from 'lodash';
 import { PuckWaypoint } from '@dosomething/puck-client';
 
 import Card from '../../utilities/Card/Card';
-import Modal from '../../utilities/Modal/Modal';
 import Button from '../../utilities/Button/Button';
+import PostCreatedModal from '../PostCreatedModal';
+import ActionInformation from '../ActionInformation';
 import { trackAnalyticsEvent } from '../../../helpers/analytics';
 import FormValidation from '../../utilities/Form/FormValidation';
 import { withoutUndefined, withoutNulls } from '../../../helpers';
-import TextContent from '../../utilities/TextContent/TextContent';
 import { getFieldErrors, formatPostPayload } from '../../../helpers/forms';
 import CharacterLimit from '../../utilities/CharacterLimit/CharacterLimit';
 import PrivacyLanguage from '../../utilities/PrivacyLanguage/PrivacyLanguage';
-import Badge from '../../pages/AccountPage/Badge';
-import Query from '../../Query';
 
 import './text-submission-action.scss';
 
 const CHARACTER_LIMIT = 500;
-
-const BADGE_QUERY = gql`
-  query AccountQuery($userId: String!) {
-    user(id: $userId) {
-      hasBadgesFlag: hasFeatureFlag(feature: "badges")
-    }
-  }
-`;
-
-const POST_COUNT_BADGE = gql`
-  query PostsCountQuery($userId: String!) {
-    postsCount(userId: $userId, limit: 4)
-  }
-`;
 
 export const TextSubmissionBlockFragment = gql`
   fragment TextSubmissionBlockFragment on TextSubmissionBlock {
@@ -218,114 +202,24 @@ class TextSubmissionAction extends React.Component {
         </div>
 
         {this.props.informationContent ? (
-          <div
+          <ActionInformation
             className={classnames(
               'text-submission-information',
               this.props.className,
             )}
-          >
-            <Card
-              className="bordered rounded"
-              title={this.props.informationTitle}
-            >
-              <TextContent className="p-3">
-                {this.props.informationContent}
-              </TextContent>
-            </Card>
-          </div>
+            title={this.props.informationTitle}
+            content={this.props.informationContent}
+          />
         ) : null}
 
         {this.state.showModal ? (
-          <Modal
+          <PostCreatedModal
+            affirmationContent={this.props.affirmationContent}
             context={context}
             onClose={() => this.setState({ showModal: false })}
-          >
-            <Card className="bordered rounded" title="We got your message!">
-              <Query
-                query={BADGE_QUERY}
-                variables={{ userId: this.props.userId }}
-                hideSpinner
-              >
-                {badgeData =>
-                  get(badgeData, 'user.hasBadgesFlag', false) ? (
-                    <Query
-                      query={POST_COUNT_BADGE}
-                      variables={{ userId: this.props.userId }}
-                      hideSpinner
-                    >
-                      {postData => {
-                        if (postData.postsCount === 1) {
-                          return (
-                            <Badge
-                              earned
-                              className="badge p-3"
-                              size="medium"
-                              name="onePostBadge"
-                            >
-                              <h4>1 Action</h4>
-                              <p>
-                                Ohhh HECK yes! You just earned a new badge for
-                                completing your first campaign. Congratulations!
-                              </p>
-                              <a href="/us/account/profile/badges">
-                                View all my badges
-                              </a>
-                            </Badge>
-                          );
-                        }
-                        if (postData.postsCount === 2) {
-                          return (
-                            <Badge
-                              earned
-                              className="badge p-3"
-                              size="medium"
-                              name="twoPostsBadge"
-                            >
-                              <h4>2 Actions</h4>
-                              <p>
-                                Ohhh HECK yes! You just earned a new badge for
-                                completing your second campaign.
-                                Congratulations!
-                              </p>
-                              <a href="/us/account/profile/badges">
-                                View all my badges
-                              </a>
-                            </Badge>
-                          );
-                        }
-                        if (postData.postsCount === 3) {
-                          return (
-                            <Badge
-                              earned
-                              className="badge p-3"
-                              size="medium"
-                              name="threePostsBadge"
-                            >
-                              <h4>3 Actions</h4>
-                              <p>
-                                Ohhh HECK yes! You just earned a new badge for
-                                completing your third campaign. Congratulations!
-                              </p>
-                              <a href="/us/account/profile/badges">
-                                View all my badges
-                              </a>
-                            </Badge>
-                          );
-                        }
-
-                        return null;
-                      }}
-                    </Query>
-                  ) : null
-                }
-              </Query>
-
-              <TextContent className="p-3">
-                {this.props.affirmationContent ||
-                  TextSubmissionAction.defaultProps.affirmationContent}
-              </TextContent>
-            </Card>
-          </Modal>
+            title="We got your message!"
+            userId={this.props.userId}
+          />
         ) : null}
       </React.Fragment>
     );
