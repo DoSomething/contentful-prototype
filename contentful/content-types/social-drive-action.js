09ca372d7f851fda7fc1e955279e5db6fd0e6a8f
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-11-21 13:31:54 -0500

    Oops - properly add the social drive action migration

--- contentful/content-types/social-drive-action.js
+++ contentful/content-types/social-drive-action.js
@@ -1,51 +1,51 @@
 module.exports = function(migration) {
   const socialDriveAction = migration
-    .createContentType("socialDriveAction")
-    .name("Social Drive Action")
+    .createContentType('socialDriveAction')
+    .name('Social Drive Action')
     .description(
-      "An Action block for social drives for link sharing and page view tracking."
+      'An Action block for social drives for link sharing and page view tracking.',
     )
-    .displayField("internalTitle");
+    .displayField('internalTitle');
 
   socialDriveAction
-    .createField("internalTitle")
-    .name("Internal Title")
-    .type("Symbol")
+    .createField('internalTitle')
+    .name('Internal Title')
+    .type('Symbol')
     .localized(false)
     .required(true)
     .validations([])
     .disabled(false)
     .omitted(false);
   socialDriveAction
-    .createField("link")
-    .name("Link")
-    .type("Symbol")
+    .createField('link')
+    .name('Link')
+    .type('Symbol')
     .localized(false)
     .required(true)
     .validations([])
     .disabled(false)
     .omitted(false);
   socialDriveAction
-    .createField("hidePageViews")
-    .name("Hide Page Views")
-    .type("Boolean")
+    .createField('hidePageViews')
+    .name('Hide Page Views')
+    .type('Boolean')
     .localized(false)
     .required(false)
     .validations([])
     .disabled(false)
     .omitted(false);
   socialDriveAction.changeFieldControl(
-    "internalTitle",
-    "builtin",
-    "singleLine",
-    {}
+    'internalTitle',
+    'builtin',
+    'singleLine',
+    {},
   );
-  socialDriveAction.changeFieldControl("link", "builtin", "singleLine", {});
+  socialDriveAction.changeFieldControl('link', 'builtin', 'singleLine', {});
 
-  socialDriveAction.changeFieldControl("hidePageViews", "builtin", "boolean", {
+  socialDriveAction.changeFieldControl('hidePageViews', 'builtin', 'boolean', {
     helpText:
       "By default, we'll display the page views for the provided Bertly link in an info card adjacent to the Social Drive Action. You can toggle this off here!",
-    trueLabel: "Yes",
-    falseLabel: "No"
+    trueLabel: 'Yes',
+    falseLabel: 'No',
   });
 };
