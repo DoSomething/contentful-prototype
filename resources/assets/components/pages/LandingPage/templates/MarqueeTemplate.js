3dd4d1ad2f999745718927101159a07960080d1d
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2019-11-22 10:42:41 -0500

    adds a check for removing footer once a user has signed up!

--- resources/assets/components/pages/LandingPage/templates/MarqueeTemplate.js
+++ resources/assets/components/pages/LandingPage/templates/MarqueeTemplate.js
@@ -119,7 +119,7 @@ const MarqueeTemplate = ({
           </Enclosure>
         </div>
       </article>
-      <CampaignInfoBarContainer />
+      {!isAffiliated ? <CampaignInfoBarContainer /> : null}
     </React.Fragment>
   );
 };
