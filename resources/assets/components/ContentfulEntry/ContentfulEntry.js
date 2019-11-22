22c334bb5500db53d62b1a4074efbcad2aeeb9cc
David Furnes <david@dfurnes.com>
2019-11-22 16:55:22 -0500

    Add ability to load AffirmationBlock via GraphQL. (#1759)
    * Add ability to load AffirmationBlock via GraphQL.
    
    * Fix legacy Affirmation if we are using the default.

--- resources/assets/components/ContentfulEntry/ContentfulEntry.js
+++ resources/assets/components/ContentfulEntry/ContentfulEntry.js
@@ -81,7 +81,21 @@ class ContentfulEntry extends React.Component<Props, State> {
         return (
           <AffirmationContainer
             {...withoutNulls(json.fields)}
+            author={
+              json.fields && json.fields.author
+                ? json.fields.author.fields
+                : undefined
+            }
             onClose={json.onClose}
+          />
+        );
+
+      // @TODO: Need to figure out 'onClose'...
+      case 'AffirmationBlock':
+        return (
+          <AffirmationContainer
+            userId={window.AUTH.id}
+            {...withoutNulls(json)}
           />
         );
 
