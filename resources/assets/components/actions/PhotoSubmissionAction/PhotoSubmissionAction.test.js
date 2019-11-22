672a652cc40aff0016480a2b7bd3460780fcdbbe
Aaron Schachter <aaronschachter@gmail.com>
2019-11-22 12:50:28 -0800

    Revert "Removes jest test for now"
    This reverts commit 5565a9496ad154f17a9b482122a42c2aaa0eea91.

---
+++ resources/assets/components/actions/PhotoSubmissionAction/PhotoSubmissionAction.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import PhotoSubmissionAction from './PhotoSubmissionAction';
+
+jest.mock('../../../helpers/api');
+
+describe('PhotoSubmissionAction component', () => {
+  const id = 'abcdefghi123456789';
+
+  const wrapper = shallow(
+    <PhotoSubmissionAction
+      campaignContentfulId="1"
+      campaignId="1234"
+      id={id}
+      pageId={id}
+      resetPostSubmissionItem={jest.fn()}
+      storeCampaignPost={jest.fn()}
+      submissions={{
+        isPending: false,
+        items: {},
+      }}
+      userId="666655554444333322221111"
+    />,
+  );
+
+  test('is rendered as a card component with a form, MediaUploader and a submit button', () => {
+    expect(wrapper.find('Card').length).toBeGreaterThanOrEqual(1);
+    expect(wrapper.find('form').length).toEqual(1);
+    expect(wrapper.find('MediaUploader').length).toEqual(1);
+    expect(wrapper.find('Button').length).toEqual(1);
+  });
+});
