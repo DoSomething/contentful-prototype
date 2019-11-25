664b3426f1671ae00ef48a361ccb9ab9849b68a7
Katie Crane <kcrane@dosomething.org>
2019-11-25 11:27:58 -0800

    Footer/nav join link updates (#1754)
    * Remove join link from footer
    
    * Change what is ds link
    
    * Change nav about link to join ds

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -28,13 +28,12 @@
     <div class="footer__column -links">
         <h4>Who We Are</h4>
         <ul>
-          <li><a href="{{ config('app.url') }}/us/about/who-we-are">What is DoSomething.org?</a></li>
+          <li><a href="https://join.dosomething.org/">What is DoSomething.org?</a></li>
           <li><a href="{{ config('app.url') }}/us/about/our-people">Our Team</a></li>
           <li><a href="{{ config('app.url') }}/us/about/our-financials">Our Financials</a></li>
           <li><a href="{{ config('app.url') }}/us/about/our-press">Press</a></li>
           <li><a href="https://lets.dosomething.org/">Web Magazine</a></li>
           <li><a href="{{ config('app.url') }}/us/about/contact-us">Contact Us</a></li>
-          <li><a href="https://join.dosomething.org/">Join Now</a></li>
          </ul>
     </div>
     <div class="footer__column -links">
