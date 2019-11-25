7a7724e249ce5c0be7b765fb47925acb1b9f89a1
David Furnes <david@dfurnes.com>
2019-11-25 17:12:42 -0500

    Simplify SoftEdge block & fix error when rendering anonymously.

---
+++ resources/assets/components/actions/SoftEdgeBlock.js
@@ -0,0 +1,96 @@
+/* global window */
+import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
+import { useQuery } from '@apollo/react-hooks';
+import React, { useState, useEffect, useRef } from 'react';
+
+import Card from '../utilities/Card/Card';
+import { makeUrl, withoutNulls, loadScript } from '../../helpers';
+
+export const SoftEdgeBlockFragment = gql`
+  fragment SoftEdgeBlockFragment on SoftEdgeBlock {
+    title
+    actionId
+    softEdgeId
+  }
+`;
+
+const CONGRESSWEB_SDK =
+  '//www.congressweb.com/cweb/js/jquery.congressweb.iframe.js';
+
+const SOFT_EDGE_AUTOFILL_QUERY = gql`
+  query SoftEdgeAutofillQuery($userId: String!) {
+    user(id: $userId) {
+      memberId: id
+      firstName
+      lastName
+      email
+      mobile
+      address: addrStreet1
+      city: addrCity
+      state: addrState
+      zip: addrZip
+    }
+  }
+`;
+
+const SoftEdgeBlock = ({ actionId, softEdgeId, title }) => {
+  const userId = window.AUTH.id;
+
+  const element = useRef(null);
+
+  // 1. Make sure the CongressWeb SDK is ready.
+  const [initialized, setInitialized] = useState(false);
+  useEffect(() => loadScript(CONGRESSWEB_SDK, () => setInitialized(true)), []);
+
+  // 2. If logged in, grab user fields to pre-fill form:
+  const { data, loading } = useQuery(SOFT_EDGE_AUTOFILL_QUERY, {
+    variables: { userId },
+    skip: !userId,
+  });
+
+  // 3. Once #1 & #2 are done, load the IFrame:
+  useEffect(() => {
+    if (!initialized || loading) {
+      return;
+    }
+
+    // We'll encode the user's profile data & action ID into
+    // the query-string for the SoftEdge form to pre-fill:
+    const prefill = data ? data.user : {};
+    const query = withoutNulls({
+      acceptAuthor: true,
+      externalActionId: actionId,
+      ...prefill,
+    });
+
+    // Finally, use the third-party SDK to render into the ref:
+    const baseUrl = `https://www.congressweb.com/dosomething/${softEdgeId}`;
+    window.$cweb(element.current).congressweb({
+      url: makeUrl(baseUrl, query).toString(),
+      responsive: true,
+    });
+  }, [initialized, loading]);
+
+  return (
+    <Card className="bordered rounded" title={title}>
+      {!initialized || loading ? (
+        <div className="spinner mx-auto my-16" />
+      ) : (
+        <div ref={element} />
+      )}
+    </Card>
+  );
+};
+
+SoftEdgeBlock.propTypes = {
+  softEdgeId: PropTypes.number.isRequired,
+  actionId: PropTypes.number.isRequired,
+  title: PropTypes.string,
+};
+
+SoftEdgeBlock.defaultProps = {
+  title: null,
+};
+
+export default SoftEdgeBlock;
