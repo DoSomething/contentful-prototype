0483ed662ad628c3ee7c50c1c0221ae646f2333a
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-11-26 10:06:06 -0500

    Adjust styling for background image tailored to viewport width

--- resources/assets/components/utilities/CtaReferralPageBanner/CtaReferralPageBanner.js
+++ resources/assets/components/utilities/CtaReferralPageBanner/CtaReferralPageBanner.js
@@ -24,8 +24,8 @@ const CtaReferralPageBanner = ({ campaignId }) => (
   <React.Fragment>
     {REFERRAL_CAMPAIGN_IDS.includes(campaignId) ? (
       <div className="p-3">
-        <div className="cta-register-banner p-3 clearfix">
-          <div className="p-3">
+        <div className="cta-register-banner md:px-6 pt-3 clearfix">
+          <div className="cta-register-banner__content p-6 md:pr-0 text-center md:text-left">
             <h3 className="text-white">Benefits With Friends</h3>
             <p className="text-white pb-3">
               Refer a friend to this campaign, and you’ll *both* earn a $5 gift
