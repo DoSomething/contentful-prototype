7ada61890641d08539dc4c0a853f9b6b7e11ff3b
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-11-27 12:30:01 -0500

    Add site navigation

--- resources/assets/components/pages/ReferralPage/Alpha/AlphaPage.js
+++ resources/assets/components/pages/ReferralPage/Alpha/AlphaPage.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import { query } from '../../../../helpers';
 import { PHOENIX_URL } from '../../../../constants';
+import SiteNavigationContainer from '../../../SiteNavigation/SiteNavigationContainer';
 import SocialDriveActionContainer from '../../../actions/SocialDriveAction/SocialDriveActionContainer';
 
 const AlphaPage = props => {
@@ -14,46 +15,49 @@ const AlphaPage = props => {
   }
 
   return (
-    <div className="main general-page base-12-grid">
-      <div className="grid-narrow">
-        <div className="my-6">
-          <div className="general-page__heading text-center">
-            <h1 className="general-page__title uppercase">
-              Want Free Money for School?
-            </h1>
-          </div>
+    <>
+      <SiteNavigationContainer />
+      <div className="main general-page base-12-grid">
+        <div className="grid-narrow">
           <div className="my-6">
-            <SocialDriveActionContainer
-              shareCardDescription="Invite your friends to join DoSomething. When your friend completes this campaign, you'll both increase your chances of winning the campaign scholarship! Every friend you refer earns you an additional shot at winning the scholarship. (Psst...there's no limit on how many you can refer!)"
-              shareCardTitle="Refer A Friend"
-              link={url}
-              hidePageViews
-            />
+            <div className="general-page__heading text-center">
+              <h1 className="general-page__title uppercase">
+                Want Free Money for School?
+              </h1>
+            </div>
+            <div className="my-6">
+              <SocialDriveActionContainer
+                shareCardDescription="Invite your friends to join DoSomething. When your friend completes this campaign, you'll both increase your chances of winning the campaign scholarship! Every friend you refer earns you an additional shot at winning the scholarship. (Psst...there's no limit on how many you can refer!)"
+                shareCardTitle="Refer A Friend"
+                link={url}
+                hidePageViews
+              />
+            </div>
+            <h3>FAQ</h3>
+            <h4>1. Who can I refer?</h4>
+            <p>
+              Earn your reward for referring NEW members to DoSomething!
+              Unfortunately, if you refer someone that already has a DoSomething
+              account, you won’t get the reward when they sign up for the shared
+              campaign.
+            </p>
+            <h4>2. How will I know if I won the scholarship?</h4>
+            <p>
+              We will email you using the same email address used to create your
+              DoSomething account. Scholarship winners are announced when the
+              campaign closes.
+            </p>
+            <h4>3. Where can I find the full rules?</h4>
+            <p>
+              This offer is for a limited time only. See the{' '}
+              <a href="/us/refer-a-friend-official-rules" target="_blank">
+                Refer A Friend Official Rules.
+              </a>
+            </p>
           </div>
-          <h3>FAQ</h3>
-          <h4>1. Who can I refer?</h4>
-          <p>
-            Earn your reward for referring NEW members to DoSomething!
-            Unfortunately, if you refer someone that already has a DoSomething
-            account, you won’t get the reward when they sign up for the shared
-            campaign.
-          </p>
-          <h4>2. How will I know if I won the scholarship?</h4>
-          <p>
-            We will email you using the same email address used to create your
-            DoSomething account. Scholarship winners are announced when the
-            campaign closes.
-          </p>
-          <h4>3. Where can I find the full rules?</h4>
-          <p>
-            This offer is for a limited time only. See the{' '}
-            <a href="/us/refer-a-friend-official-rules" target="_blank">
-              Refer A Friend Official Rules.
-            </a>
-          </p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
