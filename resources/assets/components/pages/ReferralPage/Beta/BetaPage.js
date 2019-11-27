6c1b9ff2d5b4403247e3e5a78240e4dd3592ee11
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-11-27 13:09:04 -0500

    Remove site nav re https://github.com/DoSomething/phoenix-next/pull/1781#discussion_r351426449

--- resources/assets/components/pages/ReferralPage/Beta/BetaPage.js
+++ resources/assets/components/pages/ReferralPage/Beta/BetaPage.js
@@ -6,7 +6,6 @@ import ErrorPage from '../../ErrorPage';
 import { query } from '../../../../helpers';
 import MoneyHandImage from './money-hand.svg';
 import CampaignLink from './BetaPageCampaignLink';
-import SiteNavigationContainer from '../../../SiteNavigation/SiteNavigationContainer';
 import {
   REFERRAL_CAMPAIGN_IDS,
   DEFAULT_REFERRAL_CAMPAIGN_ID,
@@ -35,84 +34,78 @@ const BetaPage = () => {
     : DEFAULT_REFERRAL_CAMPAIGN_ID;
 
   return (
-    <>
-      <SiteNavigationContainer />
-      <Query query={REFERRAL_PAGE_USER} variables={{ id: userId }}>
-        {data => {
-          if (!data.user) {
-            return <ErrorPage />;
-          }
+    <Query query={REFERRAL_PAGE_USER} variables={{ id: userId }}>
+      {data => {
+        if (!data.user) {
+          return <ErrorPage />;
+        }
 
-          const firstName = data.user.firstName;
+        const firstName = data.user.firstName;
 
-          return (
-            <div className="main general-page base-12-grid">
-              <div className="grid-narrow">
+        return (
+          <div className="main general-page base-12-grid">
+            <div className="grid-narrow">
+              <div className="my-6">
+                <div className="general-page__heading text-center">
+                  <h1 className="general-page__title uppercase">
+                    Hey, {firstName}’s friend!
+                  </h1>
+                </div>
                 <div className="my-6">
-                  <div className="general-page__heading text-center">
-                    <h1 className="general-page__title uppercase">
-                      Hey, {firstName}’s friend!
-                    </h1>
-                  </div>
-                  <div className="my-6">
-                    <img src={MoneyHandImage} alt="Hand with money envelope" />
-                  </div>
-                  <div className="my-6">
-                    <p>
-                      Your friend {firstName} just invited you to volunteer
-                      through this campaign from DoSomething.org. Once you
-                      complete your first DoSomething campaign, you’ll both
-                      increase your chances of winning the campaign scholarship!
-                    </p>
-                  </div>
-                  <div className="my-6">
-                    <CampaignLink campaignId={campaignId} userId={userId} />
-                  </div>
-                  <div className="my-6">
-                    <h3>FAQ</h3>
-                    <h4>
-                      1. Can I increase my chances of winning the scholarship if
-                      I already have a DoSomething account?
-                    </h4>
-                    <p>
-                      Unfortunately, if another DoSomething member sends you a
-                      referral link, and you already have a DoSomething account,
-                      you won’t get the reward when you sign up for the shared
-                      campaign.
-                    </p>
-                    <h4>2. How will I know if I won the scholarship?</h4>
-                    <p>
-                      We will email you using the same email address used to
-                      create your DoSomething account. Scholarship winners are
-                      announced when the campaign closes.
-                    </p>
-                    <h4>3. Where can I find the full rules?</h4>
-                    <p>
-                      This offer is for a limited time only. See the{' '}
-                      <a
-                        href="/us/refer-a-friend-official-rules"
-                        target="_blank"
-                      >
-                        Refer A Friend Official Rules.
-                      </a>
-                    </p>
-                    <h3>About Us</h3>
-                    <p className="italic">
-                      DoSomething is the largest not-for-profit for young people
-                      and social change. Using our digital platform, millions of
-                      young people make real-world impact through our volunteer,
-                      social change, and civic action campaigns. We’ve got
-                      hundreds of campaigns to choose from (but only the one
-                      above is offering the reward right now). Let’s do this!
-                    </p>
-                  </div>
+                  <img src={MoneyHandImage} alt="Hand with money envelope" />
+                </div>
+                <div className="my-6">
+                  <p>
+                    Your friend {firstName} just invited you to volunteer
+                    through this campaign from DoSomething.org. Once you
+                    complete your first DoSomething campaign, you’ll both
+                    increase your chances of winning the campaign scholarship!
+                  </p>
+                </div>
+                <div className="my-6">
+                  <CampaignLink campaignId={campaignId} userId={userId} />
+                </div>
+                <div className="my-6">
+                  <h3>FAQ</h3>
+                  <h4>
+                    1. Can I increase my chances of winning the scholarship if I
+                    already have a DoSomething account?
+                  </h4>
+                  <p>
+                    Unfortunately, if another DoSomething member sends you a
+                    referral link, and you already have a DoSomething account,
+                    you won’t get the reward when you sign up for the shared
+                    campaign.
+                  </p>
+                  <h4>2. How will I know if I won the scholarship?</h4>
+                  <p>
+                    We will email you using the same email address used to
+                    create your DoSomething account. Scholarship winners are
+                    announced when the campaign closes.
+                  </p>
+                  <h4>3. Where can I find the full rules?</h4>
+                  <p>
+                    This offer is for a limited time only. See the{' '}
+                    <a href="/us/refer-a-friend-official-rules" target="_blank">
+                      Refer A Friend Official Rules.
+                    </a>
+                  </p>
+                  <h3>About Us</h3>
+                  <p className="italic">
+                    DoSomething is the largest not-for-profit for young people
+                    and social change. Using our digital platform, millions of
+                    young people make real-world impact through our volunteer,
+                    social change, and civic action campaigns. We’ve got
+                    hundreds of campaigns to choose from (but only the one above
+                    is offering the reward right now). Let’s do this!
+                  </p>
                 </div>
               </div>
             </div>
-          );
-        }}
-      </Query>
-    </>
+          </div>
+        );
+      }}
+    </Query>
   );
 };
 
