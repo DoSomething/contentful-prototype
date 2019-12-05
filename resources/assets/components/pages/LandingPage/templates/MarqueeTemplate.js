5a89c1f5fe554c11c5540794668d5c6a1c59ca96
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2019-12-05 17:38:04 -0500

    removes beta affiliate component, removes unused props, updates utm referral used to display the modal, deletes unused test referral function

--- resources/assets/components/pages/LandingPage/templates/MarqueeTemplate.js
+++ resources/assets/components/pages/LandingPage/templates/MarqueeTemplate.js
@@ -12,10 +12,8 @@ import CampaignInfoBarContainer from '../../../CampaignInfoBar/CampaignInfoBarCo
 import AffiliatePromotion from '../../../utilities/AffiliatePromotion/AffiliatePromotion';
 import ScholarshipInfoBlockQuery from '../../../blocks/ScholarshipInfoBlock/ScholarshipInfoBlockQuery';
 import AffiliateOptInToggleContainer from '../../../AffiliateOptInToggle/AffiliateOptInToggleContainer';
-import AffiliateScholarshipBlockQuery from '../../../blocks/AffiliateScholarshipBlock/AffiliateScholarshipBlockQuery';
 import {
   contentfulImageUrl,
-  isTestReferral,
   isScholarshipAffiliateReferral,
   getScholarshipAffiliateLabel,
 } from '../../../../helpers';
@@ -38,7 +36,7 @@ const MarqueeTemplate = ({
   subtitle,
   title,
 }) => {
-  const [showModal, setShowModal] = useState(isTestReferral());
+  const [showModal, setShowModal] = useState(isScholarshipAffiliateReferral());
   const numCampaignId = Number(campaignId);
   // @TODO: If this experiment is successful we should turn generating the series urls for
   // the cover image photo at different sizes into a helper function!
@@ -49,8 +47,6 @@ const MarqueeTemplate = ({
     small: contentfulImageUrl(coverImage.url, '360', '200', 'fill'),
   };
   const scholarshipAffiliateLabel = getScholarshipAffiliateLabel();
-  const displayAffiliateScholarshipBlock =
-    scholarshipAffiliateLabel && scholarshipAmount && scholarshipDeadline;
   return (
     <React.Fragment>
       <article className="marquee-landing-page">
@@ -108,24 +104,14 @@ const MarqueeTemplate = ({
                 </div>
               ) : null}
 
-              {displayAffiliateScholarshipBlock ? (
-                <AffiliateScholarshipBlockQuery
-                  campaignId={numCampaignId}
-                  utmLabel={scholarshipAffiliateLabel.toLowerCase()}
-                  scholarshipAmount={scholarshipAmount}
-                  scholarshipDeadline={scholarshipDeadline}
-                  isScholarshipBeta
-                />
-              ) : (
-                <CampaignInfoBlock
-                  campaignId={numCampaignId}
-                  scholarshipAmount={scholarshipAmount}
-                />
-              )}
+              <CampaignInfoBlock
+                campaignId={numCampaignId}
+                scholarshipAmount={scholarshipAmount}
+              />
             </div>
           </Enclosure>
         </div>
-        {showModal ? (
+        {showModal && !isAffiliated ? (
           <Modal className="-inverted" onClose={() => setShowModal(false)}>
             <ScholarshipInfoBlockQuery
               affiliateSponsors={affiliateSponsors}
