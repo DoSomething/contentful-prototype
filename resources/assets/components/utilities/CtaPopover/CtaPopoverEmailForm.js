cb041e3098e1018e3e1ec37fe707e4a070816a19
Vanessa Mack <vanessacmack@gmail.com>
2019-12-05 15:37:42 -0500

    Adds Post Request to Cta Email Popover Form (#1760)
    * adds post request to emailpopverform
    
    * configured post request and email validation
    
    * fixed form validation message and added errorResponse conditional
    
    * removed console logs
    
    * added error reporting
    
    * made some corrections
    
    * re-added set error response
    
    * re-added error reporting

--- resources/assets/components/utilities/CtaPopover/CtaPopoverEmailForm.js
+++ resources/assets/components/utilities/CtaPopover/CtaPopoverEmailForm.js
@@ -1,29 +1,51 @@
+import get from 'lodash/get';
 import React, { useState } from 'react';
+import { RestApiClient } from '@dosomething/gateway';
 
 import Button from '../Button/Button';
+import { env, report } from '../../../helpers/index';
+import { tabularLog } from '../../../helpers/api';
+
 import './cta-popover-email-form.scss';
 
 const CtaPopoverEmailForm = () => {
   const [emailValue, setEmailValue] = useState('');
+  const [errorResponse, setErrorResponse] = useState(null);
   const handleChange = event => setEmailValue(event.target.value);
 
   const handleSubmit = event => {
     event.preventDefault();
 
-    console.log('submitted!');
+    const client = new RestApiClient(`${env('NORTHSTAR_URL')}`);
+
+    client
+      .post('/v2/subscriptions', {
+        email: emailValue,
+        email_subscription_topic: 'scholarships',
+        source: 'phoenix-next',
+        source_detail: 'scholarship_newsletter-cta_scholarship-page',
+      })
+      .then(response => {
+        tabularLog(get(response, 'data', null));
+
+        return response;
+      })
+      .catch(error => {
+        report(error);
+        setErrorResponse(error.response.error);
+
+        if (window.ENV.APP_ENV !== 'production') {
+          console.log('🚫 failed response? caught the error!', error);
+        }
+      });
   };
 
   return (
-    <div className="cta-popover-email-form">
-      <form className="email-form form pb-2 pt-4" onSubmit={handleSubmit}>
-        {/* <label className="field-label">
-              <span className="validation">
-                <div className="validation__message {{modifier_class}}">
-                  Must be a valid email address
-                </div>
-              </span>
-            </label> */}
-
+    <div className="cta-popover-email-form pt-4">
+      {errorResponse && errorResponse.fields.email ? (
+        <div className="text-red-500">{errorResponse.fields.email[0]}</div>
+      ) : null}
+      <form className="email-form form pb-2" onSubmit={handleSubmit}>
         <input
           className="text-field email-form__input"
           type="email"
