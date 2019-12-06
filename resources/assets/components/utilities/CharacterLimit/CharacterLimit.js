826a12ef5053a6318879a2b56e37f6818afd815d
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-12-06 16:51:53 -0500

    Add className prop to CharacterLimit component

--- resources/assets/components/utilities/CharacterLimit/CharacterLimit.js
+++ resources/assets/components/utilities/CharacterLimit/CharacterLimit.js
@@ -2,19 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const CharacterLimit = ({ text, limit }) => {
+const CharacterLimit = ({ className, text, limit }) => {
   const remaining = limit - text.length;
 
   return (
-    <p className={classNames('footnote', { 'color-error': remaining <= 0 })}>
+    <p
+      className={classNames(className, 'footnote', {
+        'color-error': remaining <= 0,
+      })}
+    >
       {remaining} characters remaining
     </p>
   );
 };
 
 CharacterLimit.propTypes = {
+  className: PropTypes.string,
   limit: PropTypes.number.isRequired,
   text: PropTypes.string.isRequired,
+};
+
+CharacterLimit.defaultProps = {
+  className: null,
 };
 
 export default CharacterLimit;
