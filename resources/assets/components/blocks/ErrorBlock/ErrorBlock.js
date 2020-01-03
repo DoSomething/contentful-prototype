6ae2f5c5cef8901077cb874f7b9064f653c4c462
Diego Lorenzo <diego@diegolorenzo.com>
2020-01-03 11:15:22 -0500

    Renmaing svg file.

--- resources/assets/components/blocks/ErrorBlock/ErrorBlock.js
+++ resources/assets/components/blocks/ErrorBlock/ErrorBlock.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import errorIcon from './errorIcon.svg';
+import errorIcon from './error_icon.svg';
 import { report } from '../../../helpers';
 import Card from '../../utilities/Card/Card';
 
