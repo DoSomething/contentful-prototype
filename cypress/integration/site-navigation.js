c09d02ffd9e8748d31a7147ca322d9f4ea0dc179
Diego Lorenzo <diego@diegolorenzo.com>
2020-01-06 13:48:24 -0500

    Addig some test commentary.

--- cypress/integration/site-navigation.js
+++ cypress/integration/site-navigation.js
@@ -22,40 +22,50 @@ describe('Site Navigation', () => {
 
   sizes.forEach(size => {
     it(`Search for not found content on ${size} viewport`, () => {
-      // Set the viewport
+      // Set the viewport:
       cy.viewport(size[0], size[1]);
 
+      // Go to an example site page:
       cy.withState(exampleFactPage).visit(
         '/us/facts/test-11-facts-about-testing',
       );
 
+      // Find the search icon in nav and click on it:
       cy.get('#nav .utility-nav__search-icon').click();
 
+      // Assert the search subnav is visible:
       cy.get('.utility-subnav').should('be.visible');
 
+      // Try searching for some content we know will not be found and submit the search form:
       cy.get('#utility-subnav__search input[type="search"]').type('clementine');
 
       cy.get('#utility-subnav__search').submit();
 
+      // Assert the new page url includes the search query:
       cy.url().should('include', '/search?query=clementine');
 
+      // Assert message received indicates query not found:
       cy.contains("couldn't find what you were looking for");
 
+      // Assert the subnav is not still rendered on search results page:
       cy.get('.utility-subnav').should('not.exist');
     });
 
     it(`Login and Join Now links are rendered with expected href on ${size} viewport`, () => {
-      // Set the viewport
+      // Set the viewport:
       cy.viewport(size[0], size[1]);
 
+      // Go to an example site page:
       cy.withState(exampleFactPage).visit(
         '/us/facts/test-11-facts-about-testing',
       );
 
+      // Assert the Login link is present and contains correct url in href:
       cy.get('#utility-nav__auth')
         .should('have.attr', 'href')
         .and('include', '/authorize');
 
+      // Assert the Join Now link is present and contains correct url in href:
       cy.get('#utility-nav__join')
         .should('have.attr', 'href')
         .and('include', '/authorize');
