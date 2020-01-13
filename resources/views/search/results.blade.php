58dd2b8fbc12bcec180dc1ee8fc4561ab225b338
Diego Lorenzo <diego@diegolorenzo.com>
2020-01-13 16:50:07 -0500

    Updating additional Blade views.

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -1,37 +1,43 @@
 @extends('layouts.master')
 
 @section('content')
-    <div id="nav-container"></div>
+    <div class="app bg-gray-100 min-h-full">
 
-    <main role="main" class="pb-16 pt-6">
+        <div id="nav-container"></div>
 
-        <article class="md:w-3/4 mx-auto">
+        <main role="main" class="pb-16 pt-6 relative">
 
-            {{-- @TODO:css-grid Remove horizontal padding once CSS Grid is implemented --}}
-            <h1 class="text-xl md:text-2xl mb-6 px-3">Search</h1>
+            <article class="md:w-3/4 mx-auto">
 
-            @if(empty($campaigns))
                 {{-- @TODO:css-grid Remove horizontal padding once CSS Grid is implemented --}}
-                <h2 class="text-base text-gray-700 mb-6 px-3">We searched our site, but couldn't find what you were looking for. Try a new search or explore <a href="/us/campaigns">our full list of campaigns</a>.</h2>
-            @endif
+                <h1 class="text-xl md:text-2xl mb-6 px-3">Search</h1>
 
-            {{-- @TODO:css-grid Remove horizontal padding once CSS Grid is implemented --}}
-            <form class="flex mb-6 px-3 text-base lg:w-1/2" action="/us/search" method="GET" accept-charset="UTF-8">
-                <div class="border border-solid border-gray-400 flex-1 mr-3 relative rounded">
-                    @include('svg.search-icon', [
-                        'class' => 'absolute icon icon-search text-gray-400 fill-current m-3',
-                        'style' => 'height: 18px; width: 18px; top: 3px;'
-                    ])
-                    <input class="border-none leading-none pl-10 text-input w-full" name="query" type="text" value="{{ $query }}">
-                </div>
-                <input class="btn" type="submit" value="Search">
-            </form>
+                @if(empty($campaigns))
+                    {{-- @TODO:css-grid Remove horizontal padding once CSS Grid is implemented --}}
+                    <h2 class="text-base text-gray-700 mb-6 px-3">We searched our site, but couldn't find what you were looking for. Try a new search or explore <a href="/us/campaigns">our full list of campaigns</a>.</h2>
+                @endif
 
-            @if(! empty($campaigns))
-                @include('partials.campaign_gallery', ['campaigns' => $campaigns])
-            @endif
+                {{-- @TODO:css-grid Remove horizontal padding once CSS Grid is implemented --}}
+                <form class="flex mb-6 px-3 text-base lg:w-1/2" action="/us/search" method="GET" accept-charset="UTF-8">
+                    <div class="border border-solid border-gray-400 flex-1 mr-3 relative rounded">
+                        @include('svg.search-icon', [
+                            'class' => 'absolute icon icon-search text-gray-400 fill-current m-3',
+                            'style' => 'height: 18px; width: 18px; top: 3px;'
+                        ])
+                        <input class="border-none leading-none pl-10 text-input w-full" name="query" type="text" value="{{ $query }}">
+                    </div>
+                    <input class="btn" type="submit" value="Search">
+                </form>
 
-        </article>
+                @if(! empty($campaigns))
+                    @include('partials.campaign_gallery', ['campaigns' => $campaigns])
+                @endif
 
-    </main>
+            </article>
+
+        </main>
+
+        @include('partials.footer')
+
+    </div>
 @stop
