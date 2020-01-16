48f44fb677ae686a3ae25cd79afb8d5ec690da2c
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-01-16 15:09:02 -0500

    Add zendesk form and campaign info bar Cypress tests

---
+++ cypress/integration/campaign-info-bar.js
@@ -0,0 +1,44 @@
+/// <reference types="Cypress" />
+import { userFactory } from '../fixtures/user';
+import exampleCampaign from '../fixtures/contentful/exampleCampaign';
+
+describe('Campaign Info Bar', () => {
+  beforeEach(() => cy.configureMocks());
+
+  it('Displays the affiliate sponsors or partners', () => {
+    const affiliateTitle =
+      exampleCampaign.campaign.affiliateSponsors[0].fields.title;
+
+    cy.withState(exampleCampaign).visit('/us/campaigns/test-example-campaign');
+
+    cy.get('.info-bar').should('contain', affiliateTitle);
+  });
+
+  context('Unaffiliated users', () => {
+    it("Displays a mailto link to the campaign lead's email address", () => {
+      const campaignLeadEmail =
+        exampleCampaign.campaign.campaignLead.fields.email;
+
+      cy.withState(exampleCampaign).visit(
+        '/us/campaigns/test-example-campaign',
+      );
+
+      cy.get('.info-bar .info-bar__secondary a')
+        .should('have.attr', 'href')
+        .and('include', `mailto:${campaignLeadEmail}`);
+    });
+  });
+
+  context('Affiliated users', () => {
+    it('Displays a button triggering the Zendesk Form in a modal', () => {
+      const user = userFactory();
+      cy.authVisitCampaignWithSignup(user, exampleCampaign);
+
+      cy.get('.info-bar .info-bar__secondary button')
+        .contains('button', 'Contact Us')
+        .click();
+
+      cy.get('.modal .zendesk-form').contains('h1', 'Contact Us');
+    });
+  });
+});
---
+++ cypress/integration/zendesk-form.js
@@ -0,0 +1,44 @@
+/// <reference types="Cypress" />
+import { userFactory } from '../fixtures/user';
+import exampleCampaign from '../fixtures/contentful/exampleCampaign';
+
+const QUESTION = 'causeyhippo';
+
+describe('Zendesk Modal', () => {
+  beforeEach(() => {
+    cy.configureMocks();
+
+    const user = userFactory();
+    cy.authVisitCampaignWithSignup(user, exampleCampaign);
+
+    cy.get('.info-bar .info-bar__secondary button')
+      .contains('button', 'Contact Us')
+      .click();
+
+    cy.get('.modal .zendesk-form').contains('h1', 'Contact Us');
+    cy.get('a.zendesk-form__faqs-link')
+      .should('have.attr', 'href')
+      .and('include', `/us/campaigns/${exampleCampaign.campaign.slug}/faqs`);
+  });
+
+  it('Submits a question successfully and displays affirmation', () => {
+    cy.route('POST', '/api/v2/zendesk-tickets', []);
+
+    cy.get('.zendesk-form textarea').type(QUESTION);
+
+    cy.get('.zendesk-form button').click();
+
+    cy.get('.zendesk-form h1').contains('Thank You');
+  });
+
+  it('Submits a question unsuccessfully and displays error message', () => {
+    cy.route({ method: 'POST', url: '/api/v2/zendesk-tickets', status: 500 });
+
+    cy.get('.zendesk-form textarea').type(QUESTION);
+    cy.get('.zendesk-form button').click();
+
+    cy.get('.zendesk-form h1').contains('Contact Us');
+    cy.get('.zendesk-form').contains('Something went wrong!');
+    cy.get('.zendesk-form textarea').contains(QUESTION);
+  });
+});
