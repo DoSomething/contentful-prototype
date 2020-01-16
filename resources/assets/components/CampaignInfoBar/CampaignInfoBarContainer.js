9d37ba47feb9dfbf1dccb2c691facbc74175f5c5
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-01-16 15:07:51 -0500

    Add zendesk form modal link for affiliated users

--- resources/assets/components/CampaignInfoBar/CampaignInfoBarContainer.js
+++ resources/assets/components/CampaignInfoBar/CampaignInfoBarContainer.js
@@ -2,6 +2,7 @@ import { get } from 'lodash';
 import { connect } from 'react-redux';
 
 import CampaignInfoBar from './CampaignInfoBar';
+import { isSignedUp } from '../../selectors/signup';
 
 const mapStateToProps = state => {
   return {
@@ -13,6 +14,7 @@ const mapStateToProps = state => {
     affiliateSponsors: state.campaign.affiliateSponsors,
     affiliatePartners: state.campaign.affiliatePartners,
     contactEmail: get(state, 'campaign.campaignLead.fields.email', undefined),
+    isAffiliated: isSignedUp(state),
   };
 };
 
