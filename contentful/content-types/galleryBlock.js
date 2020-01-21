88ad502598f6420990eeec856433cc4a8433ed80
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-01-21 16:53:13 -0500

    Update itemsPerRow field validation

--- contentful/content-types/galleryBlock.js
+++ contentful/content-types/galleryBlock.js
@@ -4,85 +4,107 @@ module.exports = function(migration) {
     .name('Gallery Block')
     .description('Displays a gallery of referenced entries')
     .displayField('internalTitle');
-
   galleryBlock
     .createField('internalTitle')
     .name('Internal Title')
     .type('Symbol')
+    .localized(false)
     .required(true)
-    .localized(false);
-
+    .validations([])
+    .disabled(false)
+    .omitted(false);
   galleryBlock
     .createField('title')
     .name('Title')
     .type('Symbol')
+    .localized(true)
     .required(false)
-    .localized(true);
+    .validations([])
+    .disabled(false)
+    .omitted(false);
 
   galleryBlock
     .createField('blocks')
     .name('Blocks')
     .type('Array')
-    .validations([
-      {
-        size: {
-          min: 2,
-        },
-      },
-    ])
+    .localized(false)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
     .items({
       type: 'Link',
+
       validations: [
         {
-          linkContentType: ['person', 'campaign', 'page', 'contentBlock'],
+          linkContentType: ['campaign', 'contentBlock', 'page', 'person'],
         },
       ],
+
       linkType: 'Entry',
-    })
-    .required(true)
-    .localized(false);
+    });
 
   galleryBlock
     .createField('itemsPerRow')
     .name('Items Per Row')
     .type('Integer')
+    .localized(false)
+    .required(true)
     .validations([
       {
-        in: [2, 3, 4],
+        in: [2, 3, 4, 5],
       },
     ])
-    .required(true)
-    .localized(false);
+    .disabled(false)
+    .omitted(false);
 
   galleryBlock
     .createField('imageAlignment')
     .name('Image Alignment')
     .type('Symbol')
-    .validations([{ in: ['top', 'left'] }])
+    .localized(false)
     .required(true)
-    .localized(false);
+    .validations([
+      {
+        in: ['top', 'left'],
+      },
+    ])
+    .disabled(false)
+    .omitted(false);
 
   galleryBlock
     .createField('imageFit')
     .name('Image Fit')
     .type('Symbol')
-    .validations([{ in: ['fill', 'pad'] }])
+    .localized(false)
     .required(false)
-    .localized(false);
+    .validations([
+      {
+        in: ['fill', 'pad'],
+      },
+    ])
+    .disabled(false)
+    .omitted(false);
 
-  galleryBlock.changeEditorInterface('itemsPerRow', 'radio', {
+  galleryBlock.changeFieldControl('internalTitle', 'builtin', 'singleLine', {});
+  galleryBlock.changeFieldControl('title', 'builtin', 'singleLine', {});
+
+  galleryBlock.changeFieldControl('blocks', 'builtin', 'entryLinksEditor', {
+    bulkEditing: false,
+  });
+
+  galleryBlock.changeFieldControl('itemsPerRow', 'builtin', 'radio', {
     helpText:
       'The maximum number of items in a single row when viewing the gallery in a large display.',
   });
 
-  galleryBlock.changeEditorInterface('imageAlignment', 'radio', {
+  galleryBlock.changeFieldControl('imageAlignment', 'builtin', 'radio', {
     helpText:
       "Determines where the gallery item's images are aligned relative to their text.",
   });
 
-  galleryBlock.changeEditorInterface('imageFit', 'radio', {
-    helpText: `Controls the cropping method for the gallery images. 'Fill' will resize the images to ensure they
-      fit neatly into a square, cropping the image if needed. 'Pad' will do the same but will add padding
-      to the image instead of cropping it.`,
+  galleryBlock.changeFieldControl('imageFit', 'builtin', 'radio', {
+    helpText:
+      "Controls the cropping method for the gallery images. 'Fill' will resize the images to ensure they      fit neatly into a square, cropping the image if needed. 'Pad' will do the same but will add padding      to the image instead of cropping it.",
   });
 };
