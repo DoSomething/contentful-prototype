c64b0a3712cc5b9970f78c9dc3f97e93326f179a
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-01-23 12:13:26 -0500

    Add features config file with company pages flag

---
+++ config/features.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Feature Flags
+    |--------------------------------------------------------------------------
+    |
+    | This file is a custom addition to Phoenix for storing feature flags, so
+    | features can be conditionally toggled on and off per environment.
+    |
+    */
+
+    'company_pages' => env('DS_COMPANY_PAGES', false),
+];
