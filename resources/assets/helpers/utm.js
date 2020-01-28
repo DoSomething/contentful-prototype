9ca2d65433ca1024aae53fba2e0a1327353300d4
Diego Lorenzo <diego@diegolorenzo.com>
2020-01-28 17:21:39 -0500

    Revising approach to persist UTMs for analytics. (#1889)
    * Revising approach to persist UTMs for analytics.
    
    * Update to simplify how fuctions are called.

--- resources/assets/helpers/utm.js
+++ resources/assets/helpers/utm.js
@@ -1,46 +1,45 @@
-/* global history */
-/* eslint-disable no-restricted-globals */
-
 import { isEmpty } from 'lodash';
 
-import { appendToQuery, query, withoutValueless } from '.';
+import { query, withoutValueless } from '.';
 
 const UTM_SESSION_KEY = 'ds_utm_params';
 
 /**
  * Get UTM parameters from session storage.
  *
- * @param  {String} key
  * @return {Object}
  */
-export function getSessionUtms(key) {
-  return JSON.parse(sessionStorage.getItem(key));
+export function getSessionUtms() {
+  return JSON.parse(sessionStorage.getItem(UTM_SESSION_KEY));
 }
 
 /**
  * Store UTM parameters in session storage.
  *
- * @param  {String} key
  * @param  {Object} utms
  * @return {Object}
  */
-export function setSessionUtms(key, utms) {
-  sessionStorage.setItem(key, JSON.stringify(utms));
+export function setSessionUtms(utms) {
+  sessionStorage.setItem(UTM_SESSION_KEY, JSON.stringify(utms));
 
   return utms;
 }
 
 /**
- * Get UTM parameters.
+ * Get UTMs from session storage or query parameters.
  *
  * @return {Object}
  */
 export function getUtmParameters() {
-  return {
-    utm_source: query('utm_source'),
-    utm_medium: query('utm_medium'),
-    utm_campaign: query('utm_campaign'),
-  };
+  const sessionUtms = getSessionUtms();
+
+  return !isEmpty(sessionUtms)
+    ? sessionUtms
+    : withoutValueless({
+        utm_source: query('utm_source'),
+        utm_medium: query('utm_medium'),
+        utm_campaign: query('utm_campaign'),
+      });
 }
 
 /**
@@ -49,23 +48,19 @@ export function getUtmParameters() {
  * @return void
  */
 export function persistUtms() {
-  // Get any current query param utms:
-  const utms = withoutValueless(getUtmParameters());
+  // Check to see if there are any utms in session storage:
+  const sessionUtms = getSessionUtms();
 
-  // Check to see if there are any in session storage:
-  const sessionUtms = getSessionUtms(UTM_SESSION_KEY);
-
-  // If no session utms, then store the current query param utms:
-  if (isEmpty(sessionUtms) && !isEmpty(utms)) {
-    setSessionUtms(UTM_SESSION_KEY, utms);
-
+  // If utms in session storage, no need to store them again, so exit out.
+  if (!isEmpty(sessionUtms)) {
     return;
   }
 
-  // If no current query param utms, but found session utms, add them to the URL:
-  if (isEmpty(utms) && !isEmpty(sessionUtms)) {
-    const url = appendToQuery(sessionUtms);
+  // Get any existing session utms or current query param utms:
+  const utms = getUtmParameters();
 
-    history.replaceState(null, '', url.href);
+  // If no session utms, then store the current query param utms:
+  if (isEmpty(sessionUtms) && !isEmpty(utms)) {
+    setSessionUtms(utms);
   }
 }
