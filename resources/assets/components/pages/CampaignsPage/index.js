414c3ccf7f421fdcb6d0affad473559d61243331
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2020-02-03 16:24:48 -0500

    adds site nav and footer to Campaigns Page

--- resources/assets/components/pages/CampaignsPage/index.js
+++ resources/assets/components/pages/CampaignsPage/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import CampaignCard from './CampaignCard';
+import SiteFooter from '../../utilities/SiteFooter/SiteFooter';
+import SiteNavigationContainer from '../../SiteNavigation/SiteNavigationContainer';
 
 const CampaignsPage = () => (
-  <div>
-    Hello!
-    <CampaignCard />
-  </div>
+  <>
+    <SiteNavigationContainer />
+    <main>
+      <h1>Campaigns For All Causes</h1>
+      <CampaignCard />
+    </main>
+    <SiteFooter />
+  </>
 );
 
 export default CampaignsPage;
