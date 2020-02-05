b747253c2ebb60168476b96ff78d3e3af90aae8b
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2020-02-05 11:31:59 -0500

    converts campaigns page to use gallery block

--- resources/assets/components/pages/CampaignsPage/CampaignsPage.js
+++ resources/assets/components/pages/CampaignsPage/CampaignsPage.js
@@ -1,50 +1,72 @@
 import React from 'react';
 
-import CampaignCard from './CampaignCard';
+// import CampaignCard from './CampaignCard';
 import SiteFooter from '../../utilities/SiteFooter/SiteFooter';
+import GalleryBlock from '../../blocks/GalleryBlock/GalleryBlock';
 import SiteNavigationContainer from '../../SiteNavigation/SiteNavigationContainer';
 
 const CampaignsPageTemplate = () => {
   const campaigns = [
     {
+      id: 1,
       title: 'Teens For Jeans',
       slug: 'https://dosomething.org',
-      coverImage:
-        'https://images.ctfassets.net/81iqaqpfd8fy/5ZgtT1Jpw3xIbmICwVkvo6/372fe8c45423155a80fe3402f3ebc1e8/NEW_RBC_Header_Photo.jpg?w=400&h=400&fit=fill',
-      description: 'Collect jeans for local youth experiencing homelessness.',
+      coverImage: {
+        url:
+          'https://images.ctfassets.net/81iqaqpfd8fy/5ZgtT1Jpw3xIbmICwVkvo6/372fe8c45423155a80fe3402f3ebc1e8/NEW_RBC_Header_Photo.jpg?w=400&h=400&fit=fill',
+        description: 'Teens For Jeans',
+      },
+      tagline: 'Collect jeans for local youth experiencing homelessness.',
+      type: 'campaign',
     },
     {
+      id: 2,
       title: 'Teens For Jeans',
       slug: 'https://dosomething.org',
-      coverImage:
-        'https://images.ctfassets.net/81iqaqpfd8fy/5ZgtT1Jpw3xIbmICwVkvo6/372fe8c45423155a80fe3402f3ebc1e8/NEW_RBC_Header_Photo.jpg?w=400&h=400&fit=fill',
-      description: 'Collect jeans for local youth experiencing homelessness.',
+      coverImage: {
+        url:
+          'https://images.ctfassets.net/81iqaqpfd8fy/5ZgtT1Jpw3xIbmICwVkvo6/372fe8c45423155a80fe3402f3ebc1e8/NEW_RBC_Header_Photo.jpg?w=400&h=400&fit=fill',
+        description: 'Teens For Jeans',
+      },
+      tagline: 'Collect jeans for local youth experiencing homelessness.',
+      type: 'campaign',
     },
     {
+      id: 3,
       title: 'Teens For Jeans',
       slug: 'https://dosomething.org',
-      coverImage:
-        'https://images.ctfassets.net/81iqaqpfd8fy/5ZgtT1Jpw3xIbmICwVkvo6/372fe8c45423155a80fe3402f3ebc1e8/NEW_RBC_Header_Photo.jpg?w=400&h=400&fit=fill',
-      description: 'Collect jeans for local youth experiencing homelessness.',
+      coverImage: {
+        url:
+          'https://images.ctfassets.net/81iqaqpfd8fy/5ZgtT1Jpw3xIbmICwVkvo6/372fe8c45423155a80fe3402f3ebc1e8/NEW_RBC_Header_Photo.jpg?w=400&h=400&fit=fill',
+        description: 'Teens For Jeans',
+      },
+      tagline: 'Collect jeans for local youth experiencing homelessness.',
+      type: 'campaign',
     },
     {
+      id: 4,
       title: 'Teens For Jeans',
       slug: 'https://dosomething.org',
-      coverImage:
-        'https://images.ctfassets.net/81iqaqpfd8fy/5ZgtT1Jpw3xIbmICwVkvo6/372fe8c45423155a80fe3402f3ebc1e8/NEW_RBC_Header_Photo.jpg?w=400&h=400&fit=fill',
-      description: 'Collect jeans for local youth experiencing homelessness.',
+      coverImage: {
+        url:
+          'https://images.ctfassets.net/81iqaqpfd8fy/5ZgtT1Jpw3xIbmICwVkvo6/372fe8c45423155a80fe3402f3ebc1e8/NEW_RBC_Header_Photo.jpg?w=400&h=400&fit=fill',
+        description: 'Teens For Jeans',
+      },
+      tagline: 'Collect jeans for local youth experiencing homelessness.',
+      type: 'campaign',
     },
   ];
+  const itemsPerRow = 4;
   return (
     <>
       <SiteNavigationContainer />
       <main className="md:w-3/4 mx-auto">
-        <h1 className="w-full my-6 pl-2">Campaigns For All Causes</h1>
-        <div className="gallery-grid-quartet">
-          {campaigns.map(campaign => {
-            return <CampaignCard campaign={campaign} />;
-          })}
-        </div>
+        <h1 className="w-full my-6">Campaigns For All Causes</h1>
+        <GalleryBlock
+          blocks={campaigns}
+          itemsPerRow={itemsPerRow}
+          imageAlignment="TOP"
+        />
       </main>
       <SiteFooter />
     </>
