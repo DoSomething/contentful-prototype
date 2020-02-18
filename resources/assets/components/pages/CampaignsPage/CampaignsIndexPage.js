e80260b68aa49d69b008d9840547b7c22140f3ec
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2020-02-18 14:02:20 -0500

    correctly adds labels to checkboxes

--- resources/assets/components/pages/CampaignsPage/CampaignsIndexPage.js
+++ resources/assets/components/pages/CampaignsPage/CampaignsIndexPage.js
@@ -6,21 +6,34 @@ import PaginatedCampaignGallery from '../../utilities/PaginatedCampaignGallery/P
 
 const CampaignsIndexPage = () => {
   const [causes, setCauses] = useState([]);
+  const handleSelect = event => setCauses([...causes, event.target.value]);
 
-  const handleClick = event => setCauses([...causes, event.target.value]);
-  console.log('our causes', causes);
   return (
     <>
       <SiteNavigationContainer />
       <main className="md:w-3/4 mx-auto">
         <h1 className="w-full my-6 pl-6 md:pl-0">Campaigns For All Causes</h1>
         <form>
-          <input
-            name="environment"
-            type="checkbox"
-            value="environment"
-            onClick={handleClick}
-          />
+          <label htmlFor="environment">
+            <input
+              name="environment"
+              id="environment"
+              type="checkbox"
+              value="environment"
+              onClick={handleSelect}
+            />
+            Environment
+          </label>
+          <label htmlFor="education">
+            <input
+              name="education"
+              id="education"
+              type="checkbox"
+              value="education"
+              onClick={handleSelect}
+            />
+            Education
+          </label>
         </form>
         <PaginatedCampaignGallery
           className="grid-full px-6 md:px-0"
