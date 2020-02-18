c763356bb97ee6cfbeb770b4aa43d2de8dd9da53
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2020-02-18 13:49:33 -0500

    adds a cause array on state to store the campaigns chosen by users

--- resources/assets/components/pages/CampaignsPage/CampaignsIndexPage.js
+++ resources/assets/components/pages/CampaignsPage/CampaignsIndexPage.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import SiteFooter from '../../utilities/SiteFooter/SiteFooter';
 import SiteNavigationContainer from '../../SiteNavigation/SiteNavigationContainer';
 import PaginatedCampaignGallery from '../../utilities/PaginatedCampaignGallery/PaginatedCampaignGallery';
 
-const CampaignsIndexPage = () => (
-  <>
-    <SiteNavigationContainer />
-    <main className="md:w-3/4 mx-auto">
-      <h1 className="w-full my-6 pl-6 md:pl-0">Campaigns For All Causes</h1>
-      <PaginatedCampaignGallery
-        className="grid-full px-6 md:px-0"
-        itemsPerRow={4}
-        variables={{ isOpen: true, first: 36, orderBy: 'start_date,desc' }}
-      />
-    </main>
-    <SiteFooter />
-  </>
-);
+const CampaignsIndexPage = () => {
+  const [causes, setCauses] = useState([]);
+
+  const handleClick = event => setCauses([...causes, event.target.value]);
+  console.log('our causes', causes);
+  return (
+    <>
+      <SiteNavigationContainer />
+      <main className="md:w-3/4 mx-auto">
+        <h1 className="w-full my-6 pl-6 md:pl-0">Campaigns For All Causes</h1>
+        <form>
+          <input
+            name="environment"
+            type="checkbox"
+            value="environment"
+            onClick={handleClick}
+          />
+        </form>
+        <PaginatedCampaignGallery
+          className="grid-full px-6 md:px-0"
+          itemsPerRow={4}
+          variables={{ isOpen: true, first: 36, orderBy: 'start_date,desc' }}
+        />
+      </main>
+      <SiteFooter />
+    </>
+  );
+};
 
 export default CampaignsIndexPage;
