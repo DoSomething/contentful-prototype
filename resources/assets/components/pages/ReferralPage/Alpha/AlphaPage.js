3f54d80ed3abadcae9be3de340a0aa4c1228ad6e
Aaron Schachter <aaronschachter@gmail.com>
2020-02-20 07:10:13 -0800

    Display ErrorPage if alpha page does not have campaign_id set

--- resources/assets/components/pages/ReferralPage/Alpha/AlphaPage.js
+++ resources/assets/components/pages/ReferralPage/Alpha/AlphaPage.js
@@ -1,67 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ErrorPage from '../../ErrorPage';
 import { PHOENIX_URL } from '../../../../constants';
 import { getReferralCampaignId } from '../../../../helpers';
 import SiteFooter from '../../../utilities/SiteFooter/SiteFooter';
 import SiteNavigationContainer from '../../../SiteNavigation/SiteNavigationContainer';
 import SocialDriveActionContainer from '../../../actions/SocialDriveAction/SocialDriveActionContainer';
 
-const AlphaPage = ({ userId }) => (
-  <>
-    <SiteNavigationContainer />
+const AlphaPage = ({ userId }) =>
+  getReferralCampaignId() ? (
+    <>
+      <SiteNavigationContainer />
 
-    <main className="general-page base-12-grid relative">
-      <article className="grid-narrow">
-        <div className="general-page__heading text-center my-6">
-          <h1 className="general-page__title uppercase">
-            Want Free Money for School?
-          </h1>
-        </div>
+      <main className="general-page base-12-grid relative">
+        <article className="grid-narrow">
+          <div className="general-page__heading text-center my-6">
+            <h1 className="general-page__title uppercase">
+              Want Free Money for School?
+            </h1>
+          </div>
 
-        <div className="my-6">
-          <SocialDriveActionContainer
-            shareCardDescription="Invite your friends to join DoSomething. When your friend completes this campaign, you'll both increase your chances of winning the campaign scholarship! Every friend you refer earns you an additional shot at winning the scholarship. (Psst...there's no limit on how many you can refer!)"
-            shareCardTitle="Refer A Friend"
-            link={`${PHOENIX_URL}/us/join?user_id=${userId}&campaign_id=${getReferralCampaignId()}`}
-            hidePageViews
-            fullWidth
-          />
-        </div>
+          <div className="my-6">
+            <SocialDriveActionContainer
+              shareCardDescription="Invite your friends to join DoSomething. When your friend completes this campaign, you'll both increase your chances of winning the campaign scholarship! Every friend you refer earns you an additional shot at winning the scholarship. (Psst...there's no limit on how many you can refer!)"
+              shareCardTitle="Refer A Friend"
+              link={`${PHOENIX_URL}/us/join?user_id=${userId}&campaign_id=${getReferralCampaignId()}`}
+              hidePageViews
+              fullWidth
+            />
+          </div>
 
-        <h3>FAQ</h3>
+          <h3>FAQ</h3>
 
-        <h4>1. Who can I refer?</h4>
+          <h4>1. Who can I refer?</h4>
 
-        <p>
-          Earn your reward for referring NEW members to DoSomething!
-          Unfortunately, if you refer someone that already has a DoSomething
-          account, you won’t get the reward when they sign up for the shared
-          campaign.
-        </p>
+          <p>
+            Earn your reward for referring NEW members to DoSomething!
+            Unfortunately, if you refer someone that already has a DoSomething
+            account, you won’t get the reward when they sign up for the shared
+            campaign.
+          </p>
 
-        <h4>2. How will I know if I won the scholarship?</h4>
+          <h4>2. How will I know if I won the scholarship?</h4>
 
-        <p>
-          We will email you using the same email address used to create your
-          DoSomething account. Scholarship winners are announced when the
-          campaign closes.
-        </p>
+          <p>
+            We will email you using the same email address used to create your
+            DoSomething account. Scholarship winners are announced when the
+            campaign closes.
+          </p>
 
-        <h4>3. Where can I find the full rules?</h4>
+          <h4>3. Where can I find the full rules?</h4>
 
-        <p>
-          This offer is for a limited time only. See the{' '}
-          <a href="/us/refer-a-friend-official-rules" target="_blank">
-            Refer A Friend Official Rules.
-          </a>
-        </p>
-      </article>
-    </main>
+          <p>
+            This offer is for a limited time only. See the{' '}
+            <a href="/us/refer-a-friend-official-rules" target="_blank">
+              Refer A Friend Official Rules.
+            </a>
+          </p>
+        </article>
+      </main>
 
-    <SiteFooter />
-  </>
-);
+      <SiteFooter />
+    </>
+  ) : (
+    <ErrorPage />
+  );
 
 AlphaPage.propTypes = {
   userId: PropTypes.string.isRequired,
