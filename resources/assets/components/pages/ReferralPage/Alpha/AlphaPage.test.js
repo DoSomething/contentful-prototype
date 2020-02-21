1e5cd596e84fb4420b638e00388643430072976f
Aaron Schachter <aaronschachter@gmail.com>
2020-02-20 16:32:17 -0800

    Cleanup test descriptions

--- resources/assets/components/pages/ReferralPage/Alpha/AlphaPage.test.js
+++ resources/assets/components/pages/ReferralPage/Alpha/AlphaPage.test.js
@@ -7,13 +7,15 @@ import * as helpers from '../../../../helpers';
 describe('AlphaPage component', () => {
   const userId = '55767606a59dbf3c7a8b4571';
 
-  test('is rendered as an ErrorPage if campaign_id query is not present', () => {
+  test('is rendered as an ErrorPage if getReferralCampaignId is null', () => {
+    helpers.getReferralCampaignId = jest.fn().mockReturnValue(null);
+
     const wrapper = shallow(<AlphaPage userId={userId} />);
 
     expect(wrapper.find('ErrorPage').length).toEqual(1);
   });
 
-  test('is rendered with a main.alpha-referral-page if campaign_id query is present', () => {
+  test('is rendered with a main.alpha-referral-page if getReferralCampaignId returns an id', () => {
     helpers.getReferralCampaignId = jest.fn().mockReturnValue(23);
 
     const wrapper = shallow(<AlphaPage userId={userId} />);
