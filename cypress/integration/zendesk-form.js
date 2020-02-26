934d30bf034c9729da6995f268d2d4e38c626cb8
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-02-26 13:38:11 -0500

    Update zendesk tests per info bar changes

--- cypress/integration/zendesk-form.js
+++ cypress/integration/zendesk-form.js
@@ -5,40 +5,56 @@ import exampleCampaign from '../fixtures/contentful/exampleCampaign';
 const QUESTION = 'causeyhippo';
 
 describe('Zendesk Modal', () => {
-  beforeEach(() => {
-    cy.configureMocks();
+  context('Unauthenticated users', () => {
+    beforeEach(() => {
+      cy.configureMocks();
+    });
 
-    const user = userFactory();
-    cy.authVisitCampaignWithSignup(user, exampleCampaign);
+    it('Displays a link to the help center', () => {
+      cy.anonVisitCampaign(exampleCampaign);
 
-    cy.get('.info-bar .info-bar__secondary button')
-      .contains('button', 'Contact Us')
-      .click();
-
-    cy.get('.modal .zendesk-form').contains('h1', 'Contact Us');
-    cy.get('a.zendesk-form__faqs-link')
-      .should('have.attr', 'href')
-      .and('include', `/us/campaigns/${exampleCampaign.campaign.slug}/faqs`);
+      cy.get('.info-bar .info-bar__secondary a').contains(
+        'Visit our Help Center',
+      );
+    });
   });
 
-  it('Submits a question successfully and displays affirmation', () => {
-    cy.route('POST', '/api/v2/zendesk-tickets', []);
+  context('Authenticated users', () => {
+    beforeEach(() => {
+      cy.configureMocks();
 
-    cy.get('.zendesk-form textarea').type(QUESTION);
+      const user = userFactory();
+      cy.authVisitCampaignWithoutSignup(user, exampleCampaign);
 
-    cy.get('.zendesk-form button').click();
+      cy.get('.info-bar .info-bar__secondary button')
+        .contains('button', 'Contact Us')
+        .click();
 
-    cy.get('.zendesk-form h1').contains('Thank You');
-  });
+      cy.get('.modal .zendesk-form').contains('h1', 'Contact Us');
+      cy.get('a.zendesk-form__faqs-link')
+        .should('have.attr', 'href')
+        .and('include', `/us/campaigns/${exampleCampaign.campaign.slug}/faqs`);
+    });
 
-  it('Submits a question unsuccessfully and displays error message', () => {
-    cy.route({ method: 'POST', url: '/api/v2/zendesk-tickets', status: 500 });
+    it('Submits a question successfully and displays affirmation', () => {
+      cy.route('POST', '/api/v2/zendesk-tickets', []);
 
-    cy.get('.zendesk-form textarea').type(QUESTION);
-    cy.get('.zendesk-form button').click();
+      cy.get('.zendesk-form textarea').type(QUESTION);
 
-    cy.get('.zendesk-form h1').contains('Contact Us');
-    cy.get('.zendesk-form').contains('Something went wrong!');
-    cy.get('.zendesk-form textarea').contains(QUESTION);
+      cy.get('.zendesk-form button').click();
+
+      cy.get('.zendesk-form h1').contains('Thank You');
+    });
+
+    it('Submits a question unsuccessfully and displays error message', () => {
+      cy.route({ method: 'POST', url: '/api/v2/zendesk-tickets', status: 500 });
+
+      cy.get('.zendesk-form textarea').type(QUESTION);
+      cy.get('.zendesk-form button').click();
+
+      cy.get('.zendesk-form h1').contains('Contact Us');
+      cy.get('.zendesk-form').contains('Something went wrong!');
+      cy.get('.zendesk-form textarea').contains(QUESTION);
+    });
   });
 });
