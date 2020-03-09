4b479c133d01ee3575213d31d7d9009fc3783fd4
vmack <vanessacmack@gmail.com>
2020-03-09 15:39:04 -0400

    add styling updates to scholarship modal

--- resources/assets/components/blocks/ScholarshipInfoBlock/ScholarshipRequirements.js
+++ resources/assets/components/blocks/ScholarshipInfoBlock/ScholarshipRequirements.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ScholarshipRequirements = () => (
   <div className="lg:w-1/2 float-left">
     <h4 className="font-bold uppercase text-gray-600">Requirements</h4>
-    <ul className="mt-2 pb-2 list-disc list-inside">
+    <ul className="mt-2 pb-4 list-disc list-inside">
       <li>Under 26 years old</li>
       <li>No minimum GPA</li>
       <li>No essay</li>
