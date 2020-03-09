1a8601c0ad11ae107a14b870bc3dd9cdebb67723
Diego Lorenzo <diego@diegolorenzo.com>
2020-03-09 15:58:39 -0400

    Fix for emotionjs and react bug.

--- resources/assets/components/pages/HomePage/NewHomePage.js
+++ resources/assets/components/pages/HomePage/NewHomePage.js
@@ -1,22 +1,26 @@
 /** @jsx jsx */
 
-import React from 'react';
+import { Fragment } from 'react';
 import { jsx, css } from '@emotion/core';
 
+import sponsorList from './sponsor-list';
+import { contentfulImageUrl } from '../../../helpers';
 import tailwindScreens from '../../../../../tailwind.screens';
 import SiteFooter from '../../utilities/SiteFooter/SiteFooter';
 import SiteNavigationContainer from '../../SiteNavigation/SiteNavigationContainer';
 
 const NewHomePage = () => {
-  return (
-    <>
-      <SiteNavigationContainer />
+  const centerHorizontalRule = css`
+    @media (min-width: ${tailwindScreens.md}) {
+      margin-top: -2px;
+      top: 50%;
+    }
+  `;
 
-      {/* <img
-        src="https://images.ctfassets.net/81iqaqpfd8fy/4k8rv5sN0kii0AoCawc6UQ/c22c3c132d1bb43055b6bafc248fcea5/vn7gpbosm9rx.jpg?fit=fill&h=539&w=1440"
-        alt="homepage banner"
-        className="absolute"
-      /> */}
+  return (
+    <Fragment>
+      {/* @TODO: Once EmotionJS supports shorthand syntax for React.Fragment, switch <Fragment> out for <> syntax! */}
+      <SiteNavigationContainer />
 
       <main>
         <article>
@@ -28,13 +32,16 @@ const NewHomePage = () => {
               background-size: 100% auto;
               background-repeat: no-repeat;
               @media (min-width: ${tailwindScreens.md}) {
+                background-image: url('https://images.ctfassets.net/81iqaqpfd8fy/4k8rv5sN0kii0AoCawc6UQ/c22c3c132d1bb43055b6bafc248fcea5/vn7gpbosm9rx.jpg?fit=fill&h=700&w=700');
+              }
+              @media (min-width: ${tailwindScreens.lg}) {
                 background-image: url('https://images.ctfassets.net/81iqaqpfd8fy/4k8rv5sN0kii0AoCawc6UQ/c22c3c132d1bb43055b6bafc248fcea5/vn7gpbosm9rx.jpg?fit=fill&h=539&w=1440');
               }
             `}
           >
             <div className="">
               <h1
-                className="font-league-gothic font-normal leading-none m-0 text-yellow-500 text-center uppercase"
+                className="font-league-gothic font-normal leading-none m-0 py-24 text-yellow-500 text-center uppercase"
                 css={css`
                   font-size: 84px;
                   @media (min-width: ${tailwindScreens.md}) {
@@ -46,7 +53,15 @@ const NewHomePage = () => {
               </h1>
             </div>
 
-            <div className="bg-blurple-500 py-5">
+            <div
+              className="bg-blurple-500 py-5"
+              css={css`
+                @media (min-width: ${tailwindScreens.md}) {
+                  display: grid;
+                  grid-template-columns: 1fr 1fr 1fr;
+                }
+              `}
+            >
               <div className="px-4 py-5 text-center">
                 <p className="font-league-gothic text-5xl text-teal-300 uppercase">
                   5 million+
@@ -80,60 +95,117 @@ const NewHomePage = () => {
             css={css`
               background: linear-gradient(
                 to bottom,
-                rgba(255, 255, 255, 1) 0%,
+                rgba(255, 255, 255, 1) 25%,
                 rgba(255, 255, 255, 0) 100%
               );
             `}
           >
-            <div className="grid-wide">
-              <h1>Take Action</h1>
+            <div className="grid-wide text-center">
+              <h2 className="relative">
+                <span className="bg-white font-league-gothic font-normal leading-tight inline-block py-2 px-6 relative text-3xl tracking-wide uppercase z-10">
+                  Take Action
+                </span>
+                <span
+                  className="absolute bg-purple-500 block h-1 w-full z-0"
+                  css={centerHorizontalRule}
+                />
+              </h2>
 
-              <p>
+              <p className="my-6">
                 You can even{' '}
                 <a href="/us/about/easy-scholarships">win scholarships</a> and{' '}
                 <a href="/">earn volunteer credits</a> for school! Seriously.
               </p>
 
-              <a href="/us/campaigns" className="btn bg-blurple-500">
+              <a
+                href="/us/campaigns"
+                className="btn bg-blurple-500 focus:bg-blurple-700 inline-block my-8 py-4 px-8 text-lg"
+              >
                 See More Campaigns
               </a>
             </div>
           </section>
 
           <article className="base-12-grid bg-purple-400">
-            <div className="grid-wide">
-              <h1>Get Inspired. Get Entertained. Get Active.</h1>
-              <p>Sign up for one of our newsletters.</p>
+            <div className="grid-wide text-center">
+              <h2 className="text-white">
+                <span className="block lg:inline-block font-league-gothic font-normal tracking-wide text-3xl uppercase">
+                  Get Inspired.
+                </span>{' '}
+                <span className="block lg:inline-block font-league-gothic font-normal lg:px-1 tracking-wide text-3xl uppercase">
+                  Get Entertained.
+                </span>{' '}
+                <span className="block lg:inline-block font-league-gothic font-normal tracking-wide text-3xl uppercase">
+                  Get Active.
+                </span>
+              </h2>
+
+              <p className="text-lg text-white">
+                Sign up for one of our newsletters.
+              </p>
             </div>
           </article>
 
           <section className="base-12-grid bg-gray-100">
-            <div className="grid-wide">
-              <h2>Read About It</h2>
+            <div className="grid-wide text-center">
+              <h2 className="relative">
+                <span className="bg-gray-100 font-league-gothic font-normal leading-tight inline-block py-2 px-6 relative text-3xl tracking-wide uppercase z-10">
+                  Read About It
+                </span>
+                <span
+                  className="absolute bg-purple-500 block h-1 w-full z-0"
+                  css={centerHorizontalRule}
+                />
+              </h2>
 
               <a
                 href="/us/articles"
-                className="btn bg-blurple-500 focus:bg-blurple-700"
+                className="btn bg-blurple-500 focus:bg-blurple-700 inline-block my-8 py-4 px-8 text-lg"
               >
                 See More Articles
               </a>
             </div>
           </section>
 
-          <section className="base-12-grid bg-white h-40">
-            <div className="grid-wide">
-              <h2>Sponsors</h2>
+          {/* @TODO: need to remove the top/bottom padding from base-12-grid class and
+          let components add their own padding otherwise it is hard to override. */}
+          <section className="base-12-grid bg-white py-8">
+            <div className="grid-wide text-center">
+              <h2 className="font-bold mb-3 text-base text-center text-gray-500 uppercase">
+                Sponsors
+              </h2>
+              <ul>
+                {sponsorList.map(sponsor => (
+                  <li key={sponsor.name} className="inline-block mx-6 my-3">
+                    <img
+                      className="opacity-25"
+                      src={contentfulImageUrl(sponsor.image, '125', '40')}
+                      title={sponsor.name}
+                      alt={sponsor.name}
+                    />
+                  </li>
+                ))}
+              </ul>
             </div>
           </section>
 
-          <article className="base-12-grid bg-yellow-500">
-            <div className="grid-wide">
-              <h1>Join our youth-led movement for good</h1>
-              <p>
-                Make an impact with millions of young people, and earn easy
-                scholarships for volunteering.
-              </p>
-              <a href="/authorize" className="btn bg-blurple-500">
+          {/* Need to deal with padding specified in grid class :\ */}
+          <article className="base-12-grid bg-yellow-500 py-4">
+            <div className="grid-wide py-4 text-center">
+              <div className="text-left">
+                <h1 className="font-bold text-2xl">
+                  Join our youth-led movement for good
+                </h1>
+                <p className="text-lg">
+                  Make an impact with millions of young people, and earn easy
+                  scholarships for volunteering.
+                </p>
+              </div>
+
+              <a
+                href="/authorize"
+                className="btn bg-blurple-500 inline-block mt-8 py-4 px-16 text-lg"
+              >
                 Join Now
               </a>
             </div>
@@ -142,7 +214,7 @@ const NewHomePage = () => {
       </main>
 
       <SiteFooter />
-    </>
+    </Fragment>
   );
 };
 
