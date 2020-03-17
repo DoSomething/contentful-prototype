c066a9d84009d20822a85afd39d78886f20b75f9
Lindsay Maher <lmaher@Lindsays-MBP.home>
2020-03-17 15:24:24 -0400

    adds grid class to container on cause preferences

--- resources/assets/components/pages/AccountPage/CausePreferences.js
+++ resources/assets/components/pages/AccountPage/CausePreferences.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const CausePreferences = () => (
-  <>
+  <div className="grid-wide">
     <h1>Your Cause Interests</h1>
     <p>
       We tailor your emails and other communication based on your favorite cause
       areas.
     </p>
-  </>
+  </div>
 );
 
 export default CausePreferences;
