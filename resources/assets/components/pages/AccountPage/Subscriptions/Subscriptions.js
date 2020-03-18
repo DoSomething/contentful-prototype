7c15bfdfe0acec5561160c45727277bd18152d20
Luke Patton <lpatton@dosomething.org>
2020-03-18 16:23:25 -0400

    Removes a bad copy line from the subscription tab.

--- resources/assets/components/pages/AccountPage/Subscriptions/Subscriptions.js
+++ resources/assets/components/pages/AccountPage/Subscriptions/Subscriptions.js
@@ -7,10 +7,6 @@ const Subscriptions = props => (
   <div className="grid-wide">
     <div className="pb-4">
       <h2 className="text-lg">Email Subscriptions</h2>
-      <p>
-        We tailor your emails and other communication based on your favorite
-        cause areas.
-      </p>
     </div>
 
     <EmailSubscriptions {...props} />
