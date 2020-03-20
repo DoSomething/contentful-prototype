1f1ceeec69fefb1ad28b710cc0d1527389e46f44
Lindsay Maher <lmaher@Lindsays-MBP.home>
2020-03-20 15:57:24 -0400

    updates title text size

--- resources/assets/components/pages/AccountPage/Interests/Interests.js
+++ resources/assets/components/pages/AccountPage/Interests/Interests.js
@@ -4,7 +4,7 @@ import CausePreferences from './CausePreferences';
 
 const Interests = () => (
   <div className="grid-wide">
-    <h1 className="pl-4">Your Cause Interests</h1>
+    <h1 className="pl-4 text-lg">Your Cause Interests</h1>
     <p className="pl-4">
       We tailor your emails and other communication based on your favorite cause
       areas.
