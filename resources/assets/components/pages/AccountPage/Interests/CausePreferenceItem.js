d8378e997f7db25f3e0175ce39ac9e7978082267
Lindsay Maher <lmaher@Lindsays-MBP.home>
2020-03-23 14:51:06 -0400

    removes vertical alignment

--- resources/assets/components/pages/AccountPage/Interests/CausePreferenceItem.js
+++ resources/assets/components/pages/AccountPage/Interests/CausePreferenceItem.js
@@ -45,35 +45,36 @@ const CausePreferenceItem = ({ cause, description, title }) => {
   const causes = get(data, 'user.causes', []);
 
   return (
-    <div className="border border-solid border-gray-300 p-4 rounded-md flex justify-between content-center">
+    <div className="border border-solid border-gray-300 p-4 rounded-md flex justify-between">
       <div className="w-2/3 lg:w-3/4 pr-2">
         <h1 className="text-blurple-500 text-base text-bold">{title}</h1>
         <p className="text-sm text-gray-500">{description}</p>
       </div>
-
-      <button
-        type="button"
-        className={classNames(
-          'btn w-1/3 lg:w-1/4 border border-solid border-blurple-500 focus:outline-none align-middle',
-          !causes.includes(cause)
-            ? 'bg-blurple-500 text-white hover:bg-blurple-300 focus:bg-blurple-500 focus:text-white'
-            : 'bg-white border text-blurple-500 border-solid hover:border-blurple-300 hover:text-blurple-200 focus:bg-white focus:text-blurple-500',
-        )}
-        onClick={() =>
-          updateInterest({
-            variables: {
-              cause,
-              interested: !causes.includes(cause),
-            },
-          })
-        }
-      >
-        {!loading ? (
-          <>{causes.includes(cause) ? 'Unfollow' : ' Follow '}</>
-        ) : (
-          <div className="spinner" />
-        )}
-      </button>
+      <div className="w-1/3 lg:w-1/4">
+        <button
+          type="button"
+          className={classNames(
+            'btn w-full border border-solid border-blurple-500 focus:outline-none',
+            !causes.includes(cause)
+              ? 'bg-blurple-500 text-white hover:bg-blurple-300 focus:bg-blurple-500 focus:text-white'
+              : 'bg-white border text-blurple-500 border-solid hover:border-blurple-300 hover:text-blurple-200 focus:bg-white focus:text-blurple-500',
+          )}
+          onClick={() =>
+            updateInterest({
+              variables: {
+                cause,
+                interested: !causes.includes(cause),
+              },
+            })
+          }
+        >
+          {!loading ? (
+            <>{causes.includes(cause) ? 'Unfollow' : ' Follow '}</>
+          ) : (
+            <div className="spinner" />
+          )}
+        </button>
+      </div>
     </div>
   );
 };
