1aca8a0388cb130d8734546caa363d79569ecda6
Aaron Schachter <aaronschachter@gmail.com>
2020-03-27 03:56:08 -0700

    Adds test for 50+ referrals

--- cypress/integration/voter-registration-drive-info.js
+++ cypress/integration/voter-registration-drive-info.js
@@ -7,7 +7,7 @@ const voterRegistrationDriveBlockUrl = '/us/blocks/2T5ARr1AViKw2Kw0Q4S0so';
 describe('Voter Registration Drive', () => {
   beforeEach(() => cy.configureMocks());
 
-  it('Renders count of voter registration referrals', () => {
+  it('Renders count of voter registration referrals when value is under 50', () => {
     const user = userFactory();
 
     cy.mockGraphqlOp('ContentfulBlockQuery', {
@@ -28,5 +28,28 @@ describe('Voter Registration Drive', () => {
     cy.contains('.page-views__text', 'Total page views');
     cy.contains('.voter-registrations__text', 'Total voter registrations');
     cy.contains('h1.voter-registrations__amount', 11);
+  });
+
+  it('Renders 50+ voter registration referrals if count is over 50', () => {
+    const user = userFactory();
+
+    cy.mockGraphqlOp('ContentfulBlockQuery', {
+      block: {
+        id: '2T5ARr1AViKw2Kw0Q4S0so',
+        __typename: 'SocialDriveBlock',
+        link: 'https://vote.dosomething.org',
+        hidePageViews: false,
+      },
+    });
+
+    cy.mockGraphqlOp('UserVoterRegistrationReferralCount', {
+      postsCount: (root, { referrerUserId }) => 51,
+    });
+
+    cy.login(user).visit(voterRegistrationDriveBlockUrl);
+
+    cy.contains('.page-views__text', 'Total page views');
+    cy.contains('.voter-registrations__text', 'Total voter registrations');
+    cy.contains('h1.voter-registrations__amount', '50+');
   });
 });
