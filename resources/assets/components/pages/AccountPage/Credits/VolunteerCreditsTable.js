159cc37b23060df3b65705e53d67a7bc0330a974
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-04-02 18:02:05 -0400

    Don't output VC table when there is no data

--- resources/assets/components/pages/AccountPage/Credits/VolunteerCreditsTable.js
+++ resources/assets/components/pages/AccountPage/Credits/VolunteerCreditsTable.js
@@ -9,42 +9,42 @@ import VolunteerCreditsTableRow from './VolunteerCreditsTableRow';
 
 const TableHeader = tw.th`bg-blurple-500 font-bold p-4 pr-6 text-left text-white`;
 
-const VolunteerCreditsTable = ({ posts }) => (
-  // TODO: Add 'empty' state for when the user has no applicable posts.
-  <table className="border border-solid border-gray-200 border-collapse w-full">
-    <thead>
-      <tr>
-        <TableHeader>Campaign Info</TableHeader>
-        <Media
-          query={`(min-width: ${tailwind('screens.md')})`}
-          render={() => (
-            <React.Fragment>
-              <TableHeader>Action Type</TableHeader>
-              <TableHeader>Date Completed</TableHeader>
-              <TableHeader>Volunteer Hours</TableHeader>
-              <TableHeader>Certificate</TableHeader>
-            </React.Fragment>
-          )}
-        />
-      </tr>
-    </thead>
-
-    <tbody>
-      {posts.map(post => (
-        <tr
-          key={post.id}
-          css={css`
-            :not(:first-of-type) {
-              border-top: 2px solid ${tailwind('colors.gray.200')};
-            }
-          `}
-        >
-          <VolunteerCreditsTableRow {...post} />
+const VolunteerCreditsTable = ({ posts }) =>
+  posts.length ? (
+    <table className="border border-solid border-gray-200 border-collapse w-full">
+      <thead>
+        <tr>
+          <TableHeader>Campaign Info</TableHeader>
+          <Media
+            query={`(min-width: ${tailwind('screens.md')})`}
+            render={() => (
+              <React.Fragment>
+                <TableHeader>Action Type</TableHeader>
+                <TableHeader>Date Completed</TableHeader>
+                <TableHeader>Volunteer Hours</TableHeader>
+                <TableHeader>Certificate</TableHeader>
+              </React.Fragment>
+            )}
+          />
         </tr>
-      ))}
-    </tbody>
-  </table>
-);
+      </thead>
+
+      <tbody>
+        {posts.map(post => (
+          <tr
+            key={post.id}
+            css={css`
+              :not(:first-of-type) {
+                border-top: 2px solid ${tailwind('colors.gray.200')};
+              }
+            `}
+          >
+            <VolunteerCreditsTableRow {...post} />
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  ) : null;
 
 VolunteerCreditsTable.propTypes = {
   posts: PropTypes.object.isRequired,
