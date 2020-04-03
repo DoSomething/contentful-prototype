8a08ed8db2913aa221988f9362f8149436e5aa9a
katiecrane <kcrane@dosomething.org>
2020-04-03 16:12:11 -0700

    Give PhotoSubmissionAction access to url for auth

--- resources/assets/components/actions/PhotoSubmissionAction/PhotoSubmissionActionContainer.js
+++ resources/assets/components/actions/PhotoSubmissionAction/PhotoSubmissionActionContainer.js
@@ -1,22 +1,31 @@
 import { connect } from 'react-redux';
 
 import { getUserId } from '../../../selectors/user';
+import { getDataForNorthstar } from '../../../selectors';
+import { buildAuthRedirectUrl } from '../../../helpers/auth';
 import PhotoSubmissionAction from './PhotoSubmissionAction';
 import {
   resetPostSubmissionItem,
   storeCampaignPost,
+  storePost,
 } from '../../../actions/post';
 
-const mapStateToProps = state => ({
-  campaignId: state.campaign.campaignId,
-  pageId: state.campaign.id || state.page.id,
-  submissions: state.postSubmissions,
-  userId: getUserId(state),
-});
+const mapStateToProps = state => {
+  const northstarData = getDataForNorthstar(state);
+
+  return {
+    campaignId: state.campaign.campaignId,
+    pageId: state.campaign.id || state.page.id,
+    submissions: state.postSubmissions,
+    userId: getUserId(state),
+    authRegisterUrl: buildAuthRedirectUrl(northstarData),
+  };
+};
 
 const actionCreators = {
   resetPostSubmissionItem,
   storeCampaignPost,
+  storePost,
 };
 
 export default connect(
