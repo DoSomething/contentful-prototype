e197dcec62d26713cde9838576427155c93616d0
Aaron Schachter <aaronschachter@gmail.com>
2020-04-08 15:17:23 -0700

    First pass at Voter Registration Drive content

--- resources/assets/components/pages/VoterRegistrationDrivePage/VoterRegistrationDrivePage.js
+++ resources/assets/components/pages/VoterRegistrationDrivePage/VoterRegistrationDrivePage.js
@@ -1,44 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ButtonLink from '../../utilities/ButtonLink/ButtonLink';
 import SiteFooter from '../../utilities/SiteFooter/SiteFooter';
+import ContentBlock from '../../blocks/ContentBlock/ContentBlock';
+import SignupButtonContainer from '../../SignupButton/SignupButtonContainer';
 import SiteNavigationContainer from '../../SiteNavigation/SiteNavigationContainer';
 
 const VoterRegistrationDrivePage = ({ userId }) => (
   <>
     <SiteNavigationContainer />
-    <main className="general-page alpha-referral-page base-12-grid py-3 md:py-6 relative">
-      <article className="grid-narrow">
-        <h3>FAQ</h3>
-
-        <h4>1. Who can I refer?</h4>
-
-        <p>
-          Earn your reward for referring NEW members to DoSomething!
-          Unfortunately, if you refer someone that already has a DoSomething
-          account, you won’t get the reward when they sign up for the shared
-          campaign.
-        </p>
-
-        <h4>2. How will I know if I won the scholarship?</h4>
-
-        <p>
-          We will email you using the same email address used to create your
-          DoSomething account. Scholarship winners are announced when the
-          campaign closes.
-        </p>
-
-        <h4>3. Where can I find the full rules?</h4>
-
-        <p>
-          This offer is for a limited time only. See the{' '}
-          <a href="/us/refer-a-friend-official-rules" target="_blank">
-            Refer A Friend Official Rules.
-          </a>
-        </p>
-      </article>
+    <main>
+      <div className="hero-landing-page">
+        <div className="base-12-grid bg-gray-100 cover-image py-3 md:py-6">
+          <header role="banner" className="hero-banner">
+            <h1 className="hero-banner__headline-title">Ready, Set, Vote!</h1>
+            <h2 className="hero-banner__headline-subtitle">
+              Run an online voter registration drive!
+            </h2>
+          </header>
+        </div>
+      </div>
+      <div className="bg-white">
+        <div className="md:w-3/4 mx-auto py-6 px-3 pitch-landing-page">
+          <ContentBlock
+            superTitle="Step 1"
+            title="Register To Vote"
+            content="If you aren’t pre-registered or registered to vote, take 2 minutes to register at your current address. You’ll have the option to  register with an online form or directly with your state."
+          />
+          <div className="pb-6">
+            <ButtonLink
+              link={`https://register.rockthevote.com/registrants/new?partner=37187&source=user:${userId},source:web,source_details:onlinedrivereferral,referral=true`}
+            >
+              Register To Vote
+            </ButtonLink>
+          </div>
+          <div className="pb-6">
+            <h1 className="section-header__title font-normal font-league-gothic uppercase text-4xl -underlined pb-3">
+              Learn The Facts
+            </h1>
+            <ul>
+              <li>Does my vote actually matter?</li>
+              <li>Is registering to vote online safe?</li>
+            </ul>
+          </div>
+          <ContentBlock
+            superTitle="Step 2"
+            title="Share With Your Friends"
+            content="Every time you share your voter registration page before April 30, you'll be entered for a chance at winning a $1,500 scholarship."
+          />
+          <ButtonLink link="/us/campaigns/online-registration-drive">
+            Get Started
+          </ButtonLink>
+        </div>
+      </div>
     </main>
-
     <SiteFooter />
   </>
 );
