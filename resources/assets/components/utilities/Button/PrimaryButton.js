6a0c4b18424bc3f4b67a53e31c1c1783890f7049
Diego Lorenzo <diego@diegolorenzo.com>
2020-04-10 17:45:18 -0400

    Adding new button components.

---
+++ resources/assets/components/utilities/Button/PrimaryButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PrimaryButton = () => {
+  console.log('💩');
+
+  return <button></button>;
+};
+
+PrimaryButton.propTypes = {};
+
+export default PrimaryButton;
