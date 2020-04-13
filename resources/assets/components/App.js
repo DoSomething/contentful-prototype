8a2bb7f649cb50842708472625cd7540378d6560
vmack <vanessacmack@gmail.com>
2020-04-13 11:00:29 -0400

    first commit for nps cross-site implementation - adds nps survey to entire site

--- resources/assets/components/App.js
+++ resources/assets/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { get } from 'lodash';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { ApolloProvider } from '@apollo/react-common';
@@ -8,6 +9,7 @@ import graphqlClient from '../graphql';
 import { env, featureFlag } from '../helpers';
 import { initializeStore } from '../store/store';
 import HomePage from './pages/HomePage/HomePage';
+import Modal from './utilities/Modal/Modal';
 import BlockPage from './pages/BlockPage/BlockPage';
 import CausePage from './pages/CausePage/CausePage';
 import NewHomePage from './pages/HomePage/NewHomePage';
@@ -15,17 +17,42 @@ import CompanyPage from './pages/CompanyPage/CompanyPage';
 import CampaignContainer from './Campaign/CampaignContainer';
 import BetaReferralPage from './pages/ReferralPage/Beta/BetaPage';
 import CollectionPage from './pages/CollectionPage/CollectionPage';
+import TypeFormEmbed from './utilities/TypeFormEmbed/TypeFormEmbed';
+import DelayedElement from './utilities/DelayedElement/DelayedElement';
 import CampaignsIndexPage from './pages/CampaignsPage/CampaignsIndexPage';
 import AccountContainer from './pages/AccountPage/Account/AccountContainer';
 import PageDispatcherContainer from './PageDispatcher/PageDispatcherContainer';
+import DismissableElement from './utilities/DismissableElement/DismissableElement';
+import TrafficDistribution from './utilities/TrafficDistribution/TrafficDistribution';
 import AlphaReferralPageContainer from './pages/ReferralPage/Alpha/AlphaPageContainer';
 
-const App = ({ store, history }) => {
+const App = ({ store, history, props }) => {
   initializeStore(store);
 
   return (
     <Provider store={store}>
       <ApolloProvider client={graphqlClient(env('GRAPHQL_URL'))}>
+        {props.isAuthenticated ? (
+          <TrafficDistribution percentage={5} feature="nps_survey">
+            <DismissableElement
+              name="nps_survey"
+              render={(handleClose, handleComplete) => (
+                <DelayedElement delay={60}>
+                  <Modal onClose={handleClose} trackingId="SURVEY_MODAL">
+                    <TypeFormEmbed
+                      displayType="modal"
+                      typeformUrl="https://dosomething.typeform.com/to/Bvcwvm"
+                      queryParameters={{
+                        northstar_id: get(window.AUTH, 'id', null),
+                      }}
+                      onSubmit={handleComplete}
+                    />
+                  </Modal>
+                </DelayedElement>
+              )}
+            />
+          </TrafficDistribution>
+        ) : null}
         <Router history={history}>
           <Switch>
             <Route
@@ -77,6 +104,12 @@ const App = ({ store, history }) => {
 App.propTypes = {
   store: PropTypes.object.isRequired,
   history: PropTypes.object.isRequired,
+  props: PropTypes.object.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+
+App.defaultProps = {
+  isAuthenticated: false,
 };
 
 export default App;
