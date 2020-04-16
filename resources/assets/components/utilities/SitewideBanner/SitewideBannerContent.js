b91b0a5fe862e490fc7993131c2f81b8b4b83741
Lindsay Maher <lindsaymah3r@gmail.com>
2020-04-16 15:54:39 -0400

    Updating dismiss tracking name for sitewide banner (#2043)
    * adjusts margins/padding for mobile
    
    * updates tracking name
    
    Co-authored-by: Lindsay Maher <lmaher@Lindsays-MBP.home>

--- resources/assets/components/utilities/SitewideBanner/SitewideBannerContent.js
+++ resources/assets/components/utilities/SitewideBanner/SitewideBannerContent.js
@@ -14,11 +14,11 @@ const SitewideBannerContent = ({
     <div className="w-full flex justify-center bg-yellow-500 p-4 z-50">
       <CloseButton
         callback={handleClose}
-        className="block absolute right-0 top-0 pt-2 pr-4"
+        className="block absolute right-0 top-0 pt-4 md:pt-2 pr-6 md:pr-2"
         size="14px"
       />
-      <div className="flex flex-wrap justify-center items-center pt-4 md:pt-0">
-        <h1 className="pb-2 md:pr-4 md:pb-0 text-center text-base">
+      <div className="flex flex-wrap justify-center items-center">
+        <h1 className="text-center text-base m-4 md:mx-6 md:my-2">
           {description}
         </h1>
         <a
