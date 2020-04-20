d67dd5a25fd0df6b911f236469473167da4a7a68
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-04-20 10:06:56 -0400

    Lazy load the volunteer credits query component (#2038)

--- resources/assets/components/pages/AccountPage/Credits/Credits.js
+++ resources/assets/components/pages/AccountPage/Credits/Credits.js
@@ -1,24 +1,27 @@
 import React from 'react';
 
-import VolunteerCreditsQuery from './VolunteerCreditsQuery';
+import Loader from '../../../utilities/Loader';
 import SectionHeader from '../../../utilities/SectionHeader/SectionHeader';
 
-const Credits = () => (
-  <>
-    <div className="grid-wide-2/3">
-      <SectionHeader underlined title="Volunteer Credits" />
-      <p>
-        Earn volunteer credits through volunteering. Your certificates will
-        appear here within 4 days of you completing a campaign, after our staff
-        is able to verify your work! Learn more about our volunteer credit
-        program.
-      </p>
-    </div>
+const Credits = () => {
+  const VolunteerCreditsQuery = Loader(import('./VolunteerCreditsQuery'));
 
-    <div className="grid-wide my-8">
-      <VolunteerCreditsQuery />
-    </div>
-  </>
-);
+  return (
+    <>
+      <div className="grid-wide-2/3">
+        <SectionHeader underlined title="Volunteer Credits" />
+        <p>
+          Earn volunteer credits through volunteering. Your certificates will
+          appear here within 4 days of you completing a campaign, after our
+          staff is able to verify your work! Learn more about our volunteer
+          credit program.
+        </p>
+      </div>
 
+      <div className="grid-wide my-8">
+        <VolunteerCreditsQuery />
+      </div>
+    </>
+  );
+};
 export default Credits;
