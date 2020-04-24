f8bac02befb7a846b2a59809d80400f0a5b7ac41
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-04-24 11:15:44 -0400

    Imperatively render and download the certificate
    This gives us more control to handle error reporting and loading state, and avoids auto-rendering all certificates upon page load

--- resources/assets/components/pages/AccountPage/Credits/CertificateDownloadButton.js
+++ resources/assets/components/pages/AccountPage/Credits/CertificateDownloadButton.js
@@ -1,30 +1,48 @@
 import React from 'react';
-import classNames from 'classnames';
-import { PDFDownloadLink } from '@react-pdf/renderer';
+import { pdf } from '@react-pdf/renderer';
 
 import CertificateTemplate, {
   certificatePostType,
 } from './CertificateTemplate';
 
-// The certificate PDF Download button with pending/ready state.
-const buttonClassNames = 'btn w-full py-4 text-lg';
-const CertificateDownloadButton = ({ certificatePost }) =>
-  certificatePost.pending ? (
-    <button type="button" disabled className={buttonClassNames}>
-      Pending
-    </button>
-  ) : (
-    <PDFDownloadLink
-      document={<CertificateTemplate certificatePost={certificatePost} />}
-      fileName="dosomething-volunteer-credit-certificate.pdf"
-      className={classNames(
-        buttonClassNames,
-        'bg-blue-500 hover:bg-blue-300 hover:no-underline hover:text-white',
-      )}
+const CertificateDownloadButton = ({ certificatePost }) => {
+  const handleClick = () => {
+    // Render the certificate template as a PDF in Blob data form.
+    pdf(<CertificateTemplate certificatePost={certificatePost} />)
+      .toBlob()
+      .then(blob => {
+        // Create a phantom <a> tag to download the certificate for the user.
+        const phantomDownloadLink = document.createElement('a');
+        // Assign the generated PDF blob data as the download link.
+        phantomDownloadLink.href = URL.createObjectURL(blob);
+        // Assign the certificate file name.
+        phantomDownloadLink.download =
+          // @TODO: Update filename to include campaign title and exclude 'credit'.
+          'dosomething-volunteer-credit-certificate.pdf';
+
+        // 'Click' the link to trigger the download.
+        phantomDownloadLink.click();
+      })
+      .catch(() => {
+        // Report error, update button error state.
+      });
+  };
+
+  const { pending } = certificatePost;
+
+  // @TODO Handle loading and error state.
+  return (
+    // @TODO: Use one of our in-house button components.
+    <button
+      type="button"
+      disabled={pending}
+      className="btn w-full py-4 text-lg bg-blue-500 hover:bg-blue-300 active:no-outline"
+      onClick={() => !pending && handleClick()}
     >
-      Download
-    </PDFDownloadLink>
+      {pending ? 'Pending' : 'Download'}
+    </button>
   );
+};
 
 CertificateDownloadButton.propTypes = {
   certificatePost: certificatePostType.isRequired,
