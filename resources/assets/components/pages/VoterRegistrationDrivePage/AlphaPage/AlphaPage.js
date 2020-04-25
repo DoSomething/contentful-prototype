a9d389aa2eb39f93c05b80e5d4efb12f1765680a
Aaron Schachter <aaronschachter@gmail.com>
2020-04-25 16:10:46 -0700

    Adds more ContentBlocks

--- resources/assets/components/pages/VoterRegistrationDrivePage/AlphaPage/AlphaPage.js
+++ resources/assets/components/pages/VoterRegistrationDrivePage/AlphaPage/AlphaPage.js
@@ -24,6 +24,16 @@ const AlphaPage = () => {
         content="You have registered **1 person** so far."
         className="grid-wide"
       />
+      <ContentBlock
+        title="How to share"
+        content="Use the sharing tools below to track how many people you register to vote! To enter the scholarship, you must upload a screenshot of each share.  Each upload counts as a scholarship entry - the more you share, the more your chances of winning! Check out the Official Scholarship Rules for more information."
+        className="grid-wide"
+      />
+      <ContentBlock
+        title="FAQs"
+        content="What are some tips for sharing?"
+        className="grid-wide"
+      />
     </React.Fragment>
   );
 };
