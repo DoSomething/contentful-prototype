e0173cdada439a66d204908683f1cba11814047e
David Furnes <david@dfurnes.com>
2020-04-27 14:28:15 -0400

    Add root-level error boundary.

--- resources/assets/components/App.js
+++ resources/assets/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { get } from 'lodash';
 import PropTypes from 'prop-types';
-import { Provider } from 'react-redux';
+import ErrorBoundary from 'react-error-boundary';
 import { ApolloProvider } from '@apollo/react-common';
+import { Provider as ReduxProvider } from 'react-redux';
 import { Router, Route, Switch } from 'react-router-dom';
 
 import graphqlClient from '../graphql';
+import ErrorPage from './pages/ErrorPage';
 import { initializeStore } from '../store/store';
 import HomePage from './pages/HomePage/HomePage';
 import Modal from './utilities/Modal/Modal';
@@ -32,96 +34,98 @@ const App = ({ store, history }) => {
   initializeStore(store);
 
   return (
-    <Provider store={store}>
-      {featureFlag('sitewide_cta_banner') ? (
-        <DismissableElement
-          name="sitewide_banner_call_to_action"
-          daysToReRender={7}
-          context={{ contextSource: 'voter_registration' }}
-          render={(handleClose, handleComplete) => (
-            <SitewideBanner
-              cta="Get Started"
-              description="Make your voice heard. Register to vote in less than 2 minutes."
-              handleClose={handleClose}
-              handleComplete={handleComplete}
-              link={buildVoterRegUrl('web', 'hellobar')}
-            />
-          )}
-        />
-      ) : null}
-      <ApolloProvider client={graphqlClient(env('GRAPHQL_URL'))}>
-        {featureFlag('sitewide_nps_survey') &&
-        window.location.pathname !== '/us' ? (
-          <TrafficDistribution percentage={5} feature="nps_survey">
-            <DismissableElement
-              name="nps_survey"
-              render={(handleClose, handleComplete) => (
-                <DelayedElement delay={30}>
-                  <Modal onClose={handleClose} trackingId="SURVEY_MODAL">
-                    <TypeFormEmbed
-                      displayType="modal"
-                      typeformUrl="https://dosomething.typeform.com/to/Bvcwvm"
-                      queryParameters={{
-                        northstar_id: get(window.AUTH, 'id', null),
-                        url: window.location.pathname,
-                      }}
-                      onSubmit={handleComplete}
-                    />
-                  </Modal>
-                </DelayedElement>
-              )}
-            />
-          </TrafficDistribution>
+    <ReduxProvider store={store}>
+      <ErrorBoundary FallbackComponent={ErrorPage}>
+        {featureFlag('sitewide_cta_banner') ? (
+          <DismissableElement
+            name="sitewide_banner_call_to_action"
+            daysToReRender={7}
+            context={{ contextSource: 'voter_registration' }}
+            render={(handleClose, handleComplete) => (
+              <SitewideBanner
+                cta="Get Started"
+                description="Make your voice heard. Register to vote in less than 2 minutes."
+                handleClose={handleClose}
+                handleComplete={handleComplete}
+                link={buildVoterRegUrl('web', 'hellobar')}
+              />
+            )}
+          />
         ) : null}
-        <Router history={history}>
-          <Switch>
-            <Route
-              exact
-              path="/us"
-              component={featureFlag('new_homepage') ? NewHomePage : HomePage}
-            />
-            <Route path="/us/account" component={AccountContainer} />
-            <Route path="/us/blocks/:id" component={BlockPage} />
-            {featureFlag('dynamic_explore_campaigns') ? (
+        <ApolloProvider client={graphqlClient(env('GRAPHQL_URL'))}>
+          {featureFlag('sitewide_nps_survey') &&
+          window.location.pathname !== '/us' ? (
+            <TrafficDistribution percentage={5} feature="nps_survey">
+              <DismissableElement
+                name="nps_survey"
+                render={(handleClose, handleComplete) => (
+                  <DelayedElement delay={30}>
+                    <Modal onClose={handleClose} trackingId="SURVEY_MODAL">
+                      <TypeFormEmbed
+                        displayType="modal"
+                        typeformUrl="https://dosomething.typeform.com/to/Bvcwvm"
+                        queryParameters={{
+                          northstar_id: get(window.AUTH, 'id', null),
+                          url: window.location.pathname,
+                        }}
+                        onSubmit={handleComplete}
+                      />
+                    </Modal>
+                  </DelayedElement>
+                )}
+              />
+            </TrafficDistribution>
+          ) : null}
+          <Router history={history}>
+            <Switch>
               <Route
                 exact
-                path="/us/campaigns"
-                component={CampaignsIndexPage}
+                path="/us"
+                component={featureFlag('new_homepage') ? NewHomePage : HomePage}
               />
-            ) : null}
-            <Route path="/us/campaigns/:slug" component={CampaignContainer} />
-            <Route
-              path="/us/causes/:slug"
-              render={routeProps => (
-                <CausePage slug={routeProps.match.params.slug} />
-              )}
-            />
-            <Route
-              path="/us/collections/:slug"
-              render={routeProps => (
-                <CollectionPage slug={routeProps.match.params.slug} />
-              )}
-            />
-            <Route
-              path="/us/about/:slug"
-              render={routeProps => (
-                <CompanyPage slug={routeProps.match.params.slug} />
-              )}
-            />
-            <Route path="/us/join" component={BetaReferralPage} />
-            <Route
-              path="/us/my-voter-registration-drive"
-              component={BetaVoterRegistrationDrivePage}
-            />
-            <Route
-              path="/us/refer-friends"
-              component={AlphaReferralPageContainer}
-            />
-            <Route path="/us/:slug" component={PageDispatcherContainer} />
-          </Switch>
-        </Router>
-      </ApolloProvider>
-    </Provider>
+              <Route path="/us/account" component={AccountContainer} />
+              <Route path="/us/blocks/:id" component={BlockPage} />
+              {featureFlag('dynamic_explore_campaigns') ? (
+                <Route
+                  exact
+                  path="/us/campaigns"
+                  component={CampaignsIndexPage}
+                />
+              ) : null}
+              <Route path="/us/campaigns/:slug" component={CampaignContainer} />
+              <Route
+                path="/us/causes/:slug"
+                render={routeProps => (
+                  <CausePage slug={routeProps.match.params.slug} />
+                )}
+              />
+              <Route
+                path="/us/collections/:slug"
+                render={routeProps => (
+                  <CollectionPage slug={routeProps.match.params.slug} />
+                )}
+              />
+              <Route
+                path="/us/about/:slug"
+                render={routeProps => (
+                  <CompanyPage slug={routeProps.match.params.slug} />
+                )}
+              />
+              <Route path="/us/join" component={BetaReferralPage} />
+              <Route
+                path="/us/my-voter-registration-drive"
+                component={BetaVoterRegistrationDrivePage}
+              />
+              <Route
+                path="/us/refer-friends"
+                component={AlphaReferralPageContainer}
+              />
+              <Route path="/us/:slug" component={PageDispatcherContainer} />
+            </Switch>
+          </Router>
+        </ApolloProvider>
+      </ErrorBoundary>
+    </ReduxProvider>
   );
 };
 
