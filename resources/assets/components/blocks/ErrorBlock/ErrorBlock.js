f3b56129b13ebba7e2b877b06b64c2f0e7bf61db
David Furnes <david@dfurnes.com>
2020-04-27 17:49:05 -0400

    Improve error handling & reporting. (#2068)
    * Make sure errors get reported.
    
    * Only report errors once.
    
    * Display technical details in error & pre-fill in ticket.
    
    * Add root-level error boundary.
    
    * Remove duplicated context & add "ID:" prefix to user ID.
    
    * Re-report if error changes.

--- resources/assets/components/blocks/ErrorBlock/ErrorBlock.js
+++ resources/assets/components/blocks/ErrorBlock/ErrorBlock.js
@@ -1,32 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import errorIcon from './error_icon.svg';
 import { report } from '../../../helpers';
 import Card from '../../utilities/Card/Card';
-import { HELP_REQUEST_LINK } from '../../../constants';
-
-const DEBUGGING = process.env.NODE_ENV !== 'production';
+import ErrorDetails from '../../utilities/ErrorDetails';
 
 const ErrorBlock = ({ error }) => {
   // Print error to console & report to New Relic:
-  if (error) {
-    console.error(`[ErrorBlock] ${error}`);
-    report(error);
-  }
+  useEffect(() => report(error), [error]);
 
   return (
     <Card className="rounded bordered p-3">
       <img src={errorIcon} alt="Error" className="mx-auto my-8" />
       <p className="text-center my-4">
-        <strong>Something went wrong!</strong> Try refreshing the page or{' '}
-        <a href={HELP_REQUEST_LINK}>reach out</a> to us.
+        <strong>Something went wrong!</strong> Try refreshing the page - it may
+        work the second time. If not, we&apos;ve already noted the problem &amp;
+        will try to fix it as soon as possible.
       </p>
-      {DEBUGGING && error ? (
-        <p className="color-error text-center my-4">
-          <code>{JSON.stringify(error)}</code>
-        </p>
-      ) : null}
+      <ErrorDetails error={error} />
     </Card>
   );
 };
---
+++ resources/assets/components/utilities/ErrorDetails.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { makeUrl } from '../../helpers';
+import { HELP_REQUEST_LINK } from '../../constants';
+
+const ErrorDetails = ({ error }) => {
+  const context = `${+new Date()}, ID: ${window.AUTH ? window.AUTH.id : 'N/A'}`;
+
+  let message = error;
+  if (error instanceof Error) {
+    const trace = error.stack.toString().split(/\r\n|\n/)[0];
+    message = `${error.message} ${trace}`;
+  }
+
+  const technicalDetails = `${message}, ${context}`;
+
+  return (
+    <>
+      <p className="text-sm text-gray-800 text-center m-0">
+        If you continue to run into problems, contact our{' '}
+        <a
+          href={makeUrl(HELP_REQUEST_LINK, { technicalDetails })}
+          className="font-semibold text-gray-800 hover:text-gray-600 underline"
+        >
+          support squad
+        </a>
+        !
+      </p>
+      {error ? (
+        <p className="text-sm text-gray-500 text-center my-4 leading-normal">
+          <strong>Technical Details:</strong> {technicalDetails}
+        </p>
+      ) : null}
+    </>
+  );
+};
+
+ErrorDetails.propTypes = {
+  error: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+};
+
+ErrorDetails.defaultProps = {
+  error: 'Unknown Error',
+};
+
+export default ErrorDetails;
