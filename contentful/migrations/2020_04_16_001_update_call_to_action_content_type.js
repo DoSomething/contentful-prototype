4f9899332b997c4f71247be8270dcf8d48586a1b
Luke Patton <lpatton@dosomething.org>
2020-04-27 11:36:56 -0400

    A migration for the new CTA type (#2055)
    * Started new migration script for CTA block
    
    * Cleaning up the migration file a bit
    
    * nvm we're keeping a little comment
    
    * Removed the regex validation cause it complained
    
    Will add validtions to each environment manaully
    
    * Update field name to be Destination

---
+++ contentful/migrations/2020_04_16_001_update_call_to_action_content_type.js
@@ -0,0 +1,57 @@
+// Sets up the following model for the Call To Action content type:
+// Internal title
+// Supertitle ++
+// Title ++
+// Content
+// Link Text ++
+// Link Location ++
+// Template ++
+// Alignment ++
+
+module.exports = function(migration) {
+  const callToAction = migration.editContentType('callToAction');
+
+  callToAction
+    .createField('superTitle')
+    .name('Supertitle')
+    .type('Symbol')
+    .required(false)
+    .localized(false);
+
+  callToAction
+    .createField('title')
+    .name('Title')
+    .type('Symbol')
+    .required(true)
+    .localized(false);
+
+  callToAction
+    .createField('linkText')
+    .name('Link Text')
+    .type('Symbol')
+    .required(true)
+    .localized(false);
+
+  callToAction
+    .createField('link')
+    .name('Link Destination')
+    .type('Symbol')
+    .required(true)
+    .localized(false);
+
+  callToAction
+    .createField('template')
+    .name('Template')
+    .type('Symbol')
+    .required(true)
+    .localized(false)
+    .validations([{ in: ['Purple', 'Yellow', 'Voter Registration'] }]);
+
+  callToAction
+    .createField('alignment')
+    .name('Alignment')
+    .type('Symbol')
+    .required(true)
+    .localized(false)
+    .validations([{ in: ['Left', 'Center'] }]);
+};
