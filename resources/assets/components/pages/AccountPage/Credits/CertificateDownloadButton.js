ba83ff665911c371bf24c3765e1eb7bf1c091b8a
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-04-28 14:39:02 -0400

    Volunteer Certificate Download Button - Full state flow (#2067)
    * Update download button with complete state flow
    
    * Remove superflous comment re https://github.com/DoSomething/phoenix-next/pull/2067#discussion_r416678245
    
    * Remove superflous comment re https://github.com/DoSomething/phoenix-next/pull/2067#discussion_r416714753
    
    * Remove superflous comment re https://github.com/DoSomething/phoenix-next/pull/2067#discussion_r416715436
    
    * Remove unclear comment re https://github.com/DoSomething/phoenix-next/pull/2067#discussion_r416720023
    
    * Better organize the logical flow per https://github.com/DoSomething/phoenix-next/pull/2067#discussion_r416634482
    
    * Remove more comments
    
    * Dynamically update button text per https://github.com/DoSomething/phoenix-next/pull/2067#discussion_r416684693
    
    * Oops. Don't update button loading text for pending buttons
    
    * Use in-house ElementButton & add loading state per https://github.com/DoSomething/phoenix-next/pull/2067#discussion_r416717955
    
    * I don't hate blurple @weerd :sweat_smile: https://github.com/DoSomething/phoenix-next/pull/2067#discussion_r416783700
    
    * Update error naming per https://github.com/DoSomething/phoenix-next/pull/2067#discussion_r416786267

--- resources/assets/components/pages/AccountPage/Credits/CertificateDownloadButton.js
+++ resources/assets/components/pages/AccountPage/Credits/CertificateDownloadButton.js
@@ -1,55 +1,116 @@
-import React, { useRef, useState } from 'react';
+import classNames from 'classnames';
 import { pdf } from '@react-pdf/renderer';
+import React, { useState, useEffect } from 'react';
 
+import ErrorBlock from '../../../blocks/ErrorBlock/ErrorBlock';
+import ElementButton from '../../../utilities/Button/ElementButton';
 import CertificateTemplate, {
   certificatePostType,
 } from './CertificateTemplate';
 
 const CertificateDownloadButton = ({ certificatePost }) => {
-  const [hasGeneratedPdf, setHasgeneratedPdf] = useState(false);
-  const pdfLink = useRef(null);
+  const [loading, setLoading] = useState(false);
+  const [pdfLink, setPdfLink] = useState(null);
+  const [buttonText, setButtonText] = useState(
+    certificatePost.pending ? 'pending' : 'download',
+  );
+  const [generatePdfError, setGeneratePdfError] = useState(null);
 
-  const handleClick = () => {
-    if (!hasGeneratedPdf) {
+  // Dynamically toggles the button text to 'loading' while we're in loading state.
+  useEffect(() => {
+    if (certificatePost.pending) {
+      return;
+    }
+
+    setButtonText(loading ? 'loading' : 'download');
+  }, [loading]);
+
+  /**
+   * Handle PDF rendering errors.
+   *
+   * @param {Error} error
+   */
+  const handleError = error => {
+    setLoading(false);
+
+    setGeneratePdfError(error);
+    // @TODO: Track analytics. Report error to NR.
+    console.error(error);
+  };
+
+  /**
+   * Generate the certificate PDF data, and build a phantom download link.
+   *
+   * @return {Promise|Undefined}
+   */
+  const generatePdfLink = () =>
+    new Promise((resolve, reject) => {
+      // Playing it safe to catch any errors caused by the initial PDF rendering step.
       try {
+        // Render the PDF template.
         pdf(<CertificateTemplate certificatePost={certificatePost} />)
           .toBlob()
           .then(blob => {
-            pdfLink.current.href = URL.createObjectURL(blob);
-            pdfLink.current.click();
+            // Create a 'phantom' link so we can download the PDF on the user's behalf.
+            const phantomLink = document.createElement('a');
 
-            setHasgeneratedPdf(true);
+            phantomLink.href = URL.createObjectURL(blob);
+            // Assign the PDF filename.
+            // @TODO: Update filename per https://www.pivotaltracker.com/story/show/172439408/comments/213717690.
+            phantomLink.download = 'certificate.pdf';
+
+            resolve(phantomLink);
           })
-          .catch(error => {
-            console.error(error);
-          });
+          // Catch any errors from the PDF data -> blob conversion.
+          .catch(reject);
+        // Catch any errors from the initial PDF render.
       } catch (error) {
-        console.error(error);
+        reject(error);
       }
+    });
+
+  /**
+   * Handle click events on the download button.
+   *
+   */
+  const handleClick = () => {
+    // If we've already generated a pdfLink, simply 'click' it to download the PDF.
+    if (pdfLink) {
+      pdfLink.click();
+      return;
     }
+
+    setLoading(true);
+
+    // Generate the phantom PDF link, and 'click' it to download the PDF!
+    generatePdfLink()
+      .then(link => {
+        // Save this download link in state so we can just 'click' it again if necessary.
+        setPdfLink(link);
+
+        link.click();
+
+        setLoading(false);
+      })
+      .catch(handleError);
   };
 
-  if (certificatePost.pending) {
-    return (
-      <button type="button" disabled className="btn w-full py-4 text-lg">
-        Pending
-      </button>
-    );
+  if (generatePdfError) {
+    return <ErrorBlock error={generatePdfError} />;
   }
 
-  // @TODO Handle loading and error state.
   return (
-    // eslint-disable-next-line jsx-a11y/anchor-is-valid
-    <a
-      role="link"
-      tabIndex="0"
-      download="certa.pdf"
-      ref={pdfLink}
-      className="btn w-full py-4 text-lg bg-blue-500 hover:bg-blue-300"
-      onClick={handleClick}
-    >
-      Download
-    </a>
+    <ElementButton
+      type="button"
+      className={classNames('w-full py-4 text-lg capitalize', {
+        'bg-blurple-500 hover:bg-blurple-300': !certificatePost.pending,
+      })}
+      isDisabled={certificatePost.pending || loading}
+      isLoading={loading}
+      // If a sneaky user removes the 'disabled' attribute we don't want to trigger the PDF download.
+      onClick={() => !certificatePost.pending && handleClick()}
+      text={buttonText}
+    />
   );
 };
 
