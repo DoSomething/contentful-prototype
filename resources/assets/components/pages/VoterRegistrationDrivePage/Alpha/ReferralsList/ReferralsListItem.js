69b200f147e44999ce9f74bc9889532f1962033b
Aaron Schachter <aaronschachter@gmail.com>
2020-04-28 10:47:30 -0700

    Adds empty registration icon

--- resources/assets/components/pages/VoterRegistrationDrivePage/Alpha/ReferralsList/ReferralsListItem.js
+++ resources/assets/components/pages/VoterRegistrationDrivePage/Alpha/ReferralsList/ReferralsListItem.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import VoterRegistrationImage from './registration.svg';
+import EmptyRegistrationImage from './empty-registration.svg';
 import { BaseFigure } from '../../../../utilities/Figure/Figure';
+import CompletedRegistrationImage from './completed-registration.svg';
 
 const ReferralsListItem = props => {
   const { label } = props;
 
-  const media = <img src={VoterRegistrationImage} alt="Registered to vote" />;
+  const media = (
+    <img
+      src={label ? CompletedRegistrationImage : EmptyRegistrationImage}
+      alt={label ? 'Completed voter registration icon' : 'Empty circle icon'}
+    />
+  );
 
   return (
     <BaseFigure media={media} size="medium">
