ffd991af21ef450216ae6317ad6604ecb2c01f27
Aaron Schachter <aaronschachter@gmail.com>
2020-04-29 11:17:14 -0700

    Adds test for additional count

--- cypress/integration/alpha-voter-registration-drive-page.js
+++ cypress/integration/alpha-voter-registration-drive-page.js
@@ -1,10 +1,22 @@
 /// <reference types="Cypress" />
 
+import faker from 'faker';
 import { userFactory } from '../fixtures/user';
 import exampleVoterRegistrationDriveCampaign from '../fixtures/contentful/exampleVoterRegistrationDriveCampaign';
 
 const legacyVoterRegistrationDriveActionPageBlockId =
   exampleVoterRegistrationDriveCampaign.campaign.pages[0].fields.blocks[0].id;
+
+/**
+ * @param String displayName
+ * @return Object
+ */
+function fakePost(displayName) {
+  return {
+    id: faker.random.number(),
+    user: { displayName },
+  };
+}
 
 describe('Alpha Voter Registration Drive Page', () => {
   beforeEach(() => cy.configureMocks());
@@ -61,20 +73,7 @@ describe('Alpha Voter Registration Drive Page', () => {
     const user = userFactory();
 
     cy.mockGraphqlOp('AlphaVoterRegistrationReferrals', {
-      posts: [
-        {
-          id: 2,
-          user: {
-            displayName: 'Jesus Q.',
-          },
-        },
-        {
-          id: 3,
-          user: {
-            displayName: 'Walter S.',
-          },
-        },
-      ],
+      posts: [fakePost('Jesus Q.'), fakePost('Walter S.')],
     });
 
     cy.withFeatureFlags({
@@ -90,5 +89,36 @@ describe('Alpha Voter Registration Drive Page', () => {
     });
     cy.get('.referral-list-item-empty').should('have.length', 1);
     cy.get('#additional-referrals-count').should('have.length', 0);
+  });
+
+  it('Alpha OVRD page displays 3 completed icons and additinonal count if user has 5 referrals', () => {
+    const user = userFactory();
+
+    cy.mockGraphqlOp('AlphaVoterRegistrationReferrals', {
+      posts: [
+        fakePost('Sarah C.'),
+        fakePost('Kyle R.'),
+        fakePost('John C.'),
+        fakePost('Miles D.'),
+        fakePost('Tarissa D.'),
+      ],
+    });
+
+    cy.withFeatureFlags({
+      voter_reg_alpha_page: true,
+    }).authVisitCampaignWithSignup(user, exampleVoterRegistrationDriveCampaign);
+
+    cy.get('.referral-list-item-completed').should('have.length', 3);
+    cy.nth('.referral-list-item-completed', 0).within(() => {
+      cy.get('.referral-list-item-label').contains('Sarah C.');
+    });
+    cy.nth('.referral-list-item-completed', 1).within(() => {
+      cy.get('.referral-list-item-label').contains('Kyle R.');
+    });
+    cy.nth('.referral-list-item-completed', 2).within(() => {
+      cy.get('.referral-list-item-label').contains('John C.');
+    });
+    cy.get('.referral-list-item-empty').should('have.length', 0);
+    cy.get('#additional-referrals-count').contains('+ 2 more');
   });
 });
