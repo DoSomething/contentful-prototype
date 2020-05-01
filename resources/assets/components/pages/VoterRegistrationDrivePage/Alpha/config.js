26b2ef34116d7815bc0beebd46d415cc3949b087
Aaron Schachter <aaronschachter@gmail.com>
2020-05-01 11:33:42 -0700

    Adds config comments and list of voting reasons

--- resources/assets/components/pages/VoterRegistrationDrivePage/Alpha/config.js
+++ resources/assets/components/pages/VoterRegistrationDrivePage/Alpha/config.js
@@ -1,14 +1,28 @@
 import { isDevEnvironment } from '../../../../helpers';
 
 export const faq = {
+  // ContentBlock used for FAQ section.
   contentBlockId: isDevEnvironment()
     ? '6H22Y1wmICy05pM9twIGGR'
     : '1nLV3YUhLzJdlcGrd2Mq9N',
 };
 
 export const shareLink = {
+  // Used to count total number of scholarship entries.
   actionId: isDevEnvironment() ? 27 : 1025,
+  // ContentBlock that appears above the SocialDriveAction.
   contentBlockId: isDevEnvironment()
     ? '1xcG1CTinKwn3Iyxtcc0f4'
     : '3fj7mXlyrcJZ3mUKXqco1R',
+};
+
+// Options to use for customizing the SocialDriveAction share link (not implemented yet).
+export const votingReasons = {
+  'climate-change': 'Climate Change',
+  'gun-violence': 'Gun Violence',
+  'mental-health': 'Mental Health',
+  'student-debt': 'Student Debt',
+  'covid-relief': 'COVID-19 Relief',
+  'immigration-reform': 'Immigration Reform',
+  healthcare: 'Healthcare',
 };
--- resources/assets/components/pages/VoterRegistrationDrivePage/Beta/config.js
+++ resources/assets/components/pages/VoterRegistrationDrivePage/Beta/config.js
@@ -1,19 +1,33 @@
 import { isDevEnvironment } from '../../../../helpers';
 
 export const faq = {
+  // ContentBlock used for FAQ section.
   contentBlockId: isDevEnvironment()
     ? '3cXc0RPMVNeE4surEqFujL'
     : '4yP8BdIdiGU0qwZaFyzmsm',
 };
 
 export const registerToVote = {
+  // ContentBlock used for step to start voter registration.
   contentBlockId: isDevEnvironment()
     ? 'bt0jUBYJaKoi1oab25Wmx'
     : '2d2i2M3yn4RB9pZYVzQxGm',
 };
 
 export const voterRegistrationDriveCampaignLink = {
+  // ContentBlock used for step to join the voter registration drive campaign (become an alpha).
   contentBlockId: isDevEnvironment()
     ? '3p2qz2JPCvgVitgRVBoMFz'
     : '30rCn63G1rnpzojCXC9PmF',
+};
+
+// Map of query string parameters and the voting reasons to display in the hero section quote.
+export const votingReasons = {
+  'climate-change': 'climate change',
+  'gun-violence': 'gun violence',
+  'mental-health': 'mental health',
+  'student-debt': 'student debt',
+  'covid-relief': 'COVID-19 relief',
+  'immigration-reform': 'immigration reform',
+  healthcare: 'healthcare',
 };
