773f6aed77a3ede0c08bd0d160541ef30a6f1627
dinnall <michaeldinnall@gmail.com>
2020-05-01 20:31:14 -0400

    Refactored url
    Refractored url
    added another param for buildVoterRegUrl helper function
    
    Co-Authored-By: Lindsay Maher <lindsayrmaher@users.noreply.github.com>

--- resources/assets/components/pages/VoterRegistrationDrivePage/Beta/StartVoterRegistrationForm.js
+++ resources/assets/components/pages/VoterRegistrationDrivePage/Beta/StartVoterRegistrationForm.js
@@ -1,16 +1,35 @@
-import React, { useState } from 'React';
+import React, { useState } from 'react';
 import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
 import Card from '../../../../components/utilities/Card/Card';
 import PrimaryButton from '../../../utilities/Button/PrimaryButton';
-
 import {
   EVENT_CATEGORIES,
   trackAnalyticsEvent,
 } from '../../../../helpers/analytics';
 
-const StartVoterRegistrationForm = prop => {
-  const url = `https://register.rockthevote.com/registrants/new?partner=37187&source=user:${prop.referrerUserId},source:web,source_details:onlinedrivereferral,referral=true`;
+import { buildVoterRegUrl } from '../../../../helpers/index';
+
+const StartVoterRegistrationForm = ({ referrerUserId }) => {
+  const [emailValue, setEmailValue] = useState('');
+  const [zipcodeValue, setZipcodeValue] = useState('');
+  const url = buildVoterRegUrl(
+    'web',
+    'onlinedrivereferral,referral=true',
+    'https://register.rockthevote.com/registrants/new?partner=37187&',
+    { email_address: emailValue, home_zip_code: zipcodeValue },
+  );
+  /**
+   * should we be passing the userID or referrerId ?
+   * */
+
+  // `https://register.rockthevote.com/registrants/new?partner=37187&source=user:${referrerUserId},source:web,source_details:onlinedrivereferral,referral=true`;
+
+  const handleChange = event => {
+    event.target.name === 'email'
+      ? setEmailValue(event.target.value)
+      : setZipcodeValue(event.target.value);
+  };
 
   const handleClick = () => {
     trackAnalyticsEvent('click_voter_registration_action'),
@@ -19,21 +38,50 @@ const StartVoterRegistrationForm = prop => {
         category: EVENT_CATEGORIES.campaignAction,
         label: 'voter_registration',
         context: {
-          blockId,
           campaignId,
-          pageId,
           url,
         },
       };
   };
 
+  let enabled = emailValue.length > 0 && zipcodeValue.length > 0;
   return (
     <>
-      <Card title="Register Online to vote">
+      <Card className="bordered rounded" title="Register Online to vote">
+        <form className="form pb-2">
+          <div>
+            <label>
+              zipcode
+              <input
+                className="text-field"
+                type="text"
+                name="zipcode"
+                placeholder="zipcode"
+                value={zipcodeValue}
+                onChange={handleChange}
+              />
+            </label>
+          </div>
+
+          <div>
+            <label>
+              email
+              <input
+                className="text-field"
+                type="text"
+                name="email"
+                placeholder="email"
+                onChange={handleChange}
+                value={emailValue}
+              />
+            </label>
+          </div>
+        </form>
         <PrimaryButton
           onClick={handleClick}
           href={url}
           text="Register To Vote"
+          isDisabled={enabled}
         />
       </Card>
     </>
