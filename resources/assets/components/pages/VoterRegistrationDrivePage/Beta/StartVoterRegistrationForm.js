af3e28bf95431484323f88fa7ec7f53349f2abb0
dinnall <michaeldinnall@gmail.com>
2020-05-05 17:13:06 -0400

    Update StartVoterRegistrationForm.js
    Refractored component to satisfy step 1 of beta page
    
    Co-Authored-By: Lindsay Maher <lindsayrmaher@users.noreply.github.com>

--- resources/assets/components/pages/VoterRegistrationDrivePage/Beta/StartVoterRegistrationForm.js
+++ resources/assets/components/pages/VoterRegistrationDrivePage/Beta/StartVoterRegistrationForm.js
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
-import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
-import Card from '../../../../components/utilities/Card/Card';
-import PrimaryButton from '../../../utilities/Button/PrimaryButton';
+import React, { useState, useRef } from 'react';
+
 import {
   EVENT_CATEGORIES,
   trackAnalyticsEvent,
@@ -10,10 +8,10 @@ import {
 import Card from '../../../utilities/Card/Card';
 import {
   buildVoterRegUrl,
+  isValidEmailSyntax,
+  isValidPostalCode,
 } from '../../../../helpers/index';
 import PrimaryButton from '../../../utilities/Button/PrimaryButton';
-
-import { buildVoterRegUrl } from '../../../../helpers/index';
 
 const StartVoterRegistrationForm = ({ blockId, campaignId }) => {
   const [emailValue, setEmailValue] = useState('');
@@ -27,27 +25,39 @@ const StartVoterRegistrationForm = ({ blockId, campaignId }) => {
     'https://register.rockthevote.com/registrants/new?partner=37187&',
     { email_address: emailValue, home_zip_code: zipcodeValue },
   );
- 
+  /**
+   * should we be passing the userID or referrerId ?
+   * */
+
+  // `https://register.rockthevote.com/registrants/new?partner=37187&source=user:${referrerUserId},source:web,source_details:onlinedrivereferral,referral=true`;
+
+  const isDisabled = !zipcodeValue || !emailValue;
+
   const redirectToRockTheVote = () => {
     window.location = url;
   };
 
   const handleChange = event => {
     if (event.target.name === 'email') {
-       () => {
+      isValidEmailSyntax(event.target.value)
+        ? () => {
             setEmailValue(event.target.value);
             setEmailLabel(event.target.name);
           }
+        : setEmailLabel('Please include a valid email address');
     }
 
     if (event.target.name === 'zipcode') {
-        () => {
+      isValidPostalCode(event.target.value)
+        ? () => {
             setZipcodeValue(event.target.value);
           }
+        : setZipCodeLabel('Please provide a valid zip code');
     }
   };
 
   const handleClick = () => {
+    if (!isDisabled) {
       trackAnalyticsEvent('click_voter_registration_action', {
         action: 'button_clicked',
         category: EVENT_CATEGORIES.campaignAction,
@@ -73,7 +83,6 @@ const StartVoterRegistrationForm = ({ blockId, campaignId }) => {
               <input
                 ref={zipCodeLength}
                 className="text-field"
-                pattern="[0-9]*"
                 type="text"
                 name="zipcode"
                 value={zipcodeValue}
