ba18b179c055b538667b93fde074aefb5b06d4b0
Aaron Schachter <aaronschachter@gmail.com>
2020-05-07 11:13:32 -0700

    Adds actionId prop to ShareLink

--- resources/assets/components/pages/VoterRegistrationDrivePage/Alpha/ShareLink/ShareLink.js
+++ resources/assets/components/pages/VoterRegistrationDrivePage/Alpha/ShareLink/ShareLink.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { shareLink } from '../config';
 import VotingReasons from './VotingReasons';
 import SocialDriveActionContainer from '../../../../actions/SocialDriveAction/SocialDriveActionContainer';
 
-const ShareLink = ({ referrerUserId }) => {
+const ShareLink = ({ actionId, referrerUserId }) => {
   return (
     <div className="grid-wide">
       <SocialDriveActionContainer
+        actionId={actionId}
         link={`https://vote.dosomething.org/member-drive?userId=${referrerUserId}&r=user:${referrerUserId},source:web,source_details:onlinedrivereferral,referral=true`}
-        fullWidth
-        actionId={shareLink.actionId}
         queryOptions={<VotingReasons />}
-        hidePageViews
       />
     </div>
   );
 };
 
 ShareLink.propTypes = {
+  actionId: PropTypes.number.isRequired,
   referrerUserId: PropTypes.string.isRequired,
 };
 
