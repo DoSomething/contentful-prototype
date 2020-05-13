bb7fba1a1f07b08ac1931e78c51975bf33eb7839
Aaron Schachter <aaronschachter@gmail.com>
2020-05-13 07:27:37 -0700

    Query GraphQL for quiz result

--- resources/assets/components/pages/QuizResultPage/QuizResultPage.js
+++ resources/assets/components/pages/QuizResultPage/QuizResultPage.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
+import { useQuery } from 'react-apollo';
 
+import ErrorPage from '../ErrorPage';
+import NotFoundPage from '../NotFoundPage';
+import Placeholder from '../../utilities/Placeholder';
 import SiteFooter from '../../utilities/SiteFooter/SiteFooter';
 import TextContent from '../../utilities/TextContent/TextContent';
+import { LinkBlockFragment } from '../../actions/LinkAction/LinkAction';
+import { ShareBlockFragment } from '../../actions/ShareAction/ShareAction';
 import SiteNavigationContainer from '../../SiteNavigation/SiteNavigationContainer';
 
+const QUIZ_RESULT_PAGE_QUERY = gql`
+  query QuizResultPageQuery($id: String!) {
+    block(id: $id) {
+      id
+      ... on LinkBlock {
+        ...LinkBlockFragment
+      }
+      ... on ShareBlock {
+        ...ShareBlockFragment
+      }
+    }
+  }
+
+  ${LinkBlockFragment}
+  ${ShareBlockFragment}
+`;
+
 const QuizResultPage = ({ id }) => {
+  const { loading, error, data } = useQuery(QUIZ_RESULT_PAGE_QUERY, {
+    variables: { id },
+  });
+
+  if (loading) {
+    return <Placeholder />;
+  }
+
+  if (error) {
+    return <ErrorPage error={error} />;
+  }
+
+  if (!data.block) {
+    return <NotFoundPage id={id} />;
+  }
+
+  const { title, content } = data.block;
+
   return (
     <>
       <SiteNavigationContainer />
@@ -16,18 +58,15 @@ const QuizResultPage = ({ id }) => {
             role="banner"
             className="base-12-grid py-3 md:py-6 bg-blurple-500"
           >
-            <div className="my-6">
-              <h2 className="my-3 uppercase color-white text-lg">Subtitle</h2>
-
+            <div className="my-6 grid-full">
               <h1 className="my-3 font-normal font-league-gothic color-white uppercase">
-                {id}
+                {title}
               </h1>
-
-              <TextContent className="text-white">Description</TextContent>
             </div>
           </header>
-
-          <TextContent className="base-12-grid py-3 md:py-6">Hello</TextContent>
+          <div className="bg-white base-12-grid py-3 md:py-6">
+            <TextContent className="grid-full">{content}</TextContent>
+          </div>
         </article>
       </main>
 
