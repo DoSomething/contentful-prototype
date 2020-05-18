83b90cac59add56244eaa8df81c58aac365530cb
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-05-18 11:39:32 -0400

    Fix the import ordering

--- resources/assets/components/LedeBanner/templates/HeroTemplate.js
+++ resources/assets/components/LedeBanner/templates/HeroTemplate.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
 
 import Modal from '../../utilities/Modal/Modal';
 import ContentfulEntry from '../../ContentfulEntry';
