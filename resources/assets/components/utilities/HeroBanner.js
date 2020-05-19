2148bbc15a92195dabb3540662349135dc9b3a9c
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-05-19 16:19:03 -0400

    Fix the grid column span for large screens

--- resources/assets/components/utilities/HeroBanner.js
+++ resources/assets/components/utilities/HeroBanner.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const HeroBanner = ({ title, subtitle }) => (
   <header
     role="banner"
-    className="mb-3 col-span-4 md:col-span-8 lg:col-start-2 lg:col-span-8 xxl:col-start-2 xxl:col-span-7"
+    className="mb-3 col-span-4 md:col-span-8 lg:col-start-2 lg:col-span-10 xxl:col-start-2 xxl:col-span-7"
   >
     <h1 className="uppercase text-xl text-blurple-700">{title}</h1>
     <h2 className="uppercase text-3xl md:text-4xl font-league-gothic font-normal">
