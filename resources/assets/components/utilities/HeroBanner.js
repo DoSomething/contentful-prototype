4af849292d4ddbd109638ccdfe8776aef40d639a
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-05-19 15:52:37 -0400

    Tailwind-ify grid col span

--- resources/assets/components/utilities/HeroBanner.js
+++ resources/assets/components/utilities/HeroBanner.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { css } from '@emotion/core';
-
-import { tailwind } from '../../helpers';
-
-const headerStyles = css`
-  grid-column: full-start / full-end;
-
-  @media (min-width: ${tailwind('screens.lg')}) {
-    grid-column: wide-start / wide-end;
-  }
-
-  @media (min-width: ${tailwind('screens.xxl')}) {
-    grid-column: wide-start / span 7;
-  }
-`;
 
 const HeroBanner = ({ title, subtitle }) => (
-  <header role="banner" className="mb-3" css={headerStyles}>
+  <header
+    role="banner"
+    className="mb-3 col-span-4 md:col-span-8 lg:col-start-2 lg:col-span-8 xxl:col-start-2 xxl:col-span-7"
+  >
     <h1 className="uppercase text-xl text-blurple-700">{title}</h1>
     <h2 className="uppercase text-3xl md:text-4xl font-league-gothic font-normal">
       {subtitle}
