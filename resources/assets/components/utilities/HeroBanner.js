1ba2754a963ceb96236d8335a69399360409cce2
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-05-19 16:56:37 -0400

    Oops, fix the testid on the hero banner title

--- resources/assets/components/utilities/HeroBanner.js
+++ resources/assets/components/utilities/HeroBanner.js
@@ -4,10 +4,14 @@ import PropTypes from 'prop-types';
 const HeroBanner = ({ title, subtitle }) => (
   <header
     role="banner"
-    data-testid="hero-banner-subtitle"
     className="mb-3 col-span-4 md:col-span-8 lg:col-start-2 lg:col-span-10 xxl:col-start-2 xxl:col-span-7"
   >
-    <h1 className="uppercase text-xl text-blurple-700">{title}</h1>
+    <h1
+      data-testid="hero-banner-title"
+      className="uppercase text-xl text-blurple-700"
+    >
+      {title}
+    </h1>
     <h2
       data-testid="hero-banner-subtitle"
       className="uppercase text-3xl md:text-4xl font-league-gothic font-normal"
