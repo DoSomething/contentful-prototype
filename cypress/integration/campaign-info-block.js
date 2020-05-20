be5778ac8d647659786448e2df4e1d7bf6e615a3
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-05-20 13:53:09 -0400

    Move campaign-info-block tests into it's own file & refactor
    We're testing functionality specific to the CampaignInfoBlock which has it's own independant suite.

--- cypress/integration/campaign-info-block.js
+++ cypress/integration/campaign-info-block.js
@@ -2,22 +2,154 @@
 import exampleCampaign from '../fixtures/contentful/exampleCampaign';
 
 describe('Campaign Info Block', () => {
+  const scholarshipAndReportbackAction = {
+    actionLabel: 'Sign a Petition',
+    timeCommitmentLabel: '30 minutes - 1 hour',
+    scholarshipEntry: true,
+    reportback: true,
+  };
+
+  const reportbackActionOne = {
+    actionLabel: 'Share Something',
+    timeCommitmentLabel: '1 hour',
+    scholarshipEntry: false,
+    reportback: true,
+  };
+
+  const reportbackActionTwo = {
+    actionLabel: 'Sign a Petition',
+    timeCommitmentLabel: '15 minutes - 1 hour',
+    scholarshipEntry: false,
+    reportback: true,
+  };
+
+  const plainAction = {
+    actionLabel: 'Call Tej',
+    timeCommitmentLabel: '5 minutes - 6 hours',
+    scholarshipEntry: false,
+    reportback: false,
+  };
+
   beforeEach(() => cy.configureMocks());
 
-  it('Opens the scholarship modal when "View Scholarship Details" is clicked', () => {
+  it('Displays the reportback & scholarship action data', () => {
+    cy.mockGraphqlOp('CampaignInfoQuery', {
+      campaign: (root, { campaignId }) => ({
+        id: campaignId,
+        actions: () => [
+          scholarshipAndReportbackAction,
+          reportbackActionOne,
+          reportbackActionTwo,
+          plainAction,
+        ],
+      }),
+    });
+
     cy.withState(exampleCampaign).visit('/us/campaigns/test-example-campaign');
 
-    cy.get('[data-test=campaign-info-block-container]').should(
+    cy.findByTestId('campaign-info-block-container').within(() => {
+      cy.contains('Sign a Petition');
+      cy.contains('30 minutes - 1 hour');
+    });
+  });
+
+  it('defaults to the first reportback action if there is no scholarship', () => {
+    cy.mockGraphqlOp('CampaignInfoQuery', {
+      campaign: (root, { campaignId }) => ({
+        id: campaignId,
+        actions: () => [reportbackActionOne, reportbackActionTwo, plainAction],
+      }),
+    });
+
+    cy.withState(exampleCampaign).visit('/us/campaigns/test-example-campaign');
+
+    cy.findByTestId('campaign-info-block-container').within(() => {
+      cy.contains('Share Something');
+      cy.contains('1 hour');
+    });
+  });
+
+  it('Displays a Scholarship Amount if there is one for the campaign', () => {
+    cy.mockGraphqlOp('CampaignInfoQuery', {
+      campaign: (root, { campaignId }) => ({
+        id: campaignId,
+        actions: () => [scholarshipAndReportbackAction],
+      }),
+    });
+
+    cy.withState(exampleCampaign).visit('/us/campaigns/test-example-campaign');
+
+    cy.findByTestId('campaign-info-block-container').within(() => {
+      cy.contains('$5,000');
+      cy.contains('Win A Scholarship');
+    });
+  });
+
+  describe('Displays the Volunteer Credit value', () => {
+    it('Displays Yes when the action earns volunteer credit', () => {
+      cy.mockGraphqlOp('CampaignInfoQuery', {
+        campaign: (root, { campaignId }) => ({
+          id: campaignId,
+          actions: () => [
+            {
+              ...scholarshipAndReportbackAction,
+              volunteerCredit: true,
+            },
+          ],
+        }),
+      });
+
+      cy.withFeatureFlags({ volunteer_credits: true })
+        .withState(exampleCampaign)
+        .visit('/us/campaigns/test-example-campaign');
+
+      cy.findByTestId('campaign-info-block-container').within(() => {
+        cy.contains('Volunteer Credit');
+
+        cy.findByTestId('volunteer-credit-value').contains('Yes');
+      });
+    });
+
+    it('Displays No when the action does not earn volunteer credit', () => {
+      cy.mockGraphqlOp('CampaignInfoQuery', {
+        campaign: (root, { campaignId }) => ({
+          id: campaignId,
+          actions: () => [
+            {
+              ...scholarshipAndReportbackAction,
+              volunteerCredit: false,
+            },
+          ],
+        }),
+      });
+
+      cy.withFeatureFlags({ volunteer_credits: true })
+        .withState(exampleCampaign)
+        .visit('/us/campaigns/test-example-campaign');
+
+      cy.findByTestId('campaign-info-block-container').within(() => {
+        cy.contains('Volunteer Credit');
+
+        cy.findByTestId('volunteer-credit-value').contains('No');
+      });
+    });
+  });
+
+  it('Opens the scholarship modal when "View Scholarship Details" is clicked, and closes it when the X button is clicked.', () => {
+    cy.withState(exampleCampaign).visit('/us/campaigns/test-example-campaign');
+
+    cy.findByTestId('campaign-info-block-container').within(() => {
+      cy.contains('button', 'View Scholarship Details').click();
+    });
+
+    cy.findByTestId('campaign-info-block-scholarship-details').should(
       'have.length',
       1,
     );
-    cy.contains('button', 'View Scholarship Details').click();
-    cy.get('[data-test=campaign-info-block-scholarship-details]').should(
-      'have.length',
-      1,
-    );
+
     cy.get('.modal-portal > .wrapper.modal-container').click('topRight');
-    cy.get('[data-test=campaign-info-block-scholarship-details]').should(
+
+    cy.findByTestId('campaign-info-block-scholarship-details').should(
       'have.length',
       0,
     );
