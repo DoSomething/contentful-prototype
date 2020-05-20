0d6c6221ddd8f1d6c31b76f65b7b1785d361c31f
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-05-20 11:59:41 -0400

    Tailwindify and Clean Up Lede Banner Styling  (#2162)
    * Replace cover img rule with Tailwind class
    
    * Extract 'HeroBanner' layout into utility component
    
    * Remove HeroBanner rules
    
    * Tailwind-ify grid col span
    
    * Replace .primary styles with Tailwind classes
    
    * Remove legacy .button styles
    
    Replaced with in house PrimaryButton styles in https://github.com/DoSomething/phoenix-next/pull/2127
    
    * Replace .hero-signup-button rules with Tailwind classes
    
    * Replace .secondary margin with Tailwind class
    
    * Replace .secondary grid row styles with Tailwind classes
    
    * Fix the grid column span for large screens
    
    * Remove seprecated wrapping .hero-landing-page div
    
    * Add data-testid and update broken Cypress test
    
    * Add data-testid's for Ghost Inspector selectors
    
    * Oops, fix the testid on the hero banner title
    
    * Rename HeroBanner to CampaignHeader per https://github.com/DoSomething/phoenix-next/pull/2162#discussion_r428085202
    
    * Add breathing room per https://github.com/DoSomething/phoenix-next/pull/2162#discussion_r428096345
    
    * Add sibling breathing room per https://github.com/DoSomething/phoenix-next/pull/2162#discussion_r428099160
    
    * Add sibling breathing room per https://github.com/DoSomething/phoenix-next/pull/2162#discussion_r428099455
    
    * Add sibling breathing room per https://github.com/DoSomething/phoenix-next/pull/2162#discussion_r428100627
    
    * Add sibling breathing room
    
    * Premature commit of work in a follow-up PR

--- resources/assets/components/pages/VoterRegistrationDrivePage/Beta/HeroSection.js
+++ resources/assets/components/pages/VoterRegistrationDrivePage/Beta/HeroSection.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import { votingReasons } from './config';
 import { query } from '../../../../helpers';
+import CampaignHeader from '../../../utilities/CampaignHeader';
 import CoverImage from '../../../utilities/CoverImage/CoverImage';
 import CampaignInfoBlock from '../../../blocks/CampaignInfoBlock/CampaignInfoBlock';
 
@@ -59,41 +60,44 @@ const HeroSection = ({ user, campaignInfo, modalToggle }) => {
   };
 
   return (
-    <div className="hero-landing-page">
+    <>
       <CoverImage
         attributes={{
           'data-test': 'beta-voter-registration-drive-page-cover-image',
         }}
         coverImage={coverImage}
       />
+
       <div className="clearfix bg-gray-100">
         <div className="base-12-grid bg-gray-100 cover-image py-3 md:py-6">
-          <header role="banner" className="hero-banner">
-            <h1 className="hero-banner__headline-title">{title}</h1>
-            <h2 className="hero-banner__headline-subtitle">
-              {firstName} wants you to register to vote!
-            </h2>
-          </header>
-          <div className="grid-wide-7/10 primary markdown">
+          <CampaignHeader
+            title={title}
+            subtitle={`${firstName} wants you to register to vote!`}
+          />
+
+          <div className="grid-wide-7/10 mb-6 markdown">
             <blockquote>
               <p data-test="beta-voter-registration-drive-page-quote-text">
                 Voting is one of the most impactful ways to make a difference on
                 the causes that matter to us{formatQuote()}. Take 2 minutes and
                 register to vote today!
               </p>
+
               <p data-test="beta-voter-registration-drive-page-quote-byline">
                 - {firstName}
               </p>
             </blockquote>
+
             <p data-test="beta-voter-registration-drive-page-blurb">
               150,000+ young people have registered to vote via DoSomething.
               After you register, share with your friends to enter to win a $
               {`${scholarshipAmount.toLocaleString()}`} scholarship!
             </p>
           </div>
+
           <div
             data-test="beta-voter-registration-drive-page-campaign-info-block"
-            className="grid-wide-3/10 secondary"
+            className="grid-wide-3/10 mb-6 xxl:row-start-1 xxl:row-span-3"
           >
             <CampaignInfoBlock
               campaignId={campaignId}
@@ -104,7 +108,7 @@ const HeroSection = ({ user, campaignInfo, modalToggle }) => {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
