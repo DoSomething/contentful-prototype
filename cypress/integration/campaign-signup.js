6c93ad9a1978fca3ec2737fbb68748eb0d8a3175
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-05-20 16:19:36 -0400

    Update landing-page-content to testid

--- cypress/integration/campaign-signup.js
+++ cypress/integration/campaign-signup.js
@@ -26,7 +26,7 @@ describe('Campaign Signup', () => {
     cy.contains('Example Campaign');
     cy.contains('This is an example campaign for automated testing.');
     cy.contains(exampleBlurb);
-    cy.get('[data-test=landing-page-content]').should('have.length', 1);
+    cy.findByTestId('landing-page-content').should('have.length', 1);
 
     // Mock the responses we'll be expecting once we hit "Join Now":
     cy.route(`${API}/signups?filter[northstar_id]=${user.id}`, emptyResponse);
@@ -51,7 +51,7 @@ describe('Campaign Signup', () => {
     cy.contains('Example Campaign');
     cy.contains('This is an example campaign for automated testing.');
     cy.contains(exampleBlurb);
-    cy.get('[data-test=landing-page-content]').should('have.length', 1);
+    cy.findByTestId('landing-page-content').should('have.length', 1);
 
     // Mock the response we'll be expecting once we hit "Join Now":
     cy.route('POST', `${API}/signups`, newSignup(campaignId, user));
@@ -122,7 +122,7 @@ describe('Campaign Signup', () => {
     cy.contains('Example Campaign');
     cy.contains('This is an example campaign for automated testing.');
     cy.contains(exampleBlurb);
-    cy.get('[data-test=landing-page-content]').should('not.exist');
+    cy.findByTestId('landing-page-content').should('not.exist');
 
     // We shouldn't see the "Join Now" button or affiramation modal,
     // since the user is already signed up for this campaign:
