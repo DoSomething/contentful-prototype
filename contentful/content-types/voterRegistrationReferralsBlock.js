b40d3193368558a765b3ee54f1737da832f67e18
Aaron Schachter <aaronschachter@gmail.com>
2020-05-28 15:35:03 -0700

    Adds migration for voterRegistrationReferralsBlock content type

---
+++ contentful/content-types/voterRegistrationReferralsBlock.js
@@ -0,0 +1,45 @@
+module.exports = function(migration) {
+  const voterRegistrationReferralsBlock = migration
+    .createContentType('voterRegistrationReferralsBlock')
+    .name('Voter Registration Referrals Block')
+    .description(
+      "Displays current user's number of completed voter registration referrals.",
+    )
+    .displayField('internalTitle');
+
+  voterRegistrationReferralsBlock
+    .createField('internalTitle')
+    .name('Internal Title')
+    .type('Symbol')
+    .localized(false)
+    .required(true)
+    .validations([
+      {
+        unique: true,
+      },
+    ])
+    .disabled(false)
+    .omitted(false);
+
+  voterRegistrationReferralsBlock
+    .createField('title')
+    .name('Title')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+  voterRegistrationReferralsBlock.changeFieldControl(
+    'internalTitle',
+    'builtin',
+    'singleLine',
+    {},
+  );
+  voterRegistrationReferralsBlock.changeFieldControl(
+    'title',
+    'builtin',
+    'singleLine',
+    {},
+  );
+};
