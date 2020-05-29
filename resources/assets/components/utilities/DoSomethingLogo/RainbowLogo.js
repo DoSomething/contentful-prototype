48310b42097d56f560095789198fac890bde5d5f
katiecrane <kcrane@dosomething.org>
2020-05-29 10:24:57 -0700

    Move rainbow logo and create component to display it

---
+++ resources/assets/components/utilities/DoSomethingLogo/RainbowLogo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import rainbowLogo from './rainbow_logo.svg';
+
+const RainbowLogo = () => (
+  <img src={rainbowLogo} alt="Rainbow logo" style={{ pointerEvents: 'none' }} />
+);
+
+export default RainbowLogo;
