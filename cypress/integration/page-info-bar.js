5d5c50fa7d001c104801ddec2b6b32ba5072c06f
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-06-01 11:07:35 -0400

    Organize context & test subject line

--- cypress/integration/page-info-bar.js
+++ cypress/integration/page-info-bar.js
@@ -1,42 +1,51 @@
 /// <reference types="Cypress" />
 import { userFactory } from '../fixtures/user';
 import exampleCampaign from '../fixtures/contentful/exampleCampaign';
+import { CAMPAIGN_HELP_CONTACT_EMAIL } from '../../resources/assets/constants';
 
-describe('Campaign Info Bar', () => {
+describe('Page Info Bar', () => {
   beforeEach(() => cy.configureMocks());
+  context('On a Campaign', () => {
+    it('Displays the affiliate sponsors or partners', () => {
+      const affiliateTitle =
+        exampleCampaign.campaign.affiliateSponsors[0].fields.title;
 
-  it('Displays the affiliate sponsors or partners', () => {
-    const affiliateTitle =
-      exampleCampaign.campaign.affiliateSponsors[0].fields.title;
+      cy.withState(exampleCampaign).visit(
+        '/us/campaigns/test-example-campaign',
+      );
 
-    cy.withState(exampleCampaign).visit('/us/campaigns/test-example-campaign');
-
-    cy.get('.info-bar').should('contain', affiliateTitle);
-  });
-
-  context('Unauthenticated users', () => {
-    it("Displays a mailto link to the campaign lead's email address", () => {
-      const campaignLeadEmail =
-        exampleCampaign.campaign.campaignLead.fields.email;
-
-      cy.anonVisitCampaign(exampleCampaign);
-
-      cy.get('.info-bar .info-bar__secondary a')
-        .should('have.attr', 'href')
-        .and('include', `mailto:${campaignLeadEmail}`);
+      cy.get('.info-bar').should('contain', affiliateTitle);
     });
-  });
 
-  context('Authenticated users', () => {
-    it('Displays a button triggering the Zendesk Form in a modal', () => {
-      const user = userFactory();
-      cy.authVisitCampaignWithoutSignup(user, exampleCampaign);
+    context('Unauthenticated users', () => {
+      it("Displays a mailto link to the campaign lead's email address with the campaign title as the subject", () => {
+        const campaignLeadEmail =
+          exampleCampaign.campaign.campaignLead.fields.email;
 
-      cy.get('.info-bar .info-bar__secondary button')
-        .contains('button', 'Contact Us')
-        .click();
+        const expectedSubject = encodeURIComponent(
+          `Question About ${exampleCampaign.campaign.title}`,
+        );
 
-      cy.get('.modal .zendesk-form').contains('h1', 'Contact Us');
+        cy.anonVisitCampaign(exampleCampaign);
+
+        cy.get('.info-bar .info-bar__secondary a')
+          .should('have.attr', 'href')
+          .and('include', `mailto:${campaignLeadEmail}`)
+          .and('include', `?subject=${expectedSubject}`);
+      });
+    });
+
+    context('Authenticated users', () => {
+      it('Displays a button triggering the Zendesk Form in a modal', () => {
+        const user = userFactory();
+        cy.authVisitCampaignWithoutSignup(user, exampleCampaign);
+
+        cy.get('.info-bar .info-bar__secondary button')
+          .contains('button', 'Contact Us')
+          .click();
+
+        cy.get('.modal .zendesk-form').contains('h1', 'Contact Us');
+      });
     });
   });
 });
