1be7ff84c798d9e3e936d0046d30f5b1e5c54c22
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-06-01 11:09:58 -0400

    Add example story page & hardcoded type for story page blocks

---
+++ cypress/fixtures/contentful/exampleStoryPage.js
@@ -0,0 +1,79 @@
+//
+// This is a "snapshot" of an example story page we've created on Contentful
+// for automated testing. To update the snapshot, change the campaign & copy
+// the output from `window.STATE` at the following URL into this file:
+//
+// https://dev.dosomething.org/us/stories/test-example-storypage
+
+export default {
+  page: {
+    id: '58OCYx7MyrNJodLSXYMw3J',
+    type: 'storyPage',
+    fields: {
+      internalTitle: '[Test] Example StoryPage',
+      title: 'Example StoryPage',
+      subTitle: null,
+      slug: 'stories/test-example-storypage',
+      metadata: null,
+      coverImage: {
+        description: '',
+        url: null,
+      },
+      blocks: [
+        {
+          id: '5eankfRazQF4JugI6vEufO',
+          type: 'sectionBlock',
+          fields: {
+            internalTitle: 'Test Example StoryPage Section Block',
+            backgroundColor: null,
+            hyperlinkColor: null,
+            textColor: null,
+            content: {
+              data: [],
+              content: [
+                {
+                  data: {
+                    target: {
+                      sys: {
+                        id: '4ti6d4kQ80JRrkAmvltkm',
+                        type: 'Link',
+                        linkType: 'Entry',
+                      },
+                    },
+                  },
+                  content: [],
+                  nodeType: 'embedded-entry-block',
+                },
+                {
+                  data: [],
+                  content: [
+                    {
+                      data: [],
+                      marks: [],
+                      value: '',
+                      nodeType: 'text',
+                    },
+                  ],
+                  nodeType: 'paragraph',
+                },
+                {
+                  data: [],
+                  content: [
+                    {
+                      data: [],
+                      marks: [],
+                      value: '',
+                      nodeType: 'text',
+                    },
+                  ],
+                  nodeType: 'paragraph',
+                },
+              ],
+              nodeType: 'document',
+            },
+          },
+        },
+      ],
+    },
+  },
+};
