d9bbac855f0ce0b7f870fe8b65b56a98c23e95c8
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-06-03 13:36:17 -0400

    Use Tailwind classes in favor of BaseFigure to style referrals
    See this thread https://github.com/DoSomething/phoenix-next/pull/2074#discussion_r417595077 the BaseFigure has given us heartache for this one, so taking this opportunity to clean it out so we can repurpose this component generally

--- resources/assets/components/blocks/VoterRegistrationReferralsBlock/VoterRegistrationReferralsListItem.js
+++ resources/assets/components/blocks/VoterRegistrationReferralsBlock/VoterRegistrationReferralsListItem.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { BaseFigure } from '../../utilities/Figure/Figure';
 import EmptyRegistrationImage from './empty-registration.svg';
 import CompletedRegistrationImage from './completed-registration.svg';
 
@@ -9,18 +8,19 @@ const VoterRegistrationReferralsListItem = props => {
   const { label } = props;
   const isEmpty = label === '???';
 
-  const media = (
-    <img
-      src={!isEmpty ? CompletedRegistrationImage : EmptyRegistrationImage}
-      alt={!isEmpty ? 'Completed voter registration icon' : 'Empty circle icon'}
-    />
-  );
-
   return (
-    <div data-test={`referral-list-item-${!isEmpty ? 'completed' : 'empty'}`}>
-      <BaseFigure media={media} size="medium">
-        <p className={isEmpty ? 'text-gray-500' : null}>{label}</p>
-      </BaseFigure>
+    <div
+      data-test={`referral-list-item-${!isEmpty ? 'completed' : 'empty'}`}
+      className="text-center w-20 xs:w-24 sm:w-32 md:w-40"
+    >
+      <img
+        className="mb-3"
+        src={!isEmpty ? CompletedRegistrationImage : EmptyRegistrationImage}
+        alt={
+          !isEmpty ? 'Completed voter registration icon' : 'Empty circle icon'
+        }
+      />
+      <p className={isEmpty ? 'text-gray-500' : null}>{label}</p>
     </div>
   );
 };
