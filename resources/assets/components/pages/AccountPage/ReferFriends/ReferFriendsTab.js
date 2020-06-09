587c144fc556cc1c3166502edbbc57d6351828b5
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-06-09 13:49:55 -0400

    Allow a wider grid for the account RAF signups gallery (#2206)

--- resources/assets/components/pages/AccountPage/ReferFriends/ReferFriendsTab.js
+++ resources/assets/components/pages/AccountPage/ReferFriends/ReferFriendsTab.js
@@ -5,20 +5,26 @@ import SectionHeader from '../../../utilities/SectionHeader/SectionHeader';
 import SocialDriveActionContainer from '../../../actions/SocialDriveAction/SocialDriveActionContainer';
 
 const ReferFriendsTab = () => (
-  <div className="col-span-4 md:col-span-8 lg:col-start-2 lg:col-span-7 xxl:col-start-2 xxl:col-span-6">
-    {/* @TODO Update all reward related copy once we have the updates from product. */}
-    <SectionHeader underlined title="Enter to Win A $10 Gift Card" />
+  <>
+    <div className="col-span-4 md:col-span-8 lg:col-start-2 lg:col-span-7 xxl:col-start-2 xxl:col-span-6">
+      {/* @TODO Update all reward related copy once we have the updates from product. */}
+      <SectionHeader underlined title="Enter to Win A $10 Gift Card" />
 
-    <SocialDriveActionContainer
-      shareCardTitle="Refer a friend"
-      shareCardDescription="When your friend signs up for their first DoSomething campaign, you’ll both enter to win a $10 gift card! Every 2 weeks, there will be 25 winners. The more friends you refer, the more chances you have to win. (Psst...there’s no limit on how many you can refer!)"
-      /* @TODO use refer-friends link generator helper once we establish a default campaign in https://bit.ly/3c9L7nT */
-      link="https://dosomething.org/us/campaigns/senior-homies"
-      fullWidth
-    />
+      <SocialDriveActionContainer
+        shareCardTitle="Refer a friend"
+        shareCardDescription="When your friend signs up for their first DoSomething campaign, you’ll both enter to win a $10 gift card! Every 2 weeks, there will be 25 winners. The more friends you refer, the more chances you have to win. (Psst...there’s no limit on how many you can refer!)"
+        /* @TODO use refer-friends link generator helper once we establish a default campaign in https://bit.ly/3c9L7nT */
+        link="https://dosomething.org/us/campaigns/senior-homies"
+        fullWidth
+      />
 
-    <SignupReferralsGallery />
-  </div>
+      <SignupReferralsGallery />
+    </div>
+
+    <div className="col-span-4 md:col-span-8 lg:col-start-2 lg:col-span-11 xxl:col-start-2 xxl:col-span-10">
+      <SignupReferralsGallery />
+    </div>
+  </>
 );
 
 export default ReferFriendsTab;
