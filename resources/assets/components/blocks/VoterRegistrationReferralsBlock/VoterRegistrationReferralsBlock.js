9efda497b1573690a4f02a9fce57ea9f0c3f1983
Aaron Schachter <aaronschachter@gmail.com>
2020-06-10 22:29:24 -0700

    Use Query component instead of useQuery hook

--- resources/assets/components/blocks/VoterRegistrationReferralsBlock/VoterRegistrationReferralsBlock.js
+++ resources/assets/components/blocks/VoterRegistrationReferralsBlock/VoterRegistrationReferralsBlock.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
-import { useQuery } from '@apollo/react-hooks';
 
+import Query from '../../Query';
 import {
   CAMPAIGN_SIGNUP_QUERY,
   getCampaignSignupQueryVariables,
 } from '../../../helpers/campaign';
 import GroupTemplate from './templates/Group';
 import IndividualTemplate from './templates/Individual';
-import Placeholder from '../../utilities/Placeholder';
-import ErrorBlock from '../ErrorBlock/ErrorBlock';
 
 export const VoterRegistrationReferralsBlockFragment = gql`
   fragment VoterRegistrationReferralsBlockFragment on VoterRegistrationReferralsBlock {
@@ -18,31 +16,22 @@ export const VoterRegistrationReferralsBlockFragment = gql`
   }
 `;
 
-const VoterRegistrationReferralsBlock = ({ title }) => {
-  const { loading, error, data } = useQuery(CAMPAIGN_SIGNUP_QUERY, {
-    variables: getCampaignSignupQueryVariables(),
-  });
-
-  if (loading) {
-    return <Placeholder />;
-  }
-
-  if (error) {
-    return <ErrorBlock error={error} />;
-  }
-
-  const signup = data.signups[0];
-
-  return (
-    <div className="grid-wide clearfix wrapper pb-6">
-      {signup.group ? (
-        <GroupTemplate group={signup.group} />
-      ) : (
-        <IndividualTemplate title={title} />
-      )}
-    </div>
-  );
-};
+const VoterRegistrationReferralsBlock = ({ title }) => (
+  <div className="grid-wide clearfix wrapper pb-6">
+    <Query
+      query={CAMPAIGN_SIGNUP_QUERY}
+      variables={getCampaignSignupQueryVariables()}
+    >
+      {data =>
+        data.signups[0] && data.signups[0].group ? (
+          <GroupTemplate group={data.signups[0].group} />
+        ) : (
+          <IndividualTemplate title={title} />
+        )
+      }
+    </Query>
+  </div>
+);
 
 VoterRegistrationReferralsBlock.propTypes = {
   title: PropTypes.string,
