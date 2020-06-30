a4b64dbc2cbce85127a40d4ab5f12af77353475e
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-06-30 11:55:04 -0400

    Refactor tests a tad

--- resources/assets/helpers/refer-friends.test.js
+++ resources/assets/helpers/refer-friends.test.js
@@ -6,9 +6,12 @@ describe('getReferFriendsLink', () => {
   const referralCampaignId = '456';
   const defaultReferralCampaignId = '789';
 
+  const referralUrl = `${PHOENIX_URL}/us/join?user_id=${userId}`;
+  const alphaPageUrl = `${PHOENIX_URL}/us/refer-friends`;
+
   beforeEach(() => {
     window.jsdom.reconfigure({
-      url: `http://phoenix.test/join?campaign_id=${referralCampaignId}`,
+      url: `${alphaPageUrl}?campaign_id=${referralCampaignId}`,
     });
 
     global.AUTH = { id: userId };
@@ -18,20 +21,20 @@ describe('getReferFriendsLink', () => {
   /** @test */
   it('returns referral link when user is autenticated & campaign ID query param is present', () => {
     expect(getReferFriendsLink()).toEqual(
-      `${PHOENIX_URL}/us/join?user_id=${userId}&campaign_id=${referralCampaignId}`,
+      `${referralUrl}&campaign_id=${referralCampaignId}`,
     );
   });
 
   /** @test */
   it('returns referral link when user is autenticated & default campaign ID is configured', () => {
-    window.jsdom.reconfigure({ url: 'http://phoenix.test/join' });
+    window.jsdom.reconfigure({ url: alphaPageUrl });
 
     global.ENV = {
       SITE: { default_referral_campaign_id: defaultReferralCampaignId },
     };
 
     expect(getReferFriendsLink()).toEqual(
-      `${PHOENIX_URL}/us/join?user_id=${userId}&campaign_id=${defaultReferralCampaignId}`,
+      `${referralUrl}&campaign_id=${defaultReferralCampaignId}`,
     );
   });
 
@@ -44,9 +47,7 @@ describe('getReferFriendsLink', () => {
 
   /** @test */
   it('returns undefined if there is no campaign ID query param or default config', () => {
-    window.jsdom.reconfigure({
-      url: `http://phoenix.test/join`,
-    });
+    window.jsdom.reconfigure({ url: alphaPageUrl });
 
     expect(getReferFriendsLink()).toEqual(undefined);
   });
