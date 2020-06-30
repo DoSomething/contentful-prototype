6561fb9c9bb5fafe11f2ba2f73c1023b1be1bfc3
David Furnes <david@dfurnes.com>
2020-06-30 10:26:08 -0400

    Revert "Update to rainbow logo for pride 2020! (#2184)"
    This reverts commit fcf616689cca39886a74f7ec2f3d6774602dd121.

--- resources/assets/components/utilities/DoSomethingLogo/RainbowLogo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import rainbowLogo from './rainbow_logo.svg';
-
-const RainbowLogo = () => (
-  <img src={rainbowLogo} alt="Rainbow logo" style={{ pointerEvents: 'none' }} />
-);
-
-export default RainbowLogo;
