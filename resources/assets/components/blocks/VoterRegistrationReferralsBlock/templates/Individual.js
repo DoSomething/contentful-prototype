9530030c4b6434ba6104b2db3dd9c55ddba95882
Aaron Schachter <aaronschachter@gmail.com>
2020-07-13 10:06:49 -0700

    Modify individual VoterRegistrationReferralsBlock to retrieve all referrals (#2267)
    * Modify individual referrals query to retrieve all posts
    
    * Adds check for multiple posts for a user
    
    * Adds count param
    
    * Add incomplete post after a complete post
    
    * Cleanup per https://github.com/DoSomething/phoenix-next/pull/2267/files#r453699235
    
    * Modify filter to retrieve complete and in progress

--- resources/assets/components/blocks/VoterRegistrationReferralsBlock/templates/Individual.js
+++ resources/assets/components/blocks/VoterRegistrationReferralsBlock/templates/Individual.js
@@ -14,16 +14,52 @@ const INDIVIDUAL_VOTER_REGISTRATION_REFERRALS_QUERY = gql`
   query IndividualVoterRegistrationReferralsQuery($referrerUserId: String!) {
     posts(
       referrerUserId: $referrerUserId
+      status: [REGISTER_FORM, REGISTER_OVR, STEP_1, STEP_2, STEP_3, STEP_4]
       type: "voter-reg"
-      status: [REGISTER_FORM, REGISTER_OVR]
+      count: 50
     ) {
       id
+      status
       user {
+        id
         displayName
       }
     }
   }
 `;
+
+/**
+ * @param {Array} voterRegPosts
+ * @return {Object}
+ */
+const parseVoterRegistrationReferrals = voterRegPosts => {
+  const result = { complete: {}, incomplete: {} };
+
+  voterRegPosts.forEach(({ status, user }) => {
+    const userId = user.id;
+
+    // If status is incomplete:
+    if (!['REGISTER_FORM', 'REGISTER_OVR'].includes(status)) {
+      // And user already has a completed registration, skip this registration.
+      if (result.complete[userId]) {
+        return;
+      }
+
+      result.incomplete[userId] = user;
+
+      return;
+    }
+
+    // Remove user from incomplete set, if exists.
+    if (result.incomplete[userId]) {
+      delete result.incomplete[userId];
+    }
+
+    result.complete[userId] = user;
+  });
+
+  return result;
+};
 
 const IndividualTemplate = ({ title }) => (
   <>
@@ -33,15 +69,16 @@ const IndividualTemplate = ({ title }) => (
       variables={{ referrerUserId: getUserId() }}
     >
       {data => {
-        const numberOfReferrals = data.posts.length;
+        const parsed = parseVoterRegistrationReferrals(data.posts);
+        const completed = Object.values(parsed.complete);
 
         return (
           <>
-            {numberOfReferrals ? (
+            {completed.length ? (
               <div className="pb-3 md:pb-6">
                 You have registered{' '}
                 <strong>
-                  {numberOfReferrals} {pluralize('person', numberOfReferrals)}
+                  {completed.length} {pluralize('person', completed.length)}
                 </strong>{' '}
                 so far.
               </div>
@@ -53,9 +90,7 @@ const IndividualTemplate = ({ title }) => (
             )}
 
             <ReferralsGallery
-              referralLabels={data.posts.map(
-                referral => referral.user.displayName,
-              )}
+              referralLabels={completed.map(user => user.displayName)}
               referralIcon={CompletedRegistrationImage}
               placeholderIcon={EmptyRegistrationImage}
             />
