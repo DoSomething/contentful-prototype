17ad35d7e7c0037d8d34093f419dfa34ae8299d6
Aaron Schachter <aaronschachter@gmail.com>
2020-07-14 20:54:33 -0700

    Adds test for completed but empty started

--- resources/assets/components/blocks/VoterRegistrationReferralsBlock/VoterRegistrationReferrals.test.js
+++ resources/assets/components/blocks/VoterRegistrationReferralsBlock/VoterRegistrationReferrals.test.js
@@ -2,9 +2,10 @@ import faker from 'faker';
 import React from 'react';
 import { fireEvent, render, screen, within } from '@testing-library/react';
 
+import { userFactory } from '../../../../../cypress/fixtures/user';
 import VoterRegistrationReferrals from './VoterRegistrationReferrals';
 
-const renderVoterRegistrationReferrals = (completed, started) =>
+const renderVoterRegistrationReferrals = ({ completed, started }) =>
   render(
     <VoterRegistrationReferrals completed={completed} started={started} />,
   );
@@ -12,10 +13,29 @@ const renderVoterRegistrationReferrals = (completed, started) =>
 describe('VoterRegistrationReferrals component', () => {
   /** @test */
   it('Displays help text if completed or started props are empty', async () => {
-    renderVoterRegistrationReferrals([], []);
+    renderVoterRegistrationReferrals({ completed: [], started: [] });
 
     expect(screen.getByTestId('referrals-count-description').textContent).toBe(
       'You haven’t helped anyone register to vote yet. Scroll down to get started!',
     );
+  });
+
+  /** @test */
+  it('Does not display "X out of Y" text if completed referrals exist but none started', async () => {
+    const completed = [userFactory()];
+
+    renderVoterRegistrationReferrals({
+      completed,
+      started: [],
+    });
+
+    expect(screen.getByTestId('referrals-count-description').textContent).toBe(
+      'You have registered 1 person so far.',
+    );
+
+    const completedReferrals = screen.getAllByTestId(
+      'voter-registration-referral-completed',
+    );
+    expect(completedReferrals).toHaveLength(1);
   });
 });
