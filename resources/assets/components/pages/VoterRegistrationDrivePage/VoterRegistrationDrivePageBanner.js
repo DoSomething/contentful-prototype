03e07559af2b67e81975fe659b753c5e66f63b9d
Lindsay Maher <lmaher@Lindsays-MBP.fios-router.home>
2020-07-23 16:43:07 -0400

    adds second query, moves to own component, adds placeholder text

--- resources/assets/components/pages/VoterRegistrationDrivePage/VoterRegistrationDrivePageBanner.js
+++ resources/assets/components/pages/VoterRegistrationDrivePage/VoterRegistrationDrivePageBanner.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import { votingReasons } from './config';
 import { query } from '../../../helpers';
+import GroupTypeLink from './GroupTypeLink';
 import ReferralsInfo from './ReferralsInfo';
 import CampaignHeader from '../../utilities/CampaignHeader';
 import CoverImage from '../../utilities/CoverImage/CoverImage';
@@ -98,16 +99,7 @@ const VoterRegistrationDrivePageBanner = ({
               After you register, share with your friends to enter to win a $
               {`${scholarshipAmount.toLocaleString()}`} scholarship!
             </p>
-            <div className="mt-3">
-              {group ? (
-                <a
-                  href="google.com"
-                  data-test="voter-registration-drive-page-group-campaign-link"
-                >
-                  What&lsquo;s {group.groupType.name}?
-                </a>
-              ) : null}
-            </div>
+            {group ? <GroupTypeLink group={group} /> : null}
           </div>
 
           <div className="grid-wide-3/10 mb-6 xxl:row-start-1 xxl:row-span-3">
@@ -133,6 +125,7 @@ VoterRegistrationDrivePageBanner.propTypes = {
   group: PropTypes.shape({
     goal: PropTypes.number,
     groupType: PropTypes.shape({
+      id: PropTypes.number,
       name: PropTypes.string,
     }),
     id: PropTypes.number,
