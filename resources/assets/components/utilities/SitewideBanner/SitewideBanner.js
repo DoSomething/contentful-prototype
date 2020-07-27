dde8297a5aafaf08851d5bca94adf523a5bebfaa
dinnall <michaeldinnall@gmail.com>
2020-07-27 16:35:37 -0400

    got user id and reg status

--- resources/assets/components/utilities/SitewideBanner/SitewideBanner.js
+++ resources/assets/components/utilities/SitewideBanner/SitewideBanner.js
@@ -4,7 +4,6 @@ import { createPortal } from 'react-dom';
 import { useQuery } from '@apollo/react-hooks';
 import React, { useRef, useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
-
 import { getUserId } from '../../../helpers/auth';
 import excludedPaths from './config';
 import { getUserId } from '../../../helpers/auth';
