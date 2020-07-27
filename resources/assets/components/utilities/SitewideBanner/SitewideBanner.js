084f308a4f4c06dd18ccbe80f3d23181d73e73cc
dinnall <michaeldinnall@gmail.com>
2020-07-27 16:35:37 -0400

    got user id and reg status

--- resources/assets/components/utilities/SitewideBanner/SitewideBanner.js
+++ resources/assets/components/utilities/SitewideBanner/SitewideBanner.js
@@ -1,8 +1,32 @@
 import { createPortal } from 'react-dom';
 import React, { useRef, useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import Placeholder from '../Placeholder';
+import Spinner from '../../artifacts/Spinner/Spinner';
 
+import gql from 'graphql-tag';
+
+import { getUserId } from '../../../helpers/auth';
 import excludedPaths from './config';
 import SitewideBannerContent from './SitewideBannerContent';
+
+const VOTER_REGISTRATION_STATUS = gql`
+  query VoterRegBadgeQuery($userId: String!) {
+    user(id: $userId) {
+      id
+      voterRegistrationStatus
+    }
+  }
+`;
+
+const shouldDisplayRegistrationBanner = [
+  'UNCERTAIN',
+  'CONFIRMED',
+  'UNREGISTERED',
+];
+// If the user has a voterRegistrationStatus of uncertain, unregistered,
+// or confirmed, the banner should appear as it currently does, no changes.
+// For any other status, the banner should not show up at all.
 
 const isExcludedPath = pathname => {
   return excludedPaths.find(excludedPath => {
@@ -20,6 +44,17 @@ const isExcludedPath = pathname => {
 };
 
 const SitewideBanner = props => {
+  const options = { variables: { userId: getUserId() } };
+  const { data, loading, error } = useQuery(VOTER_REGISTRATION_STATUS, options);
+
+  if (loading) {
+    return <Placeholder />;
+  }
+
+  // shouldDisplayRegistrationBanner
+  // shouldDisplayRegistrationBanner.includes(data ? data.user['voterRegistrationStatus'] : {})
+  console.log('Banner:', data ? data.user['voterRegistrationStatus'] : {});
+
   const usePortal = id => {
     const rootElem = useRef(document.createElement('div'));
 
