25d41efa1b4c6eb6380d8ec01e5ef44e4156cb78
dinnall <michaeldinnall@gmail.com>
2020-08-03 17:41:56 -0400

    if statement test

--- cypress/integration/sitewide-banner.js
+++ cypress/integration/sitewide-banner.js
@@ -154,11 +154,15 @@ describe('Site Wide Banner', () => {
 
     cy.authVisitCampaignWithoutSignup(user, exampleCampaign);
 
-    cy.findByTestId('sitewide-banner-button').should('have.length', 1);
-    cy.findByTestId('sitewide-banner-button').should(
-      'have.attr',
-      'href',
-      `https://vote.dosomething.org/?r=user:${user.id},source:web,source_details:hellobar`,
-    );
+    if ('#banner-portal > .wrapper > [data-test=site-wide-banner]') {
+      cy.authVisitCampaignWithoutSignup(user, exampleCampaign);
+
+      cy.findByTestId('sitewide-banner-button').should('have.length', 1);
+      cy.findByTestId('sitewide-banner-button').should(
+        'have.attr',
+        'href',
+        `https://vote.dosomething.org/?r=user:${user.id},source:web,source_details:hellobar`,
+      );
+    }
   });
 });
