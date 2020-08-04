67323896c23daf1977e58b71902052ebb1b9366c
vmack <vanessacmack@gmail.com>
2020-08-04 16:35:17 -0400

    created gallery block signup component to add gql to apply now button

--- resources/assets/components/utilities/ScholarshipCard/ScholarshipCard.js
+++ resources/assets/components/utilities/ScholarshipCard/ScholarshipCard.js
@@ -58,6 +58,7 @@ const ScholarshipCard = ({ campaign }) => {
   ]);
 
   const handleScholarshipCardShareClick = () => {
+    GalleryBlockSignup();
     trackAnalyticsEvent('clicked_scholarship_gallery_block_apply_now', {
       action: 'button_clicked',
       category: EVENT_CATEGORIES.siteAction,
