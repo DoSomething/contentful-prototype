28511c932123c5a38348f5156745512d2bcb5b52
dinnall <michaeldinnall@gmail.com>
2020-08-05 12:26:50 -0400

    Update App.js

--- resources/assets/components/App.js
+++ resources/assets/components/App.js
@@ -56,10 +56,26 @@ const App = ({ store, history }) => {
         <ApolloProvider client={graphqlClient(env('GRAPHQL_URL'))}>
           {featureFlag('sitewide_nps_survey') &&
           window.location.pathname !== '/us' ? (
-            <TrafficDistribution
-              percentage={5}
-              feature="nps_survey"
-            ></TrafficDistribution>
+            <TrafficDistribution percentage={5} feature="nps_survey">
+              <DismissableElement
+                name="nps_survey"
+                render={(handleClose, handleComplete) => (
+                  <DelayedElement delay={30}>
+                    <Modal onClose={handleClose} trackingId="SURVEY_MODAL">
+                      <TypeFormEmbed
+                        displayType="modal"
+                        typeformUrl="https://dosomething.typeform.com/to/Phi9pA"
+                        queryParameters={{
+                          northstar_id: get(window.AUTH, 'id', null),
+                          url: window.location.pathname,
+                        }}
+                        onSubmit={handleComplete}
+                      />
+                    </Modal>
+                  </DelayedElement>
+                )}
+              />
+            </TrafficDistribution>
           ) : null}
 
           <Router history={history}>
