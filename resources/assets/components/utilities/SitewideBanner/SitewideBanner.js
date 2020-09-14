f393664950e0b0fb6a56e53da0daae6dd4617b72
dinnall <michaeldinnall@gmail.com>
2020-09-14 12:00:25 -0400

    New banner textr updates

--- resources/assets/components/utilities/SitewideBanner/SitewideBanner.js
+++ resources/assets/components/utilities/SitewideBanner/SitewideBanner.js
@@ -132,9 +132,9 @@ const SitewideBanner = props => {
   ) {
     return createPortal(
       <SitewideBannerContent
-        cta="Refer Now"
+        cta="Start Now"
         link="/us/account/refer-friends"
-        description="Refer a friend to DoSomething. (You could win a $10 gift card!)"
+        description="Want to build our youth-led movement? Refer a friend!"
         handleClose={props.handleClose}
         handleComplete={props.handleComplete}
       />,
