31ab273d547264ae0f5866bb4b86a05c92a44ab7
Lindsay Maher <lmaher@Lindsays-MBP.home>
2020-09-18 16:56:32 -0400

    more structural updates

--- resources/assets/components/blocks/GotvBlock/GotvBlock.js
+++ resources/assets/components/blocks/GotvBlock/GotvBlock.js
@@ -1,32 +1,44 @@
 import React from 'react';
 
-const GotvBlock = () => (
-  <>
-    <div className="md:bg-blurple-500 grid-wide md:p-6">
-      <div className="bg-white border border-solid border-gray-300 p-3 rounded md:rounded-none">
-        <h1 className="text-base uppercase">Your Friends</h1>
+const GotvBlock = () => {
+  // const friendCount = 3;
 
-        <form>
-          <input
-            className="border border-dashed rounded border-gray-600 p-3"
-            type="text"
-            placeholder="Friend's First Name"
-          />
-        </form>
+  return (
+    <>
+      <div className="md:bg-blurple-500 grid-wide md:p-6">
+        <div className="bg-white border border-solid border-gray-300 p-3 rounded md:rounded-none">
+          <h2 className="text-base uppercase">Your Friends</h2>
+          <p className="text-sm">
+            (Don’t worry, we don’t save any info about your friends)
+          </p>
 
-        <h1 className="text-base uppercase">Voter Resources</h1>
-        <div className="w-full">
-          <div className="md:w-1/2 bg-blurple-300 p-6">
-            <div className="bg-white bg-opacity-25 border border-solid border-gray-300">
-              “Hey! Election Day (Tuesday, Nov. 3) is coming up soon, so I
-              wanted to reach out and ask if you’re planning to vote in-person
-              or by mail?”
+          <form>
+            <input
+              className="border border-dashed rounded border-gray-600 p-3"
+              type="text"
+              placeholder="Friend's First Name"
+            />
+          </form>
+
+          <h2 className="text-base uppercase">Voter Resources</h2>
+          <p className="text-sm">
+            Resource links & conversation starters to get the dialog flowing!
+          </p>
+
+          <div className="w-full">
+            <div className="md:w-1/2 bg-blurple-500 p-6">
+              <div className="bg-white bg-opacity-25 border border-solid border-gray-300 p-3">
+                <h3 className="italic text-base">Conversation Starters</h3>
+                “Hey! Election Day (Tuesday, Nov. 3) is coming up soon, so I
+                wanted to reach out and ask if you’re planning to vote in-person
+                or by mail?”
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default GotvBlock;
