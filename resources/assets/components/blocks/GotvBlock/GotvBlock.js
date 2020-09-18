6a53d6f5235898762b68a67ef743f42359d67863
Lindsay Maher <lmaher@Lindsays-MBP.home>
2020-09-18 13:29:28 -0400

    base css styles

--- resources/assets/components/blocks/GotvBlock/GotvBlock.js
+++ resources/assets/components/blocks/GotvBlock/GotvBlock.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const GotvBlock = () => <div>Hello!</div>;
+const GotvBlock = () => (
+  <div className="base-12-grid clear-both py-3 md:py-6">Hello!</div>
+);
 
 export default GotvBlock;
