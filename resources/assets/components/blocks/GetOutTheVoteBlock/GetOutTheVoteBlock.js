ca548ad63036f3c8f5429e68403764f5697068d7
Lindsay Maher <lmaher@Lindsays-MBP.home>
2020-09-22 13:31:02 -0400

    removes unnecessary fragments

--- resources/assets/components/blocks/GetOutTheVoteBlock/GetOutTheVoteBlock.js
+++ resources/assets/components/blocks/GetOutTheVoteBlock/GetOutTheVoteBlock.js
@@ -3,44 +3,40 @@ import React from 'react';
 import Embed from '../../utilities/Embed/Embed';
 import WhoAreYourPeopleForm from './WhoAreYourPeopleForm';
 
-const GetOutTheVoteBlock = () => {
-  return (
-    <>
-      <div className="md:bg-blurple-500 grid-wide md:p-6">
-        <div className="bg-white border border-solid border-gray-300 p-3 rounded md:rounded-none">
-          <h2 className="text-base uppercase">Your Friends</h2>
+const GetOutTheVoteBlock = () => (
+  <div className="md:bg-blurple-500 grid-wide md:p-6">
+    <div className="bg-white border border-solid border-gray-300 p-3 rounded md:rounded-none">
+      <h2 className="text-base uppercase">Your Friends</h2>
 
-          <p className="text-sm">
-            (Don’t worry, we don’t save any info about your friends)
-          </p>
+      <p className="text-sm">
+        (Don’t worry, we don’t save any info about your friends)
+      </p>
 
-          <WhoAreYourPeopleForm />
+      <WhoAreYourPeopleForm />
 
-          <h2 className="text-base uppercase">Voter Resources</h2>
+      <h2 className="text-base uppercase">Voter Resources</h2>
 
-          <p className="text-sm">
-            Resource links & conversation starters to get the dialog flowing!
-          </p>
+      <p className="text-sm">
+        Resource links & conversation starters to get the dialog flowing!
+      </p>
 
-          <div className="flex">
-            <div className="md:w-1/2 bg-blurple-500 p-6">
-              <div className="bg-white bg-opacity-25 border border-solid border-gray-300 p-3">
-                <h3 className="italic text-base">Conversation Starters</h3>
-                “Hey! Election Day (Tuesday, Nov. 3) is coming up soon, so I
-                wanted to reach out and ask if you’re planning to vote in-person
-                or by mail?”
-              </div>
-            </div>
-
-            <div className="md:w-1/2">
-              <Embed url="https://www.dosomething.org/us/about/easy-scholarships" />
-              <Embed url="https://www.dosomething.org/us/about/easy-scholarships" />
-            </div>
+      <div className="flex">
+        <div className="md:w-1/2 bg-blurple-500 p-6">
+          <div className="bg-white bg-opacity-25 border border-solid border-gray-300 p-3">
+            <h3 className="italic text-base">Conversation Starters</h3>
+            “Hey! Election Day (Tuesday, Nov. 3) is coming up soon, so I wanted
+            to reach out and ask if you’re planning to vote in-person or by
+            mail?”
           </div>
         </div>
+
+        <div className="md:w-1/2">
+          <Embed url="https://www.dosomething.org/us/about/easy-scholarships" />
+          <Embed url="https://www.dosomething.org/us/about/easy-scholarships" />
+        </div>
       </div>
-    </>
-  );
-};
+    </div>
+  </div>
+);
 
 export default GetOutTheVoteBlock;
--- resources/assets/components/blocks/GetOutTheVoteBlock/WhoAreYourPeopleForm.js
+++ resources/assets/components/blocks/GetOutTheVoteBlock/WhoAreYourPeopleForm.js
@@ -1,71 +1,67 @@
 import React from 'react';
 
-const WhoAreYourPeopleForm = () => {
-  return (
-    <>
-      <form>
-        <div className="flex">
-          <div className="w-1/3">
-            <input
-              className="border border-dashed rounded border-gray-600 p-3"
-              type="text"
-              placeholder="Friend's First Name"
-            />
-          </div>
+const WhoAreYourPeopleForm = () => (
+  <form>
+    <div className="flex">
+      <div className="w-1/3">
+        <input
+          className="border border-dashed rounded border-gray-600 p-3"
+          type="text"
+          placeholder="Friend's First Name"
+        />
+      </div>
 
-          <div className="w-2/3 flex">
-            <p className="w-1/3">Voting in-person</p>
+      <div className="w-2/3 flex">
+        <p className="w-1/3">Voting in-person</p>
 
-            <label className="w-1/3" htmlFor="voting-location">
-              <input id="voting-location" type="checkbox" />
-            </label>
+        <label className="w-1/3" htmlFor="voting-location">
+          <input id="voting-location" type="checkbox" />
+        </label>
 
-            <p className="w-1/3">Voting by mail</p>
-          </div>
-        </div>
+        <p className="w-1/3">Voting by mail</p>
+      </div>
+    </div>
 
-        <div className="flex">
-          <div className="w-1/3">
-            <input
-              className="border border-dashed rounded border-gray-600 p-3"
-              type="text"
-              placeholder="Friend's First Name"
-            />
-          </div>
+    <div className="flex">
+      <div className="w-1/3">
+        <input
+          className="border border-dashed rounded border-gray-600 p-3"
+          type="text"
+          placeholder="Friend's First Name"
+        />
+      </div>
 
-          <div className="w-2/3 flex">
-            <p className="w-1/3">Voting in-person</p>
+      <div className="w-2/3 flex">
+        <p className="w-1/3">Voting in-person</p>
 
-            <label className="w-1/3" htmlFor="voting-location">
-              <input id="voting-location" type="checkbox" />
-            </label>
+        <label className="w-1/3" htmlFor="voting-location">
+          <input id="voting-location" type="checkbox" />
+        </label>
 
-            <p className="w-1/3">Voting by mail</p>
-          </div>
-        </div>
+        <p className="w-1/3">Voting by mail</p>
+      </div>
+    </div>
 
-        <div className="flex">
-          <div className="w-1/3">
-            <input
-              className="border border-dashed rounded border-gray-600 p-3"
-              type="text"
-              placeholder="Friend's First Name"
-            />
-          </div>
+    <div className="flex">
+      <div className="w-1/3">
+        <input
+          className="border border-dashed rounded border-gray-600 p-3"
+          type="text"
+          placeholder="Friend's First Name"
+        />
+      </div>
 
-          <div className="w-2/3 flex">
-            <p className="w-1/3">Voting in-person</p>
+      <div className="w-2/3 flex">
+        <p className="w-1/3">Voting in-person</p>
 
-            <label className="w-1/3" htmlFor="voting-location">
-              <input id="voting-location" type="checkbox" />
-            </label>
+        <label className="w-1/3" htmlFor="voting-location">
+          <input id="voting-location" type="checkbox" />
+        </label>
 
-            <p className="w-1/3">Voting by mail</p>
-          </div>
-        </div>
-      </form>
-    </>
-  );
-};
+        <p className="w-1/3">Voting by mail</p>
+      </div>
+    </div>
+  </form>
+);
 
 export default WhoAreYourPeopleForm;
