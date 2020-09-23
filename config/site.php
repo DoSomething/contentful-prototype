f77be80cc6be27c8711bc039534bc3e71c527a88
Aaron Schachter <aaronschachter@gmail.com>
2020-09-22 18:34:00 -0700

    Adds  config vars for Civic Engine embed levels

--- config/site.php
+++ config/site.php
@@ -16,9 +16,15 @@ return [
 
     // These Group Type IDs have a 'chapter' group label instead of the default in Group Finders.
     'chapter_group_type_ids' => explode(',', env('DS_CHAPTER_GROUP_TYPE_IDS')),
+    /**
+     * The CivicEngine vars are used forVoting Widget configuration.
+     * @see https://developers.civicengine.com/docs/widget
+     */
+    'civic_engine_client_name' => env('CIVIC_ENGINE_CLIENT_NAME'),
+    'civic_engine_embed_type' => env('CIVIC_ENGINE_EMBED_TYPE'),
+    'civic_engine_utm_source' => env('CIVIC_ENGINE_UTM_SOURCE'),
     'default_referral_campaign_id' => env('DS_DEFAULT_REFERRAL_CAMPAIGN_ID'),
     'hide_campaign_ids' => explode(',', env('DS_HIDE_CAMPAIGN_IDS')),
-    // The source URL for the CivicEngine Voting Widget.
-    // @see https://developers.civicengine.com/docs/widget#step-one-include-supporting-scripts
+    // This will be deprecated per civic_engine config vars, expects the embed.js URL.
     'voting_widget_src_url' =>  env('DS_VOTING_WIDGET_SRC_URL'),
 ];
--- resources/assets/components/blocks/VoterWidgetBlock/VoterWidgetBlock.js
+++ resources/assets/components/blocks/VoterWidgetBlock/VoterWidgetBlock.js
@@ -2,11 +2,47 @@ import React, { useEffect } from 'react';
 
 import { siteConfig } from '../../../helpers';
 
+/**
+ * Embeds a CivicEngine Voting Widget.
+ * @see https://developers.civicengine.com/docs/widget/
+ */
 const VoterWidgetBlock = () => {
+  const baseUri = `https://app.${siteConfig(
+    'civic_engine_client_name',
+  )}.civicengine.com/`;
+  const embedType = siteConfig('civic_engine_embed_type') || 'bronze';
+
+  // This value is used to track a submission on behalf of DoSomething.
+  // @see https://www.pivotaltracker.com/story/show/174199686/comments/218082111
+  const utmSource = siteConfig('civic_engine_utm_source') || 'DST';
+
+  // If bronze level, add as an iframe.
+  // TODO: Do UTM parameters work here? The documentation suggests they don't.
+  // @see https://developers.civicengine.com/docs/widget/#bronze-level-widget-instructions
+  if (embedType === 'bronze') {
+    return (
+      <iframe
+        src={`${baseUri}w/address?utm_source=${utmSource}`}
+        title="Voting Widget"
+      />
+    );
+  }
+
+  const widget = (
+    <div className="civicengine-address" data-utm-source={utmSource} />
+  );
+
+  // If gold level, we've already added the required script tags into the <HEAD> element.
+  if (embedType === 'gold') {
+    return widget;
+  }
+
+  // If silver level, we need to include the script tags.
+  // @see https://developers.civicengine.com/docs/widget/#silver-level-widget-instructions
   useEffect(() => {
     const script = document.createElement('script');
 
-    script.src = siteConfig('voting_widget_src_url');
+    script.src = `${baseUri}embed.js`;
     script.async = true;
     document.body.appendChild(script);
 
@@ -15,11 +51,7 @@ const VoterWidgetBlock = () => {
     };
   }, []);
 
-  // This value is used to track a submission on behalf of DoSomething.
-  // @see https://www.pivotaltracker.com/story/show/174199686/comments/218082111
-  const utmSource = 'DST';
-
-  return <div className="civicengine-address" data-utm-source={utmSource} />;
+  return widget;
 };
 
 export default VoterWidgetBlock;
