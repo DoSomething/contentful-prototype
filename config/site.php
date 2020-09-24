d0809158757129ee4d012bd07355656c631b61bb
Aaron Schachter <aaronschachter@gmail.com>
2020-09-24 11:17:52 -0700

    Removes config for VoterWidgetBlock

--- config/site.php
+++ config/site.php
@@ -16,14 +16,6 @@ return [
 
     // These Group Type IDs have a 'chapter' group label instead of the default in Group Finders.
     'chapter_group_type_ids' => explode(',', env('DS_CHAPTER_GROUP_TYPE_IDS')),
-    /**
-     * The CivicEngine vars are used forVoting Widget configuration.
-     * @see https://developers.civicengine.com/docs/widget
-     */
-    'civic_engine_client_name' => env('CIVIC_ENGINE_CLIENT_NAME'),
-    'civic_engine_utm_source' => env('CIVIC_ENGINE_UTM_SOURCE'),
     'default_referral_campaign_id' => env('DS_DEFAULT_REFERRAL_CAMPAIGN_ID'),
     'hide_campaign_ids' => explode(',', env('DS_HIDE_CAMPAIGN_IDS')),
-    // This will be deprecated per civic_engine config vars, expects the embed.js URL.
-    'voting_widget_src_url' =>  env('DS_VOTING_WIDGET_SRC_URL'),
 ];
--- resources/assets/components/blocks/VoterWidgetBlock/VoterWidgetBlock.js
+++ resources/assets/components/blocks/VoterWidgetBlock/VoterWidgetBlock.js
@@ -1,25 +1,21 @@
 import React from 'react';
 
-import { siteConfig } from '../../../helpers';
-
-/**
- * Embeds a CivicEngine Voting Widget.
- * @see https://developers.civicengine.com/docs/widget/
+/*
+ * Embeds a CivicEngine Voter Widget.
+ * @see https://developers.civicengine.com/docs/widget
+ *
+ * A utm_source parameter is used to track a submission on behalf of DoSomething.
+ * Although the docs claim bronze level (using an iframe) does not support UTM params, an email
+ * from Ballot Ready to us says this should still send through the utm_source correctly.
+ * @see https://www.pivotaltracker.com/story/show/174199686
  */
-const VoterWidgetBlock = () => {
-  /**
-   * A utm_source parameter is used to track a submission on behalf of DoSomething.
-   * Although the docs claim bronze level (using an iframe) does not support UTM params, an email
-   * from Ballot Ready to us says this should still send through the utm_source correctly.
-   * @see https://www.pivotaltracker.com/story/show/174199686
-   */
-  const utmSource = siteConfig('civic_engine_utm_source') || 'DST';
-
-  const url = `https://app.${siteConfig(
-    'civic_engine_client_name',
-  )}.civicengine.com/w/address?utm_source=${utmSource}`;
-
-  return <iframe src={url} title="Voting Widget" width="100%" height="520" />;
-};
+const VoterWidgetBlock = () => (
+  <iframe
+    height="520"
+    src="https://app.requestballot.civicengine.com/w/address?utm_source=DST"
+    title="Voting Widget"
+    width="100%"
+  />
+);
 
 export default VoterWidgetBlock;
