33681a44f5388f95a9f741eb57e5e313f1dd90e6
Aaron Schachter <aaronschachter@gmail.com>
2020-09-24 07:55:26 -0700

    Changes CivicEngine voting widget embed type to bronze level (iframe) (#2362)
    * Adds  config vars for Civic Engine embed levels
    
    * Adds defer property
    
    * Updates comments
    
    * Fixes CivicEngine URL
    
    * Adds width and height per https://github.com/DoSomething/phoenix-next/pull/2362#discussion_r493940600
    
    * Commits to bronze level per https://github.com/DoSomething/phoenix-next/pull/2362#discussion_r494015621
    
    * Update config/site.php
    
    Co-authored-by: Mendel Blesofsky <mendelblesofsky@gmail.com>
    
    Co-authored-by: Mendel Blesofsky <mendelblesofsky@gmail.com>

--- resources/assets/components/blocks/VoterWidgetBlock/VoterWidgetBlock.js
+++ resources/assets/components/blocks/VoterWidgetBlock/VoterWidgetBlock.js
@@ -1,25 +1,25 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { siteConfig } from '../../../helpers';
 
+/**
+ * Embeds a CivicEngine Voting Widget.
+ * @see https://developers.civicengine.com/docs/widget/
+ */
 const VoterWidgetBlock = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
+  /**
+   * A utm_source parameter is used to track a submission on behalf of DoSomething.
+   * Although the docs claim bronze level (using an iframe) does not support UTM params, an email
+   * from Ballot Ready to us says this should still send through the utm_source correctly.
+   * @see https://www.pivotaltracker.com/story/show/174199686
+   */
+  const utmSource = siteConfig('civic_engine_utm_source') || 'DST';
 
-    script.src = siteConfig('voting_widget_src_url');
-    script.async = true;
-    document.body.appendChild(script);
+  const url = `https://app.${siteConfig(
+    'civic_engine_client_name',
+  )}.civicengine.com/w/address?utm_source=${utmSource}`;
 
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  // This value is used to track a submission on behalf of DoSomething.
-  // @see https://www.pivotaltracker.com/story/show/174199686/comments/218082111
-  const utmSource = 'DST';
-
-  return <div className="civicengine-address" data-utm-source={utmSource} />;
+  return <iframe src={url} title="Voting Widget" width="100%" height="520" />;
 };
 
 export default VoterWidgetBlock;
