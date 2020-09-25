82140ecc96e72d5b079aa19ecf0d4662ce8cc7b6
Lindsay Maher <lmaher@Lindsays-MBP.home>
2020-09-25 16:40:26 -0400

    adds span with styling and hides inputs from view

--- resources/assets/components/blocks/GetOutTheVoteBlock/PeopleFormFields/FriendOne.js
+++ resources/assets/components/blocks/GetOutTheVoteBlock/PeopleFormFields/FriendOne.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 
+import '../get-out-the-vote.scss';
+
 const FriendOne = () => {
   const [voterChoiceFriendOne, setvoterChoiceFriendOne] = useState('');
 
   const handleVotingChoiceFriendOne = event => {
     if (event.target.id === 'voting-location-friend-1-in-person') {
+      console.log('this still works');
       setvoterChoiceFriendOne('in-person');
     } else {
       setvoterChoiceFriendOne('by-mail');
+      console.log('this still works too');
     }
   };
 
@@ -23,7 +27,7 @@ const FriendOne = () => {
 
       <div className="w-full md:w-3/5 flex justify-around py-6 md:py-0">
         <p className="w-1/3">Voting in-person</p>
-        <div className="w-1/3 bg-blurple-500 flex justify-around rounded-full">
+        <div className="w-1/3 bg-blurple-500 flex justify-around items-center rounded-full switch">
           <input
             id="voting-location-friend-1-in-person"
             type="radio"
@@ -31,13 +35,16 @@ const FriendOne = () => {
             onClick={handleVotingChoiceFriendOne}
             checked={voterChoiceFriendOne === 'in-person'}
           />
-          <input
-            id="voting-location-friend-1"
-            type="radio"
-            aria-label="voting-location"
-            checked={!voterChoiceFriendOne}
-            disabled
-          />
+          <div className="bg-gray-400 rounded-full">
+            <input
+              id="voting-location-friend-1"
+              type="radio"
+              aria-label="voting-location"
+              checked={!voterChoiceFriendOne}
+              disabled
+            />
+            <span className="slider round" />
+          </div>
           <input
             id="voting-location-friend-1-by-mail"
             type="radio"
