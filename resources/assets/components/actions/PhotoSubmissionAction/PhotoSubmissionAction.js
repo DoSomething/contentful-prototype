5584957542847829b6069d681e6500cdc1d2324c
Michael Dinnall <michaeldinnall@gmail.com>
2020-09-28 13:05:13 -0400

    Update PhotoSubmissionAction.js (#2361)
    * Update PhotoSubmissionAction.js
    
    * Update resources/assets/components/actions/PhotoSubmissionAction/PhotoSubmissionAction.js
    
    Co-authored-by: Katie Crane <kcrane@dosomething.org>
    
    * updated total quantity description

--- resources/assets/components/actions/PhotoSubmissionAction/PhotoSubmissionAction.js
+++ resources/assets/components/actions/PhotoSubmissionAction/PhotoSubmissionAction.js
@@ -405,7 +405,7 @@ class PhotoSubmissionAction extends PostForm {
                           {quantity ? (
                             <div className="quantity-display py-3">
                               <span className="block font-bold leading-none mb-2 text-base text-gray-600 uppercase">
-                                total items
+                                Total Items - Current
                               </span>
 
                               <span
@@ -423,7 +423,7 @@ class PhotoSubmissionAction extends PostForm {
                             htmlFor="quantity"
                           >
                             {quantity
-                              ? 'You can enter your new total here:'
+                              ? 'Total Items - Updated'
                               : this.props.quantityFieldLabel}
                           </label>
 
@@ -440,6 +440,12 @@ class PhotoSubmissionAction extends PostForm {
                             required
                             min={Number(quantity) + 1}
                           />
+                          <p className="text-sm text-gray-600">
+                            This is the total items number above plus the new
+                            number you are adding. For instance, if you
+                            currently have 1 total item and are adding 1 more,
+                            you would enter 2 here.
+                          </p>
                         </div>
                       ) : null}
 
