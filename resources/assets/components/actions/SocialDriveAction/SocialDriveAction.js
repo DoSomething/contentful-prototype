301e8c8204d077a30c4b8f58cff670abb9ab383f
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-09-29 12:15:00 -0400

    Use className prop to allow container to control width (#2374)
    Fixes an issue where the 'two-thirds width hack' for campaign pages would apply unilaterally on other pages

--- resources/assets/components/actions/SocialDriveAction/SocialDriveAction.js
+++ resources/assets/components/actions/SocialDriveAction/SocialDriveAction.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import Card from '../../utilities/Card/Card';
 import ShortLinkShareContainer from '../../utilities/ShortLinkShare/ShortLinkShareContainer';
@@ -13,8 +14,8 @@ export const SocialDriveBlockFragment = gql`
   }
 `;
 
-const SocialDriveAction = ({ description, link, title }) => (
-  <div className="clearfix pb-6 lg:flex lg:w-2/3 lg:pr-3">
+const SocialDriveAction = ({ className, description, link, title }) => (
+  <div className={classNames('clearfix pb-6', className)}>
     <Card className="rounded bordered" title={title}>
       {description ? <p className="p-3">{description}</p> : null}
 
@@ -24,12 +25,14 @@ const SocialDriveAction = ({ description, link, title }) => (
 );
 
 SocialDriveAction.propTypes = {
+  className: PropTypes.string,
   description: PropTypes.string,
   link: PropTypes.string.isRequired,
   title: PropTypes.string,
 };
 
 SocialDriveAction.defaultProps = {
+  className: null,
   description: null,
   title: 'Your Online Drive',
 };
