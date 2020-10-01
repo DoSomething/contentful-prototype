3c56d6a95d21c585160e0332d23d51fb7adce5c9
vmack <vanessacmack@gmail.com>
2020-10-01 12:03:02 -0400

    imported SiteFotter and SiteNavigationContainer

--- resources/assets/components/pages/ShowSubmissionPage/ShowSubmissionPage.js
+++ resources/assets/components/pages/ShowSubmissionPage/ShowSubmissionPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import SiteFooter from '../../utilities/SiteFooter/SiteFooter';
+import SiteNavigationContainer from '../../SiteNavigation/SiteNavigationContainer';
+
 const ShowSubmissionPage = () => {
   return (
     <>
       <SiteNavigationContainer />
 
       <main>
-        <Header>We Got Your Submission</Header>
+        <h1>We Got Your Submission</h1>
         <p>
           Thanks for joining the movement! After we review your submission,
           we'll add it to the public gallery alongside submissions from all the
