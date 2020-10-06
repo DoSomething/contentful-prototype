382662d7e836c3fdb773976605724b5ea3077782
vmack <vanessacmack@gmail.com>
2020-10-06 18:18:42 -0400

    added some styling to confirmation page copy

--- resources/assets/components/pages/ShowSubmissionPage/ShowSubmissionPage.js
+++ resources/assets/components/pages/ShowSubmissionPage/ShowSubmissionPage.js
@@ -9,12 +9,16 @@ const ShowSubmissionPage = () => {
       <SiteNavigationContainer />
 
       <main>
-        <h1>We Got Your Submission</h1>
-        <p>
-          Thanks for joining the movement! After we review your submission,
-          we&apos;ll add it to the public gallery alongside submissions from all
-          the other members taking action in this campaign.
-        </p>
+        <div>
+          <h1 className="uppercase text-3xl md:text-4xl font-league-gothic font-normal">
+            We Got Your Submission
+          </h1>
+          <p>
+            Thanks for joining the movement! After we review your submission,
+            we&apos;ll add it to the public gallery alongside submissions from
+            all the other members taking action in this campaign.
+          </p>
+        </div>
       </main>
 
       <SiteFooter />
