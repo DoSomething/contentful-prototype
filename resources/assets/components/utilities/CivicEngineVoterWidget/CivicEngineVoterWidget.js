ed51b0ebeee1ded42639319b6b911dc89e868601
Aaron Schachter <aaronschachter@gmail.com>
2020-10-13 11:33:35 -0700

    Appends UTM params to URL if present

--- resources/assets/components/utilities/CivicEngineVoterWidget/CivicEngineVoterWidget.js
+++ resources/assets/components/utilities/CivicEngineVoterWidget/CivicEngineVoterWidget.js
@@ -1,24 +1,26 @@
 import React from 'react';
 
+import { getUtms } from '../../../helpers/utm';
+import { appendToQuery } from '../../../helpers';
+
 /*
- * Embeds a CivicEngine Voter Widget.
- * @see https://developers.civicengine.com/docs/widget
+ * Renders a CivicEngine Voter Widget via the Bronze Level embed.
+ * @see https://developers.civicengine.com/docs/widget/#bronze-level-widget-instructions
  *
- * A utm_source parameter is used to track a submission on behalf of DoSomething.
  * Although the docs claim bronze level (using an iframe) does not support UTM params, an email
- * from Ballot Ready to us says this should still send through the utm_source correctly.
+ * from Ballot Ready to us says this should still send through the UTM's correctly.
  * @see https://www.pivotaltracker.com/story/show/174199686
  */
-const CivicEngineVoterWidget = () => {
-  // TODO: Append UTM parameters to src URL
-  return (
-    <iframe
-      height="180"
-      src="https://app.dosomething.civicengine.com/w/address"
-      title="Voter Widget"
-      width="100%"
-    />
-  );
-};
+const CivicEngineVoterWidget = () => (
+  <iframe
+    height="180"
+    src={appendToQuery(
+      getUtms(),
+      'https://app.dosomething.civicengine.com/w/address',
+    )}
+    title="Voter Widget"
+    width="100%"
+  />
+);
 
 export default CivicEngineVoterWidget;
