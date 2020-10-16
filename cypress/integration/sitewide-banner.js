77a563c5527c0a5c44db67be4ca1a7bc329047ce
Lindsay Maher <lmaher@Lindsays-MBP.home>
2020-10-16 13:13:45 -0400

    updates more cypress tests for sitewide banner

--- cypress/integration/sitewide-banner.js
+++ cypress/integration/sitewide-banner.js
@@ -42,7 +42,7 @@ describe('Site Wide Banner', () => {
       },
     });
 
-    cy.mockGraphqlOp('UserSitewideBannerQuery', {
+    cy.mockGraphqlOp('UserVoterRegistrationStatusQuery', {
       user: {
         voterRegistrationStatus: null,
       },
@@ -62,7 +62,7 @@ describe('Site Wide Banner', () => {
       },
     });
 
-    cy.mockGraphqlOp('UserSitewideBannerQuery', {
+    cy.mockGraphqlOp('UserVoterRegistrationStatusQuery', {
       user: {
         voterRegistrationStatus: 'UNREGISTERED',
       },
@@ -188,7 +188,7 @@ describe('Site Wide Banner', () => {
       },
     });
 
-    cy.mockGraphqlOp('UserSitewideBannerQuery', {
+    cy.mockGraphqlOp('UserVoterRegistrationStatusQuery', {
       user: {
         voterRegistrationStatus: 'UNREGISTERED',
       },
