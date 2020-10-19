f352c392c7c584edfb433178c9769b9972e74a7c
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-10-19 12:29:50 -0400

    Update test descriptions

--- resources/assets/components/blocks/EmbedBlock/EmbedBlock.test.js
+++ resources/assets/components/blocks/EmbedBlock/EmbedBlock.test.js
@@ -6,13 +6,13 @@ import EmbedBlock from './EmbedBlock';
 jest.mock('./EmbedBlockContainer.js', () => 'EmbedBlockContainer');
 
 describe('Embed Block Component', () => {
-  test('renders Carto Template with correct URL passed in props', () => {
+  test('renders Carto template for dosomething.carto URL', () => {
     const wrapper = shallow(<EmbedBlock url="https://dosomething.carto.com" />);
 
     expect(wrapper.find('CartoTemplate').length).toEqual(1);
   });
 
-  test('renders Carto Template with correct URL passed in props', () => {
+  test('renders Typeform template for dosomething.typeform URL', () => {
     const wrapper = shallow(
       <EmbedBlock url="https://dosomething.typeform.com" />,
     );
