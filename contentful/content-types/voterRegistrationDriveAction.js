3a05195bb26c7035041fe66551549c0b75992dfd
Aaron Schachter <aaronschachter@gmail.com>
2020-10-19 12:35:11 -0700

    Removes unused OVRD Action fields, updates GraphQL schema (#2400)
    * Removes fields deleted in https://github.com/DoSomething/graphql/pull/286
    
    * Update latest GraphQL schema
    
    * Removes unused approvedPostCountActionId

--- contentful/content-types/voterRegistrationDriveAction.js
+++ contentful/content-types/voterRegistrationDriveAction.js
@@ -37,33 +37,6 @@ module.exports = function(migration) {
     .validations([])
     .disabled(false)
     .omitted(false);
-
-  voterRegistrationDriveAction
-    .createField('approvedPostCountActionId')
-    .name('Approved Post Count Action ID')
-    .type('Integer')
-    .localized(false)
-    .required(false)
-    .validations([
-      {
-        range: {
-          min: 1,
-          max: null,
-        },
-      },
-    ])
-    .disabled(false)
-    .omitted(false);
-
-  voterRegistrationDriveAction
-    .createField('approvedPostCountLabel')
-    .name('Approved Post Count Label')
-    .type('Symbol')
-    .localized(false)
-    .required(false)
-    .validations([])
-    .disabled(false)
-    .omitted(false);
   voterRegistrationDriveAction.changeFieldControl(
     'internalTitle',
     'builtin',
@@ -81,25 +54,5 @@ module.exports = function(migration) {
     'builtin',
     'markdown',
     {},
-  );
-
-  voterRegistrationDriveAction.changeFieldControl(
-    'approvedPostCountActionId',
-    'builtin',
-    'numberEditor',
-    {
-      helpText:
-        "If set, the count of current user's approved posts for this action will appear in the sidebar.",
-    },
-  );
-
-  voterRegistrationDriveAction.changeFieldControl(
-    'approvedPostCountLabel',
-    'builtin',
-    'singleLine',
-    {
-      helpText:
-        'Label text for the approved post count action, if set. Defaults to "Total scholarship entries" if blank.',
-    },
   );
 };
