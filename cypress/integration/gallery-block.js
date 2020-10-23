84d0010bbceb6c869f94457345b06349d4f24272
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-10-23 14:03:58 -0400

    Retire Gallery Block "External Link" gallery type (#2412)
    * update schema, including new CONTENT_BLOCK gallery block type
    
    * Retire the EXTERNAL_LINK gallery type
    
    * Update gallery block test per new gallery type

--- cypress/integration/gallery-block.js
+++ cypress/integration/gallery-block.js
@@ -4,36 +4,18 @@ import { userFactory } from '../fixtures/user';
 describe('Gallery Block', () => {
   beforeEach(() => cy.configureMocks());
 
-  const blockId = 'abcdefghi123456789';
-
   /** @test */
-  it.only('renders a ContentBlockGalleryItem component for "EXTERNAL_LINK" blocks', () => {
+  it.only('renders a ContentBlockGalleryItem component for "CONTENT_BLOCK" blocks', () => {
     cy.mockGraphqlOp('ContentfulBlockQuery', {
       block: {
         __typename: 'GalleryBlock',
-        galleryType: 'EXTERNAL_LINK',
+        galleryType: 'CONTENT_BLOCK',
         blocks: [{ __typename: 'ContentBlock' }],
       },
     });
 
-    cy.visit(`us/blocks/${blockId}`);
+    cy.visit('us/blocks/abcdefghi123456789');
 
     cy.findByTestId('content-block-gallery-item');
   });
-
-  // @TODO: Activate this test once we've deployed https://git.io/JTuoE and pulled in the new schema.
-  // /** @test */
-  // it.only('renders a ContentBlockGalleryItem component for "CONTENT_BLOCK" blocks', () => {
-  //   cy.mockGraphqlOp('ContentfulBlockQuery', {
-  //     block: {
-  //       __typename: 'GalleryBlock',
-  //       galleryType: 'CONTENT_BLOCK',
-  //       blocks: [{ __typename: 'ContentBlock' }],
-  //     },
-  //   });
-
-  //   cy.visit(`us/blocks/${blockId}`);
-
-  //   cy.findByTestId('content-block-gallery-item');
-  // });
 });
