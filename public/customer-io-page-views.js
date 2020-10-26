af46b3d8685bbd4a83fd7e76ed289b520e914b6d
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-10-26 14:43:43 -0400

    Update public/customer-io-page-views.js
    Co-authored-by: Aaron Schachter <aaronschachter@gmail.com>

--- public/customer-io-page-views.js
+++ public/customer-io-page-views.js
@@ -4,10 +4,12 @@ function parseQuery(queryString) {
     ? queryString.substr(1)
     : queryString
   ).split('&');
+
   for (var i = 0; i < pairs.length; i++) {
     var pair = pairs[i].split('=');
     query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
   }
+ 
   return query;
 }
 
