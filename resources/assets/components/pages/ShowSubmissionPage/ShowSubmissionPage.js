82e2cb736edab08cc0406bc24fa827fe3ba64b57
vmack <vanessacmack@gmail.com>
2020-10-29 14:28:08 -0400

    added affirmation content, query component and query helper

--- resources/assets/components/pages/ShowSubmissionPage/ShowSubmissionPage.js
+++ resources/assets/components/pages/ShowSubmissionPage/ShowSubmissionPage.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import { useQuery } from '@apollo/react-hooks';
 
-import Card from '../../utilities/Card/Card';
-import Spinner from '../../artifacts/Spinner/Spinner';
+import { query } from '../../../helpers';
+import Query from '../../Query';
 import SiteFooter from '../../utilities/SiteFooter/SiteFooter';
 import SiteNavigationContainer from '../../SiteNavigation/SiteNavigationContainer';
 
 export const GET_AFFIRMATION_CONTENT = gql`
   query submissionActionIdQuery($id: String!) {
-    block(id: $submissionActionId)
+    block(id: $id) {
+      id
+      ... on PhotoSubmissionBlock {
+        affirmationContent
+      }
+    }
   }
 `;
 
-const ShowSubmissionPage = id => {
-  const { loading, error, data } = useQuery(GET_AFFIRMATION_CONTENT, {
-    variables: { id },
-    skip: !id,
-  });
-
-  <Card className="bordered rounded" title={title}>
-    {loading ? <Spinner className="flex justify-center p-16" /> : null}
-  </Card>;
+const ShowSubmissionPage = () => {
+  const id = query('submissionActionId');
 
   return (
     <>
@@ -32,11 +29,20 @@ const ShowSubmissionPage = id => {
           <h1 className="uppercase text-3xl md:text-4xl font-league-gothic font-normal">
             We Got Your Submission
           </h1>
-          <p>
-            Thanks for joining the movement! After we review your submission,
-            we&apos;ll add it to the public gallery alongside submissions from
-            all the other members taking action in this campaign.
-          </p>
+          <Query query={GET_AFFIRMATION_CONTENT} variables={{ id }}>
+            {data => {
+              return (
+                data.block.affirmationContent || (
+                  <p>
+                    Thanks for joining the movement! After we review your
+                    submission, we&apos;ll add it to the public gallery
+                    alongside submissions from all the other members taking
+                    action in this campaign.
+                  </p>
+                )
+              );
+            }}
+          </Query>
         </div>
       </main>
 
