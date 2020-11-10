1d0e9cc9e9df82d24cef840ce4ca197f3354d75e
Lindsay Maher <lmaher@Lindsays-MBP.home>
2020-11-10 16:28:10 -0500

    remove comment

--- resources/assets/components/CampaignBanner/CampaignBanner.js
+++ resources/assets/components/CampaignBanner/CampaignBanner.js
@@ -108,7 +108,6 @@ const CampaignBanner = ({
                 internalTitle="Campaign Progress Bar Experiment"
                 convertableActions={['signup']}
                 alternatives={[
-                  // Displaying the progress bar is the default, or control test alternative.
                   <div className="mb-6" testName="Progress Bar Visible">
                     <ProgressBar percentage={percentage} />
                     <p className="text-lg">
