067afa7bf7a0e5339f53cfac71485007b09f2781
Vanessa Mack <vanessacmack@gmail.com>
2020-11-18 10:58:12 -0500

    Grab `affirmationContent` from `PhotoSubmissionAction` (#2415)
    * added gql for querying submissionActionId
    
    * added useQuery and spinner
    
    * added affirmation content, query component and query helper
    
    * added TextContent
    
    * fixed TextContent
    
    * changes
    
    * updates to re use existing query string
    
    * new line before return
    
    Co-authored-by: Lindsay Maher <lmaher@Lindsays-MBP.home>

--- resources/assets/components/pages/ShowSubmissionPage/ShowSubmissionPage.js
+++ resources/assets/components/pages/ShowSubmissionPage/ShowSubmissionPage.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import Query from '../../Query';
+import { query } from '../../../helpers';
 import SiteFooter from '../../utilities/SiteFooter/SiteFooter';
+import TextContent from '../../utilities/TextContent/TextContent';
 import SiteNavigationContainer from '../../SiteNavigation/SiteNavigationContainer';
+import { CONTENTFUL_BLOCK_QUERY } from '../../utilities/ContentfulEntryLoader/ContentfulEntryLoader';
 
 const ShowSubmissionPage = () => {
+  const id = query('submissionActionId');
+  const defaultContent =
+    'Thanks for joining the movement! After we review your submission, we&apos;ll add it to the public gallery alongside submissions from all the other members taking action in this campaign.';
+
   return (
     <>
       <SiteNavigationContainer />
@@ -13,14 +21,26 @@ const ShowSubmissionPage = () => {
           <h1 className="uppercase text-3xl md:text-4xl font-league-gothic font-normal">
             We Got Your Submission
           </h1>
-          <p>
-            Thanks for joining the movement! After we review your submission,
-            we&apos;ll add it to the public gallery alongside submissions from
-            all the other members taking action in this campaign.
-          </p>
+          {id ? (
+            <Query
+              query={CONTENTFUL_BLOCK_QUERY}
+              variables={{ id, preview: false }}
+            >
+              {data =>
+                data.block.affirmationContent ? (
+                  <TextContent className="mb-6">
+                    {data.block.affirmationContent}
+                  </TextContent>
+                ) : null
+              }
+            </Query>
+          ) : (
+            <p>
+              <TextContent className="mb-6">{defaultContent}</TextContent>
+            </p>
+          )}
         </div>
       </main>
-
       <SiteFooter />
     </>
   );
