94fb617b886144a8e9a16031a406c2c51353e131
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-12-01 17:19:18 -0500

    Update certificate staffer to Sahara

--- resources/assets/components/pages/AccountPage/Credits/CertificateTemplate.js
+++ resources/assets/components/pages/AccountPage/Credits/CertificateTemplate.js
@@ -270,17 +270,17 @@ const CertificateTemplate = ({ certificatePost }) => {
             <View style={[styles.flex, { lineHeight: 1.1 }]}>
               <View style={{ width: '50%' }}>
                 <Text style={{ marginTop: 10, fontWeight: 'bold' }}>
-                  Maddy Allison
+                  Sahara Lake
                 </Text>
                 <Text style={{ fontStyle: 'italic' }}>
-                  Community Associate, DoSomething.org
+                  Senior Manager, Community Impact
                 </Text>
               </View>
 
               <View style={{ width: '50%' }}>
                 <Text style={{ marginTop: 10, textAlign: 'right' }}>
                   For questions and verification issues, please reach out to
-                  Maddy at {'\n'}
+                  Sahara at {'\n'}
                   <Text style={{ fontWeight: 'bold' }}>
                     volunteer@dosomething.org
                   </Text>{' '}
