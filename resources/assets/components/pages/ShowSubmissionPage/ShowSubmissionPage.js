fd426e6b317eb7c088405c5cfaf42ddd4285a909
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2020-12-02 11:25:07 -0500

    adds query to pull photo url

--- resources/assets/components/pages/ShowSubmissionPage/ShowSubmissionPage.js
+++ resources/assets/components/pages/ShowSubmissionPage/ShowSubmissionPage.js
@@ -1,17 +1,50 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+import ReactRouterPropTypes from 'react-router-prop-types';
 
 import Query from '../../Query';
 import { query } from '../../../helpers';
+import Spinner from '../../artifacts/Spinner/Spinner';
+import ErrorBlock from '../../blocks/ErrorBlock/ErrorBlock';
 import SiteFooter from '../../utilities/SiteFooter/SiteFooter';
 import TextContent from '../../utilities/TextContent/TextContent';
 import RecommendedCampaignsGallery from './RecommendedCampaignsGallery';
 import SiteNavigationContainer from '../../SiteNavigation/SiteNavigationContainer';
 import { CONTENTFUL_BLOCK_QUERY } from '../../utilities/ContentfulEntryLoader/ContentfulEntryLoader';
 
-const ShowSubmissionPage = () => {
+const CAMPAIGN_POST_QUERY = gql`
+  query CampaignInfoQuery($postId: Int!) {
+    post(id: $postId) {
+      id
+      url
+    }
+  }
+`;
+
+const ShowSubmissionPage = ({ match }) => {
   const id = query('submissionActionId');
   const defaultContent =
     'Thanks for joining the movement! After we review your submission, we&apos;ll add it to the public gallery alongside submissions from all the other members taking action in this campaign.';
+  const postId = Number(match.params.post_id);
+
+  const { loading, error, postData } = useQuery(CAMPAIGN_POST_QUERY, {
+    variables: {
+      postId,
+    },
+  });
+
+  // const post = postData
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return <ErrorBlock error={error} />;
+  }
+
+  console.log('this is the post', postData);
 
   return (
     <>
@@ -36,9 +69,7 @@ const ShowSubmissionPage = () => {
               }
             </Query>
           ) : (
-            <p>
-              <TextContent className="mb-6">{defaultContent}</TextContent>
-            </p>
+            <TextContent className="mb-6">{defaultContent}</TextContent>
           )}
         </div>
 
@@ -47,6 +78,10 @@ const ShowSubmissionPage = () => {
       <SiteFooter />
     </>
   );
+};
+
+ShowSubmissionPage.propTypes = {
+  match: ReactRouterPropTypes.match.isRequired,
 };
 
 export default ShowSubmissionPage;
