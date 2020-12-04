5a3ee85d5baace1651719326d548f148e9d81b7c
Lindsay Maher <lindsaymah3r@gmail.com>
2020-12-04 10:58:19 -0500

    Adding Photo to Submission Page (#2445)
    * adds query to pull photo url
    
    * adds alias to data for the post query
    
    * adds initial grid and checks if RB post has an image to display
    
    * move the spinner to the image
    
    * makes 2/3 width with photo only
    
    * adjusts photo size and placement, adds border
    
    * adjust padding on tablet size
    
    * merges in suggested campaigns
    
    * updates from PR
    
    * updates padding for gallery and image and adds title for gallery
    
    Co-authored-by: Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>

--- resources/assets/components/pages/ShowSubmissionPage/ShowSubmissionPage.js
+++ resources/assets/components/pages/ShowSubmissionPage/ShowSubmissionPage.js
@@ -1,52 +1,108 @@
 import React from 'react';
+import { get } from 'lodash';
+import gql from 'graphql-tag';
+import classnames from 'classnames';
+import { useQuery } from '@apollo/react-hooks';
+import ReactRouterPropTypes from 'react-router-prop-types';
 
 import Query from '../../Query';
-import { query } from '../../../helpers';
+import { env, query } from '../../../helpers';
+import ErrorBlock from '../../blocks/ErrorBlock/ErrorBlock';
 import SiteFooter from '../../utilities/SiteFooter/SiteFooter';
 import TextContent from '../../utilities/TextContent/TextContent';
 import RecommendedCampaignsGallery from './RecommendedCampaignsGallery';
 import SiteNavigationContainer from '../../SiteNavigation/SiteNavigationContainer';
 import { CONTENTFUL_BLOCK_QUERY } from '../../utilities/ContentfulEntryLoader/ContentfulEntryLoader';
 
-const ShowSubmissionPage = () => {
+const POST_QUERY = gql`
+  query PostQuery($postId: Int!) {
+    post(id: $postId) {
+      id
+      url
+    }
+  }
+`;
+
+const ShowSubmissionPage = ({ match }) => {
   const id = query('submissionActionId');
   const defaultContent =
     'Thanks for joining the movement! After we review your submission, we&apos;ll add it to the public gallery alongside submissions from all the other members taking action in this campaign.';
+  const postId = Number(match.params.post_id);
+
+  const { loading, error, data: postData } = useQuery(POST_QUERY, {
+    variables: {
+      postId,
+    },
+  });
+
+  const postImageUrl = get(postData, 'post.url', null);
+
+  if (error) {
+    return <ErrorBlock error={error} />;
+  }
 
   return (
     <>
       <SiteNavigationContainer />
 
-      <main>
-        <div>
-          <h1 className="uppercase text-3xl md:text-4xl font-league-gothic font-normal">
-            We Got Your Submission
-          </h1>
-          {id ? (
-            <Query
-              query={CONTENTFUL_BLOCK_QUERY}
-              variables={{ id, preview: false }}
-            >
-              {data =>
-                data.block.affirmationContent ? (
-                  <TextContent className="mb-6">
-                    {data.block.affirmationContent}
-                  </TextContent>
-                ) : null
-              }
-            </Query>
-          ) : (
-            <p>
+      <main className="base-12-grid">
+        <div className="grid-wide lg:flex bg-white px-6">
+          {postImageUrl && !loading ? (
+            <div className="w-1/2 md:w-1/4 pt-6">
+              <img
+                className="border-2 border-gray-400 border-solid"
+                alt="Reportback submission"
+                src={postImageUrl}
+              />
+            </div>
+          ) : null}
+          <div
+            className={classnames('py-3 lg:p-6', {
+              'lg:w-3/4 ': postImageUrl,
+            })}
+          >
+            <h1 className="uppercase text-3xl md:text-4xl font-league-gothic font-normal">
+              We Got Your Submission
+            </h1>
+
+            {id ? (
+              <Query
+                query={CONTENTFUL_BLOCK_QUERY}
+                variables={{
+                  id,
+                  preview: env('CONTENTFUL_USE_PREVIEW_API', false),
+                }}
+              >
+                {data =>
+                  data.block.affirmationContent ? (
+                    <TextContent className="mb-6">
+                      {data.block.affirmationContent}
+                    </TextContent>
+                  ) : null
+                }
+              </Query>
+            ) : (
               <TextContent className="mb-6">{defaultContent}</TextContent>
-            </p>
-          )}
+            )}
+          </div>
         </div>
 
-        <RecommendedCampaignsGallery />
+        <div className="grid-wide m-6">
+          <h1 className="uppercase text-3xl md:text-4xl font-league-gothic font-normal">
+            More Scholarship Opportunities
+          </h1>
+
+          <RecommendedCampaignsGallery />
+        </div>
       </main>
+
       <SiteFooter />
     </>
   );
+};
+
+ShowSubmissionPage.propTypes = {
+  match: ReactRouterPropTypes.match.isRequired,
 };
 
 export default ShowSubmissionPage;
