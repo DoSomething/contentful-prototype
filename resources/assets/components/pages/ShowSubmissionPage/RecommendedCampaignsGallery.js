6afe95715032c766d60ddaf7b86b081f6a05e286
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-12-07 15:54:50 -0500

    Exclude site config & props-variable configured campaign IDs from the query

--- resources/assets/components/pages/ShowSubmissionPage/RecommendedCampaignsGallery.js
+++ resources/assets/components/pages/ShowSubmissionPage/RecommendedCampaignsGallery.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { get } from 'lodash';
 import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
 
 import Query from '../../Query';
+import { siteConfig } from '../../../helpers';
 import GalleryBlock from '../../blocks/GalleryBlock/GalleryBlock';
 import { scholarshipCardFragment } from '../../utilities/ScholarshipCard/ScholarshipCard';
 
 const SCHOLARSHIP_CAMPAIGNS_QUERY = gql`
-  query ScholarshipCampaignsQuery {
+  query ScholarshipCampaignsQuery($excludeIds: [Int]) {
     campaigns: searchCampaigns(
       perPage: 3
       hasWebsite: true
       hasScholarship: true
       isOpen: true
       orderBy: "start_date,desc"
+      excludeIds: $excludeIds
     ) {
       edges {
         node {
@@ -29,8 +32,16 @@ const SCHOLARSHIP_CAMPAIGNS_QUERY = gql`
   ${scholarshipCardFragment}
 `;
 
-const RecommendedCampaignsGallery = () => (
-  <Query query={SCHOLARSHIP_CAMPAIGNS_QUERY}>
+const RecommendedCampaignsGallery = ({ variables }) => (
+  <Query
+    query={SCHOLARSHIP_CAMPAIGNS_QUERY}
+    variables={{
+      excludeIds: [
+        ...(variables.excludeIds || []),
+        ...siteConfig('hide_campaign_ids', []).map(id => Number(id)),
+      ],
+    }}
+  >
     {result => (
       <GalleryBlock
         blocks={(get(result, 'campaigns.edges') || []).map(
@@ -43,5 +54,15 @@ const RecommendedCampaignsGallery = () => (
     )}
   </Query>
 );
+
+RecommendedCampaignsGallery.propTypes = {
+  variables: PropTypes.shape({
+    excludeIds: PropTypes.arrayOf(PropTypes.number),
+  }),
+};
+
+RecommendedCampaignsGallery.defaultProps = {
+  variables: {},
+};
 
 export default RecommendedCampaignsGallery;
