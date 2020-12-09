89fc1c6e2fc70dc9cc03fa211791ab01db144a3e
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-12-09 15:12:05 -0500

    Test for show-submission-page redirect following submission

--- cypress/integration/campaign-post.js
+++ cypress/integration/campaign-post.js
@@ -109,4 +109,47 @@ describe('Campaign Post', () => {
     // We should see the affirmation modal after submitting a post:
     cy.contains('We got your photo!');
   });
+
+  context('When the post_confirmation_page feature flag is on', () => {
+    it('Redirects to the show submission page after a successful photo post submission', () => {
+      const user = userFactory();
+
+      cy.mockGraphqlOp('ActionAndUserByIdQuery', {
+        action: {
+          collectSchoolId: false,
+        },
+        user: {
+          schoolId: null,
+        },
+      });
+
+      // Log in & visit the campaign action page:
+      cy.withFeatureFlags({
+        post_confirmation_page: true,
+      }).authVisitCampaignWithSignup(user, exampleCampaign);
+
+      cy.get('.photo-submission-action').within(() => {
+        // Choose an image to upload as a photo post:
+        cy.get('input[type="file"]').attachFile('upload.jpg');
+
+        // Fill out other fields:
+        cy.get('[name="caption"]').type("Let's do this!");
+        cy.get('[name="quantity"]').type('1');
+        cy.get('[name="whyParticipated"]').type('Testing');
+
+        // Mock the backend response:
+        const response = newPhotoPost(campaignId, user);
+        cy.route('POST', POSTS_API, response).as('submitPost');
+
+        // Submit the form, and assert we made the API request:
+        cy.contains('Submit a new photo').click();
+        cy.wait('@submitPost');
+
+        // We should be redirected to the show submission page after submitting a post.
+        cy.location('pathname').should('eq', `/us/posts/${response.data.id}`);
+        // We should have appended the Photo Submission Action ID as a query parameter.
+        cy.location('search').should('eq', '?submissionActionId=id');
+      });
+    });
+  });
 });
