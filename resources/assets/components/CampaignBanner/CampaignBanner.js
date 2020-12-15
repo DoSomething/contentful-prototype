e86b7984fd9cbd2af8e227c83c65082f50e88a3e
dinnall <michaeldinnall@gmail.com>
2020-12-15 15:10:36 -0500

    Update CampaignBanner.js

--- resources/assets/components/CampaignBanner/CampaignBanner.js
+++ resources/assets/components/CampaignBanner/CampaignBanner.js
@@ -11,7 +11,6 @@ import {
   isCurrentPathInPaths,
   siteConfig,
 } from '../../helpers';
-import SixpackExperiment from '../utilities/SixpackExperiment/SixpackExperiment';
 import Modal from '../utilities/Modal/Modal';
 import ContentfulEntry from '../ContentfulEntry';
 import partnerScholarshipQuizPaths from './config';
@@ -24,6 +23,7 @@ import ProgressBar from '../utilities/ProgressBar/ProgressBar';
 import TextContent from '../utilities/TextContent/TextContent';
 import { SCHOLARSHIP_SIGNUP_BUTTON_TEXT } from '../../constants';
 import CampaignInfoBlock from '../blocks/CampaignInfoBlock/CampaignInfoBlock';
+import SixpackExperiment from '../utilities/SixpackExperiment/SixpackExperiment';
 import AffiliatePromotion from '../utilities/AffiliatePromotion/AffiliatePromotion';
 import ScholarshipInfoBlock from '../blocks/ScholarshipInfoBlock/ScholarshipInfoBlock';
 import CampaignSignupFormContainer from '../CampaignSignupForm/CampaignSignupFormContainer';
