5d2da59b970e5ff625089a2ba430c5ce70ddf097
Lindsay Maher <lmaher@Lindsays-MBP.home>
2021-01-12 15:08:04 -0500

    adds tests for photo vs text posts and name display

--- cypress/integration/account-campaigns-tab.js
+++ cypress/integration/account-campaigns-tab.js
@@ -17,4 +17,82 @@ describe('User Account Campaigns Tab', () => {
     cy.visit(`/us/account/campaigns`);
     cy.findByTestId('empty-state-block').should('have.length', 1);
   });
+
+  /** @test */
+  it('Displays user name for a non anonymous post', () => {
+    const user = userFactory();
+
+    cy.mockGraphqlOp('UserPostsQuery', {
+      postsByUserId: [
+        {
+          actionDetails: {
+            anonymous: false,
+          },
+          user: {
+            firstName: user.firstName,
+          },
+        },
+      ],
+    });
+
+    cy.login(user);
+    cy.visit(`/us/account/campaigns`);
+    cy.findByTestId('post-author').should('have.length', 1);
+  });
+
+  /** @test */
+  it('Displays user name for a non anonymous post', () => {
+    const user = userFactory();
+
+    cy.mockGraphqlOp('UserPostsQuery', {
+      postsByUserId: [
+        {
+          actionDetails: {
+            anonymous: true,
+          },
+          user: {
+            firstName: user.firstName,
+          },
+        },
+      ],
+    });
+
+    cy.login(user);
+    cy.visit(`/us/account/campaigns`);
+    cy.findByTestId('anonymous-post-author').should('have.length', 1);
+  });
+
+  /** @test */
+  it('Displays the caption of a photo post under the photo', () => {
+    const user = userFactory();
+
+    cy.mockGraphqlOp('UserPostsQuery', {
+      postsByUserId: [
+        {
+          type: 'photo',
+        },
+      ],
+    });
+
+    cy.login(user);
+    cy.visit(`/us/account/campaigns`);
+    cy.findByTestId('photo-post-caption').should('have.length', 1);
+  });
+
+  /** @test */
+  it('Displays the caption of a text post at the top of the Postcard', () => {
+    const user = userFactory();
+
+    cy.mockGraphqlOp('UserPostsQuery', {
+      postsByUserId: [
+        {
+          type: 'text',
+        },
+      ],
+    });
+
+    cy.login(user);
+    cy.visit(`/us/account/campaigns`);
+    cy.findByTestId('text-post-caption').should('have.length', 1);
+  });
 });
