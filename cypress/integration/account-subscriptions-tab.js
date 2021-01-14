5ec63a30d918ffbe33be9a3f7c3285dd4f6ee913
Lindsay Maher <lmaher@Lindsays-MBP.home>
2021-01-14 15:41:44 -0500

    creates subscription center file

---
+++ cypress/integration/account-subscriptions-tab.js
@@ -0,0 +1,31 @@
+/// <reference types="Cypress" />
+
+import { userFactory } from '../fixtures/user';
+
+describe('User Account Email Subscriptions Tab', () => {
+  // Configure a new "mock" server before each test:
+  beforeEach(() => cy.configureMocks());
+
+  it('View existing subscriptions', () => {
+    // This user is just for auth purposes
+    const user = userFactory();
+
+    cy.mockGraphqlOp('AccountQuery', {
+      // Get back a particular user and subscription topics
+      user: root => ({
+        firstName: 'Delilah',
+        emailSubscriptionTopics: ['COMMUNITY', 'NEWS', 'LIFESTYLE'],
+      }),
+    });
+
+    // Log in & visit the subscription center:
+    cy.login(user).visit('/us/account/profile/subscriptions');
+
+    // We should see the user's name and the correct subscription topics checked
+    cy.contains('Welcome, Delilah!');
+    cy.get('input[name=COMMUNITY]').should('be.checked');
+    cy.get('input[name=NEWS]').should('be.checked');
+    cy.get('input[name=LIFESTYLE]').should('be.checked');
+    cy.get('input[name=SCHOLARSHIPS]').should('not.be.checked');
+  });
+});
