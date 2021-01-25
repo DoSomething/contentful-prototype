cf1da7edafab85ecb1c67e5d24391ae016b35429
Lindsay Maher <lindsaymah3r@gmail.com>
2021-01-25 15:21:39 -0500

    update with notes and start of new approach

--- resources/assets/components/utilities/ProgressBar/ProgressBar.js
+++ resources/assets/components/utilities/ProgressBar/ProgressBar.js
@@ -4,7 +4,7 @@ import { css } from '@emotion/core';
 
 import { tailwind } from '../../../helpers/display';
 
-const ProgressBar = ({ percentage, multiLevel, levelCount }) => {
+const ProgressBar = ({ percentage, multiLevel }) => {
   const tailwindYellow = tailwind('colors.yellow');
 
   const progressBarContainer = css`
@@ -19,13 +19,23 @@ const ProgressBar = ({ percentage, multiLevel, levelCount }) => {
     border-radius: inherit;
   `;
 
+  // 3 inner divs with the container
+  // set the container to 100% and 1fr x3
+  // each inner div
+  // create a multilevel helper / new component to make it work better for this type of calc
+  // for the circle marker, it is a sibling of the color linear div and it's absolutely positioned/top-right
   if (multiLevel) {
     return (
-      <div className="relative bg-gray-200" css={progressBarContainer}>
-        <div
-          css={progressBar}
-          style={{ width: `${percentage > 100 ? 100 : percentage}%` }}
-        />
+      // <div className="relative bg-gray-200" css={progressBarContainer}>
+      //   <div
+      //     css={progressBar}
+      //     style={{ width: `${percentage > 100 ? 100 : percentage}%` }}
+      //   />
+      // </div>
+      <div className="w-full grid grid-cols-3 bg-gray-300">
+        <div className="bg-purple-400">hello</div>
+        <div className="bg-teal-100">hi</div>
+        <div className="bg-yellow-300 w-1/2">ciao</div>
       </div>
     );
   }
@@ -41,13 +51,13 @@ const ProgressBar = ({ percentage, multiLevel, levelCount }) => {
 };
 
 ProgressBar.propTypes = {
-  levelCount: PropTypes.number,
+  // levelCount: PropTypes.number,
   multiLevel: PropTypes.bool,
   percentage: PropTypes.number.isRequired,
 };
 
 ProgressBar.defaultProps = {
-  levelCount: null,
+  // levelCount: null,
   multiLevel: null,
 };
 
