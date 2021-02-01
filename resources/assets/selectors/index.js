5c741443dd64b43f1208e652392223138eb660d4
Mendel Blesofsky <mendelblesofsky@gmail.com>
2021-02-01 15:10:26 -0500

    Retire campaign and story page Redux selectors

--- resources/assets/selectors/index.js
+++ resources/assets/selectors/index.js
@@ -1,13 +1,21 @@
+import { get } from 'lodash';
+
 import { getUtms, query } from '../helpers/url';
+import { getCampaign } from '../helpers/campaign';
 import { withoutValueless } from '../helpers/data';
-import { getCampaignDataForNorthstar } from './campaign';
-import { getStoryPageDataForNorthstar } from './storyPage';
+
+const getStoryPage = () => {
+  if (get(window.STATE, 'page.type', null) !== 'storyPage') {
+    return undefined;
+  }
+
+  return get(window.STATE, 'page.fields');
+};
 
 export function getDataForNorthstar(state) {
   return withoutValueless({
-    ...getCampaignDataForNorthstar(state),
-    ...getStoryPageDataForNorthstar(state),
-
+    // Used as 'destination' parameter for customized destination title on Northstar login page.
+    title: get(getCampaign(), 'title') || get(getStoryPage(), 'title'),
     // For 'source_details':
     contentful_id: state.campaign.id || state.page.id,
     mode: query('mode') || null,
