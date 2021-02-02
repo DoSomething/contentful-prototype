38450637cb9507b65c911dd36b37626150400b18
Lindsay Maher <lindsaymah3r@gmail.com>
2021-02-02 11:41:29 -0500

    uses flex box rather than background coloring to display headline and bordering lines

--- resources/assets/components/utilities/SectionHeader/GalleryBlockHeader.js
+++ resources/assets/components/utilities/SectionHeader/GalleryBlockHeader.js
@@ -1,41 +1,22 @@
 import React from 'react';
-import { css } from '@emotion/core';
 import PropTypes from 'prop-types';
 
-import { tailwind } from '../../../helpers/display';
-
-const tailwindScreens = tailwind('screens');
-
-export const centerHorizontalRule = css`
-  @media (min-width: ${tailwindScreens.md}) {
-    margin-top: -2px;
-    top: 50%;
-  }
-`;
-
-const GalleryBlockHeader = ({ bgColor, title }) => (
-  <div className="grid-wide text-center">
-    <h2 className="mb-6 relative">
-      <span
-        className={`${bgColor} font-league-gothic font-normal leading-tight inline-block px-6 relative text-3xl md:text-4xl uppercase z-10`}
-      >
-        {title}
-      </span>
-      <span
-        className="absolute bg-purple-500 block h-1 w-full z-0"
-        css={centerHorizontalRule}
-      />
+const GalleryBlockHeader = ({ title }) => (
+  <div className="grid-wide flex flex-wrap md:flex-no-wrap text-center w-full mb-6 justify-center">
+    <div className="w-0 md:w-full md:flex-shrink my-auto">
+      <div className=" bg-purple-500 h-1 w-full z-0" />
+    </div>
+    <h2 className="font-league-gothic font-normal leading-tight px-6 text-3xl md:text-4xl uppercase z-10 md:flex-shrink-0">
+      {title}
     </h2>
+    <div className="w-full md:flex-shrink my-auto">
+      <div className=" bg-purple-500 h-1 w-full z-0" />
+    </div>
   </div>
 );
 
 GalleryBlockHeader.propTypes = {
-  bgColor: PropTypes.string,
   title: PropTypes.string.isRequired,
-};
-
-GalleryBlockHeader.defaultProps = {
-  bgColor: 'bg-gray-100',
 };
 
 export default GalleryBlockHeader;
