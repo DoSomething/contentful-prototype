88aa7aa579c3d001c5c3c3b46e3661c69b51424a
Lindsay Maher <lindsaymah3r@gmail.com>
2021-02-02 15:54:37 -0500

    adds data id to strike through header

--- resources/assets/components/utilities/SectionHeader/StrikeThroughHeader.js
+++ resources/assets/components/utilities/SectionHeader/StrikeThroughHeader.js
@@ -2,7 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const StrikeThroughHeader = ({ title }) => (
-  <div className="grid-wide flex flex-wrap md:flex-no-wrap text-center w-full mb-6 justify-center">
+  <div
+    className="grid-wide flex flex-wrap md:flex-no-wrap text-center w-full mb-6 justify-center"
+    data-testid="strike-through-header"
+  >
     <div className="w-0 md:w-full md:flex-shrink my-auto">
       <div className=" bg-purple-500 h-1 w-full z-0" />
     </div>
