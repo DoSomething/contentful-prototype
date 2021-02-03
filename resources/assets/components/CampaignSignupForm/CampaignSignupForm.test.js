0d824c580693ddfce84650ca2e3461e6e3f4d265
Mendel Blesofsky <mendelblesofsky@gmail.com>
2021-02-03 14:19:26 -0500

    Update jest tests

--- resources/assets/components/CampaignSignupForm/CampaignSignupForm.test.js
+++ resources/assets/components/CampaignSignupForm/CampaignSignupForm.test.js
@@ -1,28 +1,54 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { MockedProvider } from '@apollo/react-testing';
 
 import CampaignSignupForm from './CampaignSignupForm';
+import { SEARCH_USER_CAMPAIGN_QUERY } from '../../helpers/campaign';
 
 jest.mock('../../helpers/analytics');
 
 const storeCampaignSignup = jest.fn();
+const signupCreated = jest.fn();
+
+window.STATE = { campaign: { id: '123' } };
 
 const props = {
   campaignId: '123',
   pageId: '456',
   storeCampaignSignup,
+  signupCreated,
 };
 
-describe('The CampaignSignupForm component', () => {
-  it('displays a Button', () => {
-    const wrapper = mount(<CampaignSignupForm {...props} />);
+// Mock the GraphQL query and response.
+const mocks = [
+  {
+    request: {
+      query: SEARCH_USER_CAMPAIGN_QUERY,
+      variables: {
+        campaignId: 123,
+        userId: '123',
+      },
+    },
+    result: {
+      data: {
+        signups: [],
+      },
+    },
+  },
+];
 
+describe('The CampaignSignupForm component', () => {
+  let wrapper = mount(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <CampaignSignupForm {...props} />
+    </MockedProvider>,
+  );
+
+  it('displays a Button', () => {
     expect(wrapper.find('PrimaryButton')).toHaveLength(1);
   });
 
   it('invokes the storeCampaignSignup function when the button is clicked', () => {
-    const wrapper = mount(<CampaignSignupForm {...props} />);
-
     wrapper.find('PrimaryButton').simulate('click');
 
     expect(storeCampaignSignup).toHaveBeenCalled();
@@ -30,21 +56,28 @@ describe('The CampaignSignupForm component', () => {
 
   describe('The signup button text', () => {
     it('defaults to the campaignActiontext', () => {
-      const wrapper = mount(<CampaignSignupForm {...props} />);
-
       expect(wrapper.find('PrimaryButton').text()).toEqual('Take Action');
     });
 
     it('is overriden by the "text" prop', () => {
       const text = 'Text Override!';
-      const wrapper = mount(<CampaignSignupForm {...props} text={text} />);
+
+      wrapper = mount(
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <CampaignSignupForm {...props} text={text} />
+        </MockedProvider>,
+      );
 
       expect(wrapper.find('PrimaryButton').text()).toEqual(text);
     });
 
     it('is overriden by the closed state text if the campaign is closed', () => {
-      const wrapper = mount(
-        <CampaignSignupForm {...props} endDate="2019-09-08" />,
+      window.STATE = { campaign: { endDate: '2019-09-08' } };
+
+      wrapper = mount(
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <CampaignSignupForm {...props} />
+        </MockedProvider>,
       );
 
       expect(wrapper.find('PrimaryButton').text()).toEqual('Notify Me');
