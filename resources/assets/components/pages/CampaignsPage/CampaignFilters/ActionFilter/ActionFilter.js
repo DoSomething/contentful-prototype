f4f7469c2eb431afac6d4c7028052ccd71290cdc
Lindsay Maher <lindsaymah3r@gmail.com>
2021-02-05 12:04:24 -0500

    adds state preservation for causes/filters on state change

--- resources/assets/components/pages/CampaignsPage/CampaignFilters/ActionFilter/ActionFilter.js
+++ resources/assets/components/pages/CampaignsPage/CampaignFilters/ActionFilter/ActionFilter.js
@@ -78,7 +78,6 @@ const ActionFilter = ({ filters, setFilters }) => {
     }
     setActionLocation('');
   };
-  console.log('hello?');
   return (
     <form>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full p-4">
