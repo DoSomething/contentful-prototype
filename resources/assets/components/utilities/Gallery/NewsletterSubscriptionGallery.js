80be2e568348ce42af2f6b857b03d725b626adb8
Diego Lorenzo <diego@diegolorenzo.com>
2021-03-03 19:58:00 -0500

    Using constants for topics.

--- resources/assets/components/utilities/Gallery/NewsletterSubscriptionGallery.js
+++ resources/assets/components/utilities/Gallery/NewsletterSubscriptionGallery.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
+import { NEWSLETTER_TOPICS } from '../NewsletterSubscriptionCard/config';
 import NewsletterSubscriptionCard from '../NewsletterSubscriptionCard/NewsletterSubscriptionCard';
 
 const NewsletterSubscriptionGallery = () => (
   <div className="newsletter-gallery mt-0 gap-8 grid grid-cols-1 md:grid-cols-2 xxl:grid-cols-4">
-    <NewsletterSubscriptionCard topic="community" />
+    <NewsletterSubscriptionCard topic={NEWSLETTER_TOPICS.community} />
 
-    <NewsletterSubscriptionCard topic="news" />
+    <NewsletterSubscriptionCard topic={NEWSLETTER_TOPICS.news} />
 
-    <NewsletterSubscriptionCard topic="lifestyle" />
+    <NewsletterSubscriptionCard topic={NEWSLETTER_TOPICS.lifestyle} />
 
-    <NewsletterSubscriptionCard topic="scholarships" />
+    <NewsletterSubscriptionCard topic={NEWSLETTER_TOPICS.scholarships} />
   </div>
 );
 
