e03e075d675689a6c39b24ed870bd5b5e6c9c4ed
Mendel Blesofsky <mendelblesofsky@gmail.com>
2021-03-08 17:29:31 -0500

    Add basic form validation/interaction test

---
+++ cypress/integration/questionnaire-action.js
@@ -0,0 +1,32 @@
+/// <reference types="Cypress" />
+
+import faker from 'faker';
+
+describe('Questionnaire Action', () => {
+  beforeEach(() => cy.configureMocks());
+
+  it('Submission is disabled until all questions are answered with valid character count', () => {
+    cy.visit('/us/questionnaire');
+
+    cy.findByTestId('questionnaire-action').within(() => {
+      cy.findByTestId('questionnaire-submit-button').should('be.disabled');
+
+      cy.findByTestId('question-1').type(faker.lorem.words());
+
+      cy.findByTestId('questionnaire-submit-button').should('be.disabled');
+
+      cy.findByTestId('question-2').type(faker.random.alphaNumeric(501));
+
+      cy.findByTestId('questionnaire-submit-button').should('be.disabled');
+
+      cy.findByTestId('question-2')
+        .clear()
+        .type(faker.lorem.words());
+
+      cy.findByTestId('questionnaire-submit-button').should(
+        'not.have.attr',
+        'disabled',
+      );
+    });
+  });
+});
