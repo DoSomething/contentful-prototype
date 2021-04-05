045d718b8c2ddc0efb8cc05a0e21c8fa6b4dbdc8
Lindsay Maher <lindsaymah3r@gmail.com>
2021-04-05 16:23:53 -0400

    Create Articles Page content type (#2594)
    * creates migration file for Articles Page
    
    * removes Title Field since we will pull from gallery
    
    * adds docs for content type and fields
    
    * give more generic names to top and bottom galleries
    
    * adds header title and article field
    
    * add custom link text field
    
    * updates Header Article name
    
    * updates documentation for new fields and updated descriptions

---
+++ contentful/content-types/articlesPage.js
@@ -0,0 +1,373 @@
+module.exports = function(migration) {
+  const articlesPage = migration
+    .createContentType('articlesPage')
+    .name('Articles Page')
+    .description('The page for us to showcase articles on DoSomething.org')
+    .displayField('internalTitle');
+  articlesPage
+    .createField('internalTitle')
+    .name('Internal Title')
+    .type('Symbol')
+    .localized(false)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  articlesPage
+    .createField('coverImage')
+    .name('Cover Image')
+    .type('Link')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        linkMimetypeGroup: ['image'],
+      },
+      {
+        assetFileSize: {
+          min: null,
+          max: 20971520,
+        },
+      },
+    ])
+    .disabled(false)
+    .omitted(false)
+    .linkType('Asset');
+
+  articlesPage
+    .createField('headerTitle')
+    .name('Header Title')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  articlesPage
+    .createField('headerArticle')
+    .name('Header Article')
+    .type('Link')
+    .localized(false)
+    .required(true)
+    .validations([
+      {
+        linkContentType: ['page'],
+        message: 'Only Page entries (articles) are valid for this field.',
+      },
+    ])
+    .disabled(false)
+    .omitted(false)
+    .linkType('Entry');
+
+  articlesPage
+    .createField('headerLinkText')
+    .name('Header Link Text')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+  articlesPage
+    .createField('featuredArticlesGalleryTopTitle')
+    .name('Featured Articles Gallery Top Title')
+    .type('Symbol')
+    .localized(false)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  articlesPage
+    .createField('featuredArticlesGalleryTop')
+    .name('Featured Articles Gallery Top')
+    .type('Array')
+    .localized(false)
+    .required(true)
+    .validations([
+      {
+        size: {
+          min: 6,
+          max: 6,
+        },
+      },
+    ])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+
+      validations: [
+        {
+          linkContentType: ['page'],
+          message: 'Only Page entries are valid in this gallery.',
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+
+  articlesPage
+    .createField('topicArticlesGalleryOneTitle')
+    .name('Topic Articles Gallery One Title')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  articlesPage
+    .createField('topicArticlesGalleryOne')
+    .name('Topic Articles Gallery One')
+    .type('Array')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        size: {
+          min: 3,
+          max: 6,
+        },
+
+        message: 'Please add 3 or 6 articles to this gallery.',
+      },
+    ])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+
+      validations: [
+        {
+          linkContentType: ['page'],
+          message: 'Only Page entries are valid.',
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+
+  articlesPage
+    .createField('topicArticlesGalleryTwoTitle')
+    .name('Topic Articles Gallery Two Title')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  articlesPage
+    .createField('topicArticlesGalleryTwo')
+    .name('Topic Articles Gallery Two')
+    .type('Array')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        size: {
+          min: 3,
+          max: 6,
+        },
+
+        message: 'Please add 3 or 6 articles to this gallery.',
+      },
+    ])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+
+      validations: [
+        {
+          linkContentType: ['page'],
+          message: 'Only Page entries are valid.',
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+
+  articlesPage
+    .createField('featuredArticlesGalleryBottomTitle')
+    .name('Featured Articles Gallery Bottom Title')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  articlesPage
+    .createField('featuredArticlesGalleryBottom')
+    .name('Featured Articles Gallery Bottom')
+    .type('Array')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        size: {
+          min: 3,
+          max: 6,
+        },
+      },
+    ])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+
+      validations: [
+        {
+          linkContentType: ['page'],
+          message: 'Only Page entries are valid in this gallery.',
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+
+  articlesPage
+    .createField('ctaTitle')
+    .name('CTA Title')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+  articlesPage
+    .createField('ctaText')
+    .name('CTA Text')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+  articlesPage
+    .createField('ctaButtonText')
+    .name('CTA Button Text')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+  articlesPage.changeFieldControl('internalTitle', 'builtin', 'singleLine', {});
+
+  articlesPage.changeFieldControl('coverImage', 'builtin', 'assetLinkEditor', {
+    helpText:
+      'Cover image to display as background for the top banner on the articles page.',
+    showLinkEntityAction: true,
+    showCreateEntityAction: true,
+  });
+
+  articlesPage.changeFieldControl('headerTitle', 'builtin', 'singleLine', {
+    helpText: 'Add a Title here to showcase the header article content.',
+  });
+
+  articlesPage.changeFieldControl(
+    'headerArticle',
+    'builtin',
+    'entryLinkEditor',
+    {},
+  );
+
+  articlesPage.changeFieldControl('headerLinkText', 'builtin', 'singleLine', {
+    helpText: 'Add custom text for the article link ie. "Read More" etc.',
+  });
+
+  articlesPage.changeFieldControl(
+    'featuredArticlesGalleryTopTitle',
+    'builtin',
+    'singleLine',
+    {},
+  );
+
+  articlesPage.changeFieldControl(
+    'featuredArticlesGalleryTop',
+    'builtin',
+    'entryLinksEditor',
+    {
+      helpText:
+        "Add stories you'd like to feature (latest articles, etc) here.",
+      bulkEditing: false,
+      showLinkEntityAction: true,
+      showCreateEntityAction: true,
+    },
+  );
+
+  articlesPage.changeFieldControl(
+    'topicArticlesGalleryOneTitle',
+    'builtin',
+    'singleLine',
+    {
+      helpText:
+        'Add a title for first gallery of articles on a featured topic.',
+    },
+  );
+
+  articlesPage.changeFieldControl(
+    'topicArticlesGalleryOne',
+    'builtin',
+    'entryLinksEditor',
+    {
+      helpText:
+        'Add articles about the same topic to showcase on the articles page.',
+      bulkEditing: false,
+      showLinkEntityAction: true,
+      showCreateEntityAction: true,
+    },
+  );
+
+  articlesPage.changeFieldControl(
+    'topicArticlesGalleryTwoTitle',
+    'builtin',
+    'singleLine',
+    {
+      helpText:
+        'Add a title for the second gallery of articles on a specific topic.',
+    },
+  );
+
+  articlesPage.changeFieldControl(
+    'topicArticlesGalleryTwo',
+    'builtin',
+    'entryLinksEditor',
+    {
+      helpText:
+        'Add articles on the same topic to showcase on the articles page.',
+      bulkEditing: false,
+      showLinkEntityAction: true,
+      showCreateEntityAction: true,
+    },
+  );
+
+  articlesPage.changeFieldControl(
+    'featuredArticlesGalleryBottomTitle',
+    'builtin',
+    'singleLine',
+    {},
+  );
+
+  articlesPage.changeFieldControl(
+    'featuredArticlesGalleryBottom',
+    'builtin',
+    'entryLinksEditor',
+    {
+      helpText:
+        'Use this gallery to feature 11 facts articles on the articles page.',
+      bulkEditing: false,
+      showLinkEntityAction: true,
+      showCreateEntityAction: true,
+    },
+  );
+
+  articlesPage.changeFieldControl('ctaTitle', 'builtin', 'singleLine', {});
+  articlesPage.changeFieldControl('ctaText', 'builtin', 'singleLine', {});
+  articlesPage.changeFieldControl('ctaButtonText', 'builtin', 'singleLine', {});
+};
---
+++ docs/development/content-types/articles-page.md
@@ -0,0 +1,47 @@
+# Articles Page
+
+## Overview
+
+This content type is an in-house replacement for the page we formerly housed on Instapage. It features articles from our site on a variety of topics.
+
+## Content Type Fields
+
+-   **Internal Title**: This is for our internal Contentful organization and will be how the block shows up in search results, etc. (does _not_ display to the user on the company page).
+
+-   **Cover Image**: Displays at the top of the page and links to a featured Article.
+
+-   **Header Title**: Custom Title for the featured header article (alternative to headline in article page itself).
+
+-   **Header Article**: Article that should be displayed and linked to at the top of the Articles page.
+
+-   **Header Link Text**: Optional custom text for the link out to the header article.
+
+-   **Featured Articles Gallery Top Title**: The title above the first gallery of articles.
+
+-   **Featured Articles Gallery Top**: Collection of 6 articles, for launch these will be the latest articles published on the site.
+
+-   **Topic Articles Gallery One Title**: The title above the first gallery of articles on a specific topic.
+
+-   **Topic Articles Gallery One**: Collection of 3 or 6 articles all on a related topic.
+
+-   **Topic Articles Gallery Two Title**: The title above the second gallery of articles on a specific topic.
+
+-   **Topic Articles Gallery Two**: Collection of 3 or 6 articles all on a related topic.
+
+-   **Featured Articles Gallery Bottom Title**: The title above the gallery showcasing all 11 Facts Articles.
+
+-   **Featured Articles Gallery Bottom**: Collection of 3 or 6 11 Facts articles.
+
+-   **CTA Title**: Main CTA text for CTA banner at the bottom of the page.
+
+-   **CTA Text**: Sub CTA text for CTA banner at the bottom of the page.
+
+-   **CTA Button Text**: Custom text for the CTA button.
+
+## Technical Notes
+
+Page entries are the currently the only type of entry we are validating for in the galleries since this is an articles specific page.
+
+## Additional Information
+
+We chose to define each block of articles explicitly vs a rich text content field because of clarity for editors and being able to work in a stand alone component on the page itself.
