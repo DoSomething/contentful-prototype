e75440decf760f0c3bbd4b8778f7c5f832cc8db0
Mendel Blesofsky <mendelblesofsky@gmail.com>
2021-04-07 16:13:58 -0400

    Delete the snapshot test cuz like eh....

--- resources/assets/components/utilities/MediaUploader/test.js
+++ resources/assets/components/utilities/MediaUploader/test.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { shallowToJson } from 'enzyme-to-json';
 
 import MediaUploader from './MediaUploader';
 
 test('MediaUploader snapshot test', () => {
-  const component = shallow(<MediaUploader onChange={() => {}} />);
-  const tree = shallowToJson(component);
-  expect(tree).toMatchSnapshot();
+  shallow(<MediaUploader onChange={() => {}} />);
+
+  // Add unit tests here.
 });
