e2fe77338ff7f3844a62e813dce54e4b7c7ee73b
Mendel Blesofsky <mendelblesofsky@gmail.com>
2021-04-08 12:03:06 -0400

    Media Uploader - size limit label + refactor (#2600)
    * Add media uploader size limit messaging to label
    
    * Update test snapshot
    
    * Refactor MediaUploader to functional component
    
    * Rename MediaUploader component per our newer standard
    
    * Apply standard spacing (via JS prettier)
    
    * Port top level styling to tailwind/emotion
    
    * Post label/img styles over to tailwind
    
    * Refactor content rendering
    
    * Post media-uploader__content styles to tailwind/emotion
    
    * Use img tag to display plus sign
    
    * Port remaining image state styles to tailwind/emotion
    
    * Post over error rules to Tailwind
    
    * Fix up this stuff
    
    I don't quite think these rules are taking effect
    
    * Remove surrounding div
    
    This felt confusing -- there were some rules being overridden because they were defined on the parent div and re-defined on the label...
    
    * Remove scss file and hide input via width utility class
    
    * Remove classnames used for styling
    
    * Delete the snapshot test cuz like eh....
    
    * Update test

---
+++ resources/assets/components/utilities/MediaUploader/MediaUploader.js
@@ -0,0 +1,118 @@
+/* global FileReader, URL, Blob */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { css } from '@emotion/core';
+
+import { processFile } from '../../../helpers/file';
+import plusSign from '../../../images/plus_sign.svg';
+
+const MediaUploader = ({ label, onChange, hasError, media }) => {
+  const readFile = file => {
+    const fileReader = new FileReader();
+    let blob;
+
+    fileReader.readAsArrayBuffer(file);
+
+    fileReader.onloadend = () => {
+      try {
+        blob = processFile(fileReader.result);
+
+        onChange({
+          file: blob,
+          filePreviewUrl: URL.createObjectURL(blob),
+        });
+      } catch (error) {
+        // @todo: need a nice way to handle this, display message?
+        console.log(error);
+      }
+    };
+  };
+
+  const handleChange = event => {
+    event.preventDefault();
+
+    readFile(event.target.files[0]);
+  };
+
+  const { filePreviewUrl } = media;
+
+  return (
+    <label
+      data-testid="media-uploader"
+      htmlFor="media-uploader"
+      className={classnames(
+        'cursor-pointer block h-0 overflow-hidden relative bg-gray-200 text-gray-600 mb-3 w-full hover:bg-gray-300 focus:bg-gray-300',
+        {
+          'border border-solid border-red-500 shake': hasError,
+        },
+      )}
+      css={css`
+        padding-bottom: 100%;
+      `}
+    >
+      <div
+        className={classnames(
+          'items-center flex h-full justify-center left-0 absolute top-0 w-full',
+          {
+            'bg-gray-100': filePreviewUrl,
+            'flex-col': !filePreviewUrl,
+          },
+        )}
+      >
+        {filePreviewUrl ? (
+          <img
+            src={filePreviewUrl}
+            alt="uploaded file"
+            className="max-h-full"
+          />
+        ) : (
+          <>
+            <img src={plusSign} alt="plus symbol" className="mb-2" />
+
+            <span
+              className="underline font-semibold leading-normal text-center"
+              css={css`
+                max-width: 170px;
+              `}
+            >
+              {label}
+            </span>
+            <p className="text-gray-600 pt-2 italic">{'must be <10MB'}</p>
+          </>
+        )}
+      </div>
+
+      <input
+        className="w-0"
+        type="file"
+        id="media-uploader"
+        name="media-uploader"
+        onChange={handleChange}
+        required
+      />
+    </label>
+  );
+};
+
+MediaUploader.propTypes = {
+  hasError: PropTypes.bool,
+  label: PropTypes.string,
+  media: PropTypes.shape({
+    file: PropTypes.instanceOf(Blob),
+    filePreviewUrl: PropTypes.string,
+  }),
+  onChange: PropTypes.func.isRequired,
+};
+
+MediaUploader.defaultProps = {
+  hasError: false,
+  label: 'Upload Media',
+  media: {
+    file: null,
+    filePreviewUrl: null,
+  },
+};
+
+export default MediaUploader;
