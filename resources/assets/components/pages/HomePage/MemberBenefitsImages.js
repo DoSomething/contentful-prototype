602a37853a65487c6644e4d98f21f1df7fb0c71f
Diego Lorenzo <diego@diegolorenzo.com>
2021-04-15 17:44:48 -0500

    Updating Homepage with new Member Benefits content.

---
+++ resources/assets/components/pages/HomePage/MemberBenefitsImages.js
@@ -0,0 +1,4 @@
+export Community from './Images/benefits_community.svg';
+export Content from './Images/benefits_content.svg';
+export Scholarships from './Images/benefits_scholarships.svg';
+export VolunteerCredits from './Images/benefits_volunteer_credits.svg';
--- resources/assets/components/pages/HomePage/NewsletterImages.js
+++
@@ -1,4 +0,0 @@
-export News from './Images/newsletter_news.svg';
-export Community from './Images/newsletter_community.svg';
-export Lifestyle from './Images/newsletter_lifestyle.svg';
-export Scholarships from './Images/newsletter_scholarships.svg';
