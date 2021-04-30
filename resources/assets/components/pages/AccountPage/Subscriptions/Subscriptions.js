0eb3e4dbdb0624546606445b3f2d0e0aa2371fb5
Diego Lorenzo <diego@diegolorenzo.com>
2021-04-29 19:18:33 -0500

    Renaming to deprecate LegacyEmailSubscriptions component.

--- resources/assets/components/pages/AccountPage/Subscriptions/Subscriptions.js
+++ resources/assets/components/pages/AccountPage/Subscriptions/Subscriptions.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import EmailSubscriptions from './EmailSubscriptions';
 import CancelEmailSubscription from './CancelEmailSubscription';
+import LegacyEmailSubscriptions from '../../../utilities/EmailSubscription/LegacyEmailSubscriptions';
 
 const Subscriptions = props => (
   <div className="grid-wide">
     <h1 className="text-xl">Email Subscriptions</h1>
+
     <p className="text-gray-600">
       Our newsletters can help you stay informed, stay entertained, and even
       help you win scholarships.
     </p>
 
-    <EmailSubscriptions {...props} />
-    <div>
-      <CancelEmailSubscription {...props} />
-    </div>
+    <LegacyEmailSubscriptions {...props} />
+
+    <CancelEmailSubscription {...props} />
   </div>
 );
 
