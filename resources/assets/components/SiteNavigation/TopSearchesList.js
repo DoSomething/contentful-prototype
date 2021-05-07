70f94f7c916bfafd7c0be0f5589ebeb6f8646aa0
Mendel Blesofsky <mendelblesofsky@gmail.com>
2021-05-07 09:36:06 -0400

    Refactor top searches list

--- resources/assets/components/SiteNavigation/TopSearchesList.js
+++ resources/assets/components/SiteNavigation/TopSearchesList.js
@@ -6,90 +6,42 @@ import {
   getPageContext,
 } from '../../helpers/analytics';
 
+const topSearches = [
+  { searchTerm: 'scholarships', path: '/us/about/easy-scholarships' },
+  { searchTerm: 'bullying', path: '/us/search?query=bullying' },
+  { searchTerm: 'animals', path: '/us/search?query=animals' },
+  { searchTerm: 'covid', path: '/us/collections/corona-virus-campaigns' },
+  {
+    searchTerm: 'volunteering',
+    path: '/us/articles/volunteer-opportunities-for-teens',
+  },
+];
+
 const TopSearchesList = () => (
   <div className="top-searches">
     <h1>Top Searches</h1>
 
     <ul className="top-searches__link-list">
-      <li>
-        <a
-          href="/us/about/easy-scholarships"
-          onClick={() =>
-            trackAnalyticsEvent('clicked_subnav_link_scholarships_top_search', {
-              action: 'link_clicked',
-              category: EVENT_CATEGORIES.navigation,
-              label: 'scholarships_top_search',
-              context: getPageContext(),
-            })
-          }
-        >
-          scholarships
-        </a>
-      </li>
-
-      <li>
-        <a
-          href="/us/search?query=bullying"
-          onClick={() =>
-            trackAnalyticsEvent('clicked_subnav_link_bullying_top_search', {
-              action: 'link_clicked',
-              category: EVENT_CATEGORIES.navigation,
-              label: 'bullying_top_search',
-              context: getPageContext(),
-            })
-          }
-        >
-          bullying
-        </a>
-      </li>
-
-      <li>
-        <a
-          href="/us/search?query=animals"
-          onClick={() =>
-            trackAnalyticsEvent('clicked_subnav_link_animals_top_search', {
-              action: 'link_clicked',
-              category: EVENT_CATEGORIES.navigation,
-              label: 'animals_top_search',
-              context: getPageContext(),
-            })
-          }
-        >
-          animals
-        </a>
-      </li>
-
-      <li>
-        <a
-          href="/us/collections/corona-virus-campaigns"
-          onClick={() =>
-            trackAnalyticsEvent('clicked_subnav_link_covid_top_search', {
-              action: 'link_clicked',
-              category: EVENT_CATEGORIES.navigation,
-              label: 'covid_top_search',
-              context: getPageContext(),
-            })
-          }
-        >
-          covid
-        </a>
-      </li>
-
-      <li>
-        <a
-          href="/us/articles/volunteer-opportunities-for-teens"
-          onClick={() =>
-            trackAnalyticsEvent('clicked_subnav_link_volunteering_top_search', {
-              action: 'link_clicked',
-              category: EVENT_CATEGORIES.navigation,
-              label: 'volunteering_top_search',
-              context: getPageContext(),
-            })
-          }
-        >
-          volunteering
-        </a>
-      </li>
+      {topSearches.map(({ searchTerm, path }) => (
+        <li key={searchTerm}>
+          <a
+            href={path}
+            onClick={() =>
+              trackAnalyticsEvent(
+                `clicked_subnav_link_${searchTerm}_top_search`,
+                {
+                  action: 'link_clicked',
+                  category: EVENT_CATEGORIES.navigation,
+                  label: `${searchTerm}_top_search`,
+                  context: getPageContext(),
+                },
+              )
+            }
+          >
+            {searchTerm}
+          </a>
+        </li>
+      ))}
     </ul>
   </div>
 );
