91dfde89d9a0189438701303f702f41a19535302
Lindsay Maher <lindsaymah3r@gmail.com>
2021-05-27 16:49:36 -0400

    adds refer friends banner file

--- resources/assets/components/pages/ShowSubmissionPage/ShowSubmissionPage.js
+++ resources/assets/components/pages/ShowSubmissionPage/ShowSubmissionPage.js
@@ -17,7 +17,7 @@ import SiteFooter from '../../utilities/SiteFooter/SiteFooter';
 import SiteNavigation from '../../SiteNavigation/SiteNavigation';
 import TextContent from '../../utilities/TextContent/TextContent';
 import RecommendedCampaignsGallery from './RecommendedCampaignsGallery';
-import BannerCallToAction from '../../utilities/CallToAction/BannerCallToAction';
+import BannerReferFriends from '../../utilities/CallToAction/BannerCallToAction';
 import SixpackExperiment from '../../utilities/SixpackExperiment/SixpackExperiment';
 import ShortLinkShareContainer from '../../utilities/ShortLinkShare/ShortLinkShareContainer';
 import { CONTENTFUL_BLOCK_QUERY } from '../../utilities/ContentfulEntryLoader/ContentfulEntryLoader';
@@ -157,11 +157,18 @@ const ShowSubmissionPage = ({ match }) => {
           </div>
         </div>
 
-        <BannerCallToAction
+        <BannerReferFriends
           text="Refer a friend to this campaign, and you'll *both* earn a $5 gift card! Learn More"
           title="Benefits with Friends"
-          stacked
-        />
+          colorClasses={{
+            background: 'bg-purple-500',
+            text: 'text-white',
+          }}
+        >
+          <ShortLinkShareContainer
+            link={getSixPackReferFriendsLink(userId, campaignId)}
+          />
+        </BannerReferFriends>
       </main>
 
       <SiteFooter />
---
+++ resources/assets/components/utilities/CallToAction/BannerReferFriends.js
@@ -0,0 +1,90 @@
+import { React } from 'react';
+import PropTypes from 'prop-types';
+import { snakeCase } from 'lodash';
+import classnames from 'classnames';
+
+import AnalyticsWaypoint from '../AnalyticsWaypoint/AnalyticsWaypoint';
+
+const BannerReferFriends = ({
+  attributes,
+  children,
+  colorClasses,
+  stacked,
+  text,
+  title,
+  waypointName,
+}) => {
+  return (
+    <article
+      className={classnames(
+        'base-12-grid py-16 relative',
+        colorClasses.background || 'bg-yellow-500',
+      )}
+      {...attributes}
+    >
+      <AnalyticsWaypoint
+        className="absolute top-0 w-full"
+        name={snakeCase(`${waypointName} top`)}
+      />
+
+      <div
+        className={classnames('grid-wide text-center', {
+          'xl:flex xl:items-center': !stacked,
+        })}
+      >
+        <div className={classnames({ 'text-left xl:w-8/12': !stacked })}>
+          <h1
+            className={classnames(
+              'tracking-wide',
+              stacked
+                ? 'font-league-gothic font-normal text-4xl uppercase'
+                : 'text-2xl',
+              colorClasses.text || 'text-gray-900',
+            )}
+          >
+            {title}
+          </h1>
+
+          <p
+            className={classnames(
+              'text-lg',
+              { 'm-4': stacked },
+              colorClasses.text || 'text-gray-900',
+            )}
+          >
+            {text}
+          </p>
+        </div>
+
+        <div className={classnames({ 'flex-grow': !stacked })}>{children}</div>
+      </div>
+
+      <AnalyticsWaypoint
+        className="absolute bottom-0 w-full"
+        name={snakeCase(`${waypointName} bottom`)}
+      />
+    </article>
+  );
+};
+
+BannerReferFriends.propTypes = {
+  attributes: PropTypes.object,
+  children: PropTypes.node.isRequired,
+  colorClasses: PropTypes.shape({
+    background: PropTypes.string,
+    text: PropTypes.string,
+  }),
+  stacked: PropTypes.bool,
+  text: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  waypointName: PropTypes.string,
+};
+
+BannerReferFriends.defaultProps = {
+  attributes: {},
+  colorClasses: {},
+  stacked: false,
+  waypointName: 'banner cta',
+};
+
+export default BannerReferFriends;
