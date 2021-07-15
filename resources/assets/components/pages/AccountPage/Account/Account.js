bd70b1a615296908fbc4e03a89e13710049f1737
Mendel Blesofsky <mendelblesofsky@gmail.com>
2021-07-15 14:03:04 -0400

    Use return statement for account

--- resources/assets/components/pages/AccountPage/Account/Account.js
+++ resources/assets/components/pages/AccountPage/Account/Account.js
@@ -11,42 +11,44 @@ import {
   EVENT_CATEGORIES,
 } from '../../../../helpers/analytics';
 
-const Account = props => (
-  <>
-    <ScrollConcierge />
+const Account = props => {
+  return (
+    <>
+      <ScrollConcierge />
 
-    <SiteNavigation />
+      <SiteNavigation />
 
-    <main>
-      <article className="account-page base-12-grid py-3 md:py-6">
-        {/* Account landing page breadcrumb link: */}
-        <div className="grid-wide-2/3 pb-1">
-          <Link
-            to="/us/account"
-            className="font-bold no-underline hover:no-underline"
-            onClick={() =>
-              trackAnalyticsEvent('clicked_breadcrumb_link_my_account', {
-                action: 'link_clicked',
-                category: EVENT_CATEGORIES.navigation,
-                label: 'breadcrumb_my_account',
-                context: {
-                  url: `${window.location.origin}/us/account`,
-                },
-              })
-            }
-          >
-            My Account
-          </Link>{' '}
-          <span className="font-bold">/</span>
-        </div>
+      <main>
+        <article className="account-page base-12-grid py-3 md:py-6">
+          {/* Account landing page breadcrumb link: */}
+          <div className="grid-wide-2/3 pb-1">
+            <Link
+              to="/us/account"
+              className="font-bold no-underline hover:no-underline"
+              onClick={() =>
+                trackAnalyticsEvent('clicked_breadcrumb_link_my_account', {
+                  action: 'link_clicked',
+                  category: EVENT_CATEGORIES.navigation,
+                  label: 'breadcrumb_my_account',
+                  context: {
+                    url: `${window.location.origin}/us/account`,
+                  },
+                })
+              }
+            >
+              My Account
+            </Link>{' '}
+            <span className="font-bold">/</span>
+          </div>
 
-        <AccountRoute {...props} />
-      </article>
-    </main>
+          <AccountRoute {...props} />
+        </article>
+      </main>
 
-    <SiteFooter />
-  </>
-);
+      <SiteFooter />
+    </>
+  );
+};
 
 Account.propTypes = {
   user: PropTypes.shape({
